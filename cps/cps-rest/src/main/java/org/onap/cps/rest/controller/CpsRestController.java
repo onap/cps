47598117ee0b411cfc6fad323598f5644b51c9c9
puthuparambil.aditya <aditya.puthuparambil@bell.ca>
2020-11-02 11:31:39 +0000

    Swagger codegen using Spring framework
    1. Pom.xml updated to generate API code using Swagger codegen.
    2. Moved the createModule code to the method implemented by Swagger.
    
    JIRA: https://jira.onap.org/browse/CCSDK-2897
    
    Issue-ID: CCSDK-2897
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>
    Change-Id: Iadd3dc0402f06a597a179148fd5f43e0fe7c48f6
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>

--- cps/cps-rest/src/main/java/org/onap/cps/rest/controller/CpsRestController.java
+++ cps/cps-rest/src/main/java/org/onap/cps/rest/controller/CpsRestController.java
@@ -27,8 +27,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import javax.persistence.PersistenceException;
+import javax.validation.Valid;
 import org.hibernate.exception.ConstraintViolationException;
 import org.onap.cps.api.CpService;
+import org.onap.cps.rest.api.CpsRestApi;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.parser.api.YangParserException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,37 +46,74 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-public class CpsRestController {
+public class CpsRestController implements CpsRestApi {
 
     @Autowired
     private CpService cpService;
 
+    @Override
+    public ResponseEntity<Object> createAnchor(@Valid MultipartFile multipartFile, String dataspaceName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> createModules(@Valid MultipartFile multipartFile, String dataspaceName) {
+        try {
+            final File fileToParse = saveToFile(multipartFile);
+            final SchemaContext schemaContext = cpService.parseAndValidateModel(fileToParse);
+            cpService.storeSchemaContext(schemaContext, dataspaceName);
+            return new ResponseEntity<>("Resource successfully created", HttpStatus.CREATED);
+        } catch (final YangParserException | ConstraintViolationException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (final Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Object> createNode(@Valid MultipartFile multipartFile, String dataspaceName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteAnchor(String dataspaceName, String anchorName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteDataspace(String dataspaceName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getAnchor(String dataspaceName, String anchorName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getAnchors(String dataspaceName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getModule(String dataspaceName, @Valid String namespaceName, @Valid String revision) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getNode(@Valid String body, String dataspaceName) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getNodeByDataspaceAndAnchor(@Valid String body, String dataspaceName,
+        String anchorName) {
+        return null;
+    }
+
     /*
     Old rest endpoints before contract first approach (Need to be removed).
      */
-
-    /**
-     * Upload a yang model file.
-     *
-     * @param uploadedFile the yang model Multipart File.
-     * @param dataspaceName the dataspace name linked to the model.
-     * @return a ResponseEntity.
-     */
-    @PostMapping("/dataspaces/{dataspace_name}/modules")
-    public final ResponseEntity<String> uploadYangModelFile(
-        @RequestParam("file") MultipartFile uploadedFile,
-        @PathVariable("dataspace_name") String dataspaceName) {
-        try {
-            final File fileToParse = saveToFile(uploadedFile);
-            final SchemaContext schemaContext = cpService.parseAndValidateModel(fileToParse);
-            cpService.storeSchemaContext(schemaContext, dataspaceName);
-            return new ResponseEntity<String>("Resource successfully created", HttpStatus.CREATED);
-        } catch (final YangParserException | ConstraintViolationException e) {
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (final Exception e) {
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
     /**
      * Upload a JSON file.
