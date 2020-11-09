9d06d18187570a462979206039e110d313619ef3
Rishi.Chail <rishi.chail@est.tech>
2020-11-09 03:28:44 +0000

    VSE: Create an anchor in the given dataspace
    Issue-ID: CPS-42
    https://jira.onap.org/browse/CPS-42
    
    Signed-off-by: Rishi.Chail <rishi.chail@est.tech>
    Change-Id: If67be6f13889808da4d9fe830595766af67e4fdf

--- cps-ri/src/main/java/org/onap/cps/spi/repository/ModuleRepository.java
+++ cps-ri/src/main/java/org/onap/cps/spi/repository/ModuleRepository.java
@@ -20,10 +20,35 @@
 package org.onap.cps.spi.repository;
 
 
+import java.util.Optional;
+import javax.validation.constraints.NotNull;
+import org.onap.cps.exceptions.CpsNotFoundException;
+import org.onap.cps.spi.entities.Dataspace;
 import org.onap.cps.spi.entities.ModuleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleEntity, Integer> {
+
+    Optional<ModuleEntity> findByNamespaceAndRevisionAndDataspace(@NotNull String namespace,
+        @NotNull String revision, @NotNull Dataspace dataspace);
+
+    /**
+     * This get a ModuleEntity by namespace, revision and Dataspace.
+     * If not found throws a CpsNotFoundException.
+     *
+     * @param namespace the namespace
+     * @param revision the revision
+     * @param dataspace the dataspace
+     * @return
+     */
+    default ModuleEntity getByNamespaceAndRevisionAndDataspace(@NotNull String namespace,
+        @NotNull String revision, @NotNull Dataspace dataspace) {
+        return findByNamespaceAndRevisionAndDataspace(namespace,
+            revision, dataspace)
+                .orElseThrow(() -> new CpsNotFoundException("Validation Error", String.format(
+                "Module with dataspace %s, revision %s does not exist in namespace %s.",
+                dataspace.getName(), revision, namespace)));
+    }
 }
