e234ab2285e7715e407892ef46c401f8fd7d7029
Rishi.Chail <rishi.chail@est.tech>
2020-11-09 03:28:44 +0000

    VSE: Create an anchor in the given dataspace
    Issue-ID: CPS-42
    https://jira.onap.org/browse/CPS-42
    
    Signed-off-by: Rishi.Chail <rishi.chail@est.tech>
    Change-Id: If67be6f13889808da4d9fe830595766af67e4fdf

--- cps-service/src/main/java/org/onap/cps/api/CpService.java
+++ cps-service/src/main/java/org/onap/cps/api/CpService.java
@@ -21,9 +21,8 @@
 package org.onap.cps.api;
 
 import java.io.File;
-import java.io.IOException;
+import org.onap.cps.api.model.AnchorDetails;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
-import org.opendaylight.yangtools.yang.model.parser.api.YangParserException;
 
 /**
  * Configuration and persistency service interface which holds methods for parsing and storing yang models and data.
@@ -76,4 +75,11 @@ public interface CpService {
      * @param jsonObjectId the JSON object identifier.
      */
     void deleteJsonById(final int jsonObjectId);
+
+    /**
+     * Create an anchor using provided anchorDetails object.
+     *
+     * @param anchorDetails the anchor details object.
+     */
+    void createAnchor(final AnchorDetails anchorDetails);
 }
