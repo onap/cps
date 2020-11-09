ff02ab428324b081e8340674747e4b46f42c8cfc
Rishi.Chail <rishi.chail@est.tech>
2020-11-09 03:28:44 +0000

    VSE: Create an anchor in the given dataspace
    Issue-ID: CPS-42
    https://jira.onap.org/browse/CPS-42
    
    Signed-off-by: Rishi.Chail <rishi.chail@est.tech>
    Change-Id: If67be6f13889808da4d9fe830595766af67e4fdf

--- cps-ri/src/main/java/org/onap/cps/spi/repository/DataspaceRepository.java
+++ cps-ri/src/main/java/org/onap/cps/spi/repository/DataspaceRepository.java
@@ -20,6 +20,9 @@
 package org.onap.cps.spi.repository;
 
 
+import java.util.Optional;
+import javax.validation.constraints.NotNull;
+import org.onap.cps.exceptions.CpsNotFoundException;
 import org.onap.cps.spi.entities.Dataspace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -28,5 +31,10 @@ import org.springframework.stereotype.Repository;
 public interface DataspaceRepository extends JpaRepository<Dataspace, Integer> {
     Boolean existsByName(String name); //Checks if there are any records by name()
 
-    Dataspace findByName(String name);
+    Optional<Dataspace> findByName(@NotNull String name);
+
+    default Dataspace getByName(@NotNull String name) {
+        return findByName(name).orElseThrow(
+            () -> new CpsNotFoundException("Not Found", "Dataspace " + name + " does not exist."));
+    }
 }
