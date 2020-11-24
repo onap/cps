c7ddc44fe21aae24dd9630104ea3c5c295550970
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2020-11-24 11:13:43 +0200

    ModuleSet SPI, persistence layer implementation (store) and tests
    Issue-ID: CPS-92
    Change-Id: I11257726e4f847e3337d8bafad824853e6a458e2
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ cps-ri/src/test/java/org/onap/cps/DatabaseTestContainer.java
@@ -0,0 +1,64 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 Pantheon.tech
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+/**
+ * The Postgresql database test container wrapper.
+ * Singleton implementation allows saving time on database initialization which
+ * otherwise would occur on each test.
+ */
+public class DatabaseTestContainer extends PostgreSQLContainer<DatabaseTestContainer> {
+    private static final String IMAGE_VERSION = "postgres:11.1";
+    private static DatabaseTestContainer container;
+
+    private DatabaseTestContainer() {
+        super(IMAGE_VERSION);
+    }
+
+    /**
+     * Provides an instance of test container wrapper.
+     * The returned value expected to be assigned to static variable annotated with @ClassRule.
+     * This will allow to initialize DB connection env variables before DataSource object
+     * is initialized by Spring framework.
+     *
+     */
+    public static DatabaseTestContainer getInstance() {
+        if (container == null) {
+            container = new DatabaseTestContainer();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("DB_URL", container.getJdbcUrl());
+        System.setProperty("DB_USERNAME", container.getUsername());
+        System.setProperty("DB_PASSWORD", container.getPassword());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+
+}
--- cps-service/src/main/java/org/onap/cps/spi/ModelPersistenceService.java
+++ cps-service/src/main/java/org/onap/cps/spi/ModelPersistenceService.java
@@ -20,6 +20,9 @@
 
 package org.onap.cps.spi;
 
+import java.util.List;
+import org.onap.cps.spi.model.SchemaSet;
+
 /**
  * Defines methods to access and manipulate data using the chosen database solution.
  */
@@ -28,12 +31,42 @@ public interface ModelPersistenceService {
     /**
      * Store the module from a yang model in the database.
      *
-     * @param namespace module namespace
+     * @param namespace     module namespace
      * @param moduleContent module content
-     * @param revision module revision
+     * @param revision      module revision
      * @param dataspaceName the name of the dataspace the module is associated with
      */
     void storeModule(final String namespace, final String moduleContent, final String revision,
         final String dataspaceName);
 
+
+    /**
+     * Stores the moduleset.
+     *
+     * @param schemaSet the data descriptor including dataspace name, moduleset name and modules descriptors
+     */
+    void storeSchemaSet(SchemaSet schemaSet);
+
+    /**
+     * Deletes the ModuleSet and associated modules.
+     *
+     * @param dataspaceName the dataspace name
+     * @param moduleSetName the moduleset name
+     */
+    void deleteSchemaSet(String dataspaceName, String moduleSetName);
+
+    /**
+     * Retrieves the ModuleSet by dataspace and name.
+     *
+     * @param dataspaceName the dataspace name
+     * @param moduleSetName the moduleset name
+     */
+    SchemaSet getSchemaSet(String dataspaceName, String moduleSetName);
+
+    /**
+     * Retrieves list of modulesets by dataspace. Moduleset names only returned, module content is omitted
+     *
+     * @param dataspaceName the dataspace name
+     */
+    List<SchemaSet> getAllSchemaSets(String dataspaceName);
 }
