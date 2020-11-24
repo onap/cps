22bd5aaf64fd47a8fe7809b0c4777c09be56fce7
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2020-11-24 11:13:43 +0200

    Associate anchor to schema set
    Issue-ID: CPS-99
    Change-Id: Ideeb83fa9e91ec1816308d8327a6589b999c64c5
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- cps-ri/src/main/java/org/onap/cps/spi/repository/FragmentRepository.java
+++ cps-ri/src/main/java/org/onap/cps/spi/repository/FragmentRepository.java
@@ -1,7 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2020 Nordix Foundation. All rights reserved.
- *  Modifications Copyright (C) 2020 Bell Canada. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +21,8 @@
 package org.onap.cps.spi.repository;
 
 import java.util.Collection;
+import java.util.Optional;
+import javax.validation.constraints.NotNull;
 import org.onap.cps.spi.entities.Dataspace;
 import org.onap.cps.spi.entities.Fragment;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -31,6 +32,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FragmentRepository extends JpaRepository<Fragment, Integer> {
+    Optional<Fragment> findByDataspaceAndAnchorName(@NotNull Dataspace dataspace, @NotNull String anchorName);
+
 
     default Collection<Fragment> findFragmentsThatAreAnchorsByDataspace(Dataspace dataspace) {
         return findFragmentsByDataspaceAndParentFragmentIsNull(dataspace);
