36fcdb68dfd74c4dd313efdf972538a6cafbab49
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2020-11-24 11:13:43 +0200

    ModuleSet SPI, persistence layer implementation (store) and tests
    Issue-ID: CPS-92
    Change-Id: I11257726e4f847e3337d8bafad824853e6a458e2
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- cps-ri/src/main/java/org/onap/cps/spi/impl/ModelPersistenceServiceImpl.java
+++ cps-ri/src/main/java/org/onap/cps/spi/impl/ModelPersistenceServiceImpl.java
@@ -20,39 +20,107 @@
 
 package org.onap.cps.spi.impl;
 
-import org.onap.cps.exceptions.CpsValidationException;
+import static org.onap.cps.exceptions.CpsExceptionBuilder.duplicateSchemaSetException;
+import static org.onap.cps.exceptions.CpsExceptionBuilder.emptySchemaSetException;
+import static org.onap.cps.exceptions.CpsExceptionBuilder.invalidDataspaceException;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 import org.onap.cps.spi.ModelPersistenceService;
 import org.onap.cps.spi.entities.Dataspace;
-import org.onap.cps.spi.entities.Module;
+import org.onap.cps.spi.entities.SchemaSetEntity;
+import org.onap.cps.spi.entities.YangFileEntity;
+import org.onap.cps.spi.model.SchemaSet;
+import org.onap.cps.spi.model.YangFile;
 import org.onap.cps.spi.repository.DataspaceRepository;
-import org.onap.cps.spi.repository.ModuleRepository;
+import org.onap.cps.spi.repository.SchemaSetRepository;
+import org.onap.cps.spi.repository.YangFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
 @Component
 public class ModelPersistenceServiceImpl implements ModelPersistenceService {
 
     @Autowired
-    private ModuleRepository moduleRepository;
+    private YangFileRepository yangFileRepository;
+
+    @Autowired
+    private SchemaSetRepository schemaSetRepository;
 
     @Autowired
     private DataspaceRepository dataspaceRepository;
 
     @Override
     public void storeModule(final String namespace, final String moduleContent, final String revision,
-        final String dataspaceName) {
-        final Dataspace dataspace = new Dataspace(dataspaceName);
-        if (Boolean.FALSE.equals(dataspaceRepository.existsByName(dataspaceName))) {
-            dataspaceRepository.save(dataspace);
+                            final String dataspaceName) {
+        // TODO this method should be removed as obsolete.
+        // Modules to be processed within modulesets only.
+    }
+
+    @Override
+    @Transactional
+    public void storeSchemaSet(final SchemaSet schemaSet) {
+        final Dataspace dataspace = dataspaceRepository.findByName(schemaSet.getDataspace())
+            .orElseThrow(() -> invalidDataspaceException(schemaSet.getDataspace()));
+        if (schemaSet.getYangFiles() == null || schemaSet.getYangFiles().isEmpty()) {
+            throw emptySchemaSetException();
         }
-        dataspace.setId(dataspaceRepository.getByName(dataspaceName).getId());
-        final Module module = new Module(namespace, moduleContent, revision, dataspace);
+        final Set<YangFileEntity> yangFiles = persistYangFiles(schemaSet.getYangFiles());
+        final SchemaSetEntity schemaSetEntity = new SchemaSetEntity();
+        schemaSetEntity.setName(schemaSet.getName());
+        schemaSetEntity.setDataspace(dataspace);
+        schemaSetEntity.setYangFiles(yangFiles);
         try {
-            moduleRepository.save(module);
-        } catch (final DataIntegrityViolationException ex) {
-            throw new CpsValidationException("Duplicate Entry",
-                String.format("Module already exist in dataspace %s.", dataspaceName));
+            schemaSetRepository.save(schemaSetEntity);
+        } catch (DataIntegrityViolationException e) {
+            throw duplicateSchemaSetException(schemaSet.getDataspace(), schemaSet.getName());
         }
     }
+
+    @Override
+    public void deleteSchemaSet(final String dataspaceName, final String moduleSetName) {
+        // not implemented yet
+    }
+
+    @Override
+    public List<SchemaSet> getAllSchemaSets(final String dataspaceName) {
+        // not implemented yet
+        return null;
+    }
+
+    @Override
+    public SchemaSet getSchemaSet(final String dataspaceName, final String moduleSetName) {
+        // not implemented yet
+        return null;
+    }
+
+    private Set<YangFileEntity> persistYangFiles(final Set<YangFile> yangFiles) {
+        final Map<String, YangFile> checksumMap = yangFiles.stream().collect(Collectors.toMap(
+            yangFile -> DigestUtils.md5DigestAsHex(yangFile.getContent().getBytes()),
+            yangFile -> yangFile));
+
+        final List<YangFileEntity> existingYangFiles = yangFileRepository.findAllByChecksumIn(checksumMap.keySet());
+        existingYangFiles.forEach(yangFile -> checksumMap.remove(yangFile.getChecksum()));
+
+        final List<YangFileEntity> newYangFiles = checksumMap.entrySet().stream().map(entry -> {
+            final YangFileEntity entity = new YangFileEntity();
+            entity.setName(entry.getValue().getName());
+            entity.setContent(entry.getValue().getContent());
+            entity.setChecksum(entry.getKey());
+            return entity;
+        }).collect(Collectors.toList());
+        yangFileRepository.saveAll(newYangFiles);
+
+        final Set<YangFileEntity> result = new HashSet<>();
+        result.addAll(existingYangFiles);
+        result.addAll(newYangFiles);
+        return result;
+    }
+
 }
---
+++ cps-ri/src/test/java/org/onap/cps/spi/impl/ModelPersistenceServiceTest.java
@@ -0,0 +1,157 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 Pantheon.tech
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.spi.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onap.cps.DatabaseTestContainer;
+import org.onap.cps.exceptions.CpsValidationException;
+import org.onap.cps.spi.ModelPersistenceService;
+import org.onap.cps.spi.entities.Dataspace;
+import org.onap.cps.spi.entities.SchemaSetEntity;
+import org.onap.cps.spi.entities.YangFileEntity;
+import org.onap.cps.spi.model.SchemaSet;
+import org.onap.cps.spi.model.YangFile;
+import org.onap.cps.spi.repository.DataspaceRepository;
+import org.onap.cps.spi.repository.SchemaSetRepository;
+import org.onap.cps.spi.repository.YangFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelPersistenceServiceTest {
+
+    private static final String CLEAR_DATA = "/data/clear-all.sql";
+    private static final String SET_DATA = "/data/schemaset.sql";
+
+    private static final String DATASPACE_NAME1 = "DATASPACE-001";
+    private static final String DATASPACE_NAME_INVALID = "DATASPACE-X";
+    private static final String SCHEMASET_NAME1 = "SCHEMASET-001";
+    private static final String SCHEMASET_NAME_NEW = "SCHEMASET-NEW";
+    private static final String OLD_FILE_CONTENT = "CONTENT-001";
+    private static final String NEW_FILE_NAME = "new.jang";
+    private static final String NEW_FILE_CONTENT = "CONTENT-NEW";
+    private static final String NEW_FILE_CHECKSUM = "c94d40a1350eb1c0b1c1949eac84fc59";
+
+    @ClassRule
+    public static DatabaseTestContainer testContainer = DatabaseTestContainer.getInstance();
+
+    @Autowired
+    private ModelPersistenceService modelPersistenceService;
+
+    @Autowired
+    private DataspaceRepository dataspaceRepository;
+
+    @Autowired
+    private YangFileRepository yangFileRepository;
+
+    @Autowired
+    private SchemaSetRepository schemaSetRepository;
+
+
+    @Test(expected = CpsValidationException.class)
+    @Sql(CLEAR_DATA)
+    public void testStoreSchemaSetToInvalidDataspace() {
+        modelPersistenceService.storeSchemaSet(buildSchemaSetToStore(DATASPACE_NAME_INVALID, SCHEMASET_NAME_NEW));
+    }
+
+    @Test(expected = CpsValidationException.class)
+    @SqlGroup({@Sql(CLEAR_DATA), @Sql(SET_DATA)})
+    public void testStoreEmptySchemaSet() {
+        modelPersistenceService.storeSchemaSet(
+            SchemaSet.builder().dataspace(DATASPACE_NAME1).name(SCHEMASET_NAME_NEW).build()
+        );
+    }
+
+    @Test(expected = CpsValidationException.class)
+    @SqlGroup({@Sql(CLEAR_DATA), @Sql(SET_DATA)})
+    public void testStoreDuplicateSchemaSet() {
+        modelPersistenceService.storeSchemaSet(buildSchemaSetToStore(DATASPACE_NAME1, SCHEMASET_NAME1));
+    }
+
+    @Test
+    @SqlGroup({@Sql(CLEAR_DATA), @Sql(SET_DATA)})
+    public void testStoreSchemaSet() {
+        modelPersistenceService.storeSchemaSet(buildSchemaSetToStore(DATASPACE_NAME1, SCHEMASET_NAME_NEW));
+
+        // validate the data is persisted
+        SchemaSetEntity schemaSetEntity = getEntityFromDatabase(DATASPACE_NAME1, SCHEMASET_NAME_NEW);
+        assertEquals(DATASPACE_NAME1, schemaSetEntity.getDataspace().getName());
+        assertEquals(SCHEMASET_NAME_NEW, schemaSetEntity.getName());
+
+        Set<YangFileEntity> yangFileEntities = schemaSetEntity.getYangFiles();
+        assertNotNull(yangFileEntities);
+        assertEquals(1, yangFileEntities.size());
+
+        YangFileEntity yangFileEntity = yangFileEntities.iterator().next();
+        assertEquals(NEW_FILE_NAME, yangFileEntity.getName());
+        assertEquals(NEW_FILE_CONTENT, yangFileEntity.getContent());
+        assertEquals(NEW_FILE_CHECKSUM, yangFileEntity.getChecksum());
+    }
+
+    @Test
+    @SqlGroup({@Sql(CLEAR_DATA), @Sql(SET_DATA)})
+    public void testStoreSchemaSetReuseFiles() throws Exception {
+
+        SchemaSet schemaSet = buildSchemaSetToStore(DATASPACE_NAME1, SCHEMASET_NAME_NEW);
+        schemaSet.setYangFiles(new HashSet<>(Arrays.asList(
+            YangFile.builder().name("new-file.yang").content(OLD_FILE_CONTENT).build()
+        )));
+        modelPersistenceService.storeSchemaSet(schemaSet);
+
+        // validate the data is persisted
+        SchemaSetEntity schemaSetEntity = getEntityFromDatabase(DATASPACE_NAME1, SCHEMASET_NAME_NEW);
+        assertEquals(DATASPACE_NAME1, schemaSetEntity.getDataspace().getName());
+        assertEquals(SCHEMASET_NAME_NEW, schemaSetEntity.getName());
+
+        Set<YangFileEntity> yangFileEntities = schemaSetEntity.getYangFiles();
+        assertNotNull(yangFileEntities);
+        assertEquals(1, yangFileEntities.size());
+
+        YangFileEntity yangFileEntity = yangFileEntities.iterator().next();
+        assertEquals("file-001.yang", yangFileEntity.getName());
+        assertEquals(OLD_FILE_CONTENT, yangFileEntity.getContent());
+        assertEquals("877e65a9f36d54e7702c3f073f6bc42b", yangFileEntity.getChecksum());
+    }
+
+    private static SchemaSet buildSchemaSetToStore(String dataspaceName, String moduleSetName) {
+        YangFile yangFile = YangFile.builder()
+            .name(NEW_FILE_NAME).content(NEW_FILE_CONTENT).build();
+        return SchemaSet.builder().dataspace(dataspaceName).name(moduleSetName)
+            .yangFiles(new HashSet<>(Arrays.asList(yangFile))).build();
+    }
+
+    private SchemaSetEntity getEntityFromDatabase(String dataspaceName, String moduleSetName) {
+        Dataspace dataspaceEntity = dataspaceRepository.findByName(DATASPACE_NAME1).orElseThrow();
+        return schemaSetRepository.findByDataspaceAndName(dataspaceEntity, SCHEMASET_NAME_NEW).orElseThrow();
+    }
+}
