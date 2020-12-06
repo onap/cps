e6ca09fc1b4b99ef402f58a5d16d273716ef7c65
puthuparambil.aditya <aditya.puthuparambil@bell.ca>
2020-12-06 22:18:16 +0000

    CPS Exception handling - extending the CPSValidationException
    Issue-ID: CPS-8
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>
    Change-Id: Id0728dad26edd79e3853c0d32c8e27dc7bedd369

---
+++ cps-service/src/main/java/org/onap/cps/exceptions/DataspaceNotFoundException.java
@@ -0,0 +1,58 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 Bell Canada. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.exceptions;
+
+
+/**
+ * Dataspace Not Found Exception. Indicates the requested data being absent.
+ */
+public class DataspaceNotFoundException extends CpsAdminException {
+
+    private static final long serialVersionUID = -1852996415384288431L;
+
+    /**
+     * Constructor.
+     *
+     * @param cause the cause of the exception
+     */
+    public DataspaceNotFoundException(final Throwable cause) {
+        super(cause.getMessage(), cause);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message the error message
+     * @param cause   the cause of the exception
+     */
+    public DataspaceNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message the error message
+     * @param details the error details
+     */
+    public DataspaceNotFoundException(final String message, final String details) {
+        super(message, details);
+    }
+}
