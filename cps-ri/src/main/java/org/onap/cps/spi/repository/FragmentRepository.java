e182a6b6cb4a87b4f461adf83fe60a65948fc230
puthuparambil.aditya <aditya.puthuparambil@bell.ca>
2020-12-10 16:49:53 +0000

    Retrieve All anchors for a given Dataspace
    Issue-ID: CPS-8
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>
    Change-Id: Idb2e4f83d390f078345e556d89781e0bf4a9a41f

--- cps-ri/src/main/java/org/onap/cps/spi/repository/FragmentRepository.java
+++ cps-ri/src/main/java/org/onap/cps/spi/repository/FragmentRepository.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2020 Nordix Foundation. All rights reserved.
+ *  Modifications Copyright (C) 2020 Bell Canada. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,10 +21,20 @@
 
 package org.onap.cps.spi.repository;
 
+import java.util.Collection;
+import org.onap.cps.spi.entities.Dataspace;
 import org.onap.cps.spi.entities.Fragment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FragmentRepository extends JpaRepository<Fragment, Integer> {
+
+    default Collection<Fragment> findFragmentsThatAreAnchorsByDataspace(Dataspace dataspace) {
+        return findFragmentsByDataspaceAndParentFragmentIsNull(dataspace);
+    }
+
+    Collection<Fragment> findFragmentsByDataspaceAndParentFragmentIsNull(Dataspace dataspace);
 }
