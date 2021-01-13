1b8a4dd237077944df7bef5fa04c412da01029f0
Claudio David Gasparini <claudio.gasparini@pantheon.tech>
2021-01-13 19:12:25 +0100

    Introduce caffeine cache
    Issue-ID: CPS-163
    Signed-off-by: Claudio David Gasparini <claudio.gasparini@pantheon.tech>
    Change-Id: Iff9b831c2d895d82aff419f60a8dd86a38b545d0

--- cps-service/src/main/java/org/onap/cps/api/impl/CpsModuleServiceImpl.java
+++ cps-service/src/main/java/org/onap/cps/api/impl/CpsModuleServiceImpl.java
@@ -20,7 +20,6 @@
 package org.onap.cps.api.impl;
 
 import java.util.Map;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.onap.cps.api.CpsModuleService;
 import org.onap.cps.spi.CascadeDeleteAllowed;
 import org.onap.cps.spi.CpsModulePersistenceService;
@@ -28,34 +27,32 @@ import org.onap.cps.spi.model.SchemaSet;
 import org.onap.cps.yang.YangTextSchemaSourceSet;
 import org.onap.cps.yang.YangTextSchemaSourceSetBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component("CpsModuleServiceImpl")
+@Service("CpsModuleServiceImpl")
 public class CpsModuleServiceImpl implements CpsModuleService {
 
     @Autowired
     private CpsModulePersistenceService cpsModulePersistenceService;
 
+    @Autowired
+    private YangTextSchemaSourceSetCache yangTextSchemaSourceSetCache;
+
     @Override
     public void createSchemaSet(final String dataspaceName, final String schemaSetName,
-                                final Map<String, String> yangResourcesNameToContentMap) {
-
-        YangTextSchemaSourceSetBuilder.validate(yangResourcesNameToContentMap);
-        cpsModulePersistenceService
-            .storeSchemaSet(dataspaceName, schemaSetName, yangResourcesNameToContentMap);
+            final Map<String, String> yangResourcesNameToContentMap) {
+        final YangTextSchemaSourceSet yangTextSchemaSourceSet
+                = YangTextSchemaSourceSetBuilder.of(yangResourcesNameToContentMap);
+        cpsModulePersistenceService.storeSchemaSet(dataspaceName, schemaSetName, yangResourcesNameToContentMap);
+        yangTextSchemaSourceSetCache.updateCache(dataspaceName, schemaSetName, yangTextSchemaSourceSet);
     }
 
     @Override
     public SchemaSet getSchemaSet(final String dataspaceName, final String schemaSetName) {
-        final Map<String, String> yangResourceNameToContent =
-                cpsModulePersistenceService.getYangSchemaResources(dataspaceName, schemaSetName);
-        final YangTextSchemaSourceSet yangTextSchemaSourceSet = YangTextSchemaSourceSetBuilder
-                                                                        .of(yangResourceNameToContent);
-        return SchemaSet.builder()
-                       .name(schemaSetName)
-                       .dataspaceName(dataspaceName)
-                       .moduleReferences(yangTextSchemaSourceSet.getModuleReferences())
-                .build();
+        final YangTextSchemaSourceSet yangTextSchemaSourceSet = yangTextSchemaSourceSetCache
+                                                                        .get(dataspaceName, schemaSetName);
+        return SchemaSet.builder().name(schemaSetName).dataspaceName(dataspaceName)
+                       .moduleReferences(yangTextSchemaSourceSet.getModuleReferences()).build();
     }
 
     @Override
