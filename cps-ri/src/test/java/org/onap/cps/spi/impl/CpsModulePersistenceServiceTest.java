2b862dc5d3dbf3844504660f4800d2f65a79e3dc
Claudio David Gasparini <claudio.gasparini@pantheon.tech>
2021-01-21 12:54:38 +0100

    Add checkstyle rule for unused imports
    Issue-ID: CPS-89
    Signed-off-by: Claudio David Gasparini <claudio.gasparini@pantheon.tech>
    Change-Id: I2cb1f758c9d2f451c11a607a84e1f196ae087165

--- cps-ri/src/test/java/org/onap/cps/spi/impl/CpsModulePersistenceServiceTest.java
+++ cps-ri/src/test/java/org/onap/cps/spi/impl/CpsModulePersistenceServiceTest.java
@@ -50,9 +50,7 @@ import org.onap.cps.spi.repository.YangResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
