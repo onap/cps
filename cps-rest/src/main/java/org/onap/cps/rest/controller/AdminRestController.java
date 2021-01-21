0ef16c7bf4be450f5dc624387f79f2a46d8ec8fb
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2021-01-21 09:37:05 +0200

    Enable log level management via actuator endpoint, fix /cps/api URI being for REST API only
    Issue-ID: CPS-158
    Change-Id: I1f9ff4429f331ca4204d1acaf73ac896a68994d0
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

--- cps-rest/src/main/java/org/onap/cps/rest/controller/AdminRestController.java
+++ cps-rest/src/main/java/org/onap/cps/rest/controller/AdminRestController.java
@@ -33,10 +33,12 @@ import org.onap.cps.spi.model.SchemaSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("${rest.api.base-path}")
 public class AdminRestController implements CpsAdminApi {
 
     @Autowired
