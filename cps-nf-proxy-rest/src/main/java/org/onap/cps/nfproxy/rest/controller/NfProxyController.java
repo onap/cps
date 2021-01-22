f02ac682f076eeb9ba777d82d29b3003cb8e82de
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2021-01-22 12:53:01 +0200

    Initial cps-nf-proxy-rest module setup in CPS project
    Issue-ID: CPS-171
    Change-Id: I8998dc2818b6bc07fc4fe25a2d735b4ab8b4b817
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>

---
+++ cps-nf-proxy-rest/src/main/java/org/onap/cps/nfproxy/rest/controller/NfProxyController.java
@@ -0,0 +1,42 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Pantheon.tech
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.nfproxy.rest.controller;
+
+import org.onap.cps.nfproxy.rest.api.NfProxyApi;
+import org.onap.cps.spi.exceptions.CpsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${rest.api.base-path}")
+public class NfProxyController implements NfProxyApi {
+
+    @Override
+    public ResponseEntity<Object> helloWorld() {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Object> helloError() {
+        throw new CpsException("Example error Message", "Example error description");
+    }
+}
