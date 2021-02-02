162a14feec75155f05c99e8a98cda02015bfe1ca
puthuparambil.aditya <aditya.puthuparambil@bell.ca>
2021-02-02 15:20:48 +0000

    E2E Network Slice test for 'Coverage Area-Tracking Area & TA-Cell mapping'
    Issue-ID: CPS-199
    Signed-off-by: puthuparambil.aditya <aditya.puthuparambil@bell.ca>
    Change-Id: Id8f7f595d04eb492c3ea065b8fcbf8ac9f3641bc

--- cps-service/src/test/groovy/org/onap/cps/api/impl/E2ENetworkSliceSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/api/impl/E2ENetworkSliceSpec.groovy
@@ -21,17 +21,27 @@
 package org.onap.cps.api.impl
 
 import org.onap.cps.TestUtils
+import org.onap.cps.api.CpsAdminService
+import org.onap.cps.spi.CpsDataPersistenceService
 import org.onap.cps.spi.CpsModulePersistenceService
+import org.onap.cps.spi.model.Anchor
+import org.onap.cps.yang.YangTextSchemaSourceSetBuilder
 import spock.lang.Specification
 
 class E2ENetworkSliceSpec extends Specification {
     def mockModuleStoreService = Mock(CpsModulePersistenceService)
+    def mockDataStoreService = Mock(CpsDataPersistenceService)
     def mockYangTextSchemaSourceSetCache = Mock(YangTextSchemaSourceSetCache)
+    def mockCpsAdminService = Mock(CpsAdminService)
     def objectUnderTest = new CpsModuleServiceImpl()
+    def cpsDataServiceImple = new CpsDataServiceImpl()
 
     def setup() {
         objectUnderTest.cpsModulePersistenceService = mockModuleStoreService
         objectUnderTest.yangTextSchemaSourceSetCache = mockYangTextSchemaSourceSetCache
+        cpsDataServiceImple.cpsDataPersistenceService = mockDataStoreService
+        cpsDataServiceImple.yangTextSchemaSourceSetCache = mockYangTextSchemaSourceSetCache
+        cpsDataServiceImple.cpsAdminService = mockCpsAdminService
     }
 
     def 'E2E model can be parsed by CPS.'() {
@@ -41,5 +51,35 @@ class E2ENetworkSliceSpec extends Specification {
             objectUnderTest.createSchemaSet('someDataspace', 'someSchemaSet', yangResourcesNameToContentMap)
         then: 'Parameters are validated and processing is delegated to persistence service'
             1 * mockModuleStoreService.storeSchemaSet('someDataspace', 'someSchemaSet', yangResourcesNameToContentMap)
+    }
+
+    def 'E2E Coverage Area-Tracking Area & TA-Cell mapping model can be parsed by CPS.'() {
+        given: 'Valid yang resource as name-to-content map'
+            def yangResourcesNameToContentMap = TestUtils.getYangResourcesAsMap(
+                    'e2e/basic/ietf-inet-types.yang','e2e/basic/ietf-yang-types.yang',
+                    'e2e/basic/cps-cavsta-onap-internal2021-01-28.yang')
+        when: 'Create schema set method is invoked'
+            objectUnderTest.createSchemaSet('someDataspace', 'someSchemaSet', yangResourcesNameToContentMap)
+        then: 'Parameters are validated and processing is delegated to persistence service'
+            1 * mockModuleStoreService.storeSchemaSet('someDataspace', 'someSchemaSet', yangResourcesNameToContentMap)
+    }
+
+    def 'E2E Coverage Area-Tracking Area & TA-Cell mapping data can be parsed.'() {
+        given: 'Valid yang resource as name-to-content map'
+            def yangResourcesNameToContentMap = TestUtils.getYangResourcesAsMap(
+                    'e2e/basic/ietf-inet-types.yang','e2e/basic/ietf-yang-types.yang',
+                    'e2e/basic/cps-cavsta-onap-internal2021-01-28.yang')
+            def schemaContext = YangTextSchemaSourceSetBuilder.of(yangResourcesNameToContentMap).getSchemaContext()
+        and : 'a valid json is provided for the model'
+            def jsonData = TestUtils.getResourceFileContent('e2e/basic/Data.txt')
+        and : 'all the further dependencies are mocked '
+            mockCpsAdminService.getAnchor('someDataspace', 'someAnchor') >>
+                    new Anchor().builder().name('someAnchor').schemaSetName('someSchemaSet').build()
+            mockYangTextSchemaSourceSetCache.get('someDataspace', 'someSchemaSet') >> YangTextSchemaSourceSetBuilder.of(yangResourcesNameToContentMap)
+            mockModuleStoreService.getYangSchemaResources('someDataspace', 'someSchemaSet') >> schemaContext
+        when: 'saveData method is invoked'
+            cpsDataServiceImple.saveData('someDataspace', 'someAnchor', jsonData)
+        then: 'Parameters are validated and processing is delegated to persistence service'
+            1 * mockDataStoreService.storeDataNode('someDataspace', 'someAnchor', _)
     }
 }
