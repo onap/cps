d75a929a5965e0059fc31506481dfa14f660569d
Hanif Kukkalli <lte-ng@outlook.com>
2021-02-23 12:26:39 +0100

    Implement getDataNode(anchorName, xPath) in NF-Proxy
    Added spring-boot-starter-validation instead of cps-ri.
    Added test 'Data node without leaves and without children.' to MultipartFileUtilSpec groovy test
    Updated review comments.
    Updated License
    Created a method getNodeByCmHandleIdAndXpath(cmHandleId, cpsPath)
    Added hardcoded value of NFS DataspaceName as: "NFP-Operation".
    Issue-ID: CPS-172
    
    Signed-off-by: Hanif Kukkalli <lte-ng@outlook.com>
    Change-Id: Ie8086ede01a9c5069b1ee75864b04a0ee5e8a4f6

---
+++ cps-nf-proxy-service/src/main/java/org/onap/cps/nfproxy/api/NfProxyDataService.java
@@ -0,0 +1,47 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  ONAP CPS
+ *  ================================================================================
+ *  Copyright (C) 2021 highstreet technologies GmbH Intellectual Property.
+ *  All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.nfproxy.api;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.onap.cps.spi.FetchDescendantsOption;
+import org.onap.cps.spi.model.DataNode;
+
+/*
+ * Datastore interface for handling CPS data.
+ */
+public interface NfProxyDataService {
+
+    /**
+     * Retrieves datanode by XPath for given cm handle id.
+     * The dataspace name is set to NFP-Operational
+     *
+     * @param cmHandleId             cm handle id
+     * @param xpath                  xpath
+     * @param fetchDescendantsOption defines the scope of data to fetch: either single node or all the descendant nodes
+     *                               (recursively) as well
+     * @return data node object
+     */
+    DataNode getDataNode(@NonNull String cmHandleId, @NonNull String xpath,
+                         @NonNull FetchDescendantsOption fetchDescendantsOption);
+
+}
