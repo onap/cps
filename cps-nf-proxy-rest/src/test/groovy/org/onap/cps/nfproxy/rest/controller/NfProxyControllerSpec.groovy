6138844a3ce8988b098a7e7c455288c204b8a79b
Hanif Kukkalli <lte-ng@outlook.com>
2021-02-23 12:26:39 +0100

    Implement getDataNode(anchorName, xPath) in NF-Proxy
    Updated License
    Updated review comments.
    Created a method getNodeByCmHandleIdAndXpath(cmHandleId, cpsPath)
    Added hardcoded value of NFS DataspaceName as: "NFP-Operation".
    Issue-ID: CPS-172
    
    Signed-off-by: Hanif Kukkalli <lte-ng@outlook.com>
    Change-Id: Ie8086ede01a9c5069b1ee75864b04a0ee5e8a4f6

--- cps-nf-proxy-rest/src/test/groovy/org/onap/cps/nfproxy/rest/controller/NfProxyControllerSpec.groovy
+++ cps-nf-proxy-rest/src/test/groovy/org/onap/cps/nfproxy/rest/controller/NfProxyControllerSpec.groovy
@@ -1,6 +1,9 @@
 /*
  *  ============LICENSE_START=======================================================
- *  Copyright (C) 2021 Pantheon.tech
+ *  ONAP CPS
+ *  ================================================================================
+ *  Copyright (C) 2021 highstreet technologies GmbH Intellectual Property.
+ *  All rights reserved.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,20 +15,25 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
- *  SPDX-License-Identifier: Apache-2.0
  *  ============LICENSE_END=========================================================
  */
 
 package org.onap.cps.nfproxy.rest.controller
 
+import com.google.gson.Gson
+import org.onap.cps.api.NfProxyDataService
+import org.onap.cps.spi.model.DataNodeBuilder
+import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import static org.onap.cps.spi.FetchDescendantsOption.OMIT_DESCENDANTS
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 @WebMvcTest
 class NfProxyControllerSpec extends Specification {
@@ -33,22 +41,31 @@ class NfProxyControllerSpec extends Specification {
     @Autowired
     MockMvc mvc
 
+    @SpringBean
+    NfProxyDataService mockNfProxyDataService = Mock()
+
     @Value('${rest.api.xnf-base-path}')
     def basePath
 
-    def 'Hello world method invocation.'(){
-        when: 'hello-world request performed'
-            def response = mvc.perform(MockMvcRequestBuilders.get("$basePath/v1/hello-world")).andReturn().response
-        then: 'success response returned'
-            response.status == HttpStatus.OK.value()
-            response.getContentAsString().contains("Hello World!")
+    def dataNodeBaseEndpoint
+
+    def setup() {
+        dataNodeBaseEndpoint = "$basePath/v1"
     }
 
-    def 'Example error handling.'(){
-        when: 'hello-error request performed'
-            def response = mvc.perform(MockMvcRequestBuilders.get("$basePath/v1/hello-error")).andReturn().response
-        then: 'error response returned'
-            response.status == HttpStatus.INTERNAL_SERVER_ERROR.value()
-            response.getContentAsString().contains("Example error")
+    @Unroll
+    def 'Get data node.'() {
+        given: 'the service returns a data node'
+            def dataNode = new DataNodeBuilder().withXpath('some xpath').build()
+            def cmHandleId = 'some handle'
+            def xpath = 'some xPath'
+            def endpoint = "$dataNodeBaseEndpoint/cm-handles/$cmHandleId/node"
+            mockNfProxyDataService.getDataNode(cmHandleId, xpath, OMIT_DESCENDANTS) >> dataNode
+        when: 'get request is performed through REST API'
+            def response = mvc.perform(get(endpoint).param('cps-path', xpath)).andReturn().response
+        then: 'a success response is returned'
+            response.status == HttpStatus.OK.value()
+        and: 'response contains expected datanode in json format'
+            response.getContentAsString().contains(new Gson().toJson(dataNode))
     }
 }
