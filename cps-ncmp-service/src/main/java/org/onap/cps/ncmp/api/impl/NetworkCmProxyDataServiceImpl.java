7c229309329569004f12f306b9c883f2b4ccebe3
tragait <rahul.tyagi@est.tech>
2021-03-30 12:02:27 +0100

    rename nf-proxy to network-cm-proxy
    This commit renames two modules cps-nf-proxy-rest and
    cps-nf-proxy-service to cps-ncmp-rest, cps-ncmp-service.
    Docker image names are also changed respectively.
    Swagger auto generated api files are also modified.
    
    Signed-off-by: tragait <rahul.tyagi@est.tech>
    Issue-ID: CPS-301
    Change-Id: Ic2a2f8c4bafe8cffa3c83ccb52499720aaba1415

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImpl.java
@@ -0,0 +1,64 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 highstreet technologies GmbH
+ *  Copyright (C) 2021 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl;
+
+import java.util.Collection;
+import org.onap.cps.api.CpsDataService;
+import org.onap.cps.api.CpsQueryService;
+import org.onap.cps.ncmp.api.NetworkCmProxyDataService;
+import org.onap.cps.spi.FetchDescendantsOption;
+import org.onap.cps.spi.model.DataNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NetworkCmProxyDataServiceImpl implements NetworkCmProxyDataService {
+
+    private static final String NF_PROXY_DATASPACE_NAME = "NFP-Operational";
+
+    @Autowired
+    private CpsDataService cpsDataService;
+
+    @Autowired
+    private CpsQueryService cpsQueryService;
+
+    @Override
+    public DataNode getDataNode(final String cmHandle, final String xpath,
+        final FetchDescendantsOption fetchDescendantsOption) {
+        return cpsDataService.getDataNode(NF_PROXY_DATASPACE_NAME, cmHandle, xpath, fetchDescendantsOption);
+    }
+
+    @Override
+    public Collection<DataNode> queryDataNodes(final String cmHandle, final String cpsPath,
+        final FetchDescendantsOption fetchDescendantsOption) {
+        return cpsQueryService.queryDataNodes(NF_PROXY_DATASPACE_NAME, cmHandle, cpsPath, fetchDescendantsOption);
+    }
+
+    @Override
+    public void updateNodeLeaves(final String cmHandle, final String parentNodeXpath, final String jsonData) {
+        cpsDataService.updateNodeLeaves(NF_PROXY_DATASPACE_NAME, cmHandle, parentNodeXpath, jsonData);
+    }
+
+    @Override
+    public void replaceNodeTree(final String cmHandle, final String parentNodeXpath, final String jsonData) {
+        cpsDataService.replaceNodeTree(NF_PROXY_DATASPACE_NAME, cmHandle, parentNodeXpath, jsonData);
+    }
+}
--- docker-compose/README.md
+++ docker-compose/README.md
@@ -6,7 +6,7 @@ Following command builds all Java components to `cps-application/target/cps-appl
 without generating any docker images:  
 
 ```bash
-mvn clean install -Pcps-docker -Pxnf-docker -Pcps-xnf-docker -Djib.skip
+mvn clean install -Pcps-docker -Pncmp-docker -Pcps-ncmp-docker -Djib.skip
 ```
 
 ## Building Java Archive and Docker images
@@ -14,7 +14,7 @@ mvn clean install -Pcps-docker -Pxnf-docker -Pcps-xnf-docker -Djib.skip
 * Following command builds the JAR file and also generates the Docker image for all CPS components:
 
 ```bash
-mvn clean install -Pcps-docker -Pxnf-docker -Pcps-xnf-docker -Dnexus.repository=
+mvn clean install -Pcps-docker -Pncmp-docker -Pcps-ncmp-docker -Dnexus.repository=
 ```
 
 * Following command builds the JAR file and generates the Docker image for specified CPS component:
@@ -29,8 +29,8 @@ mvn clean install -P<docker-profile> -Dnexus.repository=
 `docker-compose/docker-compose.yml` file is provided to be run with `docker-compose` tool and images previously built.
 It starts both Postgres database and CPS services.
 
-1. Edit `docker-compose.yml` and uncomment desired service to be deployed, by default `cps-and-nf-proxy`
-   is enabled. You can comment it and uncomment `cps-standalone` or `nf-proxy-standalone`.
+1. Edit `docker-compose.yml` and uncomment desired service to be deployed, by default `cps-and-network-cm-proxy`
+   is enabled. You can comment it and uncomment `cps-standalone` or `network-cm-proxy-standalone`.
 2. Execute following command from `docker-compose` folder:
 
 ```bash
