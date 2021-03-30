006db5c54cf9c0df7845d335e35183e9b0f6e5b9
tragait <rahul.tyagi@est.tech>
2021-03-30 12:02:27 +0100

    rename nf-proxy to network-cm-proxy
    This commit renames two modules cps-nf-proxy-rest and
    cps-nf-proxy-service to cps-ncmp-rest, cps-ncmp-service.
    Docker image names are also changed respectively.
    Swagger auto generated api files are also modified.
    
    Signed-off-by: tragait <rahul.tyagi@est.tech>
    Issue-ID: CPS-315
    Change-Id: Ic2a2f8c4bafe8cffa3c83ccb52499720aaba1415

---
+++ cps-ncmp-rest/src/test/groovy/org/onap/cps/ncmp/config/NetworkCmProxyConfigSpec.groovy
@@ -0,0 +1,34 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 highstreet technologies GmbH
+ *  Modification Copyright (C) 2021 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.config
+
+import org.onap.cps.ncmp.config.NetworkCmProxyConfig
+import spock.lang.Specification
+import springfox.documentation.spring.web.plugins.Docket
+
+class NetworkCmProxyConfigSpec extends Specification {
+    def objectUnderTest = new NetworkCmProxyConfig()
+
+    def 'NetworkCmProxy configuration has a Docket API.'() {
+        expect: 'the NetworkCmProxy configuration has a Docket API'
+            objectUnderTest.api() instanceof Docket
+    }
+}
