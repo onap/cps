1861485c27a05ff7168a31308067bbac3379dade
niamhcore <niamh.core@est.tech>
2021-04-23 11:07:43 +0100

    Implement cps path query to get ancestor by schema node identifier
    Issue-ID: CPS-305
    
    Signed-off-by: niamhcore <niamh.core@est.tech>
    Change-Id: Ic4b21308478f399e3a454dbcd73943e077b0f3f2

--- cps-ri/src/test/groovy/org/onap/cps/spi/query/CpsPathQuerySpec.groovy
+++ cps-ri/src/test/groovy/org/onap/cps/spi/query/CpsPathQuerySpec.groovy
@@ -38,10 +38,10 @@ class CpsPathQuerySpec extends Specification {
             result.leafValue == expectedLeafValue
         where: 'the following data is used'
             scenario               | cpsPath                                                  || expectedXpathPrefix | expectedLeafName       | expectedLeafValue
-            'leaf of type String'  | '/parent/child[@common-leaf-name=\'common-leaf-value\']' || '/parent/child'     |'common-leaf-name'      | 'common-leaf-value'
-            'leaf of type Integer' | '/parent/child[@common-leaf-name-int=5]'                 || '/parent/child'     |'common-leaf-name-int'  | 5
-            'spaces around ='      | '/parent/child[@common-leaf-name-int = 5]'               || '/parent/child'     |'common-leaf-name-int'  | 5
-            'key in top container' | '/parent[@common-leaf-name-int=5]'                       || '/parent'           |'common-leaf-name-int'  | 5
+            'leaf of type String'  | '/parent/child[@common-leaf-name=\'common-leaf-value\']' || '/parent/child'     | 'common-leaf-name'     | 'common-leaf-value'
+            'leaf of type Integer' | '/parent/child[@common-leaf-name-int=5]'                 || '/parent/child'     | 'common-leaf-name-int' | 5
+            'spaces around ='      | '/parent/child[@common-leaf-name-int = 5]'               || '/parent/child'     | 'common-leaf-name-int' | 5
+            'key in top container' | '/parent[@common-leaf-name-int=5]'                       || '/parent'           | 'common-leaf-name-int' | 5
     }
 
     def 'Parse cps path of type ends with a #scenario.'() {
@@ -52,9 +52,9 @@ class CpsPathQuerySpec extends Specification {
         and: 'the right ends with parameters are set'
             result.descendantName == expectedEndsWithValue
         where: 'the following data is used'
-            scenario         | cpsPath                  || expectedEndsWithValue
-            'yang container' | '//cps-path'             || 'cps-path'
-            'parent & child' | '//parent/child'         || 'parent/child'
+            scenario         | cpsPath          || expectedEndsWithValue
+            'yang container' | '//cps-path'     || 'cps-path'
+            'parent & child' | '//parent/child' || 'parent/child'
     }
 
     def 'Parse cps path that ends with a yang list containing #scenario.'() {
@@ -67,8 +67,8 @@ class CpsPathQuerySpec extends Specification {
             result.leavesData.size() == expectedNumberOfLeaves
         where: 'the following data is used'
             scenario                  | cpsPath                                            || expectedNumberOfLeaves
-            'one attribute'           | '//child[@common-leaf-name-int=5]'                 ||  1
-            'more than one attribute' | '//child[@int-leaf=5 and @leaf-name="leaf value"]' ||  2
+            'one attribute'           | '//child[@common-leaf-name-int=5]'                 || 1
+            'more than one attribute' | '//child[@int-leaf=5 and @leaf-name="leaf value"]' || 2
     }
 
     def 'Parse cps path with #scenario.'() {
@@ -86,6 +86,7 @@ class CpsPathQuerySpec extends Specification {
             'too many containers'                                               | '/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50/51/52/53/54/55/56/57/58/59/60/61/62/63/64/65/66/67/68/69/70/71/72/73/74/75/76/77/78/79/80/81/82/83/84/85/86/87/88/89/90/91/92/93/94/95/96/97/98/99/100[@a=1]'
             'end with descendant and more than one attribute separated by "or"' | '//child[@int-leaf=5 or @leaf-name="leaf value"]'
             'missing attribute value'                                           | '//child[@int-leaf=5 and @name]'
+            'incomplete ancestor value'                                         | '//books/ancestor::'
     }
 
     def 'Convert cps leaf value to valid type with leaf of type #scenario.'() {
@@ -94,12 +95,22 @@ class CpsPathQuerySpec extends Specification {
         then: 'the leaf value returned is of the right type'
             result == expectedLeafOutputValue
         where: "the following data is used"
-            scenario                         | leafValueInputString         ||  expectedLeafOutputValue
-            'Integer'                        | "5"                          ||  5
+            scenario                         | leafValueInputString         || expectedLeafOutputValue
+            'Integer'                        | "5"                          || 5
             'String with single quotes'      | '\'value in single quotes\'' || 'value in single quotes'
             'String with double quotes'      | '"value in double quotes"'   || 'value in double quotes'
             'String containing single quote' | '"value with \'"'            || 'value with \''
             'String containing double quote' | '\'value with "\''           || 'value with "'
     }
-    
+
+    def 'Parse cps path using ancestor by schema node identifier.'() {
+        when: 'the given cps path is parsed'
+            def result = objectUnderTest.createFrom('//someXpath/ancestor::someAncestor')
+        then: 'the query has the right type'
+            result.cpsPathQueryType == CpsPathQueryType.XPATH_HAS_DESCENDANT_ANYWHERE
+        and: 'the correct ancestor schema node identifier is set'
+            result.ancestorSchemaNodeIdentifier == 'someAncestor'
+        and: 'the result has ancestor axis'
+            result.hasAncestorAxis()
+    }
 }
