51ecaead8d6461c1d7e3c18d7d5a4b413ef9337b
niamhcore <niamh.core@est.tech>
2021-04-23 11:07:43 +0100

    Implement cps path query to get ancestor by schema node identifier
    Issue-ID: CPS-305
    
    Signed-off-by: niamhcore <niamh.core@est.tech>
    Change-Id: Ic4b21308478f399e3a454dbcd73943e077b0f3f2

--- cps-ri/src/test/resources/data/fragment.sql
+++ cps-ri/src/test/resources/data/fragment.sql
@@ -30,3 +30,10 @@ INSERT INTO FRAGMENT (ID, DATASPACE_ID, ANCHOR_ID, PARENT_ID, XPATH, ATTRIBUTES)
     (4205, 1001, 3003, 4204, '/parent-200/child-202/grand-child-202', '{"common-leaf-name": "common-leaf value", "common-leaf-name-int" : 5}'),
     (4206, 1001, 3003, null, '/parent-201', '{"leaf-value": "original"}'),
     (4207, 1001, 3003, 4201, '/parent-201/child-202', '{"common-leaf-name": "common-leaf other value", "common-leaf-name-int" : 5}');
+
+INSERT INTO FRAGMENT (ID, DATASPACE_ID, ANCHOR_ID, PARENT_ID, XPATH) VALUES
+    (1, 1001, 3001, null, '/bookstore'),
+    (2, 1001, 3001, 1, '/bookstore/categories[@name="SciFi"]'),
+    (3, 1001, 3001, 2, '/bookstore/categories[@name="SciFi"]/books'),
+    (4, 1001, 3001, 1, '/bookstore/categories[@name="kids"]'),
+    (5, 1001, 3001, 4, '/bookstore/categories[@name="kids"]/books');
