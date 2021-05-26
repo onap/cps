c3835a3e625a63401f174c20cb10338873ef256f
Renu Kumari <renu.kumari@bell.ca>
2021-05-26 08:47:52 -0400

    Load only required components in the application contexts
    Issue-ID: CPS-434
    Signed-off-by: Renu Kumari <renu.kumari@bell.ca>
    Change-Id: Iacc12946495b9e6ec42b4b7d4e3b5b010fecceb5

--- cps-service/src/test/groovy/org/onap/cps/utils/YangTextSchemaSourceSetSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/utils/YangTextSchemaSourceSetSpec.groovy
@@ -1,6 +1,7 @@
 /*
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2020 Nordix Foundation
+ *  Modifications Copyright (C) 2021 Bell Canada.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -31,7 +32,7 @@ class YangTextSchemaSourceSetSpec extends Specification {
         given: 'a yang model (file)'
             def yangResourceNameToContent = [filename: TestUtils.getResourceFileContent('bookstore.yang')]
         when: 'the content is parsed'
-            def result = YangTextSchemaSourceSetBuilder.of(yangResourceNameToContent).getSchemaContext();
+            def result = YangTextSchemaSourceSetBuilder.of(yangResourceNameToContent).getSchemaContext()
         then: 'the result contains 1 module of the correct name and revision'
             result.modules.size() == 1
             def optionalModule = result.findModule('stores', Revision.of('2020-09-15'))
