5575ea0fab2785ba42f10b593c2c12016224017b
JosephKeenan <joseph.keenan@est.tech>
2021-06-23 14:15:52 +0100

    CPS-401 Update Open API YAML with data types and example for output (cpsData)
    -added response for get data nodes based on dataspace and anchor
    -modified modelmapper jn DataRestControllerSpec to be Spy not Mock
    -when openapi.yaml is entered into https://editor.swagger.io/ it
    displays however locally it does not, This may be due to file
    segmentation but this is still unclear.
    
    Issue-ID: CPS-401
    
    Change-Id: I45286530d9a9ca85b56c0a656a44675398a003f4
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>

--- cps-rest/src/test/groovy/org/onap/cps/rest/controller/DataRestControllerSpec.groovy
+++ cps-rest/src/test/groovy/org/onap/cps/rest/controller/DataRestControllerSpec.groovy
@@ -61,7 +61,7 @@ class DataRestControllerSpec extends Specification {
     CpsQueryService mockCpsQueryService = Mock()
 
     @SpringBean
-    ModelMapper modelMapper = Mock()
+    ModelMapper modelMapper = Spy()
 
     @Autowired
     MockMvc mvc
