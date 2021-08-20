cf3f5b90cfa98aa29eeafd39c2955014892d1baa
JosephKeenan <joseph.keenan@est.tech>
2021-08-20 10:33:54 +0100

    CPS-505 Retrieving modules for new CM handle
    -Added some production code for getting missing modules for new CM
    handle
    -Groovy test template added by Toine for getting msissing modules
    -Added json example for test
    -Modified test to check map contents
    -Differentiated restTemplate calls based on URL
    -Fixed code review comment`s
    -Groovy test now passing
    
    Issue-ID: CPS-505
    
    Change-Id: I91ef65467496caea7834ba2e8af99cfe58d4f880
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImpl.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImpl.java
@@ -24,17 +24,24 @@ package org.onap.cps.ncmp.api.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.onap.cps.api.CpsDataService;
+import org.onap.cps.api.CpsModuleService;
 import org.onap.cps.api.CpsQueryService;
 import org.onap.cps.ncmp.api.NetworkCmProxyDataService;
+import org.onap.cps.ncmp.api.impl.config.NcmpConfiguration.DmiProperties;
 import org.onap.cps.ncmp.api.impl.exception.NcmpException;
 import org.onap.cps.ncmp.api.impl.operation.DmiOperations;
 import org.onap.cps.ncmp.api.models.CmHandle;
@@ -42,14 +49,21 @@ import org.onap.cps.ncmp.api.models.DmiPluginRegistration;
 import org.onap.cps.ncmp.api.models.GenericRequestBody;
 import org.onap.cps.ncmp.api.models.PersistenceCmHandle;
 import org.onap.cps.ncmp.api.models.PersistenceCmHandlesList;
+import org.onap.cps.ncmp.api.models.YangResource;
 import org.onap.cps.spi.FetchDescendantsOption;
 import org.onap.cps.spi.exceptions.DataNodeNotFoundException;
 import org.onap.cps.spi.exceptions.DataValidationException;
 import org.onap.cps.spi.model.DataNode;
+import org.onap.cps.spi.model.ModuleReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @Slf4j
@@ -70,60 +84,72 @@ public class NetworkCmProxyDataServiceImpl implements NetworkCmProxyDataService 
 
     private DmiOperations dmiOperations;
 
+    private CpsModuleService cpsModuleService;
+
+    private RestTemplate restTemplate;
+
+    private DmiProperties dmiProperties;
+
     /**
      * Constructor Injection for Dependencies.
-     *
-     * @param dmiOperations   dmi operation
-     * @param cpsDataService  Data Service Interface
+     * @param dmiProperties dmi properties
+     * @param dmiOperations dmi operation
+     * @param cpsModuleService cps module service
+     * @param cpsDataService Data Service Interface
      * @param cpsQueryService Query Service Interface
-     * @param objectMapper    Object Mapper
+     * @param objectMapper Object Mapper
+     * @param restTemplate rest template
      */
-    public NetworkCmProxyDataServiceImpl(final DmiOperations dmiOperations, final CpsDataService cpsDataService,
-        final CpsQueryService cpsQueryService, final ObjectMapper objectMapper) {
+    public NetworkCmProxyDataServiceImpl(final DmiProperties dmiProperties,
+                                         final DmiOperations dmiOperations,
+                                         final CpsModuleService cpsModuleService,
+                                         final CpsDataService cpsDataService,
+                                         final CpsQueryService cpsQueryService,
+                                         final ObjectMapper objectMapper,
+                                         final RestTemplate restTemplate) {
+        this.dmiProperties = dmiProperties;
         this.dmiOperations = dmiOperations;
+        this.cpsModuleService = cpsModuleService;
         this.cpsDataService = cpsDataService;
         this.cpsQueryService = cpsQueryService;
         this.objectMapper = objectMapper;
-    }
-
-    private String getDataspaceName() {
-        return NF_PROXY_DATASPACE_NAME;
+        this.restTemplate = restTemplate;
     }
 
     @Override
     public DataNode getDataNode(final String cmHandle, final String xpath,
         final FetchDescendantsOption fetchDescendantsOption) {
-        return cpsDataService.getDataNode(getDataspaceName(), cmHandle, xpath, fetchDescendantsOption);
+        return cpsDataService.getDataNode(NF_PROXY_DATASPACE_NAME, cmHandle, xpath, fetchDescendantsOption);
     }
 
     @Override
     public Collection<DataNode> queryDataNodes(final String cmHandle, final String cpsPath,
-        final FetchDescendantsOption fetchDescendantsOption) {
-        return cpsQueryService.queryDataNodes(getDataspaceName(), cmHandle, cpsPath, fetchDescendantsOption);
+                                               final FetchDescendantsOption fetchDescendantsOption) {
+        return cpsQueryService.queryDataNodes(NF_PROXY_DATASPACE_NAME, cmHandle, cpsPath, fetchDescendantsOption);
     }
 
     @Override
     public void createDataNode(final String cmHandle, final String parentNodeXpath, final String jsonData) {
         if (!StringUtils.hasText(parentNodeXpath) || "/".equals(parentNodeXpath)) {
-            cpsDataService.saveData(getDataspaceName(), cmHandle, jsonData);
+            cpsDataService.saveData(NF_PROXY_DATASPACE_NAME, cmHandle, jsonData);
         } else {
-            cpsDataService.saveData(getDataspaceName(), cmHandle, parentNodeXpath, jsonData);
+            cpsDataService.saveData(NF_PROXY_DATASPACE_NAME, cmHandle, parentNodeXpath, jsonData);
         }
     }
 
     @Override
     public void addListNodeElements(final String cmHandle, final String parentNodeXpath, final String jsonData) {
-        cpsDataService.saveListNodeData(getDataspaceName(), cmHandle, parentNodeXpath, jsonData);
+        cpsDataService.saveListNodeData(NF_PROXY_DATASPACE_NAME, cmHandle, parentNodeXpath, jsonData);
     }
 
     @Override
     public void updateNodeLeaves(final String cmHandle, final String parentNodeXpath, final String jsonData) {
-        cpsDataService.updateNodeLeaves(getDataspaceName(), cmHandle, parentNodeXpath, jsonData);
+        cpsDataService.updateNodeLeaves(NF_PROXY_DATASPACE_NAME, cmHandle, parentNodeXpath, jsonData);
     }
 
     @Override
     public void replaceNodeTree(final String cmHandle, final String parentNodeXpath, final String jsonData) {
-        cpsDataService.replaceNodeTree(getDataspaceName(), cmHandle, parentNodeXpath, jsonData);
+        cpsDataService.replaceNodeTree(NF_PROXY_DATASPACE_NAME, cmHandle, parentNodeXpath, jsonData);
     }
 
     @Override
@@ -141,13 +167,13 @@ public class NetworkCmProxyDataServiceImpl implements NetworkCmProxyDataService 
 
     private void parseAndCreateCmHandlesInDmiRegistration(final DmiPluginRegistration dmiPluginRegistration) {
         try {
-            final List<PersistenceCmHandle> createdPersistenceCmHandles =
-                new LinkedList<>();
+            final var persistenceCmHandlesList = new PersistenceCmHandlesList();
             for (final CmHandle cmHandle : dmiPluginRegistration.getCreatedCmHandles()) {
-                createdPersistenceCmHandles.add(toPersistenceCmHandle(dmiPluginRegistration, cmHandle));
+                final PersistenceCmHandle persistenceCmHandle = toPersistenceCmHandle(dmiPluginRegistration, cmHandle);
+                persistenceCmHandlesList.add(persistenceCmHandle);
+                modelSync(persistenceCmHandle.getId());
             }
-            final PersistenceCmHandlesList persistenceCmHandlesList = new PersistenceCmHandlesList();
-            persistenceCmHandlesList.setCmHandles(createdPersistenceCmHandles);
+
             final String cmHandleJsonData = objectMapper.writeValueAsString(persistenceCmHandlesList);
             cpsDataService.saveListNodeData(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR, "/dmi-registry",
                 cmHandleJsonData);
@@ -161,18 +187,17 @@ public class NetworkCmProxyDataServiceImpl implements NetworkCmProxyDataService 
 
     private void parseAndUpdateCmHandlesInDmiRegistration(final DmiPluginRegistration dmiPluginRegistration) {
         try {
-            final List<PersistenceCmHandle> updatedPersistenceCmHandles =
-                new LinkedList<>();
-            for (final CmHandle cmHandle : dmiPluginRegistration.getUpdatedCmHandles()) {
-                updatedPersistenceCmHandles.add(toPersistenceCmHandle(dmiPluginRegistration, cmHandle));
-            }
             final PersistenceCmHandlesList persistenceCmHandlesList = new PersistenceCmHandlesList();
-            persistenceCmHandlesList.setCmHandles(updatedPersistenceCmHandles);
+            for (final CmHandle cmHandle : dmiPluginRegistration.getUpdatedCmHandles()) {
+                final PersistenceCmHandle persistenceCmHandle = toPersistenceCmHandle(dmiPluginRegistration, cmHandle);
+                persistenceCmHandlesList.add(persistenceCmHandle);
+            }
+
             final String cmHandlesJsonData = objectMapper.writeValueAsString(persistenceCmHandlesList);
             cpsDataService.updateNodeLeavesAndExistingDescendantLeaves(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR,
                 "/dmi-registry", cmHandlesJsonData);
         } catch (final JsonProcessingException e) {
-            log.error("Parsing error occurred while converting Object to JSON Dmi Registry.");
+            log.error("Parsing error occurred while converting Object to JSON DMI Registry.");
             throw new DataValidationException(
                 "Parsing error occurred while processing DMI Plugin Registration" + dmiPluginRegistration, e
                 .getMessage(), e);
@@ -278,6 +303,7 @@ public class NetworkCmProxyDataServiceImpl implements NetworkCmProxyDataService 
         }
     }
 
+
     private PersistenceCmHandle toPersistenceCmHandle(final DmiPluginRegistration dmiPluginRegistration,
         final CmHandle cmHandle) {
         final PersistenceCmHandle persistenceCmHandle = new PersistenceCmHandle();
@@ -291,4 +317,85 @@ public class NetworkCmProxyDataServiceImpl implements NetworkCmProxyDataService 
         return persistenceCmHandle;
     }
 
+    protected void modelSync(final String cmHandle) {
+        final var response = callDmi(cmHandle, "modules");
+
+        final List<ModuleReference> modulesFromDmiForCmHandle = getModuleReferences(response);
+
+        final var knownModulesInCps = cpsModuleService.getAllYangResourcesModuleReferences();
+
+        final List<ModuleReference> modulesNotAlreadyInCps = new ArrayList<>();
+        for (final ModuleReference moduleFromDmiForCmHandle : modulesFromDmiForCmHandle) {
+            if (!knownModulesInCps.contains(moduleFromDmiForCmHandle)) {
+                modulesNotAlreadyInCps.add(moduleFromDmiForCmHandle);
+            }
+        }
+
+        final Map<String, String> newYangResourcesModuleNameToContentMap =
+            getYangResources(cmHandle, modulesNotAlreadyInCps);
+
+        cpsModuleService.createSchemaSetFromModules(NCMP_DATASPACE_NAME, cmHandle,
+            newYangResourcesModuleNameToContentMap, modulesFromDmiForCmHandle);
+    }
+
+    private Map<String, String> getYangResources(final String cmHandle,
+                                                 final List<ModuleReference> modulesNotAlreadyInCps) {
+        final var moduleResourcesAsJsonString = callDmi(cmHandle, "moduleResources");
+        final JsonArray moduleResources = new Gson().fromJson(moduleResourcesAsJsonString.getBody(), JsonArray.class);
+        final Map<String, String> newYangResourcesModuleNameToContentMap = new HashMap<>();
+
+        for (final JsonElement moduleResource : moduleResources) {
+            final YangResource yangResource = toYangResource((JsonObject) moduleResource);
+            newYangResourcesModuleNameToContentMap.put(yangResource.getName(), yangResource.getYangSource());
+        }
+        return newYangResourcesModuleNameToContentMap;
+    }
+
+    private YangResource toYangResource(final JsonObject yangResourceAsJson) {
+        final var name = yangResourceAsJson.get("name").getAsString();
+        final var revision = yangResourceAsJson.get("revision").getAsString();
+        final var yangSource = yangResourceAsJson.get("yang-source").getAsString();
+
+        final YangResource yangResource = new YangResource();
+        yangResource.setName(name);
+        yangResource.setRevision(revision);
+        yangResource.setYangSource(yangSource);
+        return yangResource;
+    }
+
+    private List<ModuleReference> getModuleReferences(final ResponseEntity<String> response) {
+        final List<ModuleReference> modulesFromDmiForCmHandle = new ArrayList<>();
+        final JsonObject convertedObject = new Gson().fromJson(response.getBody(), JsonObject.class);
+        final JsonObject schemas = convertedObject.getAsJsonObject("schemas");
+        final JsonArray modules = schemas.getAsJsonArray("schema");
+        for (final JsonElement module : modules) {
+            final ModuleReference moduleReference = toModuleReference((JsonObject) module);
+            modulesFromDmiForCmHandle.add(moduleReference);
+        }
+        return modulesFromDmiForCmHandle;
+    }
+
+    private ModuleReference toModuleReference(final JsonObject module) {
+        final var moduleReference = new ModuleReference();
+        moduleReference.setName(module.get("identifier").getAsString());
+        moduleReference.setNamespace(module.get("namespace").getAsString());
+        moduleReference.setRevision(module.get("version").getAsString());
+        return moduleReference;
+    }
+
+    private ResponseEntity<String> callDmi(final String cmHandle, final String endpoint) {
+        final var url = buildDmiUrl(cmHandle, endpoint);
+        final var httpHeaders = new HttpHeaders();
+        httpHeaders.setBasicAuth(dmiProperties.getAuthUsername(), dmiProperties.getAuthPassword());
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        final var httpEntity = new HttpEntity<>(httpHeaders);
+        return restTemplate.postForEntity(url, httpEntity, String.class);
+    }
+
+    private  String buildDmiUrl(final String cmHandle, final String endpoint) {
+        return UriComponentsBuilder
+            .fromHttpUrl("http://" + dmiProperties.getDmiPluginBasePath())
+            .path("/ch/" + cmHandle + "/" + endpoint)
+            .toUriString();
+    }
 }
--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImplSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImplSpec.groovy
@@ -24,7 +24,9 @@ package org.onap.cps.ncmp.api.impl
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.onap.cps.api.CpsDataService
+import org.onap.cps.api.CpsModuleService
 import org.onap.cps.api.CpsQueryService
+import org.onap.cps.ncmp.api.impl.config.NcmpConfiguration
 import org.onap.cps.ncmp.api.impl.exception.NcmpException
 import org.onap.cps.ncmp.api.impl.operation.DmiOperations
 import org.onap.cps.ncmp.api.models.CmHandle
@@ -34,6 +36,7 @@ import org.onap.cps.spi.model.DataNode
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import spock.lang.Shared
+import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
 class NetworkCmProxyDataServiceImplSpec extends Specification {
@@ -46,7 +49,12 @@ class NetworkCmProxyDataServiceImplSpec extends Specification {
     def mockCpsDataService = Mock(CpsDataService)
     def mockCpsQueryService = Mock(CpsQueryService)
     def mockDmiOperations = Mock(DmiOperations)
-    def objectUnderTest = new NetworkCmProxyDataServiceImpl(mockDmiOperations, mockCpsDataService, mockCpsQueryService, new ObjectMapper())
+    def mockCpsModuleService = Mock(CpsModuleService)
+    def mockRestTemplate = Mock(RestTemplate)
+    def mockDmiProperties = Mock(NcmpConfiguration.DmiProperties)
+
+    def objectUnderTest = new NetworkCmProxyDataServiceImpl(mockDmiProperties, mockDmiOperations, mockCpsModuleService,
+            mockCpsDataService, mockCpsQueryService, new ObjectMapper(), mockRestTemplate)
 
     def cmHandle = 'some handle'
 
@@ -112,6 +120,8 @@ class NetworkCmProxyDataServiceImplSpec extends Specification {
 
     def 'Register or re-register a DMI Plugin with #scenario cm handles.'() {
         given: 'a registration '
+            def dmiRegistryAnchor = 'ncmp-dmi-registry'
+            NetworkCmProxyDataServiceImpl objectUnderTest = getObjectUnderTestWithModelSyncDisabled()
             def dmiPluginRegistration = new DmiPluginRegistration()
             dmiPluginRegistration.dmiPlugin = 'my-server'
             persistenceCmHandle.cmHandleID = '123'
@@ -139,6 +149,7 @@ class NetworkCmProxyDataServiceImplSpec extends Specification {
 
     def 'Register a DMI Plugin for the given cmHandle without additional properties.'() {
         given: 'a registration without cmHandle properties '
+            NetworkCmProxyDataServiceImpl objectUnderTest = getObjectUnderTestWithModelSyncDisabled()
             def dmiPluginRegistration = new DmiPluginRegistration()
             dmiPluginRegistration.dmiPlugin = 'my-server'
             persistenceCmHandle.cmHandleID = '123'
@@ -317,4 +328,12 @@ class NetworkCmProxyDataServiceImplSpec extends Specification {
         then: 'exception is thrown'
             thrown(NcmpException.class)
     }
+
+    def getObjectUnderTestWithModelSyncDisabled() {
+        def objectUnderTest = Spy(new NetworkCmProxyDataServiceImpl(mockDmiProperties, mockDmiOperations, mockCpsModuleService,
+                mockCpsDataService, mockCpsQueryService, new ObjectMapper(), mockRestTemplate))
+        objectUnderTest.modelSync(_) >> null
+        return objectUnderTest
+    }
+
 }
