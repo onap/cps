c5e9904bb0d3a9c5ced186a461053484f3c7f164
Renu Kumari <renu.kumari@bell.ca>
2021-08-30 11:50:43 -0400

    Add ncmp endpoints to swagger-ui
    - added ncmp-enpoints to swagger-ui
    - removed springfox dependencies
    - removed Docket configuration as we are loading ui from openapi.yml
    - removed endpoints which are not implemented
    - Fixed ncmp basepath as per CPS-616
    
    Issue-ID: CPS-601
    Issue-ID: CPS-616
    Signed-off-by: Renu Kumari <renu.kumari@bell.ca>
    Change-Id: I03c7789f48a12d3ffd5fcb934d7ba6347d4044e4

--- cps-rest/src/main/java/org/onap/cps/config/CpsConfig.java
+++ cps-rest/src/main/java/org/onap/cps/config/CpsConfig.java
@@ -26,26 +26,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableRetry
 public class CpsConfig {
-
-    /**
-     * Swagger configuration.
-     */
-    @Bean("cps-docket")
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-            .groupName("cps-docket")
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any()).build();
-    }
 
     /**
      * ModelMapper configuration.
