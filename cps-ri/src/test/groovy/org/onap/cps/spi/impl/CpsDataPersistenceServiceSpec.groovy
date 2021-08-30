2c682f58d7e0d3bdc42a19f9d3ca90977290b406
lukegleeson <luke.gleeson@est.tech>
2021-08-30 10:24:30 +0100

    Ensure Leaf value retains Integer type
    BUG
    GSON.fromJson() is unable to parse numerical values and defaults values to Doubles
    Added a datatype conversion which forces Double values which can be Integers to being Integers
    
    Issue-ID: CPS-591
    Signed-off-by: lukegleeson <luke.gleeson@est.tech>
    Change-Id: I72d54ad06823a8755ee407f39104f3edf9a8cc75

--- cps-ri/src/test/groovy/org/onap/cps/spi/impl/CpsDataPersistenceServiceSpec.groovy
+++ cps-ri/src/test/groovy/org/onap/cps/spi/impl/CpsDataPersistenceServiceSpec.groovy
@@ -19,6 +19,7 @@
 package org.onap.cps.spi.impl
 
 import org.hibernate.StaleStateException
+import org.onap.cps.spi.FetchDescendantsOption
 import org.onap.cps.spi.entities.FragmentEntity
 import org.onap.cps.spi.exceptions.ConcurrencyException
 import org.onap.cps.spi.model.DataNodeBuilder
@@ -66,6 +67,40 @@ class CpsDataPersistenceServiceSpec extends Specification {
             assert concurrencyException.getDetails().contains(myDataspaceName)
             assert concurrencyException.getDetails().contains(myAnchorName)
             assert concurrencyException.getDetails().contains(parentXpath)
+    }
+
+    def 'Converting fragment with a property JSON value of #scenario'() {
+
+        def xpath = 'parent-01'
+        def myDataspaceName = 'my-dataspace'
+        def myAnchorName = 'my-anchor'
+        def fetchDescendants = FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS
+
+        given: 'fragment to be parsed'
+        mockFragmentRepository.getByDataspaceAndAnchorAndXpath(_, _, _) >> {
+            def fragmentEntity = new FragmentEntity()
+            fragmentEntity.setXpath(xpath)
+            fragmentEntity.setChildFragments(Collections.emptySet())
+            fragmentEntity.setAttributes('{"cellLocalId": '+dataString+'}')
+            return fragmentEntity
+        }
+        when: 'fragment converted to DataNode'
+            def dataNode = objectUnderTest.getDataNode(myDataspaceName, myAnchorName, xpath, fetchDescendants)
+        then: 'the data type is as expected and the value is retained'
+            assert dataNode.leaves.get("cellLocalId").class == expectedDataClass
+            assert dataNode.leaves.get("cellLocalId") == expectedDataObject
+        where: 'the following Data Type is passed'
+            scenario                                |dataString             ||expectedDataObject    |expectedDataClass
+            'just numbers'                          |'15174'                ||15174                 |Integer
+            'number with dot'                       |'15174.32'             ||15174.32              |Double
+            'number with 0 value after dot'         |'15174.0'              ||15174.0               |Double
+            'number with 0 value before dot'        |'0.32'                 ||0.32                  |Double
+            'number higher than max int'            |'2147483648'           ||2147483648            |Long
+            'just text'                             |'"Test"'               ||'Test'                |String
+            'number with exponent'                  |'1.2345e5'             ||1.2345e5              |Double
+            'number higher than max int with dot'   |'123456789101112.0'    ||123456789101112.0     |Double
+            'text and numbers'                      |'"String = \'1234\'"'  ||'String = \'1234\''   |String
+            'number as String'                      |'"12345"'              ||'12345'               |String
     }
 
 
