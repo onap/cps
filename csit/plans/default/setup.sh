8cc770c676143e363da5a71075740cf177c87f76
JosephKeenan <joseph.keenan@est.tech>
2021-10-01 10:40:47 +0100

    CPS-615 - CSIT for model sync & passthrough
    -Passthrough write test case is a WIP
    -Tests now passing
    -Certs folder is required by SDNC
    -rebased after fixes for CPS-730
    -Manually creating docker network as part of setup for build
    
    Issue-ID: CPS-615
    
    Change-Id: I1ad70dbc8c0af96bd5cf69476acdad42cf3c179d
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>

--- csit/plans/default/setup.sh
+++ csit/plans/default/setup.sh
@@ -18,6 +18,7 @@
 # Modifications copyright (c) 2020-2021 Samsung Electronics Co., Ltd.
 # Modifications Copyright (C) 2021 Pantheon.tech
 # Modifications Copyright (C) 2021 Bell Canada.
+# Modifications Copyright (C) 2021 Nordix Foundation.
 #
 # Branched from ccsdk/distribution to this repository Feb 23, 2021
 #
@@ -39,7 +40,57 @@ curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compos
 chmod +x docker-compose
 
 # start CPS and PostgreSQL containers with docker compose
+docker network create test_network
 ./docker-compose up -d
+
+# Allow time for netconf-pnp-simulator & SDNC to come up fully
+
+sleep 2m 30s
+
+SDNC_TIME_OUT=250
+SDNC_INTERVAL=10
+SDNC_TIME=0
+
+while [ "$SDNC_TIME" -le "$SDNC_TIME_OUT" ]; do
+
+	# Mount netconf node
+
+	curl --location --request PUT 'http://localhost:8282/restconf/config/network-topology:network-topology/topology/topology-netconf/node/PNFDemo' \
+	--header 'Authorization: Basic YWRtaW46S3A4Yko0U1hzek0wV1hsaGFrM2VIbGNzZTJnQXc4NHZhb0dHbUp2VXkyVQ==' \
+	--header 'Content-Type: application/json' \
+	--data-raw '{
+	  "node": [
+		{
+		  "node-id": "PNFDemo",
+		  "netconf-node-topology:protocol": {
+			"name": "TLS"
+		  },
+		  "netconf-node-topology:host": "172.17.0.1",
+		  "netconf-node-topology:key-based": {
+			"username": "netconf",
+			"key-id": "ODL_private_key_0"
+		  },
+		  "netconf-node-topology:port": 6512,
+		  "netconf-node-topology:tcp-only": false,
+		  "netconf-node-topology:max-connection-attempts": 5
+		}
+	  ]
+	 }'
+
+	# Verify node has been mounted
+
+	RESPONSE=$( curl --location --request GET 'http://localhost:8282/restconf/config/network-topology:network-topology/topology/topology-netconf' --header 'Authorization: basic YWRtaW46S3A4Yko0U1hzek0wV1hsaGFrM2VIbGNzZTJnQXc4NHZhb0dHbUp2VXkyVQ==')
+
+	  if [[ "$RESPONSE" == *"PNFDemo"* ]]; then
+	    echo "Node mounted in $SDNC_TIME"
+		  break;
+	  fi
+
+	 sleep $SDNC_INTERVAL
+	 SDNC_TIME=$((SDNC_TIME + SDNC_INTERVAL))
+
+done
+
 # Validate CPS service initialization completed via periodic log checking for line like below:
 # org.onap.cps.Application ... Started Application in X.XXX seconds
 
@@ -73,4 +124,3 @@ MANAGEMENT_PORT="8887"
 
 # Pass variables required for Robot test suites in ROBOT_VARIABLES
 ROBOT_VARIABLES="-v CPS_HOST:$CPS_HOST -v CPS_PORT:$CPS_PORT -v MANAGEMENT_PORT:$MANAGEMENT_PORT -v DATADIR:$WORKSPACE/data"
-
