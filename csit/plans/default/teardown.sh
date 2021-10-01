5db5e7aa85b9523f73a4849ce2c054cefe621ea9
tragait <rahul.tyagi@est.tech>
2021-10-01 10:40:47 +0100

    CPS-615 - CSIT for model sync & passthrough
    This commit contains
    -Registration and sync test cases
    -Passthrough read and  write test case
    
    Issue-ID: CPS-615
    Signed-off-by: tragait <rahul.tyagi@est.tech>
    Change-Id: Icfdf967430cafdd9a6fdcd8391f14937320dcd0a

--- csit/plans/default/teardown.sh
+++ csit/plans/default/teardown.sh
@@ -21,5 +21,12 @@
 # Branched from ccsdk/distribution to this repository Feb 23, 2021
 #
 
-cd $WORKSPACE/archives/docker-compose
-./docker-compose down -v
+#echo 'Stop, Removing all running containers...'
+#docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
+#
+#echo 'Removing Volumes...'
+#echo y | docker volume prune
+#
+#echo 'Removing Networks...'
+#echo y | docker network prune
+
