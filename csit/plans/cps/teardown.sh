6213f10e3e521f7650a08b3a24b73972bfd0dddb
JosephKeenan <joseph.keenan@est.tech>
2021-12-06 11:02:03 +0000

    Modifying responsentity string to object
    Issue-ID: CPS-736
    Change-Id: Ie44e528064402649a2585964599bde7d1b4da978
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>

--- csit/plans/cps/teardown.sh
+++ csit/plans/cps/teardown.sh
@@ -21,6 +21,12 @@
 # Branched from ccsdk/distribution to this repository Feb 23, 2021
 #
 
+echo 'Print out docker logs for CPS and DMI..'
+docker logs cps-and-ncmp
+
+docker logs ncmp-dmi-plugin
+
+
 echo 'Stopping, Removing all running containers...'
 docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
 
