016fca5b31708ae2afb8427be4c090e12ae70fd6
mpriyank <priyank.maheshwari@est.tech>
2022-01-18 01:07:00 +0530

    [TECHDEBT] Remove deprecated API methods related to NCMP
    CpsQueryService instantiation removed as it was not in use.
    License Header updated for Nordix Foundation to 2021-2022.
    
    Issue-ID: CPS-642
    Change-Id: I1aa204730f5d347d379cfdaaf0740323a317a559
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/NetworkCmProxyDataService.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/NetworkCmProxyDataService.java
@@ -1,7 +1,7 @@
 /*
  *  ============LICENSE_START=======================================================
  *  Copyright (C) 2021 highstreet technologies GmbH
- *  Modifications Copyright (C) 2021 Nordix Foundation
+ *  Modifications Copyright (C) 2021-2022 Nordix Foundation
  *  Modifications Copyright (C) 2021 Pantheon.tech
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,82 +25,13 @@ package org.onap.cps.ncmp.api;
 import static org.onap.cps.ncmp.api.impl.operations.DmiRequestBody.OperationEnum;
 
 import java.util.Collection;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.onap.cps.ncmp.api.models.DmiPluginRegistration;
-import org.onap.cps.spi.FetchDescendantsOption;
-import org.onap.cps.spi.model.DataNode;
 import org.onap.cps.spi.model.ModuleReference;
 
 /*
  * Datastore interface for handling CPS data.
  */
 public interface NetworkCmProxyDataService {
-
-    /**
-     * Retrieves datanode by XPath for a given cm handle.
-     *
-     * @param cmHandle               The identifier for a network function, network element, subnetwork or any other cm
-     *                               object by managed Network CM Proxy
-     * @param xpath                  xpath
-     * @param fetchDescendantsOption defines the scope of data to fetch: either single node or all the descendant nodes
-     *                               (recursively) as well
-     * @return data node object
-     */
-    DataNode getDataNode(@NonNull String cmHandle, @NonNull String xpath,
-        @NonNull FetchDescendantsOption fetchDescendantsOption);
-
-    /**
-     * Get datanodes for the given cm handle by cps path.
-     *
-     * @param cmHandle               The identifier for a network function, network element, subnetwork or any other cm
-     *                               object by managed Network CM Proxy
-     * @param cpsPath                cps path
-     * @param fetchDescendantsOption defines whether the descendants of the node(s) found by the query should be
-     *                               included in the output
-     * @return a collection of datanodes
-     */
-    Collection<DataNode> queryDataNodes(@NonNull String cmHandle, @NonNull String cpsPath,
-        @NonNull FetchDescendantsOption fetchDescendantsOption);
-
-    /**
-     * Creates data node with descendants at root level or under existing node (if parent node xpath is provided).
-     *
-     * @param cmHandle        The identifier for a network function, network element, subnetwork or any other cm
-     *                        object managed by Network CM Proxy
-     * @param parentNodeXpath xpath to parent node or '/' for root level
-     * @param jsonData        data as JSON string
-     */
-    void createDataNode(@NonNull String cmHandle, @NonNull String parentNodeXpath, @NonNull String jsonData);
-
-    /**
-     * Creates one or more child node elements with descendants under existing node from list-node data fragment.
-     *
-     * @param cmHandle        The identifier for a network function, network element, subnetwork or any other cm
-     *                        object managed by Network CM Proxy
-     * @param parentNodeXpath xpath to parent node
-     * @param jsonData        data as JSON string
-     */
-    void addListNodeElements(@NonNull String cmHandle, @NonNull String parentNodeXpath, @NonNull String jsonData);
-
-    /**
-     * Updates data node for given cm handle using xpath to parent node.
-     *
-     * @param cmHandle        The identifier for a network function, network element, subnetwork or any other cm object
-     *                        by managed Network CM Proxy
-     * @param parentNodeXpath xpath to parent node
-     * @param jsonData        json data
-     */
-    void updateNodeLeaves(@NonNull String cmHandle, @NonNull String parentNodeXpath, @NonNull String jsonData);
-
-    /**
-     * Replaces existing data node content including descendants.
-     *
-     * @param cmHandle        The identifier for a network function, network element, subnetwork or any other cm object
-     *                        by managed Network CM Proxy
-     * @param parentNodeXpath xpath to parent node
-     * @param jsonData        json data
-     */
-    void replaceNodeTree(@NonNull String cmHandle, @NonNull String parentNodeXpath, @NonNull String jsonData);
 
     /**
      * Registration of New CM Handles.
