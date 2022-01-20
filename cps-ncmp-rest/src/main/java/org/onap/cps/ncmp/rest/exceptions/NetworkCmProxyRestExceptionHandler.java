d648a84a53f365b614055b56103903fed07f4646
sourabh_sourabh <sourabh.sourabh@est.tech>
2022-01-21 01:46:12 +0530

    Improve logging in NCMP
    Issue-ID: CPS-855
    
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>
    Change-Id: I5d8a6a711d1c6b13d5d8eeba52a1dcd689dbdae2

--- cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/exceptions/NetworkCmProxyRestExceptionHandler.java
+++ cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/exceptions/NetworkCmProxyRestExceptionHandler.java
@@ -1,7 +1,7 @@
 /*
  *  ============LICENSE_START=======================================================
  *  Copyright (C) 2021 Pantheon.tech
- *  Modifications Copyright (C) 2021 Nordix Foundation
+ *  Modifications Copyright (C) 2021-2022 Nordix Foundation
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,8 +20,6 @@
 
 package org.onap.cps.ncmp.rest.exceptions;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.onap.cps.ncmp.api.impl.exception.NcmpException;
 import org.onap.cps.ncmp.rest.controller.NetworkCmProxyController;
@@ -36,7 +34,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * Exception handler with error message return.
  */
 @Slf4j
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @RestControllerAdvice(assignableTypes = {NetworkCmProxyController.class})
 public class NetworkCmProxyRestExceptionHandler {
 
