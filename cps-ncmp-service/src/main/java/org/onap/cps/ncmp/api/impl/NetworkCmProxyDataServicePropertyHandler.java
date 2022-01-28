d7741a21a5307475dc99857bb290d9797b399de6
mpriyank <priyank.maheshwari@est.tech>
2022-01-28 16:33:56 +0530

    Core logic to update,add or remove cmHandle properties
    Issue-ID: CPS-837
    Change-Id: Ia078b6a0291ae916931259a309dd592b0554da28
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServicePropertyHandler.java
@@ -0,0 +1,171 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl;
+
+import static org.onap.cps.ncmp.api.impl.NetworkCmProxyDataServicePropertyHandler.PropertyType.DMI_PROPERTY;
+import static org.onap.cps.ncmp.api.impl.NetworkCmProxyDataServicePropertyHandler.PropertyType.PUBLIC_PROPERTY;
+import static org.onap.cps.ncmp.api.impl.constants.DmiRegistryConstants.NCMP_DATASPACE_NAME;
+import static org.onap.cps.ncmp.api.impl.constants.DmiRegistryConstants.NCMP_DMI_REGISTRY_ANCHOR;
+import static org.onap.cps.ncmp.api.impl.constants.DmiRegistryConstants.NCMP_DMI_REGISTRY_PARENT;
+import static org.onap.cps.ncmp.api.impl.constants.DmiRegistryConstants.NO_TIMESTAMP;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.api.CpsDataService;
+import org.onap.cps.ncmp.api.models.CmHandle;
+import org.onap.cps.spi.FetchDescendantsOption;
+import org.onap.cps.spi.exceptions.DataNodeNotFoundException;
+import org.onap.cps.spi.model.DataNode;
+import org.onap.cps.spi.model.DataNodeBuilder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+//Accepting the security hotspot as the string checked is generated from inside code and not user input.
+@SuppressWarnings("squid:S5852")
+public class NetworkCmProxyDataServicePropertyHandler {
+
+    private static final String CM_HANDLE_XPATH_TEMPLATE = NCMP_DMI_REGISTRY_PARENT + "/cm-handles[@id='%s']";
+
+    private final CpsDataService cpsDataService;
+
+    /**
+     * Iterates over incoming cmHandles and update the dataNodes based on the updated attributes.
+     * The attributes which are not passed will remain as is.
+     *
+     * @param cmHandles collection of cmHandles
+     */
+    public void updateDataNodeLeaves(final Collection<CmHandle> cmHandles) throws DataNodeNotFoundException {
+        for (final CmHandle cmHandle : cmHandles) {
+            try {
+                final String cmHandleXpath = String.format(CM_HANDLE_XPATH_TEMPLATE, cmHandle.getCmHandleID());
+                final DataNode existingCmHandleDataNode =
+                        cpsDataService.getDataNode(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR, cmHandleXpath,
+                                FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS);
+                processUpdates(existingCmHandleDataNode, cmHandle);
+            } catch (final DataNodeNotFoundException e) {
+                log.error("Unable to find dataNode for cmHandleId : {} , caused by : {}", cmHandle.getCmHandleID(),
+                        e.getMessage());
+                throw e;
+            }
+        }
+    }
+
+    private void processUpdates(final DataNode existingCmHandleDataNode, final CmHandle incomingCmHandle) {
+        if (!incomingCmHandle.getPublicProperties().isEmpty()) {
+            updateProperties(existingCmHandleDataNode, PUBLIC_PROPERTY, incomingCmHandle.getPublicProperties());
+        }
+        if (!incomingCmHandle.getDmiProperties().isEmpty()) {
+            updateProperties(existingCmHandleDataNode, DMI_PROPERTY, incomingCmHandle.getDmiProperties());
+        }
+    }
+
+    private void updateProperties(final DataNode existingCmHandleDataNode, final PropertyType propertyType,
+            final Map<String, String> incomingProperties) {
+        final Collection<DataNode> replacementPropertyDataNodes =
+                getReplacementDataNodes(existingCmHandleDataNode, propertyType, incomingProperties);
+        replacementPropertyDataNodes.addAll(
+                getUnchangedPropertyDataNodes(existingCmHandleDataNode, propertyType, incomingProperties));
+        if (replacementPropertyDataNodes.isEmpty()) {
+            removeAllProperties(existingCmHandleDataNode, propertyType);
+        } else {
+            cpsDataService.replaceListContent(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR,
+                    existingCmHandleDataNode.getXpath(), replacementPropertyDataNodes, NO_TIMESTAMP);
+        }
+    }
+
+    private void removeAllProperties(final DataNode existingCmHandleDataNode, final PropertyType propertyType) {
+        existingCmHandleDataNode.getChildDataNodes().forEach(dataNode -> {
+            final Matcher matcher = propertyType.propertyXpathPattern.matcher(dataNode.getXpath());
+            if (matcher.find()) {
+                log.info("Deleting dataNode with xpath : [{}]", dataNode.getXpath());
+                cpsDataService.deleteDataNode(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR, dataNode.getXpath(),
+                        NO_TIMESTAMP);
+            }
+        });
+    }
+
+    private Collection<DataNode> getUnchangedPropertyDataNodes(final DataNode existingCmHandleDataNode,
+            final PropertyType propertyType, final Map<String, String> incomingProperties) {
+        final Collection<DataNode> unchangedPropertyDataNodes = new HashSet<>();
+        for (final DataNode existingPropertyDataNode : existingCmHandleDataNode.getChildDataNodes()) {
+            final Matcher matcher = propertyType.propertyXpathPattern.matcher(existingPropertyDataNode.getXpath());
+            if (matcher.find()) {
+                final String keyName = matcher.group(2);
+                if (!incomingProperties.containsKey(keyName)) {
+                    unchangedPropertyDataNodes.add(existingPropertyDataNode);
+                }
+            }
+        }
+        return unchangedPropertyDataNodes;
+    }
+
+    private Collection<DataNode> getReplacementDataNodes(final DataNode existingCmHandleDataNode,
+            final PropertyType propertyType, final Map<String, String> incomingProperties) {
+        final Collection<DataNode> replacementPropertyDataNodes = new HashSet<>();
+        incomingProperties.forEach((updatedAttributeKey, updatedAttributeValue) -> {
+            final String propertyXpath = getAttributeXpath(existingCmHandleDataNode, propertyType, updatedAttributeKey);
+            if (updatedAttributeValue != null) {
+                log.info("Creating a new DataNode with xpath {} , key : {} and value : {}", propertyXpath,
+                        updatedAttributeKey, updatedAttributeValue);
+                replacementPropertyDataNodes.add(
+                        buildDataNode(propertyXpath, updatedAttributeKey, updatedAttributeValue));
+            }
+        });
+        return replacementPropertyDataNodes;
+    }
+
+    private String getAttributeXpath(final DataNode cmHandle, final PropertyType propertyType,
+            final String attributeKey) {
+        return cmHandle.getXpath() + "/" + propertyType.xpathPrefix + String.format("[@name='%s']", attributeKey);
+    }
+
+    private DataNode buildDataNode(final String xpath, final String attributeKey, final String attributeValue) {
+        final Map<String, String> updatedLeaves = new LinkedHashMap<>(1);
+        updatedLeaves.put("name", attributeKey);
+        updatedLeaves.put("value", attributeValue);
+        log.debug("Building a new node with xpath {} with leaves (name : {} , value : {})", xpath, attributeKey,
+                attributeValue);
+        return new DataNodeBuilder().withXpath(xpath).withLeaves(ImmutableMap.copyOf(updatedLeaves)).build();
+    }
+
+    enum PropertyType {
+        DMI_PROPERTY("additional-properties"), PUBLIC_PROPERTY("public-properties");
+
+        private static final String LIST_INDEX_PATTERN = "\\[@(\\w+)[^\\/]'([^']+)']";
+
+        final String xpathPrefix;
+        final Pattern propertyXpathPattern;
+
+        PropertyType(final String xpathPrefix) {
+            this.xpathPrefix = xpathPrefix;
+            this.propertyXpathPattern = Pattern.compile(xpathPrefix + LIST_INDEX_PATTERN);
+        }
+    }
+}
---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServicePropertyHandlerSpec.groovy
@@ -0,0 +1,135 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2022 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl
+
+import org.onap.cps.api.CpsDataService
+import org.onap.cps.ncmp.api.models.CmHandle
+import org.onap.cps.spi.FetchDescendantsOption
+import org.onap.cps.spi.exceptions.DataNodeNotFoundException
+import org.onap.cps.spi.exceptions.DataValidationException
+import org.onap.cps.spi.model.DataNode
+import org.onap.cps.spi.model.DataNodeBuilder
+import spock.lang.Specification
+
+class NetworkCmProxyDataServicePropertyHandlerSpec extends Specification {
+
+    def mockCpsDataService = Mock(CpsDataService)
+
+    def objectUnderTest = new NetworkCmProxyDataServicePropertyHandler(mockCpsDataService)
+    def dataspaceName = 'NCMP-Admin'
+    def anchorName = 'ncmp-dmi-registry'
+    def static cmHandleId = 'myHandle1'
+    def static cmHandleXpath = "/dmi-registry/cm-handles[@id='${cmHandleId}']"
+    def noTimeStamp = null
+
+    def static propertyDataNodes = [new DataNodeBuilder().withXpath("/dmi-registry/cm-handles[@id='${cmHandleId}']/additional-properties[@name='additionalProp1']").withLeaves(['name': 'additionalProp1', 'value': 'additionalValue1']).build(),
+                                    new DataNodeBuilder().withXpath("/dmi-registry/cm-handles[@id='${cmHandleId}']/additional-properties[@name='additionalProp2']").withLeaves(['name': 'additionalProp2', 'value': 'additionalValue2']).build(),
+                                    new DataNodeBuilder().withXpath("/dmi-registry/cm-handles[@id='${cmHandleId}']/public-properties[@name='publicProp3']").withLeaves(['name': 'publicProp3', 'value': 'publicValue3']).build(),
+                                    new DataNodeBuilder().withXpath("/dmi-registry/cm-handles[@id='${cmHandleId}']/public-properties[@name='publicProp4']").withLeaves(['name': 'publicProp4', 'value': 'publicValue4']).build()]
+    def static cmHandleDataNode = new DataNode(xpath: cmHandleXpath, childDataNodes: propertyDataNodes)
+
+    def 'Update CM Handle Public Properties: #scenario'() {
+        given: 'the CPS service return a CM handle'
+            mockCpsDataService.getDataNode(dataspaceName, anchorName, cmHandleXpath, FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS) >> cmHandleDataNode
+        and: 'an update cm handle request with public properties updates'
+            def cmHandleUpdateRequest = [new CmHandle(cmHandleID: cmHandleId, publicProperties: updatedPublicProperties)]
+        when: 'update data node leaves is called with the update request'
+            objectUnderTest.updateDataNodeLeaves(cmHandleUpdateRequest)
+        then: 'the replace list method is called with correct params'
+            1 * mockCpsDataService.replaceListContent(dataspaceName, anchorName, cmHandleXpath, _, noTimeStamp) >> { args ->
+                {
+                    assert args[3].leaves.size() == expectedPropertiesAfterUpdate.size()
+                    assert args[3].leaves.containsAll(convertToProperties(expectedPropertiesAfterUpdate))
+                }
+            }
+        where: 'following public properties updates are made'
+            scenario                          | updatedPublicProperties      || expectedPropertiesAfterUpdate
+            'property added'                  | ['newPubProp1': 'pub-val']   || [['publicProp3': 'publicValue3'], ['publicProp4': 'publicValue4'], ['newPubProp1': 'pub-val']]
+            'property updated'                | ['publicProp4': 'newPubVal'] || [['publicProp3': 'publicValue3'], ['publicProp4': 'newPubVal']]
+            'property removed'                | ['publicProp4': null]        || [['publicProp3': 'publicValue3']]
+            'property ignored(value is null)' | ['pub-prop': null]           || [['publicProp3': 'publicValue3'], ['publicProp4': 'publicValue4']]
+    }
+
+    def 'Update DMI Properties: #scenario'() {
+        given: 'the CPS service return a CM handle'
+            mockCpsDataService.getDataNode(dataspaceName, anchorName, cmHandleXpath, FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS) >> cmHandleDataNode
+        and: 'an update cm handle request with DMI properties updates'
+            def cmHandleUpdateRequest = [new CmHandle(cmHandleID: cmHandleId, dmiProperties: updatedDmiProperties)]
+        when: 'update data node leaves is called with the update request'
+            objectUnderTest.updateDataNodeLeaves(cmHandleUpdateRequest)
+        then: 'replace list method should is called with correct params'
+            expectedCallsToReplaceMethod * mockCpsDataService.replaceListContent(dataspaceName, anchorName, cmHandleXpath, _, noTimeStamp) >> { args ->
+                {
+                    assert args[3].leaves.size() == expectedPropertiesAfterUpdate.size()
+                    assert args[3].leaves.containsAll(convertToProperties(expectedPropertiesAfterUpdate))
+                }
+            }
+        where: 'following DMI properties updates are made'
+            scenario                          | updatedDmiProperties                || expectedPropertiesAfterUpdate                                                                                           | expectedCallsToReplaceMethod
+            'property added'                  | ['newAdditionalProp1': 'add-value'] || [['additionalProp1': 'additionalValue1'], ['additionalProp2': 'additionalValue2'], ['newAdditionalProp1': 'add-value']] | 1
+            'property updated'                | ['additionalProp1': 'newValue']     || [['additionalProp2': 'additionalValue2'], ['additionalProp1': 'newValue']]                                              | 1
+            'property removed'                | ['additionalProp1': null]           || [['additionalProp2': 'additionalValue2']]                                                                               | 1
+            'property ignored(value is null)' | ['new-prop': null]                  || [['additionalProp1': 'additionalValue1'], ['additionalProp2': 'additionalValue2']]                                      | 1
+            'no property changes'             | [:]                                 || [['additionalProp1': 'additionalValue1'], ['additionalProp2': 'additionalValue2']]                                      | 0
+    }
+
+    def 'Update CM Handle Properties, remove all properties: #scenario'() {
+        given: 'the CPS service return a CM handle'
+            def cmHandleDataNode = new DataNode(xpath: cmHandleXpath, childDataNodes: originalPropertyDataNodes)
+            mockCpsDataService.getDataNode(dataspaceName, anchorName, cmHandleXpath, FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS) >> cmHandleDataNode
+        and: 'an update cm handle request that removes all public properties(existing and non-existing)'
+            def cmHandleUpdateRequest = [new CmHandle(cmHandleID: cmHandleId, publicProperties: ['publicProp3': null, 'publicProp4': null])]
+        when: 'update data node leaves is called with the update request'
+            objectUnderTest.updateDataNodeLeaves(cmHandleUpdateRequest)
+        then: 'the replace list method is not called'
+            0 * mockCpsDataService.replaceListContent(*_)
+        then: 'delete data node will be called for any existing property'
+            expectedCallsToDeleteDataNode * mockCpsDataService.deleteDataNode(dataspaceName, anchorName, _, noTimeStamp) >> { arg ->
+                {
+                    assert arg[2].contains("@name='publicProp")
+                }
+            }
+        where: 'following public properties updates are made'
+            scenario                              | originalPropertyDataNodes || expectedCallsToDeleteDataNode
+            '2 original properties, both removed' | propertyDataNodes         || 2
+            'no original properties'              | []                        || 0
+    }
+
+    def 'Exception thrown when we try to update cmHandle'() {
+        given: 'cm handles request'
+            def cmHandleUpdateRequest = [new CmHandle(cmHandleID: cmHandleId, publicProperties: [:], dmiProperties: [:])]
+        and: 'data node cannot be found'
+            mockCpsDataService.getDataNode(*_) >> { throw new DataNodeNotFoundException(dataspaceName, anchorName, cmHandleXpath) }
+        when: 'update data node leaves is called using correct parameters'
+            objectUnderTest.updateDataNodeLeaves(cmHandleUpdateRequest)
+        then: 'data validation exception is thrown'
+            def exceptionThrown = thrown(DataValidationException.class)
+            assert exceptionThrown.getMessage().contains('DataNode not found')
+    }
+
+    def convertToProperties(expectedPropertiesAfterUpdateAsMap) {
+        def properties = [].withDefault { [:] }
+        expectedPropertiesAfterUpdateAsMap.forEach(property ->
+            property.forEach((key, val) -> {
+                properties.add(['name': key, 'value': val])
+            }))
+        return properties
+    }
+}
