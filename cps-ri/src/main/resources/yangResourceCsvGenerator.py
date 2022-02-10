5741db35baa26193ff13b27518efb1a4f20d7f89
lukegleeson <luke.gleeson@est.tech>
2022-02-10 10:58:29 +0000

    Update dmi-registry yang to support cmHandle State
    Issue-ID: CPS-872
    Signed-off-by: lukegleeson <luke.gleeson@est.tech>
    Change-Id: I4ab949f0bc00a52376b42a5ac8a33d397eefcedf

--- cps-ri/src/main/resources/yangResourceCsvGenerator.py
+++ cps-ri/src/main/resources/yangResourceCsvGenerator.py
@@ -27,12 +27,12 @@ checksum = ''
 for yang_source in sys.argv[1:]:
     checksum = hashlib.sha256(str(yang_source).encode()).hexdigest()
 
-with open('changelog/db/changes/data/yang-models/' + yang_source + '.yang') as content:
-    dmiRegistry = content.read()
+    with open('changelog/db/changes/data/yang-models/' + yang_source + '.yang') as content:
+        dmiRegistry = content.read()
 
-# open the file in the write mode
-with open('changelog/db/changes/data/dmi/generated-csv/generated_yang_resource_' + yang_source + '.csv', 'w', newline='') \
-        as file:
-    writer = csv.writer(file, delimiter='|')
-    writer.writerow(["name", "content", "checksum"])
-    writer.writerow([yang_source + '.yang', dmiRegistry, checksum])
+    # open the file in the write mode
+    with open('changelog/db/changes/data/dmi/generated-csv/generated_yang_resource_' + yang_source + '.csv', 'w', newline='') \
+            as file:
+        writer = csv.writer(file, delimiter='|')
+        writer.writerow(["name", "content", "checksum"])
+        writer.writerow([yang_source + '.yang', dmiRegistry, checksum])
