245ae48f1c93fba82a3fcb092b0a7321f6706acd
JosephKeenan <joseph.keenan@est.tech>
2022-02-15 18:23:05 +0000

    CPS-887 Decreasing performance of cmHandle registration
    - Creating temporary table module references from the
      node
    - Query returning new moduleReferences for node to be added to CPS
    - Modified registration for node to use new implementation (incl. tests)
    - Refactored some interface to use collection instead of list
    - Modified SQL statements to use String.format()
    - Fixed tests
    
    Issue-ID: CPS-887
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>
    Change-Id: I58b17d29e8b9d6f7a644d33bf6cf8bf46f892580

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImplModelSyncSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/NetworkCmProxyDataServiceImplModelSyncSpec.groovy
@@ -50,7 +50,7 @@ class NetworkCmProxyDataServiceImplModelSyncSpec extends Specification {
             def cmHandle = new CmHandle()
             def dmiServiceName = 'some service name'
             cmHandle.cmHandleID = 'cm handle id 1'
-            cmHandle.dmiProperties = dmiProperties
+
             def persistenceCmHandle = PersistenceCmHandle.toPersistenceCmHandle(dmiServiceName, '' , '', cmHandle)
         and: 'DMI operations returns some module references'
             def moduleReferences =  [ new ModuleReference(moduleName:'module1',revision:'1'),
@@ -61,15 +61,16 @@ class NetworkCmProxyDataServiceImplModelSyncSpec extends Specification {
         and: 'DMI-Plugin returns resource(s) for "new" module(s)'
             mockDmiModelOperations.getNewYangResourcesFromDmi(persistenceCmHandle, [new ModuleReference('module1', '1')]) >> yangResourceToContentMap
         when: 'module sync is triggered'
+            mockCpsModuleService.identifyNewModuleReferences(moduleReferences) >> identifiedNewModuleReferences
             objectUnderTest.syncModulesAndCreateAnchor(persistenceCmHandle)
         then: 'the CPS module service is called once with the correct parameters'
             1 * mockCpsModuleService.createSchemaSetFromModules(expectedDataspaceName, persistenceCmHandle.getId(), yangResourceToContentMap, expectedKnownModules)
         and: 'admin service create anchor method has been called with correct parameters'
             1 * mockCpsAdminService.createAnchor(expectedDataspaceName, persistenceCmHandle.getId(), persistenceCmHandle.getId())
         where: 'the following parameters are used'
-            scenario                        | dmiProperties        | existingModuleResourcesInCps                                               | yangResourceToContentMap      || expectedKnownModules
-            'one unknown module'            | ['name1': 'value1']  | [new ModuleReference('module2', '2'), new ModuleReference('module3', '3')] | [module1: 'some yang source'] || [new ModuleReference('module2', '2')]
-            'no add. properties'            | [:]                  | [new ModuleReference('module2', '2'), new ModuleReference('module3', '3')] | [module1: 'some yang source'] || [new ModuleReference('module2', '2')]
-            'no unknown module'             | [:]                  | [new ModuleReference('module1', '1'), new ModuleReference('module2', '2')] | [:]                           || [new ModuleReference('module1', '1'), new ModuleReference('module2', '2')]
+            scenario             | existingModuleResourcesInCps                                               | identifiedNewModuleReferences                            | yangResourceToContentMap      || expectedKnownModules
+            'one new module'     | [new ModuleReference('module2', '2'), new ModuleReference('module3', '3')] | [new ModuleReference(moduleName:'module1',revision:'1')] | [module1: 'some yang source'] || [new ModuleReference('module2', '2')]
+            'no add. properties' | [new ModuleReference('module2', '2'), new ModuleReference('module3', '3')] | [new ModuleReference(moduleName:'module1',revision:'1')] | [module1: 'some yang source'] || [new ModuleReference('module2', '2')]
+            'no new module'      | [new ModuleReference('module1', '1'), new ModuleReference('module2', '2')] | []                                                       | [:]                           || [new ModuleReference('module1', '1'), new ModuleReference('module2', '2')]
     }
 }
