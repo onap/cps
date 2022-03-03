3f1e4b0f28d91ff3658718c2cdd840d9265fd53a
sourabh_sourabh <sourabh.sourabh@est.tech>
2022-03-04 00:31:07 +0530

    Removed ExtendedModuleReference Object
    -Introduced ModuleReference with 2 prop name and revision
    -Removed default constructor of ModuleReference
    
    Issue-ID: CPS-592
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>
    Change-Id: I27a8a521f133c0773ef0aa0b9700ac42f6bc613b

--- cps-service/src/main/java/org/onap/cps/spi/model/SchemaSet.java
+++ cps-service/src/main/java/org/onap/cps/spi/model/SchemaSet.java
@@ -1,12 +1,14 @@
 /*
  *  ============LICENSE_START=======================================================
  *  Copyright (C) 2020 Pantheon.tech
+ *  Copyright (C) 2022 Nordix Foundation.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *        http://www.apache.org/licenses/LICENSE-2.0
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,5 +37,5 @@ public class SchemaSet implements Serializable {
     private static final long serialVersionUID = 1464791260718603291L;
     private String name;
     private String dataspaceName;
-    private List<ExtendedModuleReference> extendedModuleReferences;
+    private List<ModuleReference> moduleReferences;
 }
