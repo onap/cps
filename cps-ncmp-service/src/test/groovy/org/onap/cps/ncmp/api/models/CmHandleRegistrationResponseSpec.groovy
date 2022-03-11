f1545fdd8783be57b75dc08fbe583443088f0d91
Renu Kumari <renu.kumari@bell.ca>
2022-03-11 08:05:11 -0500

    Return Registration response for updating cmhandles
    - Added CMHandleRegistrationResponse class for registration response
    - Send response in the case of success and failure
    - Each update operation in the request is independent on previous one
    
    Issue-ID: CPS-896
    Signed-off-by: Renu Kumari <renu.kumari@bell.ca>
    Change-Id: Ic6cc69da48bd2078029784cabede4f5965054c03

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/models/CmHandleRegistrationResponseSpec.groovy
@@ -0,0 +1,68 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 2022 Bell Canada
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.models
+
+import org.onap.cps.ncmp.api.models.CmHandleRegistrationResponse.RegistrationError
+import org.onap.cps.ncmp.api.models.CmHandleRegistrationResponse.Status
+import spock.lang.Specification
+
+class CmHandleRegistrationResponseSpec extends Specification {
+
+    def 'Failed Cm Handle Registration Response: for unexpected exception'() {
+        when: 'CMHandle response is created'
+            def cmHandleRegistrationResponse =
+                CmHandleRegistrationResponse.createFailureResponse('cmHandle', new RuntimeException('unexpected error'))
+        then: 'the response is created with expected value'
+            with(cmHandleRegistrationResponse) {
+                it.registrationError == RegistrationError.UNKNOWN_ERROR
+                it.cmHandle == 'cmHandle'
+                errorText == 'unexpected error'
+            }
+    }
+
+    def 'Failed Cm Handle Registration Response: for known error'() {
+        when: 'CMHandle response is created'
+            def cmHandleRegistrationResponse =
+                CmHandleRegistrationResponse.createFailureResponse('cmHandle', RegistrationError.CM_HANDLE_ALREADY_EXIST)
+        then: 'the error text should have expected value'
+            with(cmHandleRegistrationResponse) {
+                it.registrationError == RegistrationError.CM_HANDLE_ALREADY_EXIST
+                it.cmHandle == 'cmHandle'
+                it.status == Status.FAILURE
+                errorText == RegistrationError.CM_HANDLE_ALREADY_EXIST.errorText
+            }
+
+    }
+
+    def 'Successful CmHandle Registration Response'() {
+        when: 'CMHandle response is created'
+            def cmHandleRegistrationResponse = CmHandleRegistrationResponse.createSuccessResponse('cmHandle')
+        then: 'the error text should have expected value'
+            with(cmHandleRegistrationResponse) {
+                it.cmHandle == 'cmHandle'
+                it.status == Status.SUCCESS
+            }
+        and: 'error details are null'
+            cmHandleRegistrationResponse.registrationError == null
+            cmHandleRegistrationResponse.errorText == null
+    }
+
+}
