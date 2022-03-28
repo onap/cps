67544b5cd616cca5fcd00581d258d9e42a985c45
mpriyank <priyank.maheshwari@est.tech>
2022-03-28 15:47:47 +0530

    Structured Exception details for DMI
    - Introduced DmiErrorMessage in API docs with 502 Bad Gateway
    - HttpClientRequestException will be thrown which will be exposed as 502 BAD Gateway for the client from NCMP
    
    Issue-ID: CPS-917
    Change-Id: Iba8f159e8216bc1f63a9ab86208e5c802437e2e8
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-ncmp-rest/src/test/groovy/org/onap/cps/ncmp/rest/exceptions/NetworkCmProxyRestExceptionHandlerSpec.groovy
+++ cps-ncmp-rest/src/test/groovy/org/onap/cps/ncmp/rest/exceptions/NetworkCmProxyRestExceptionHandlerSpec.groovy
@@ -21,14 +21,13 @@
 
 package org.onap.cps.ncmp.rest.exceptions
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import groovy.json.JsonSlurper
 import org.mapstruct.factory.Mappers
 import org.onap.cps.TestUtils
 import org.onap.cps.ncmp.api.NetworkCmProxyDataService
 import org.onap.cps.ncmp.api.impl.exception.DmiRequestException
+import org.onap.cps.ncmp.api.impl.exception.HttpClientRequestException
 import org.onap.cps.ncmp.api.impl.exception.ServerNcmpException
-import org.onap.cps.ncmp.api.models.NcmpServiceCmHandle
 import org.onap.cps.ncmp.rest.controller.NcmpRestInputMapper
 import org.onap.cps.spi.exceptions.CpsException
 import org.onap.cps.spi.exceptions.DataNodeNotFoundException
@@ -38,6 +37,7 @@ import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Shared
@@ -99,6 +99,19 @@ class NetworkCmProxyRestExceptionHandlerSpec extends Specification {
             'DataNode Validation' | new DataNodeNotFoundException('myDataspaceName', 'myAnchorName') || null                 | 'DataNode not found' | NOT_FOUND
             'other'               | new IllegalStateException(sampleErrorMessage)                    || null                 | sampleErrorMessage   | INTERNAL_SERVER_ERROR
             'Data Node Not Found' | new DataNodeNotFoundException('myDataspaceName', 'myAnchorName') || 'DataNode not found' | 'DataNode not found' | NOT_FOUND
+    }
+
+    def 'Failing DMI Request - passthrough scenario'() {
+        given: 'failing DMI request'
+            mockNetworkCmProxyDataService.getResourceDataPassThroughRunningForCmHandle(*_) >> { throw new HttpClientRequestException('Error Message Details NCMP', 'Bad Request from DMI', 400) }
+        when: 'the DMI request is executed'
+            def response = mvc.perform(get("$dataNodeBaseEndpointNcmp/ch/testCmHandle/data/ds/ncmp-datastore:passthrough-running?resourceIdentifier=stores:bookstore/categories=100"))
+                .andReturn().response
+        then: 'NCMP service responds with 502 Bad Gateway status'
+            response.status.equals(HttpStatus.BAD_GATEWAY.value())
+        and: 'the NCMP response also contains the original DMI response details'
+            response.contentAsString.contains('400')
+            response.contentAsString.contains('Bad Request from DMI')
     }
 
     def 'Post request with exception returns correct HTTP Status.'() {
