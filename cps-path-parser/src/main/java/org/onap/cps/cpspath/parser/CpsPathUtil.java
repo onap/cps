b7e86d2b3fff9fd1bb790ce6ae39ab8e32ee1a03
Lathish <lathishbabu.ganesan@est.tech>
2022-03-31 17:29:22 +0100

    Fix Absolute Path to list with Integer/String key
    Issue-ID: CPS-961
    Change-Id: Ie10f5c6cfc466387e79eec184b933297d1d79587
    Signed-off-by: Lathish <lathishbabu.ganesan@est.tech>

---
+++ cps-path-parser/src/main/java/org/onap/cps/cpspath/parser/CpsPathUtil.java
@@ -0,0 +1,70 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.cpspath.parser;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.onap.cps.cpspath.parser.antlr4.CpsPathLexer;
+import org.onap.cps.cpspath.parser.antlr4.CpsPathParser;
+
+@Getter
+@Setter(AccessLevel.PACKAGE)
+public class CpsPathUtil {
+
+    /**
+     * Returns a normalized xpath path query.
+     *
+     * @param xpathSource xpath
+     * @return a normalized xpath String.
+     */
+    public static String getNormalizedXpath(final String xpathSource) {
+        final CpsPathBuilder cpsPathBuilder = getCpsPathBuilder(xpathSource);
+        return cpsPathBuilder.build().getNormalizedXpath();
+    }
+
+    public static CpsPathQuery getCpsPathQuery(final String cpsPathSource) {
+        final CpsPathBuilder cpsPathBuilder = getCpsPathBuilder(cpsPathSource);
+        return cpsPathBuilder.build();
+    }
+
+    private static CpsPathBuilder getCpsPathBuilder(final String cpsPathSource) {
+        final var inputStream = CharStreams.fromString(cpsPathSource);
+        final var cpsPathLexer = new CpsPathLexer(inputStream);
+        final var cpsPathParser = new CpsPathParser(new CommonTokenStream(cpsPathLexer));
+        cpsPathParser.addErrorListener(new BaseErrorListener() {
+            @Override
+            public void syntaxError(final Recognizer<?, ?> recognizer, final Object offendingSymbol, final int line,
+                                    final int charPositionInLine, final String msg, final RecognitionException e) {
+                throw new IllegalStateException("failed to parse at line " + line + " due to " + msg, e);
+            }
+        });
+        final var cpsPathBuilder = new CpsPathBuilder();
+        cpsPathParser.addParseListener(cpsPathBuilder);
+        cpsPathParser.cpsPath();
+        return cpsPathBuilder;
+    }
+}
---
+++ cps-path-parser/src/main/java/org/onap/cps/cpspath/parser/InvalidPathException.java
@@ -0,0 +1,38 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.cpspath.parser;
+
+import lombok.Getter;
+
+/**
+ * DataNode Not Found Exception. Indicates the requested data being absent.
+ */
+@SuppressWarnings("squid:S110")  // Team agreed to accept 6 levels of inheritance for CPS Exceptions
+public class InvalidPathException extends RuntimeException {
+
+    @Getter
+    final String details;
+
+    public InvalidPathException(final String message, final String details) {
+        super(message);
+        this.details = details;
+    }
+}
