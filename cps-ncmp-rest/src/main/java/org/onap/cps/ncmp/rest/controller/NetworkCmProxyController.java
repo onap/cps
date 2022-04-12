06e9fb021b6833d3722652d8279377dd54d7c261
JosephKeenan <joseph.keenan@est.tech>
2022-04-12 12:24:43 +0100

    Async request response dmi -> NCMP
    -Added consumer for DMI events and producer for forwarding to client
    
    Issue-ID: CPS-830
    Change-Id: I096d08af9d69092cf8651e11eaa00ce441fc3605
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>

--- cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/controller/NetworkCmProxyController.java
+++ cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/controller/NetworkCmProxyController.java
@@ -36,6 +36,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -96,16 +98,24 @@ public class NetworkCmProxyController implements NetworkCmProxyApi {
         final ResponseEntity<Map<String, Object>> asyncResponse = populateAsyncResponse(topicParamInQuery);
         final Map<String, Object> asyncResponseData = asyncResponse.getBody();
 
-        final Object responseObject = networkCmProxyDataService.getResourceDataOperationalForCmHandle(cmHandle,
-                resourceIdentifier,
-                optionsParamInQuery,
-                asyncResponseData == null ? NO_TOPIC : topicParamInQuery,
-                asyncResponseData == null ? NO_REQUEST_ID : asyncResponseData.get(ASYNC_REQUEST_ID).toString());
-
-        if (asyncResponseData == null) {
-            return ResponseEntity.ok(responseObject);
+        if (hasTopic(topicParamInQuery)) {
+            final Future<?> result = Executors.newSingleThreadExecutor().submit(() -> {
+                networkCmProxyDataService.getResourceDataOperationalForCmHandle(
+                    cmHandle,
+                    resourceIdentifier,
+                    optionsParamInQuery,
+                    topicParamInQuery,
+                    asyncResponseData.get(ASYNC_REQUEST_ID).toString()
+                );
+            });
+            log.debug("Result completed {}", result.isDone());
+            return ResponseEntity.ok(asyncResponse);
         }
-        return ResponseEntity.ok(asyncResponse);
+
+        final Object responseObject = networkCmProxyDataService.getResourceDataOperationalForCmHandle(
+            cmHandle, resourceIdentifier, optionsParamInQuery, NO_TOPIC, NO_REQUEST_ID);
+
+        return ResponseEntity.ok(responseObject);
     }
 
     /**
@@ -125,16 +135,27 @@ public class NetworkCmProxyController implements NetworkCmProxyApi {
         final ResponseEntity<Map<String, Object>> asyncResponse = populateAsyncResponse(topicParamInQuery);
         final Map<String, Object> asyncResponseData = asyncResponse.getBody();
 
-        final Object responseObject = networkCmProxyDataService.getResourceDataPassThroughRunningForCmHandle(cmHandle,
-                resourceIdentifier,
-                optionsParamInQuery,
-                asyncResponseData == null ? NO_TOPIC : topicParamInQuery,
-                asyncResponseData == null ? NO_REQUEST_ID : asyncResponseData.get(ASYNC_REQUEST_ID).toString());
-
-        if (asyncResponseData == null) {
-            return ResponseEntity.ok(responseObject);
+        if (hasTopic(topicParamInQuery)) {
+            final Future<?> result = Executors.newSingleThreadExecutor().submit(() -> {
+                networkCmProxyDataService.getResourceDataPassThroughRunningForCmHandle(
+                    cmHandle,
+                    resourceIdentifier,
+                    optionsParamInQuery,
+                    topicParamInQuery,
+                    asyncResponseData.get(ASYNC_REQUEST_ID).toString());
+            });
+            log.debug("Result completed {}", result.isDone());
+            return ResponseEntity.ok(asyncResponse);
         }
-        return ResponseEntity.ok(asyncResponse);
+
+        final Object responseObject = networkCmProxyDataService.getResourceDataPassThroughRunningForCmHandle(
+            cmHandle, resourceIdentifier, optionsParamInQuery, NO_TOPIC, NO_REQUEST_ID);
+
+        return ResponseEntity.ok(responseObject);
+    }
+
+    private boolean hasTopic(final String topicParamInQuery) {
+        return topicParamInQuery != null && !topicParamInQuery.isEmpty() && !topicParamInQuery.isBlank();
     }
 
     @Override
---
+++ cps-service/src/main/java/org/onap/cps/notification/CpsAsyncRequestResponseEventUtil.java
@@ -0,0 +1,92 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2022 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.notification;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+import org.onap.cps.event.model.CpsAsyncRequestResponseEvent;
+import org.onap.cps.event.model.CpsAsyncRequestResponseEventWithOrigin;
+import org.onap.cps.event.model.EventOrigin;
+import org.onap.cps.event.model.EventOriginContent;
+import org.onap.cps.event.model.ResponseOriginData;
+
+/**
+ * Util for converting CpsAsyncRequestResponseEvent to CpsAsyncRequestResponseEventWithOrigin.
+ */
+public class CpsAsyncRequestResponseEventUtil {
+
+    /**
+     * Convert CpsAsyncRequestResponseEvent to CpsAsyncRequestResponseEventWithOrigin.
+     *
+     * @param cpsAsyncRequestResponseEvent the CpsAsyncRequestResponseEvent to convert
+     * @return new CpsAsyncRequestResponseEventWithOrigin
+     */
+    public CpsAsyncRequestResponseEventWithOrigin toCpsAsyncRequestResponseEventWithOrigin(
+        final CpsAsyncRequestResponseEvent cpsAsyncRequestResponseEvent) {
+        final CpsAsyncRequestResponseEventWithOrigin cpsAsyncRequestResponseEventWithOrigin =
+            new CpsAsyncRequestResponseEventWithOrigin();
+
+        cpsAsyncRequestResponseEventWithOrigin.setEventId(UUID.randomUUID().toString());
+        cpsAsyncRequestResponseEventWithOrigin.setEventCorrelationId(
+            cpsAsyncRequestResponseEvent.getEventCorrelationId());
+        cpsAsyncRequestResponseEventWithOrigin.setEventTime(getEventDateTime());
+        cpsAsyncRequestResponseEventWithOrigin.setEventType(NcmpEvents.ASYNC_REQUEST.toString());
+        cpsAsyncRequestResponseEventWithOrigin.setEventOrigin(getEventOrigin(cpsAsyncRequestResponseEvent));
+        cpsAsyncRequestResponseEventWithOrigin.setEventSchema(cpsAsyncRequestResponseEvent.getEventSchema());
+        cpsAsyncRequestResponseEventWithOrigin.setEventSource(cpsAsyncRequestResponseEvent.getEventSource());
+        cpsAsyncRequestResponseEventWithOrigin.setEventTarget(cpsAsyncRequestResponseEvent.getEventTarget());
+        cpsAsyncRequestResponseEventWithOrigin.setEvent(getEventOriginContent(cpsAsyncRequestResponseEvent));
+
+        return cpsAsyncRequestResponseEventWithOrigin;
+    }
+
+    private EventOrigin getEventOrigin(final CpsAsyncRequestResponseEvent cpsAsyncRequestResponseEvent) {
+        final EventOrigin eventOrigin = new EventOrigin();
+        eventOrigin.setEventId(cpsAsyncRequestResponseEvent.getEventId());
+        eventOrigin.setEventCorrelationId(cpsAsyncRequestResponseEvent.getEventCorrelationId());
+        eventOrigin.setEventSchema(cpsAsyncRequestResponseEvent.getEventSchema());
+        eventOrigin.setEventSource(cpsAsyncRequestResponseEvent.getEventSource());
+        eventOrigin.setEventTarget(cpsAsyncRequestResponseEvent.getEventTarget());
+        eventOrigin.setEventTime(cpsAsyncRequestResponseEvent.getEventTime());
+        eventOrigin.setEventType(cpsAsyncRequestResponseEvent.getEventType());
+        return eventOrigin;
+    }
+
+    private EventOriginContent getEventOriginContent(final CpsAsyncRequestResponseEvent cpsAsyncRequestResponseEvent) {
+        final EventOriginContent eventOriginContent = new EventOriginContent();
+        eventOriginContent.setResponseCode(cpsAsyncRequestResponseEvent.getEvent().getResponseCode());
+        eventOriginContent.setResponseStatus(cpsAsyncRequestResponseEvent.getEvent().getResponseCode());
+        eventOriginContent.setResponseDataSchema(cpsAsyncRequestResponseEvent.getEvent().getResponseCode());
+
+        final ResponseOriginData responseOriginData = new ResponseOriginData();
+        responseOriginData.setAdditionalProperty("data",
+            cpsAsyncRequestResponseEvent.getEvent().getResponseData().getAdditionalProperties().get("data"));
+        eventOriginContent.setResponseOriginData(responseOriginData);
+
+        return eventOriginContent;
+    }
+
+    private String getEventDateTime() {
+        return ZonedDateTime.now().format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
+    }
+}
