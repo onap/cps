583a26d8f4b0d7e8f31fb8471b399953dad782f7
JosephKeenan <joseph.keenan@est.tech>
2022-04-12 12:24:43 +0100

    Async request response dmi -> NCMP
    -Added consumer for DMI events and producer for forwarding to client
    
    Issue-ID: CPS-830
    Change-Id: I096d08af9d69092cf8651e11eaa00ce441fc3605
    Signed-off-by: JosephKeenan <joseph.keenan@est.tech>

---
+++ cps-service/src/main/java/org/onap/cps/notification/CpsAsyncRequestResponseEventConsumer.java
@@ -0,0 +1,84 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2022 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.notification;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.event.model.CpsAsyncRequestResponseEvent;
+import org.onap.cps.event.model.CpsAsyncRequestResponseEventWithOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Listener for cps async request response events.
+ */
+@Component
+@Slf4j
+@AllArgsConstructor
+public class CpsAsyncRequestResponseEventConsumer {
+
+    private static final String EVENT_SCHEMA_URN_PREFIX = "urn:cps:org.onap.cps:async-request-response-event-schema:v1";
+    private static URI EVENT_SOURCE = null;
+
+    @Autowired
+    private CpsAsyncRequestResponseEventProducer cpsAsyncRequestResponseEventProducer;
+
+    static {
+        try {
+            EVENT_SOURCE = new URI("urn:cps:org.onap.cps");
+        } catch (final URISyntaxException exc) {
+            exc.printStackTrace();
+        }
+
+    }
+
+    private static final String EVENT_TYPE = "org.onap.cps.async-request-response-event";
+
+    /**
+     * Constructor for CpsAsyncRequestResponseEventConsumer.
+     */
+    public CpsAsyncRequestResponseEventConsumer() {}
+
+    /**
+     * Consume the specified event.
+     *
+     * @param cpsAsyncRequestResponseEvent the event to be consumed and persisted.
+     */
+    @KafkaListener(topics = "${app.listener.ncmp.async-m2m.topic}",
+        errorHandler = "cpsAsyncRequestResponseEventErrorHandler")
+    public void consume(final CpsAsyncRequestResponseEvent cpsAsyncRequestResponseEvent) {
+        log.info("Consuming event {} ...", cpsAsyncRequestResponseEvent);
+
+        final CpsAsyncRequestResponseEventUtil cpsAsyncRequestResponseEventUtil =
+            new CpsAsyncRequestResponseEventUtil();
+
+        final CpsAsyncRequestResponseEventWithOrigin cpsAsyncRequestResponseEventWithOrigin =
+            cpsAsyncRequestResponseEventUtil.toCpsAsyncRequestResponseEventWithOrigin(cpsAsyncRequestResponseEvent);
+
+        cpsAsyncRequestResponseEventProducer.sendMessage(
+            cpsAsyncRequestResponseEventWithOrigin.getEventId(),
+            cpsAsyncRequestResponseEventWithOrigin);
+    }
+
+}
