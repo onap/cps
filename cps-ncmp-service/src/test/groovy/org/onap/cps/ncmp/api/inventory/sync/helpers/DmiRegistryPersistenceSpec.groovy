4b7f48392564516f4f450b3c83052af925641930
DylanB95EST <dylan.byrne@est.tech>
2022-05-19 14:09:58 +0100

    Module Sync Lock State implementation
    Implementation of Lock state for module sync watchdog
    Cm Handle state is locked if any exception is found
    during sync process
    Make changes around READY state method in line with the
    new schema set
    
    Issue-ID: CPS-875
    Change-Id: I6bd159faefef2fa84dbf536c292ff0a132793381
    Signed-off-by: DylanB95EST <dylan.byrne@est.tech>

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/inventory/sync/helpers/DmiRegistryPersistenceSpec.groovy
@@ -0,0 +1,44 @@
+package org.onap.cps.ncmp.api.inventory.sync.helpers
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.onap.cps.api.CpsDataService
+import org.onap.cps.ncmp.api.inventory.CmHandleState
+import org.onap.cps.ncmp.api.inventory.LockReason
+import org.onap.cps.utils.JsonObjectMapper
+import spock.lang.Specification
+
+import java.time.OffsetDateTime
+
+class DmiRegistryPersistenceSpec extends Specification {
+
+    def spiedJsonObjectMapper = Spy(new JsonObjectMapper(new ObjectMapper()))
+
+    def mockCpsDataService = Mock(CpsDataService)
+
+    def objectUnderTest = new DmiRegistryPersistence(spiedJsonObjectMapper, mockCpsDataService)
+
+    def 'Update Cm Handle #scenario State'() {
+        given: 'a cm handle'
+            def cmHandleId = 'Some-Cm-Handle'
+        when: 'update cm handle state is invoked with the #scenario state'
+            objectUnderTest.updateCmHandleState(cmHandleState, cmHandleId)
+        then: 'update node leaves is invoked with the correct params'
+            1 * mockCpsDataService.updateNodeLeaves('NCMP-Admin', 'ncmp-dmi-registry', '/dmi-registry/cm-handles[@id=\'Some-Cm-Handle\']', expectedJsonData, _ as OffsetDateTime)
+        where: 'the following states are used'
+             scenario | cmHandleState        || expectedJsonData
+            'READY'   | CmHandleState.READY  || '{"state":{"cm-handle-state":"READY"}}'
+            'LOCKED'  | CmHandleState.LOCKED || '{"state":{"cm-handle-state":"LOCKED"}}'
+    }
+
+    def 'Save Lock Reason and Details'() {
+        given: 'a cm handle'
+            def cmHandleId = 'Some-Cm-Handle'
+        and: 'the expected Json data'
+            def expectedJsonData = '{"lock-reason":{"reason":"LOCKED_MISBEHAVING","details":"some lock reason details"}}'
+        when: 'save lock reason and details is invoked'
+            objectUnderTest.saveLockReasonAndDetails(cmHandleId, LockReason.LOCKED_MISBEHAVING, 'some lock reason details')
+        then: 'save data is invoked with the correct params'
+            1 * mockCpsDataService.saveData('NCMP-Admin', 'ncmp-dmi-registry', '/dmi-registry/cm-handles[@id=\'Some-Cm-Handle\']/state', expectedJsonData, _ as OffsetDateTime)
+    }
+
+}
