feb35af4ea691a13fd2d42827bc21d8c80dd807c
mpriyank <priyank.maheshwari@est.tech>
2022-06-02 11:27:55 +0100

    Ncmp Producer Setup
    - Ncmp Kafka Publisher setup
    - Test scenarios
    - Refactored existing test class which were using kafka producer and
      consumer properties to use from common MessageSpec
    - Upcoming : Implementation Proposal and Actual logic to publish the
      correct event
    
    Issue-ID: CPS-1035
    Change-Id: I93ae392e8c4e4c85d88ca7858332e79b59e85535
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/async/CpsAsyncRequestResponseEventIntegrationSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/async/CpsAsyncRequestResponseEventIntegrationSpec.groovy
@@ -20,68 +20,29 @@
 
 package org.onap.cps.ncmp.api.impl.async
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.ProducerConfig
-import org.apache.kafka.common.serialization.StringSerializer
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.mapstruct.factory.Mappers
+import org.onap.cps.ncmp.api.utils.MessagingSpec
 import org.onap.cps.ncmp.event.model.DmiAsyncRequestResponseEvent
 import org.onap.cps.ncmp.event.model.NcmpAsyncRequestResponseEvent
+import org.onap.cps.ncmp.utils.TestUtils
+import org.onap.cps.utils.JsonObjectMapper
+import org.spockframework.spring.SpringBean
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.support.serializer.JsonSerializer
-import org.testcontainers.containers.KafkaContainer
+import org.springframework.test.annotation.DirtiesContext
 import org.testcontainers.spock.Testcontainers
-import org.testcontainers.utility.DockerImageName
 
 import java.time.Duration
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.onap.cps.utils.JsonObjectMapper
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.onap.cps.ncmp.utils.TestUtils;
-import org.springframework.boot.test.context.SpringBootTest
-import org.spockframework.spring.SpringBean
-import org.springframework.test.annotation.DirtiesContext
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import spock.lang.Specification
 
 @SpringBootTest(classes = [NcmpAsyncRequestResponseEventProducer, NcmpAsyncRequestResponseEventConsumer])
 @Testcontainers
 @DirtiesContext
-class NcmpAsyncRequestResponseEventProducerIntegrationSpec extends Specification {
+class NcmpAsyncRequestResponseEventProducerIntegrationSpec extends MessagingSpec {
 
-    static kafkaTestContainer = new KafkaContainer(
-        DockerImageName.parse('confluentinc/cp-kafka:6.2.1')
-    )
-
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(kafkaTestContainer::stop))
-    }
-
-    def setupSpec() {
-        kafkaTestContainer.start()
-    }
-
-    def producerConfigProperties = [
-        (ProducerConfig.BOOTSTRAP_SERVERS_CONFIG)      : kafkaTestContainer.getBootstrapServers().split(',')[0],
-        (ProducerConfig.RETRIES_CONFIG)                : 0,
-        (ProducerConfig.BATCH_SIZE_CONFIG)             : 16384,
-        (ProducerConfig.LINGER_MS_CONFIG)              : 1,
-        (ProducerConfig.BUFFER_MEMORY_CONFIG)          : 33554432,
-        (ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG)   : StringSerializer,
-        (ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG) : JsonSerializer
-    ]
-
-    def consumerConfigProperties = [
-        (ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG)       : kafkaTestContainer.getBootstrapServers().split(',')[0],
-        (ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG)  : StringDeserializer,
-        (ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG): StringDeserializer,
-        (ConsumerConfig.AUTO_OFFSET_RESET_CONFIG)       : 'earliest',
-        (ConsumerConfig.GROUP_ID_CONFIG)                : 'test'
-    ]
-
-    def kafkaTemplate = new KafkaTemplate<>(new DefaultKafkaProducerFactory<Integer, String>(producerConfigProperties))
+    def kafkaTemplate = new KafkaTemplate<>(new DefaultKafkaProducerFactory<Integer, String>(producerConfigProperties()))
 
     @SpringBean
     NcmpAsyncRequestResponseEventProducer cpsAsyncRequestResponseEventProducerService =
@@ -98,7 +59,7 @@ class NcmpAsyncRequestResponseEventProducerIntegrationSpec extends Specification
 
     def jsonObjectMapper = new JsonObjectMapper(new ObjectMapper())
 
-    def kafkaConsumer = new KafkaConsumer<>(getConsumerConfigProperties())
+    def kafkaConsumer = new KafkaConsumer<>(consumerConfigProperties('test'))
 
     def 'Consume and forward valid message'() {
         given: 'consumer has a subscription'
@@ -116,11 +77,6 @@ class NcmpAsyncRequestResponseEventProducerIntegrationSpec extends Specification
             def record = records.iterator().next()
             assert testEventSent.eventId.equalsIgnoreCase(jsonObjectMapper.convertJsonString(record.value(),
                     NcmpAsyncRequestResponseEvent).getForwardedEvent().getEventId())
-    }
-
-    @DynamicPropertySource
-    static void registerKafkaProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
-        dynamicPropertyRegistry.add('spring.kafka.bootstrap-servers', kafkaTestContainer::getBootstrapServers)
     }
 
 }
