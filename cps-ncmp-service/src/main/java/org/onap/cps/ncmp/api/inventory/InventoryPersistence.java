5b66360e13713116e23952f43f03fd8a0dbca9e6
emaclee <lee.anjella.macabuhay@est.tech>
2022-06-07 18:32:29 +0100

    Fix sonar code smells
    - Remove useless curly braces around statement
    (https://sonarcloud.io/project/issues?resolved=false&types=CODE_SMELL&id=onap_cps&open=AYD_5_HLFWip2-KRhcLs)
    
    - Define a constant
    (https://sonarcloud.io/project/issues?resolved=false&types=CODE_SMELL&id=onap_cps&open=AYEj9y6sq272EuFEMiu-)
    
    Issue-ID: CPS-475
    Signed-off-by: emaclee <lee.anjella.macabuhay@est.tech>
    Change-Id: Ib430fac481b29dba8725ed0dac4da3daa56a3984

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/InventoryPersistence.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/InventoryPersistence.java
@@ -20,6 +20,7 @@
 
 package org.onap.cps.ncmp.api.inventory;
 
+import java.lang.module.Configuration;
 import java.time.OffsetDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -43,6 +44,8 @@ public class InventoryPersistence {
 
     private static final String NCMP_DMI_REGISTRY_ANCHOR = "ncmp-dmi-registry";
 
+    private static final String XPATH_TO_CM_HANDLE = "/dmi-registry/cm-handles[@id='" + "%s" + "']";
+
     private final JsonObjectMapper jsonObjectMapper;
 
     private final CpsDataService cpsDataService;
@@ -59,7 +62,7 @@ public class InventoryPersistence {
      */
     public CompositeState getCmHandleState(final String cmHandleId) {
         final DataNode stateAsDataNode = cpsDataService.getDataNode(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR,
-            "/dmi-registry/cm-handles[@id='" + cmHandleId + "']/state",
+                String.format(XPATH_TO_CM_HANDLE, cmHandleId) + "/state",
             FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS);
         return compositeStateBuilder.fromDataNode(stateAsDataNode).build();
     }
@@ -74,7 +77,7 @@ public class InventoryPersistence {
         final String cmHandleJsonData = String.format("{\"state\":%s}",
             jsonObjectMapper.asJsonString(compositeState));
         cpsDataService.replaceNodeTree(NCMP_DATASPACE_NAME, NCMP_DMI_REGISTRY_ANCHOR,
-            "/dmi-registry/cm-handles[@id='" + cmHandleId + "']",
+            String.format(XPATH_TO_CM_HANDLE, cmHandleId),
             cmHandleJsonData, OffsetDateTime.now());
     }
 
@@ -111,10 +114,9 @@ public class InventoryPersistence {
     }
 
     private DataNode getCmHandleDataNode(final String cmHandle) {
-        final String xpathForDmiRegistryToFetchCmHandle = "/dmi-registry/cm-handles[@id='" + cmHandle + "']";
         return cpsDataService.getDataNode(NCMP_DATASPACE_NAME,
             NCMP_DMI_REGISTRY_ANCHOR,
-            xpathForDmiRegistryToFetchCmHandle,
+            String.format(XPATH_TO_CM_HANDLE, cmHandle),
             FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS);
     }
 
