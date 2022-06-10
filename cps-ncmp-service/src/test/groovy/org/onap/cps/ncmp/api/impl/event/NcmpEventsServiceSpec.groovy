133cc1e63008567ce65e9974afd9cfef7b235c9a
mpriyank <priyank.maheshwari@est.tech>
2022-06-10 12:09:20 +0100

    NcmpEvent creation and Mapping
    - Mapping class to create header and event payload based on type of
      operation i.e CREATE, UPDATE and DELETE.
    - Service class to get CmHandle public properties, create event and
      delegate request to event publisher.
    - Modification in NcmpEvent schema json to mark field as String instead
      of URI
    - Test scenarios
    - UPCOMING: Call the service method from relevant code to actually
      publish the event.
    
    Issue-ID: CPS-1037
    Change-Id: I6bb7de4b27e602c4d8ee6a5528a866e5f7e8799a
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/event/NcmpEventsServiceSpec.groovy
@@ -0,0 +1,62 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2022 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.event
+
+import org.onap.cps.ncmp.api.NetworkCmProxyDataService
+import org.onap.cps.ncmp.api.models.NcmpServiceCmHandle
+import org.onap.ncmp.cmhandle.lcm.event.NcmpEvent
+import spock.lang.Specification
+
+import static org.onap.ncmp.cmhandle.lcm.event.Event.Operation.CREATE
+import static org.onap.ncmp.cmhandle.lcm.event.Event.Operation.DELETE
+import static org.onap.ncmp.cmhandle.lcm.event.Event.Operation.UPDATE
+
+class NcmpEventsServiceSpec extends Specification {
+
+    def mockNetworkCmProxyDataService = Mock(NetworkCmProxyDataService)
+    def mockNcmpEventsPublisher = Mock(NcmpEventsPublisher)
+    def mockNcmpEventsMapper = Mock(NcmpEventsGenerator)
+
+    def objectUnderTest = new NcmpEventsService(mockNetworkCmProxyDataService, mockNcmpEventsPublisher, mockNcmpEventsMapper)
+
+    def 'Create and Publish event for #operation'() {
+        given: 'a cm handle id and operation and responses are mocked'
+            mockResponses('test-cm-handle-id', operation, 'test-topic')
+        when: 'service is called to publish ncmp event'
+            objectUnderTest.publishNcmpEvent('test-cm-handle-id', operation)
+        then: 'no exception is thrown'
+            noExceptionThrown()
+        where: 'for following operations'
+            operation << [CREATE, UPDATE, DELETE]
+    }
+
+    def mockResponses(cmHandleId, operation, topicName) {
+
+        def ncmpServiceCmhandle = new NcmpServiceCmHandle(cmHandleId: cmHandleId, publicProperties: ['publicProperty1': 'value1'])
+        def ncmpEvent = new NcmpEvent(eventId: UUID.randomUUID().toString(), eventCorrelationId: cmHandleId)
+
+        mockNetworkCmProxyDataService.getNcmpServiceCmHandle(cmHandleId) >> ncmpServiceCmhandle
+        mockNcmpEventsMapper.populateNcmpEvent(cmHandleId, operation, ncmpServiceCmhandle) >> ncmpEvent
+        mockNcmpEventsPublisher.publishEvent(topicName, cmHandleId, ncmpEvent) >> {}
+    }
+
+
+}
