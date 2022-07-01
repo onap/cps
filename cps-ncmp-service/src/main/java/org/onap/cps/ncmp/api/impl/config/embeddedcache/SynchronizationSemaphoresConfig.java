ec78ecad400a9925421ffb36f5d8610dc3a9bcef
mpriyank <priyank.maheshwari@est.tech>
2022-07-01 14:47:09 +0100

    Distributed map setup for Module and Data Sync
    - Introduce the hazelcast dependency and check how the builds react to
      it.
    - Data Sync and Module Sync Map config.
    - Added basic test scenarios.
    - Updating the artifact-id from hazelcast-all to hazelcast-spring and
      version is 4.2.5. (5.1.2 is latest but has known vulnerabilities)
    - Refactored the code as per the onsite review session
    - Upcoming : Actual sub-task which will use the distributed maps in
      respective use cases.
    
    Issue-ID: CPS-1050
    Change-Id: I60314c4ff95c267b186b20862aef93a5c389a971
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/config/embeddedcache/SynchronizationSemaphoresConfig.java
@@ -0,0 +1,58 @@
+/*
+ * ============LICENSE_START========================================================
+ *  Copyright (C) 2022 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.config.embeddedcache;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import java.util.Map;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Core infrastructure of the hazelcast distributed map for Module Sync and Data Sync use cases.
+ */
+@Configuration
+public class SynchronizationSemaphoresConfig {
+
+    @Bean
+    public Map<String, String> moduleSyncSemaphore() {
+        return Hazelcast.newHazelcastInstance(initializeDefaultMapConfig("moduleSyncSemaphore"))
+                .getMap("moduleSyncSemaphore");
+    }
+
+    @Bean
+    public Map<String, String> dataSyncSemaphore() {
+        return Hazelcast.newHazelcastInstance(initializeDefaultMapConfig("dataSyncSemaphore"))
+                .getMap("dataSyncSemaphore");
+    }
+
+    private Config initializeDefaultMapConfig(final String configName) {
+        final Config config = new Config();
+        final MapConfig mapConfig = new MapConfig(configName);
+        mapConfig.setTimeToLiveSeconds(30);
+        mapConfig.setBackupCount(3);
+        mapConfig.setAsyncBackupCount(3);
+        config.addMapConfig(mapConfig);
+        config.setClusterName("synchronization-semaphores");
+        return config;
+    }
+}
