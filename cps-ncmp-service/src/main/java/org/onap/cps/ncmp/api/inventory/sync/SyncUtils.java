a210b407b037654088e03c2583bd4faeb55efe31
sourabh_sourabh <sourabh.sourabh@est.tech>
2022-07-07 16:53:45 +0100

    CmHandle creation performance degradation
    - Created a dedicated threadpool for scheduler.
    - Tuned async threadpool of notification executor from setting RejectedExecutionHandler and application.yml.
    
    Issue-ID: CPS-1126
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>
    Change-Id: I2afe3c76c1aec78751777df0d2f08ddb8dcee102

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/sync/SyncUtils.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/sync/SyncUtils.java
@@ -22,7 +22,6 @@
 package org.onap.cps.ncmp.api.inventory.sync;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import java.security.SecureRandom;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
@@ -55,9 +54,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SyncUtils {
-
-    private static final SecureRandom secureRandom = new SecureRandom();
-
     private final InventoryPersistence inventoryPersistence;
 
     private final DmiDataOperations dmiDataOperations;
@@ -69,17 +65,18 @@ public class SyncUtils {
     /**
      * Query data nodes for cm handles with an "ADVISED" cm handle state, and select a random entry for processing.
      *
-     * @return a random yang model cm handle with an ADVISED state, return null if not found
+     * @return a randomized yang model cm handle list with ADVISED state, return empty list if not found
      */
-    public YangModelCmHandle getAnAdvisedCmHandle() {
+    public List<YangModelCmHandle> getAdvisedCmHandles() {
         final List<DataNode> advisedCmHandles = inventoryPersistence.getCmHandlesByState(CmHandleState.ADVISED);
         if (advisedCmHandles.isEmpty()) {
-            return null;
+            return Collections.EMPTY_LIST;
         }
-        final int randomElementIndex = secureRandom.nextInt(advisedCmHandles.size());
-        final String cmHandleId = advisedCmHandles.get(randomElementIndex).getLeaves()
-            .get("id").toString();
-        return inventoryPersistence.getYangModelCmHandle(cmHandleId);
+        log.debug("Total number of fetched advised cm handle is {}", advisedCmHandles.size());
+        Collections.shuffle(advisedCmHandles);
+        return advisedCmHandles.stream()
+                .map(dataNode -> dataNode.getLeaves().get("id").toString())
+                .map(inventoryPersistence::getYangModelCmHandle).collect(Collectors.toList());
     }
 
     /**
