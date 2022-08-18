f2dad933f393ddc9cb038aec7e80b50e0717ed79
ToineSiebelink <toine.siebelink@est.tech>
2022-08-18 15:03:16 +0100

    Performance Improvement: Retreive Yang Resources
    Native query to more efficiently get all yangResourceIds
    Combined Hibernate Fragment Repository with Native impl.
    
    Issue-ID: CPS-1206
    Issue-ID: CPS-1126
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>
    Change-Id: I93fbd5347dd8f9fc48d44e69e47e8aed2d7ac030

---
+++ cps-ri/src/main/java/org/onap/cps/spi/repository/YangResourceNativeRepositoryImpl.java
@@ -0,0 +1,58 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.spi.repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.StringJoiner;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.hibernate.type.StandardBasicTypes;
+import org.onap.cps.spi.model.ModuleReference;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class YangResourceNativeRepositoryImpl implements YangResourceNativeRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public List<Long> getResourceIdsByModuleReference(final Collection<ModuleReference> moduleReferences) {
+        final Query query = entityManager.createNativeQuery(getCombinedSelectSqlQuery(moduleReferences))
+            .unwrap(org.hibernate.query.NativeQuery.class)
+            .addScalar("id", StandardBasicTypes.LONG);
+        return query.getResultList();
+    }
+
+    private String getCombinedSelectSqlQuery(final Collection<ModuleReference> moduleReferences) {
+        final StringJoiner sqlQueryJoiner = new StringJoiner(" UNION ALL ");
+        moduleReferences.stream().forEach(moduleReference -> {
+            sqlQueryJoiner.add(String.format("SELECT id FROM yang_resource WHERE module_name='%s' and revision='%s'",
+                moduleReference.getModuleName(),
+                moduleReference.getRevision()));
+        });
+        return sqlQueryJoiner.toString();
+    }
+}
