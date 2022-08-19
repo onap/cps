0a965a6ddd295bf034d06725803afe1652df2759
emaclee <lee.anjella.macabuhay@est.tech>
2022-08-19 09:26:39 +0100

    Get all cm handles by DMI plugin Identifier
    Issue-ID: CPS-1136
    Signed-off-by: emaclee <lee.anjella.macabuhay@est.tech>
    Change-Id: Ia3bdc16172a90ad3a3f9ae11cddcad1352188726

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/NetworkCmProxyDataService.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/NetworkCmProxyDataService.java
@@ -162,4 +162,12 @@ public interface NetworkCmProxyDataService {
      * @param dataSyncEnabled data sync enabled flag
      */
     void setDataSyncEnabled(String cmHandleId, boolean dataSyncEnabled);
+
+    /**
+     * Get all cm handles by DMI plugin identifier
+     *
+     * @param dmiPluginIdentifier DMI plugin identifier
+     * @return collection of cm handle ids
+     */
+    Set<String> getAllCmHandlesByDmiPluginIdentifier(String dmiPluginIdentifier);
 }
