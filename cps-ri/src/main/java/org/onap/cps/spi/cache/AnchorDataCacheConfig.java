481e1800718641fb857c9b44cd7c3ca44ff82576
ToineSiebelink <toine.siebelink@est.tech>
2022-10-24 14:12:32 +0100

    Creation of DataNodeBuilder with module name prefix is very slow
    - Created a new hazelcast distributed cache for anchor data config use cases.
    - Module name prefix is added for root data node only.
    - Cached module name prefix by anchor name on demand from database at
      first call.
    - Introduced a new cache holder to have module name prefix of diff.
      levels.
    - Removed hazelxast config test becasue it led to a hazelcast duplciate bean issue during test?!
    
    Issue-ID: CPS-1326
    Change-Id: I9072f5efdeea59843cd827ac556d3c0547a3a0cf
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ri/src/main/java/org/onap/cps/spi/cache/AnchorDataCacheConfig.java
@@ -0,0 +1,75 @@
+/*
+ * ============LICENSE_START========================================================
+ *  Copyright (C) 2022 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.spi.cache;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NamedConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import java.util.concurrent.TimeUnit;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Core infrastructure of the hazelcast distributed cache for anchor data config use cases.
+ */
+@Configuration
+public class AnchorDataCacheConfig {
+
+    public static final long ANCHOR_DATA_CACHE_TTL_SECS = TimeUnit.HOURS.toSeconds(1);
+    private static final MapConfig anchorDataCacheSemaphoreConfig =
+            createMapConfig("anchorDataCacheSemaphoreConfig");
+
+    /**
+     * Distributed instance of anchor data cache that contains module prefix by anchor name as properties.
+     *
+     * @return configured map of anchor data cache
+     */
+    @Bean
+    public IMap<String, AnchorDataCacheEntry> anchorDataCache() {
+        return createHazelcastInstance("anchorDataCache", anchorDataCacheSemaphoreConfig)
+                .getMap("anchorDataCache");
+    }
+
+    private HazelcastInstance createHazelcastInstance(final String hazelcastInstanceName,
+            final NamedConfig namedConfig) {
+        return Hazelcast.newHazelcastInstance(initializeConfig(hazelcastInstanceName, namedConfig));
+    }
+
+    private Config initializeConfig(final String instanceName, final NamedConfig namedConfig) {
+        final Config config = new Config(instanceName);
+        if (namedConfig instanceof MapConfig) {
+            config.addMapConfig((MapConfig) namedConfig);
+        }
+        config.setClusterName("anchor-data-cache");
+        return config;
+    }
+
+    private static MapConfig createMapConfig(final String configName) {
+        final MapConfig mapConfig = new MapConfig(configName);
+        mapConfig.setBackupCount(3);
+        mapConfig.setAsyncBackupCount(3);
+        return mapConfig;
+    }
+
+}
