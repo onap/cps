8c712c24f0d7859e960312abb607758836011100
seanbeirne <sean.beirne@est.tech>
2022-10-27 09:44:29 +0100

    CPS-RI performance profile implementation
    Issue-ID: CPS-1351
    Signed-off-by: seanbeirne <sean.beirne@est.tech>
    Change-Id: I640bac61d0ae8eb3d01f7f7d7288d7c844731f31

--- cps-ri/src/test/groovy/org/onap/cps/spi/perf/CpsToDataNodePerfTest.groovy
+++ cps-ri/src/test/groovy/org/onap/cps/spi/perf/CpsToDataNodePerfTest.groovy
@@ -1,12 +1,13 @@
 /*
- * ============LICENSE_START=======================================================
- *  Copyright (C) 2022 <organization name>
+*  ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *        http://www.apache.org/licenses/LICENSE-2.0
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +16,6 @@
  *
  *  SPDX-License-Identifier: Apache-2.0
  *  ============LICENSE_END=========================================================
- *
- *
  */
 
 package org.onap.cps.spi.perf
@@ -29,6 +28,7 @@ import org.onap.cps.spi.model.DataNodeBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.jdbc.Sql
 import static org.onap.cps.spi.FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS
+import static org.onap.cps.spi.impl.CpsPersistenceSpecBase.CLEAR_DATA
 
 class CpsToDataNodePerfTest extends CpsPersistenceSpecBase {
 
@@ -38,7 +38,7 @@ class CpsToDataNodePerfTest extends CpsPersistenceSpecBase {
     static final String SET_DATA = '/data/fragment.sql'
     static final String  XPATH_DATA_NODE_WITH_DESCENDANTS = '/parent-1'
 
-    @Sql([CpsPersistenceSpecBase.CLEAR_DATA, SET_DATA])
+    @Sql([CLEAR_DATA, SET_DATA])
     def 'Get data node by xpath with all descendants with many children'() {
         given: 'nodes and grandchildren have been persisted'
             def setupStopWatch = new StopWatch()
@@ -50,7 +50,7 @@ class CpsToDataNodePerfTest extends CpsPersistenceSpecBase {
             def readStopWatch = new StopWatch()
             readStopWatch.start()
             def result = objectUnderTest.getDataNode(
-                    CpsPersistenceSpecBase.DATASPACE_NAME, CpsPersistenceSpecBase.ANCHOR_NAME1, XPATH_DATA_NODE_WITH_DESCENDANTS, INCLUDE_ALL_DESCENDANTS)
+                    DATASPACE_NAME, ANCHOR_NAME1, XPATH_DATA_NODE_WITH_DESCENDANTS, INCLUDE_ALL_DESCENDANTS)
             readStopWatch.stop()
             def readDurationInMillis = readStopWatch.getTime()
         then : 'setup duration is under 8 seconds'
@@ -67,7 +67,7 @@ class CpsToDataNodePerfTest extends CpsPersistenceSpecBase {
         (1..numOfChildren).each {
             def childName = "perf-test-child-${it}".toString()
             def newChild = goForthAndMultiply(XPATH_DATA_NODE_WITH_DESCENDANTS, childName, numOfGrandChildren)
-            objectUnderTest.addChildDataNode(CpsPersistenceSpecBase.DATASPACE_NAME, CpsPersistenceSpecBase.ANCHOR_NAME1, XPATH_DATA_NODE_WITH_DESCENDANTS, newChild)
+            objectUnderTest.addChildDataNode(DATASPACE_NAME, ANCHOR_NAME1, XPATH_DATA_NODE_WITH_DESCENDANTS, newChild)
         }
     }
 
