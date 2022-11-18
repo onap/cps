17cbd7b1b51f8d961ad6a908d12c767497afceb9
Matthew Watkins <mwatkins@linuxfoundation.org>
2022-11-17 16:39:39 -0800

    Fix: Swap prepare-csit.sh to Python3 robot framework
    Issue-ID: RELENG-4523
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: I246f9f76996943826afc7d05796e6ddc5c0de6dc

--- csit/prepare-csit.sh
+++ csit/prepare-csit.sh
@@ -26,25 +26,25 @@ fi
 
 TESTPLANDIR=${WORKSPACE}/${TESTPLAN}
 
-# Assume that if ROBOT_VENV is set and virtualenv with system site packages can be activated,
+# Assume that if ROBOT3_VENV is set and virtualenv with system site packages can be activated,
 # ci-management/jjb/integration/include-raw-integration-install-robotframework.sh has already
 # been executed
 
 if [ -f ${WORKSPACE}/env.properties ]; then
     source ${WORKSPACE}/env.properties
 fi
-if [ -f ${ROBOT_VENV}/bin/activate ]; then
-   source ${ROBOT_VENV}/bin/activate
-else
-    rm -rf /tmp/ci-management
-    rm -f ${WORKSPACE}/env.properties
-    cd /tmp
-    source ${WORKSPACE}/install-robotframework.sh
+if [ -f ${ROBOT3_VENV}/bin/activate ]; then
+   source ${ROBOT3_VENV}/bin/activate
+#else
+#    rm -rf /tmp/ci-management
+#    rm -f ${WORKSPACE}/env.properties
+#    cd /tmp
+#    source ${WORKSPACE}/install-robotframework.sh
 fi
 
 # install eteutils
-mkdir -p ${ROBOT_VENV}/src/onap
-rm -rf ${ROBOT_VENV}/src/onap/testsuite
+mkdir -p ${ROBOT3_VENV}/src/onap
+rm -rf ${ROBOT3_VENV}/src/onap/testsuite
 python3 -m pip install --upgrade --extra-index-url="https://nexus3.onap.org/repository/PyPi.staging/simple" 'robotframework-onap==0.5.1.*' --pre
 
 pip freeze
