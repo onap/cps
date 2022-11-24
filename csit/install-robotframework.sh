cfd87692eada791b7c45fd1f306df26eb7a43382
Matthew Watkins <mwatkins@linuxfoundation.org>
2022-11-24 09:22:55 +0000

    Fix: Update python environment to ROBOT3_VENV
    Issue-ID: RELENG-4523
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: Id5ea08481bdf14cce149f047d7cd5b8d8254d200

--- csit/install-robotframework.sh
+++ csit/install-robotframework.sh
@@ -1,12 +1,13 @@
-ROBOT_VENV=$(mktemp -d --suffix=robot_venv)
-echo "ROBOT_VENV=${ROBOT_VENV}" >> "${WORKSPACE}/env.properties"
+set -exu
+
+ROBOT3_VENV=$(mktemp -d --suffix=robot_venv)
+echo "ROBOT3_VENV=${ROBOT_VENV}" >> "${WORKSPACE}/env.properties"
 
 echo "Python version is: $(python3 --version)"
 
-python3 -m venv "${ROBOT_VENV}"
-source "${ROBOT_VENV}/bin/activate"
+python3 -m venv "${ROBOT3_VENV}"
+source "${ROBOT3_VENV}/bin/activate"
 
-set -exu
 
 # Make sure pip3 itself us up-to-date.
 python3 -m pip install --upgrade pip
