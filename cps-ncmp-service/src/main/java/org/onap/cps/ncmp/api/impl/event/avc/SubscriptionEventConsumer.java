ee7d388af447935eee76e7487cd51d897aafb2a4
seanbeirne <sean.beirne@est.tech>
2022-12-06 11:12:18 +0000

    Consume Subscription Creation Event
    Issue-ID: CPS-1392
    Signed-off-by: seanbeirne <sean.beirne@est.tech>
    Change-Id: I0a5a8c256319a1a2944ee6606db1c14b50e8f8e4

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/event/avc/SubscriptionEventConsumer.java
@@ -0,0 +1,53 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.event.avc;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.ncmp.event.model.SubscriptionEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SubscriptionEventConsumer {
+
+    /**
+     * Consume the specified event.
+     *
+     * @param subscriptionEvent the event to be consumed
+     */
+    @KafkaListener(topics = "${ncmp.avc.subscription-topic}")
+    public void consumeSubscriptionEvent(final SubscriptionEvent subscriptionEvent) {
+        if (subscriptionEvent.getEvent().getDataType().getDataCategory().equals("CM")) {
+            log.debug("Consuming event {} ...", subscriptionEvent.toString());
+            if (subscriptionEvent.getEventType().value().equals("CREATE")) {
+                log.info("Subscription for ClientID {} with name{} ...",
+                        subscriptionEvent.getEvent().getSubscription().getClientID(),
+                        subscriptionEvent.getEvent().getSubscription().getName());
+            }
+        } else {
+            log.trace("Non-CM subscription event ignored");
+        }
+    }
+}
