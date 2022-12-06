7b98297638e3f9f087d2511eaebeebf5b84b5197
seanbeirne <sean.beirne@est.tech>
2022-12-06 11:12:18 +0000

    Consume Subscription Creation Event
    Issue-ID: CPS-1392
    Signed-off-by: seanbeirne <sean.beirne@est.tech>
    Change-Id: I0a5a8c256319a1a2944ee6606db1c14b50e8f8e4

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/event/avc/AvcSubscriptionEventSpec.groovy
@@ -0,0 +1,72 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2022 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.event.avc
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jdk.jfr.EventType
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.onap.cps.ncmp.api.kafka.MessagingBaseSpec
+import org.onap.cps.ncmp.event.model.SubscriptionEvent
+import org.onap.cps.ncmp.utils.TestUtils
+import org.onap.cps.utils.JsonObjectMapper
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.testcontainers.spock.Testcontainers
+
+import java.time.Duration
+
+@SpringBootTest(classes = [SubscriptionEventConsumer, ObjectMapper, JsonObjectMapper])
+@Testcontainers
+@DirtiesContext
+class AvcSubscriptionEventSpec extends MessagingBaseSpec {
+
+    @SpringBean
+    SubscriptionEventConsumer subscriptionEventConsumer = new SubscriptionEventConsumer();
+
+    @Autowired
+    JsonObjectMapper jsonObjectMapper
+
+    def kafkaConsumer = new KafkaConsumer<>(consumerConfigProperties('test'))
+
+    def 'Consume valid message'() {
+        given: 'consumer has a subscription'
+            kafkaConsumer.subscribe(['cm-avc-subscription'] as List<String>)
+        when: 'an event is sent'
+            def jsonData = TestUtils.getResourceFileContent('avcSubscriptionCreationEvent.json')
+            def testEventSent = jsonObjectMapper.convertJsonString(jsonData, SubscriptionEvent.class)
+        then: 'the valid event is consumed'
+            subscriptionEventConsumer.consumeSubscriptionEvent(testEventSent)
+    }
+
+    def 'Consume message with invalid dataCategory'() {
+        given: 'consumer has a subscription'
+            kafkaConsumer.subscribe(['cm-avc-subscription'] as List<String>)
+        when: 'an event is sent'
+            def jsonData = TestUtils.getResourceFileContent('avcSubscriptionCreationEvent.json')
+            def testEventSent = jsonObjectMapper.convertJsonString(jsonData, SubscriptionEvent.class)
+        and: 'with updated data to make it invalid'
+            testEventSent.getEvent().getDataType().setDataCategory("FM")
+        then: 'the invalid event is consumed'
+            subscriptionEventConsumer.consumeSubscriptionEvent(testEventSent)
+    }
+}
