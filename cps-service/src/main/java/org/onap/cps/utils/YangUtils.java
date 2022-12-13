a22489e0fd9b6833b7cbefae66898141020c7e4a
Michal Jagiello <michal.jagiello@t-mobile.pl>
2022-12-13 07:40:19 +0000

    XML content on create anchors node support
    Add XML content type support on anchor node creation.
    
    Issue-ID: CPS-1257
    Change-Id: I7e7a9a1961b6e81de93a4e32e842b47f8a163a09
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Signed-off-by: Lee Anjella Macabuhay <lee.anjella.macabuhay@est.tech>

--- cps-service/src/main/java/org/onap/cps/utils/YangUtils.java
+++ cps-service/src/main/java/org/onap/cps/utils/YangUtils.java
@@ -3,6 +3,7 @@
  *  Copyright (C) 2020-2022 Nordix Foundation
  *  Modifications Copyright (C) 2021 Bell Canada.
  *  Modifications Copyright (C) 2021 Pantheon.tech
+ *  Modifications Copyright (C) 2022 Deutsche Telekom AG
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -26,24 +27,34 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.onap.cps.spi.exceptions.DataValidationException;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactory;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
 import org.opendaylight.yangtools.yang.data.codec.gson.JsonParserStream;
+import org.opendaylight.yangtools.yang.data.codec.xml.XmlParserStream;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
 import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
@@ -53,6 +64,7 @@ import org.opendaylight.yangtools.yang.model.api.EffectiveStatementInference;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier;
 import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
+import org.xml.sax.SAXException;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -60,6 +72,44 @@ public class YangUtils {
 
     private static final String XPATH_DELIMITER_REGEX = "\\/";
     private static final String XPATH_NODE_KEY_ATTRIBUTES_REGEX = "\\[.*?\\]";
+
+    /**
+     * Parses data into NormalizedNode according to given schema context.
+     *
+     * @param nodeData      data string
+     * @param schemaContext schema context describing associated data model
+     * @return the NormalizedNode object
+     */
+    public static NormalizedNode parseData(final ContentType contentType, final String nodeData,
+                                           final SchemaContext schemaContext) {
+        if (contentType == ContentType.JSON) {
+            return parseJsonData(nodeData, schemaContext, Optional.empty());
+        }
+        return parseXmlData(XmlFileUtils.prepareXmlContent(nodeData, schemaContext), schemaContext,
+                Optional.empty());
+    }
+
+    /**
+     * Parses data into NormalizedNode according to given schema context.
+     *
+     * @param nodeData      data string
+     * @param schemaContext schema context describing associated data model
+     * @return the NormalizedNode object
+     */
+    public static NormalizedNode parseData(final ContentType contentType, final String nodeData,
+                                           final SchemaContext schemaContext, final String parentNodeXpath) {
+        final DataSchemaNode parentSchemaNode =
+                (DataSchemaNode) getDataSchemaNodeAndIdentifiersByXpath(parentNodeXpath, schemaContext)
+                        .get("dataSchemaNode");
+        final Collection<QName> dataSchemaNodeIdentifiers =
+                (Collection<QName>) getDataSchemaNodeAndIdentifiersByXpath(parentNodeXpath, schemaContext)
+                        .get("dataSchemaNodeIdentifiers");
+        if (contentType == ContentType.JSON) {
+            return parseJsonData(nodeData, schemaContext, Optional.of(dataSchemaNodeIdentifiers));
+        }
+        return parseXmlData(XmlFileUtils.prepareXmlContent(nodeData, parentSchemaNode, parentNodeXpath), schemaContext,
+                Optional.of(dataSchemaNodeIdentifiers));
+    }
 
     /**
      * Parses jsonData into NormalizedNode according to given schema context.
@@ -81,19 +131,20 @@ public class YangUtils {
      * @return the NormalizedNode object
      */
     public static NormalizedNode parseJsonData(final String jsonData, final SchemaContext schemaContext,
-        final String parentNodeXpath) {
+                                               final String parentNodeXpath) {
         final Collection<QName> dataSchemaNodeIdentifiers =
-                getDataSchemaNodeIdentifiersByXpath(parentNodeXpath, schemaContext);
+                (Collection<QName>) getDataSchemaNodeAndIdentifiersByXpath(parentNodeXpath, schemaContext)
+                        .get("dataSchemaNodeIdentifiers");
         return parseJsonData(jsonData, schemaContext, Optional.of(dataSchemaNodeIdentifiers));
     }
 
     private static NormalizedNode parseJsonData(final String jsonData, final SchemaContext schemaContext,
-        final Optional<Collection<QName>> dataSchemaNodeIdentifiers) {
+                                                final Optional<Collection<QName>> dataSchemaNodeIdentifiers) {
         final JSONCodecFactory jsonCodecFactory = JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02
-            .getShared((EffectiveModelContext) schemaContext);
+                .getShared((EffectiveModelContext) schemaContext);
         final NormalizedNodeResult normalizedNodeResult = new NormalizedNodeResult();
         final NormalizedNodeStreamWriter normalizedNodeStreamWriter = ImmutableNormalizedNodeStreamWriter
-            .from(normalizedNodeResult);
+                .from(normalizedNodeResult);
         final JsonReader jsonReader = new JsonReader(new StringReader(jsonData));
         final JsonParserStream jsonParserStream;
 
@@ -101,7 +152,7 @@ public class YangUtils {
             final EffectiveModelContext effectiveModelContext = ((EffectiveModelContext) schemaContext);
             final EffectiveStatementInference effectiveStatementInference =
                     SchemaInferenceStack.of(effectiveModelContext,
-                    SchemaNodeIdentifier.Absolute.of(dataSchemaNodeIdentifiers.get())).toInference();
+                            SchemaNodeIdentifier.Absolute.of(dataSchemaNodeIdentifiers.get())).toInference();
             jsonParserStream =
                     JsonParserStream.create(normalizedNodeStreamWriter, jsonCodecFactory, effectiveStatementInference);
         } else {
@@ -111,22 +162,55 @@ public class YangUtils {
         try {
             jsonParserStream.parse(jsonReader);
             jsonParserStream.close();
-        } catch (final JsonSyntaxException exception) {
+        } catch (final IOException | JsonSyntaxException exception) {
             throw new DataValidationException(
-                "Failed to parse json data: " + jsonData, exception.getMessage(), exception);
-        } catch (final IOException | IllegalStateException illegalStateException) {
+                    "Failed to parse json data: " + jsonData, exception.getMessage(), exception);
+        } catch (final IllegalStateException | IllegalArgumentException exception) {
             throw new DataValidationException(
-                "Failed to parse json data. Unsupported xpath or json data:" + jsonData, illegalStateException
-                .getMessage(), illegalStateException);
+                    "Failed to parse json data. Unsupported xpath or json data:" + jsonData, exception
+                    .getMessage(), exception);
         }
         return normalizedNodeResult.getResult();
+    }
+
+    private static NormalizedNode parseXmlData(final String xmlData, final SchemaContext schemaContext,
+                                               final Optional<Collection<QName>> dataSchemaNodeIdentifiers) {
+        final XMLInputFactory factory = XMLInputFactory.newInstance();
+        factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+        final NormalizedNodeResult normalizedNodeResult = new NormalizedNodeResult();
+        final NormalizedNodeStreamWriter normalizedNodeStreamWriter = ImmutableNormalizedNodeStreamWriter
+                .from(normalizedNodeResult);
+
+        final XmlParserStream xmlParser;
+        final EffectiveModelContext effectiveModelContext = ((EffectiveModelContext) schemaContext);
+
+        if (dataSchemaNodeIdentifiers.isPresent()) {
+            final EffectiveStatementInference effectiveStatementInference =
+                    SchemaInferenceStack.of(effectiveModelContext,
+                            SchemaNodeIdentifier.Absolute.of(dataSchemaNodeIdentifiers.get())).toInference();
+            xmlParser = XmlParserStream.create(normalizedNodeStreamWriter, effectiveStatementInference);
+        } else {
+            xmlParser = XmlParserStream.create(normalizedNodeStreamWriter, effectiveModelContext);
+        }
+
+        try {
+            final XMLStreamReader reader = factory.createXMLStreamReader(new StringReader(xmlData));
+            xmlParser.parse(reader);
+            xmlParser.close();
+        } catch (final XMLStreamException | URISyntaxException | IOException
+                       | SAXException | NullPointerException exception) {
+            throw new DataValidationException(
+                    "Failed to parse xml data: " + xmlData, exception.getMessage(), exception);
+        }
+        final NormalizedNode normalizedNode = normalizedNodeResult.getResult();
+        return getFirstChildXmlRoot(normalizedNode);
     }
 
     /**
      * Create an xpath form a Yang Tools NodeIdentifier (i.e. PathArgument).
      *
      * @param nodeIdentifier the NodeIdentifier
-     * @return an xpath
+     * @return a xpath
      */
     public static String buildXpath(final YangInstanceIdentifier.PathArgument nodeIdentifier) {
         final StringBuilder xpathBuilder = new StringBuilder();
@@ -134,20 +218,20 @@ public class YangUtils {
 
         if (nodeIdentifier instanceof YangInstanceIdentifier.NodeIdentifierWithPredicates) {
             xpathBuilder.append(getKeyAttributesStatement(
-                (YangInstanceIdentifier.NodeIdentifierWithPredicates) nodeIdentifier));
+                    (YangInstanceIdentifier.NodeIdentifierWithPredicates) nodeIdentifier));
         }
         return xpathBuilder.toString();
     }
 
 
     private static String getKeyAttributesStatement(
-        final YangInstanceIdentifier.NodeIdentifierWithPredicates nodeIdentifier) {
+            final YangInstanceIdentifier.NodeIdentifierWithPredicates nodeIdentifier) {
         final List<String> keyAttributes = nodeIdentifier.entrySet().stream().map(
-            entry -> {
-                final String name = entry.getKey().getLocalName();
-                final String value = String.valueOf(entry.getValue()).replace("'", "\\'");
-                return String.format("@%s='%s'", name, value);
-            }
+                entry -> {
+                    final String name = entry.getKey().getLocalName();
+                    final String value = String.valueOf(entry.getValue()).replace("'", "\\'");
+                    return String.format("@%s='%s'", name, value);
+                }
         ).collect(Collectors.toList());
 
         if (keyAttributes.isEmpty()) {
@@ -158,10 +242,10 @@ public class YangUtils {
         }
     }
 
-    private static Collection<QName> getDataSchemaNodeIdentifiersByXpath(final String parentNodeXpath,
-                                                                      final SchemaContext schemaContext) {
+    private static Map<String, Object> getDataSchemaNodeAndIdentifiersByXpath(final String parentNodeXpath,
+                                                                              final SchemaContext schemaContext) {
         final String[] xpathNodeIdSequence = xpathToNodeIdSequence(parentNodeXpath);
-        return findDataSchemaNodeIdentifiersByXpathNodeIdSequence(xpathNodeIdSequence, schemaContext.getChildNodes(),
+        return findDataSchemaNodeAndIdentifiersByXpathNodeIdSequence(xpathNodeIdSequence, schemaContext.getChildNodes(),
                 new ArrayList<>());
     }
 
@@ -177,7 +261,7 @@ public class YangUtils {
         return xpathNodeIdSequence;
     }
 
-    private static Collection<QName> findDataSchemaNodeIdentifiersByXpathNodeIdSequence(
+    private static Map<String, Object> findDataSchemaNodeAndIdentifiersByXpathNodeIdSequence(
             final String[] xpathNodeIdSequence,
             final Collection<? extends DataSchemaNode> dataSchemaNodes,
             final Collection<QName> dataSchemaNodeIdentifiers) {
@@ -187,11 +271,15 @@ public class YangUtils {
             .findFirst().orElseThrow(() -> schemaNodeNotFoundException(currentXpathNodeId));
         dataSchemaNodeIdentifiers.add(currentDataSchemaNode.getQName());
         if (xpathNodeIdSequence.length <= 1) {
-            return dataSchemaNodeIdentifiers;
+            final Map<String, Object> dataSchemaNodeAndIdentifiers =
+                    new HashMap<>();
+            dataSchemaNodeAndIdentifiers.put("dataSchemaNode", currentDataSchemaNode);
+            dataSchemaNodeAndIdentifiers.put("dataSchemaNodeIdentifiers", dataSchemaNodeIdentifiers);
+            return dataSchemaNodeAndIdentifiers;
         }
         if (currentDataSchemaNode instanceof DataNodeContainer) {
-            return findDataSchemaNodeIdentifiersByXpathNodeIdSequence(
-                getNextLevelXpathNodeIdSequence(xpathNodeIdSequence),
+            return findDataSchemaNodeAndIdentifiersByXpathNodeIdSequence(
+                    getNextLevelXpathNodeIdSequence(xpathNodeIdSequence),
                     ((DataNodeContainer) currentDataSchemaNode).getChildNodes(),
                     dataSchemaNodeIdentifiers);
         }
@@ -207,5 +295,20 @@ public class YangUtils {
     private static DataValidationException schemaNodeNotFoundException(final String schemaNodeIdentifier) {
         return new DataValidationException("Invalid xpath.",
             String.format("No schema node was found for xpath identifier '%s'.", schemaNodeIdentifier));
+    }
+
+    private static NormalizedNode getFirstChildXmlRoot(final NormalizedNode parent) {
+        final String rootNodeType = parent.getIdentifier().getNodeType().getLocalName();
+        final Collection<DataContainerChild> children = (Collection<DataContainerChild>) parent.body();
+        final Iterator<DataContainerChild> iterator = children.iterator();
+        NormalizedNode child = null;
+        while (iterator.hasNext()) {
+            child = iterator.next();
+            if (!child.getIdentifier().getNodeType().getLocalName().equals(rootNodeType)
+                    && !(child instanceof LeafNode)) {
+                return child;
+            }
+        }
+        return getFirstChildXmlRoot(child);
     }
 }
