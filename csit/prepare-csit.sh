438fbd45a9ddca70712e147175948f1f8e78d4db
Matthew Watkins <mwatkins@linuxfoundation.org>
2022-12-15 17:03:16 +0000

    Chore: Update robot-framework scripts to use Python3.10
    Migrate these CSIT scripts to explicitly use the latest Python 3.x
    version available in pyenv, the same as the earlier jobs that setup
    the robot framework environment. Also, output some useful versioning
    information to aid debugging.
    
    Issue-ID: CCSDK-3820
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>
    Change-Id: I332f2c46018d4c3433f0a3df9a0f35bb90241b07

--- csit/prepare-csit.sh
+++ csit/prepare-csit.sh
@@ -28,6 +28,21 @@ fi
 
 TESTPLANDIR=${WORKSPACE}/${TESTPLAN}
 
+# Version should match those used to setup robot-framework in other jobs/stages
+# Use pyenv for selecting the python version
+if [[ -d "/opt/pyenv" ]]; then
+  echo "Setup pyenv:"
+  export PYENV_ROOT="/opt/pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  pyenv versions
+  if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init - --no-rehash)"
+    # Choose the latest numeric Python version from installed list
+    version=$(pyenv versions --bare | sed '/^[^0-9]/d' | sort -V | tail -n 1)
+    pyenv local "${version}"
+  fi
+fi
+
 # Assume that if ROBOT3_VENV is set and virtualenv with system site packages can be activated,
 # ci-management/jjb/integration/include-raw-integration-install-robotframework.sh has already
 # been executed
@@ -48,7 +63,10 @@ fi
 # install eteutils
 mkdir -p ${ROBOT3_VENV}/src/onap
 rm -rf ${ROBOT3_VENV}/src/onap/testsuite
-python3 -m pip install --upgrade --extra-index-url="https://nexus3.onap.org/repository/PyPi.staging/simple" 'robotframework-onap==0.5.1.*' --pre
 
+python3 -m pip install --upgrade --extra-index-url="https://nexus3.onap.org/repository/PyPi.staging/simple" 'robotframework-onap==11.0.0.dev17' --pre
+
+echo "Versioning information:"
+python3 --version
 pip freeze
-
+python3 -m robot.run --version || :
