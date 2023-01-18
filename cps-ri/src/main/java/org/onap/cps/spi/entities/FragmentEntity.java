3ca3b5d6480a774e9edfd2df10e136a1aae78ee2
sourabh_sourabh <sourabh.sourabh@est.tech>
2023-01-18 09:38:36 +0000

    Use native query to delete data nodes
    - Used hashcode to remove child fragment based on it's xpath
    
    Issue-ID: CPS-1439
    
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>
    Change-Id: Ibfec65cf1ba5378cce4cb1f8831bfe71df645f7b
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>

--- cps-ri/src/main/java/org/onap/cps/spi/entities/FragmentEntity.java
+++ cps-ri/src/main/java/org/onap/cps/spi/entities/FragmentEntity.java
@@ -40,6 +40,7 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -58,6 +59,7 @@ import org.hibernate.annotations.TypeDef;
 @Entity
 @Table(name = "fragment")
 @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class FragmentEntity implements Serializable {
 
     private static final long serialVersionUID = 7737669789097119667L;
@@ -68,6 +70,7 @@ public class FragmentEntity implements Serializable {
 
     @NotNull
     @Column(columnDefinition = "text")
+    @EqualsAndHashCode.Include
     private String xpath;
 
     @Column(name = "parent_id")
