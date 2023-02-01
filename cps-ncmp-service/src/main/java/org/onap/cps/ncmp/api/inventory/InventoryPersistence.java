5eb9a39e81bda9966419e341dfcd9e0fe91cb9a9
danielhanrahan <daniel.hanrahan@est.tech>
2023-02-01 14:48:52 +0000

    CmHandle batch deletion
    - Use plural deleteDataNodes to remove CmHandles in batches,
      falling back to individual delete on error
    - Use single deleteDataNode instead of deleteListOrListElement for
      individual delete
    
    Issue-ID: CPS-1464
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: If09f22478df8703290c8fc24aa6fe2a11c90788a

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/InventoryPersistence.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/InventoryPersistence.java
@@ -1,6 +1,6 @@
 /*
  *  ============LICENSE_START=======================================================
- *  Copyright (C) 2022 Nordix Foundation
+ *  Copyright (C) 2022-2023 Nordix Foundation
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -147,9 +147,16 @@ public interface InventoryPersistence {
     void replaceListContent(String parentNodeXpath, Collection<DataNode> dataNodes);
 
     /**
-     * Deletes data node for given anchor and dataspace.
+     * Deletes data node.
      *
      * @param dataNodeXpath data node xpath
      */
     void deleteDataNode(String dataNodeXpath);
+
+    /**
+     * Deletes multiple data nodes.
+     *
+     * @param dataNodeXpaths data node xpaths
+     */
+    void deleteDataNodes(Collection<String> dataNodeXpaths);
 }
