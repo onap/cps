bcf41b93d09f14e99732438840d790c087a67c00
danielhanrahan <daniel.hanrahan@est.tech>
2023-03-28 16:43:10 +0100

    Performance tests for getDataNodes and queryDataNodes
    Issue-ID: CPS-1524
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: Id9ec2a86d984d6c50c9ae6093e7a62729cb851da

--- integration-test/src/test/groovy/org/onap/cps/integration/performance/cps/GetPerfTest.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/performance/cps/GetPerfTest.groovy
@@ -21,7 +21,9 @@
 package org.onap.cps.integration.performance.cps
 
 import org.onap.cps.integration.performance.base.CpsPerfTestBase
-import org.onap.cps.spi.FetchDescendantsOption
+import org.springframework.dao.DataAccessResourceFailureException
+
+import static org.onap.cps.spi.FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS
 
 class GetPerfTest extends CpsPerfTestBase {
 
@@ -33,7 +35,7 @@ class GetPerfTest extends CpsPerfTestBase {
         when: 'get data nodes for 5 anchors'
             stopWatch.start()
             (1..5).each {
-                def result = objectUnderTest.getDataNodes(CPS_PERFORMANCE_TEST_DATASPACE, anchorPrefix + it, xpath, FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS)
+                def result = objectUnderTest.getDataNodes(CPS_PERFORMANCE_TEST_DATASPACE, anchorPrefix + it, xpath, INCLUDE_ALL_DESCENDANTS)
                 assert countDataNodesInTree(result) == expectedNumberOfDataNodes
             }
             stopWatch.stop()
@@ -42,10 +44,34 @@ class GetPerfTest extends CpsPerfTestBase {
             recordAndAssertPerformance("Read datatrees using ${scenario}", durationLimit, durationInMillis)
         where: 'the following xpaths are used'
             scenario                | anchorPrefix | xpath                || durationLimit | expectedNumberOfDataNodes
-            'bookstore root'        | 'bookstore'  | '/'                  || 130           | 78
-            'bookstore top element' | 'bookstore'  | '/bookstore'         || 130           | 78
+            'bookstore root'        | 'bookstore'  | '/'                  || 180           | 78
+            'bookstore top element' | 'bookstore'  | '/bookstore'         || 180           | 78
             'openroadm root'        | 'openroadm'  | '/'                  || 750           | 2151
             'openroadm top element' | 'openroadm'  | '/openroadm-devices' || 750           | 2151
+    }
+
+    def 'Read data trees from multiple xpaths'() {
+        given: 'a collection of xpaths to get'
+            def xpaths = (1..10).collect { "/openroadm-devices/openroadm-device[@device-id='C201-7-" + it + "A-NODE_ID_HERE']" }
+        when: 'get data nodes for 5 anchors'
+            stopWatch.start()
+            (1..5).each {
+                def result = objectUnderTest.getDataNodesForMultipleXpaths(CPS_PERFORMANCE_TEST_DATASPACE, 'openroadm' + it, xpaths, INCLUDE_ALL_DESCENDANTS)
+                assert countDataNodesInTree(result) == 860
+            }
+            stopWatch.stop()
+            def durationInMillis = stopWatch.getTotalTimeMillis()
+        then: 'all data is read within 350 ms'
+            recordAndAssertPerformance("Read multiple datatrees using openroadm", 350, durationInMillis)
+    }
+
+    def 'Multiple get limit exceeded: 32,764 (~ 2^15) xpaths.'() {
+        given: 'more than 32,764 xpaths)'
+            def xpaths = (0..32_764).collect { "/size/of/this/path/does/not/matter/for/limit[@id='" + it + "']" }
+        when: 'single get is executed to get all the parent objects and their descendants'
+            cpsDataService.getDataNodesForMultipleXpaths(CPS_PERFORMANCE_TEST_DATASPACE, 'bookstore1', xpaths, INCLUDE_ALL_DESCENDANTS)
+        then: 'an exception is thrown'
+            thrown(DataAccessResourceFailureException.class)
     }
 
 }
