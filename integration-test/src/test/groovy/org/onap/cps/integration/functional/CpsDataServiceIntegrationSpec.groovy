cdbcef19aefca954fed6364d54f96c628c238bbc
danielhanrahan <daniel.hanrahan@est.tech>
2023-04-04 19:00:44 +0100

    Migrate query tests to integration-test module #2
    Issue-ID: CPS-1597
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: If50bf15ad7d7f147448f6b60d464efc1cdc91005

--- integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsDataServiceIntegrationSpec.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsDataServiceIntegrationSpec.groovy
@@ -21,12 +21,13 @@
 
 package org.onap.cps.integration.functional
 
+import org.onap.cps.api.CpsDataService
 import org.onap.cps.integration.base.FunctionalSpecBase
 import org.onap.cps.spi.FetchDescendantsOption
 
 class CpsDataServiceIntegrationSpec extends FunctionalSpecBase {
 
-    def objectUnderTest
+    CpsDataService objectUnderTest
 
     def setup() { objectUnderTest = cpsDataService }
 
@@ -44,4 +45,5 @@ class CpsDataServiceIntegrationSpec extends FunctionalSpecBase {
             FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS || 8
             new FetchDescendantsOption(2)                  || 8
     }
+
 }
--- integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsQueryServiceIntegrationSpec.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsQueryServiceIntegrationSpec.groovy
@@ -20,18 +20,22 @@
 
 package org.onap.cps.integration.functional
 
+import org.onap.cps.api.CpsQueryService
 import org.onap.cps.integration.base.FunctionalSpecBase
 import org.onap.cps.spi.FetchDescendantsOption
 
+import static org.onap.cps.spi.FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS
+import static org.onap.cps.spi.FetchDescendantsOption.OMIT_DESCENDANTS
+
 class CpsQueryServiceIntegrationSpec extends FunctionalSpecBase {
 
-    def objectUnderTest
+    CpsQueryService objectUnderTest
 
     def setup() { objectUnderTest = cpsQueryService }
 
     def 'Query bookstore using CPS path where #scenario.'() {
         when: 'query data nodes for bookstore container'
-            def result = objectUnderTest.queryDataNodes(FUNCTIONAL_TEST_DATASPACE, BOOKSTORE_ANCHOR, cpsPath, FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS)
+            def result = objectUnderTest.queryDataNodes(FUNCTIONAL_TEST_DATASPACE, BOOKSTORE_ANCHOR, cpsPath, INCLUDE_ALL_DESCENDANTS)
         then: 'the result contains expected number of nodes'
             assert result.size() == expectedResultSize
         and: 'the result contains the expected leaf values'
@@ -42,7 +46,43 @@ class CpsQueryServiceIntegrationSpec extends FunctionalSpecBase {
             })
         where:
             scenario                                      | cpsPath                                    || expectedResultSize | expectedLeaves
-            'the and condition is used'                   | '//books[@lang="English" and @price=15]'   || 2                  | [lang:"English", price:15]
-            'the and is used where result does not exist' | '//books[@lang="English" and @price=1000]' || 0                  | []
+            'the AND condition is used'                   | '//books[@lang="English" and @price=15]'   || 2                  | [lang:"English", price:15]
+            'the AND is used where result does not exist' | '//books[@lang="English" and @price=1000]' || 0                  | []
     }
+
+    def 'Cps Path query using descendant anywhere with #scenario.'() {
+        when: 'a query is executed to get a data node by the given cps path'
+            def result = objectUnderTest.queryDataNodes(FUNCTIONAL_TEST_DATASPACE, BOOKSTORE_ANCHOR, cpsPath, OMIT_DESCENDANTS)
+        then: 'xpaths of the retrieved data nodes are as expected'
+            assert result.xpath.sort() == expectedXPaths.sort()
+        where: 'the following data is used'
+            scenario                                                  | cpsPath                                                           || expectedXPaths
+            // TODO Move this case to a separate test
+            'descendant name match end of other node'                 | '//books'                                                         || ["/bookstore/categories[@code='1']/books[@title='Matilda']",
+                                                                                                                                             "/bookstore/categories[@code='1']/books[@title='The Gruffalo']",
+                                                                                                                                             "/bookstore/categories[@code='2']/books[@title='Annihilation']",
+                                                                                                                                             "/bookstore/categories[@code='3']/books[@title='Good Omens']",
+                                                                                                                                             "/bookstore/categories[@code='3']/books[@title='A Book with No Language']",
+                                                                                                                                             "/bookstore/categories[@code='4']/books[@title='Debian GNU/Linux']",
+                                                                                                                                             "/bookstore/categories[@code='5']/books[@title='Catching Fire: The Hunger Games [Book 2]']"]
+            'descendant with string leaf condition'                   | '//categories[@code="2"]'                                         || ["/bookstore/categories[@code='2']"]
+            'descendant with text condition on leaf'                  | '//books/title[text()="Matilda"]'                                 || ["/bookstore/categories[@code='1']/books[@title='Matilda']"]
+            'descendant with text condition case mismatch'            | '//books/title[text()="matilda"]'                                 || []
+            'descendant with text condition on int leaf'              | '//books/price[text()="10"]'                                      || ["/bookstore/categories[@code='1']/books[@title='Matilda']"]
+            'descendant with text condition on leaf-list'             | '//books/authors[text()="Terry Pratchett"]'                       || ["/bookstore/categories[@code='3']/books[@title='Good Omens']"]
+            'descendant with text condition partial match'            | '//books/authors[text()="Terry"]'                                 || []
+            'descendant with text condition (existing) empty string'  | '//books/lang[text()=""]'                                         || ["/bookstore/categories[@code='3']/books[@title='A Book with No Language']"]
+            // TODO bookstore.yaml model does not have int leaf-list
+            // 'descendant with text condition on int leaf-list'         | '//books/editions[text()="2000"]'                                 || ["/bookstore/categories[@code='2']/book"]
+            'descendant name match of leaf containing /'              | '//books[@lang="N/A"]'                                            || ["/bookstore/categories[@code='4']/books[@title='Logarithm tables']"]
+            'descendant with text condition on leaf containing /'     | '//books/lang[text()="N/A"]'                                      || ["/bookstore/categories[@code='4']/books[@title='Logarithm tables']"]
+            'descendant name match of key containing /'               | '//books[@title="Debian GNU/Linux"]'                              || ["/bookstore/categories[@code='4']/books[@title='Debian GNU/Linux']"]
+            'descendant with text condition on key containing /'      | '//books/title[text()="Debian GNU/Linux"]'                        || ["/bookstore/categories[@code='4']/books[@title='Debian GNU/Linux']"]
+            // TODO These cases fail
+            // 'descendant name match of leaf containing ['              | '//books[@author="Joe Bloggs [author]"]'                          || ["/bookstore/categories[@code='4']/books[@title='Logarithm tables']"]
+            // 'descendant with text condition on leaf containing ['     | '//books/author[text()="Joe Bloggs [author]"]'                    || ["/bookstore/categories[@code='4']/books[@title='Logarithm tables']"]
+            // 'descendant name match of key containing ['               | '//books[@title="Catching Fire: The Hunger Games [Book 2]"]'      || ["/bookstore/categories[@code='5']/books[@title='Catching Fire: The Hunger Games [Book 2]']"]
+            // 'descendant with text condition on key containing ['      | '//books/title[text()="Catching Fire: The Hunger Games [Book 2]"]'|| ["/bookstore/categories[@code='5']/books[@title='Catching Fire: The Hunger Games [Book 2]']"]
+    }
+
 }
