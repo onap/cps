8618cfda2031fc1789648a980e90f230b93463de
danielhanrahan <daniel.hanrahan@est.tech>
2023-04-04 14:56:09 +0100

    Migrate query tests to integration-test module #6
    - Remove old tests and broken test data, where fragments in
      ANCHOR-005 have parent fragments in ANCHOR-004
    - Migrate tests to new test framework using bookstore model
    - Add two bookstore anchors to test querying across both
    - Add bookstore data to another test dataspace, to verify querying
      across anchors should be limited to one dataspace
    - Lower minimum module coverage, since cps-ri tests are now in
      integration-test
    
    Issue-ID: CPS-1595
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: I808de288961cb84a486052f9e1dc8fed5f2afe03

--- integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsQueryServiceIntegrationSpec.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsQueryServiceIntegrationSpec.groovy
@@ -186,4 +186,64 @@ class CpsQueryServiceIntegrationSpec extends FunctionalSpecBase {
             thrown(CpsPathException)
     }
 
+    def 'Cps Path query across anchors with #scenario.'() {
+        when: 'a query is executed to get a data nodes across anchors by the given CpsPath'
+            def result = objectUnderTest.queryDataNodesAcrossAnchors(FUNCTIONAL_TEST_DATASPACE, cpsPath, OMIT_DESCENDANTS)
+        then: 'the correct dataspace was queried'
+            assert result.dataspace.toSet() == [FUNCTIONAL_TEST_DATASPACE].toSet()
+        and: 'the correct queried nodes are returned'
+            assert result.size() == expectedXpathsPerAnchor.size() * 2
+            assert result.xpath.toSet().containsAll(expectedXpathsPerAnchor)
+        and: 'correct anchors were queried'
+            assert result.anchorName.toSet() == ['bookstoreAnchor1', 'bookstoreAnchor2'].toSet()
+        where: 'the following data is used'
+            scenario                                    | cpsPath                                               || expectedXpathsPerAnchor
+            'container node'                            | '/bookstore'                                          || ["/bookstore"]
+            'list node'                                 | '/bookstore/categories'                               || ["/bookstore/categories[@code='1']", "/bookstore/categories[@code='2']", "/bookstore/categories[@code='3']"]
+            'string leaf-condition'                     | '/bookstore[@bookstore-name="Easons"]'                || ["/bookstore"]
+            'integer leaf-condition'                    | '/bookstore/categories[@code="1"]/books[@price=15]'   || ["/bookstore/categories[@code='1']/books[@title='The Gruffalo']"]
+            'multiple list-ancestors'                   | '//books/ancestor::categories'                        || ["/bookstore/categories[@code='1']", "/bookstore/categories[@code='2']", "/bookstore/categories[@code='3']"]
+            'one ancestor with list value'              | '//books/ancestor::categories[@code="1"]'             || ["/bookstore/categories[@code='1']"]
+            'list with index value in the xpath prefix' | '//categories[@code="1"]/books/ancestor::bookstore'   || ["/bookstore"]
+            'ancestor with parent list'                 | '//books/ancestor::bookstore/categories'              || ["/bookstore/categories[@code='1']", "/bookstore/categories[@code='2']", "/bookstore/categories[@code='3']"]
+            'ancestor with parent list element'         | '//books/ancestor::bookstore/categories[@code="2"]'   || ["/bookstore/categories[@code='2']"]
+            'ancestor combined with text condition'     | '//books/title[text()="Matilda"]/ancestor::bookstore' || ["/bookstore"]
+    }
+
+    def 'Cps Path query across anchors with #scenario descendants.'() {
+        when: 'a query is executed to get a data node by the given cps path'
+            def result = objectUnderTest.queryDataNodesAcrossAnchors(FUNCTIONAL_TEST_DATASPACE, '/bookstore', fetchDescendantsOption)
+        then: 'the correct dataspace was queried'
+            assert result.dataspace.toSet() == [FUNCTIONAL_TEST_DATASPACE].toSet()
+        and: 'correct number of datanodes are returned'
+            assert countDataNodesInTree(result) == expectedNumberOfNodes
+        where: 'the following data is used'
+            scenario | fetchDescendantsOption  || expectedNumberOfNodes
+            'no'     | OMIT_DESCENDANTS        || 2 * 1
+            'direct' | DIRECT_CHILDREN_ONLY    || 2 * 4
+            // FIXME The next case throws a NullPointerException. This will be fixed in CPS-1582. Uncomment after fix.
+            // 'all'    | INCLUDE_ALL_DESCENDANTS || 2 * 8
+    }
+
+    def 'Cps Path query across anchors with ancestors and #scenario descendants.'() {
+        when: 'a query is executed to get a data node by the given cps path'
+            def result = objectUnderTest.queryDataNodesAcrossAnchors(FUNCTIONAL_TEST_DATASPACE, '//books/ancestor::bookstore', fetchDescendantsOption)
+        then: 'the correct dataspace was queried'
+            assert result.dataspace.toSet() == [FUNCTIONAL_TEST_DATASPACE].toSet()
+        and: 'correct number of datanodes are returned'
+            assert countDataNodesInTree(result) == expectedNumberOfNodes
+        where: 'the following data is used'
+            scenario | fetchDescendantsOption  || expectedNumberOfNodes
+            'no'     | OMIT_DESCENDANTS        || 2 * 1
+            'direct' | DIRECT_CHILDREN_ONLY    || 2 * 4
+            'all'    | INCLUDE_ALL_DESCENDANTS || 2 * 8
+    }
+
+    def 'Cps Path query across anchors with syntax error throws a CPS Path Exception.'() {
+        when: 'trying to execute a query with a syntax (parsing) error'
+            objectUnderTest.queryDataNodesAcrossAnchors(FUNCTIONAL_TEST_DATASPACE, 'cpsPath that cannot be parsed' , OMIT_DESCENDANTS)
+        then: 'a cps path exception is thrown'
+            thrown(CpsPathException)
+    }
+
 }
