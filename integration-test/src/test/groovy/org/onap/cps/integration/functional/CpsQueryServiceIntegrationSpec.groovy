cc2f092280d1df5094688a9448193ea43a7ae8e5
danielhanrahan <daniel.hanrahan@est.tech>
2023-04-05 17:11:24 +0100

    Migrate query tests to integration-test module #3
    Issue-ID: CPS-1597
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: I1d92355f272271424a7014057d251cf88eac0203

--- integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsQueryServiceIntegrationSpec.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/functional/CpsQueryServiceIntegrationSpec.groovy
@@ -20,18 +20,21 @@
 
 package org.onap.cps.integration.functional
 
+import org.onap.cps.api.CpsQueryService
 import org.onap.cps.integration.base.FunctionalSpecBase
-import org.onap.cps.spi.FetchDescendantsOption
+
+import static org.onap.cps.spi.FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS
+import static org.onap.cps.spi.FetchDescendantsOption.OMIT_DESCENDANTS
 
 class CpsQueryServiceIntegrationSpec extends FunctionalSpecBase {
 
-    def objectUnderTest
+    CpsQueryService objectUnderTest
 
     def setup() { objectUnderTest = cpsQueryService }
 
     def 'Query bookstore using CPS path where #scenario.'() {
         when: 'query data nodes for bookstore container'
-            def result = objectUnderTest.queryDataNodes(FUNCTIONAL_TEST_DATASPACE, BOOKSTORE_ANCHOR, cpsPath, FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS)
+            def result = objectUnderTest.queryDataNodes(FUNCTIONAL_TEST_DATASPACE, BOOKSTORE_ANCHOR, cpsPath, INCLUDE_ALL_DESCENDANTS)
         then: 'the result contains expected number of nodes'
             assert result.size() == expectedResultSize
         and: 'the result contains the expected leaf values'
@@ -42,7 +45,24 @@ class CpsQueryServiceIntegrationSpec extends FunctionalSpecBase {
             })
         where:
             scenario                                      | cpsPath                                    || expectedResultSize | expectedLeaves
-            'the and condition is used'                   | '//books[@lang="English" and @price=15]'   || 2                  | [lang:"English", price:15]
-            'the and is used where result does not exist' | '//books[@lang="English" and @price=1000]' || 0                  | []
+            'the AND condition is used'                   | '//books[@lang="English" and @price=15]'   || 2                  | [lang:"English", price:15]
+            'the AND is used where result does not exist' | '//books[@lang="English" and @price=1000]' || 0                  | []
     }
+
+    def 'Cps Path query using descendant anywhere with #scenario condition for a container element.'() {
+        when: 'a query is executed to get a data node by the given cps path'
+            def result = objectUnderTest.queryDataNodes(FUNCTIONAL_TEST_DATASPACE, BOOKSTORE_ANCHOR, cpsPath, OMIT_DESCENDANTS)
+        then: 'xpaths of the retrieved data nodes are as expected'
+            assert result.xpath.sort() == expectedXPaths.sort()
+        where: 'the following data is used'
+            scenario                   | cpsPath                                                     || expectedXPaths
+            'one leaf'                 | '//books[@pub_year=1986]'                                   || ["/bookstore/categories[@code='3']/books[@title='The Light Fantastic']"]
+            'one text'                 | '//books/authors[text()="Terry Pratchett"]'                 || ["/bookstore/categories[@code='3']/books[@title='Good Omens']",
+                                                                                                         "/bookstore/categories[@code='3']/books[@title='The Colour of Magic']",
+                                                                                                         "/bookstore/categories[@code='3']/books[@title='The Light Fantastic']"]
+            'more than one leaf'       | '//books[@price=12 and @pub_year=1983]'                     || ["/bookstore/categories[@code='3']/books[@title='The Colour of Magic']"]
+            'leaves reversed in order' | '//books[@pub_year=1983 and @price=12]'                     || ["/bookstore/categories[@code='3']/books[@title='The Colour of Magic']"]
+            'leaf and text'            | '//books[@pub_year=1986]/authors[text()="Terry Pratchett"]' || ["/bookstore/categories[@code='3']/books[@title='The Light Fantastic']"]
+    }
+
 }
