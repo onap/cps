133339be3205389dab3b1b56e40eb352e87f0328
lukegleeson <luke.gleeson@est.tech>
2023-04-06 15:28:56 +0100

    Timeout for Subscription Create Partial Response
    Issue-ID: CPS-1599
    Signed-off-by: lukegleeson <luke.gleeson@est.tech>
    Change-Id: I2b1a35e93939daa0524d379ac4736d714ef61a6f

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/events/avcsubscription/SubscriptionEventForwarderSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/events/avcsubscription/SubscriptionEventForwarderSpec.groovy
@@ -21,6 +21,7 @@
 package org.onap.cps.ncmp.api.impl.events.avcsubscription
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.hazelcast.map.IMap
 import org.onap.cps.ncmp.api.impl.events.EventsPublisher
 import org.onap.cps.ncmp.api.impl.yangmodels.YangModelCmHandle
 import org.onap.cps.ncmp.api.inventory.InventoryPersistence
@@ -29,20 +30,28 @@ import org.onap.cps.ncmp.event.model.SubscriptionEvent
 import org.onap.cps.ncmp.utils.TestUtils
 import org.onap.cps.spi.exceptions.OperationNotYetSupportedException
 import org.onap.cps.utils.JsonObjectMapper
+import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import spock.util.concurrent.BlockingVariable
 
-@SpringBootTest(classes = [ObjectMapper, JsonObjectMapper])
+@SpringBootTest(classes = [ObjectMapper, JsonObjectMapper, SubscriptionEventForwarder])
 class SubscriptionEventForwarderSpec extends MessagingBaseSpec {
 
-    def mockInventoryPersistence = Mock(InventoryPersistence)
-    def mockSubscriptionEventPublisher = Mock(EventsPublisher<SubscriptionEvent>)
-    def objectUnderTest = new SubscriptionEventForwarder(mockInventoryPersistence, mockSubscriptionEventPublisher)
+    @Autowired
+    SubscriptionEventForwarder objectUnderTest
+
+    @SpringBean
+    InventoryPersistence mockInventoryPersistence = Mock(InventoryPersistence)
+    @SpringBean
+    EventsPublisher<SubscriptionEvent> mockSubscriptionEventPublisher = Mock(EventsPublisher<SubscriptionEvent>)
+    @SpringBean
+    IMap<String, Set<String>> mockForwardedSubscriptionEventCache = Mock(IMap<String, Set<String>>)
 
     @Autowired
     JsonObjectMapper jsonObjectMapper
 
-    def 'Forward valid CM create subscription'() {
+    def 'Forward valid CM create subscription and simulate timeout where #scenario'() {
         given: 'an event'
             def jsonData = TestUtils.getResourceFileContent('avcSubscriptionCreationEvent.json')
             def testEventSent = jsonObjectMapper.convertJsonString(jsonData, SubscriptionEvent.class)
@@ -52,9 +61,17 @@ class SubscriptionEventForwarderSpec extends MessagingBaseSpec {
                 createYangModelCmHandleWithDmiProperty(2, 1,"shape","square"),
                 createYangModelCmHandleWithDmiProperty(3, 2,"shape","triangle")
             ]
+        and: 'the thread creation delay is reduced to 2 seconds for testing'
+            objectUnderTest.dmiResponseTimeoutInMs = 2000
+        and: 'a Blocking Variable is used for the Asynchronous call with a timeout of 5 seconds'
+            def block = new BlockingVariable<Object>(5)
         when: 'the valid event is forwarded'
             objectUnderTest.forwardCreateSubscriptionEvent(testEventSent)
-        then: 'the event is forwarded twice with the CMHandle private properties and provides a valid listenable future'
+        then: 'An asynchronous call is made to the blocking variable'
+            block.get()
+        then: 'the event is added to the forwarded subscription event cache'
+            1 * mockForwardedSubscriptionEventCache.put("SCO-9989752cm-subscription-001", ["DMIName1", "DMIName2"] as Set)
+        and: 'the event is forwarded twice with the CMHandle private properties and provides a valid listenable future'
             1 * mockSubscriptionEventPublisher.publishEvent("ncmp-dmi-cm-avc-subscription-DMIName1", "SCO-9989752-cm-subscription-001-DMIName1",
                 subscriptionEvent -> {
                     Map targets = subscriptionEvent.getEvent().getPredicates().getTargets().get(0)
@@ -68,6 +85,14 @@ class SubscriptionEventForwarderSpec extends MessagingBaseSpec {
                     targets["CMHandle3"] == ["shape":"triangle"]
                 }
             )
+        and: 'a separate thread has been created where the map is polled'
+            1 * mockForwardedSubscriptionEventCache.get(_) >> (DMINamesInMap)
+        and: 'the subscription id is removed from the event cache map returning the asynchronous blocking variable'
+            1 * mockForwardedSubscriptionEventCache.remove("SCO-9989752cm-subscription-001") >> {block.set(_)}
+        where:
+            scenario                                  | DMINamesInMap
+            'there are dmis which have not responded' | ["DMIName1", "DMIName2"] as Set
+            'all dmis have responded '                | [] as Set
     }
 
     def 'Forward CM create subscription where target CM Handles are #scenario'() {
