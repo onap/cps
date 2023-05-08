baad5c0c961c7ee77d12dc557579bea62bebf5f9
halil.cakal <halil.cakal@est.tech>
2023-05-08 14:18:26 +0100

    Subscription Create Event Outcome Kafka Part
    - Add subscription event outcome schema with java type for pojos
    - Add subscription event outcome json for testing
    - Add mapper to convert subscription response to event outcome
    - Change response consumer to publish outcome for client app
    - Change response timeout task to publish outcome for flient app
    - Change subscription persistance to read datanodes
    - Add helper to extract cm handle to status mapping from data nodes
    - Change subscription forwarder to support publish outcome operation
    - Change subscription forwarder to stop using deprecated method publish
      event
    
    Issue-ID: CPS-1507
    Change-Id: I70195073490f456f014e53c1f59d1b6761d18cd4
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/event/avc/ResponseTimeoutTask.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/event/avc/ResponseTimeoutTask.java
@@ -20,32 +20,93 @@
 
 package org.onap.cps.ncmp.api.impl.event.avc;
 
+import static org.onap.cps.ncmp.events.avc.subscription.v1.SubscriptionEventOutcome.EventType.COMPLETE_OUTCOME;
+import static org.onap.cps.ncmp.events.avc.subscription.v1.SubscriptionEventOutcome.EventType.PARTIAL_OUTCOME;
+
 import com.hazelcast.map.IMap;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.ncmp.api.impl.events.avcsubscription.SubscriptionEventForwarder;
+import org.onap.cps.ncmp.api.impl.subscriptions.SubscriptionPersistence;
+import org.onap.cps.ncmp.api.impl.subscriptions.SubscriptionStatus;
+import org.onap.cps.ncmp.api.impl.utils.DataNodeHelper;
+import org.onap.cps.ncmp.api.models.SubscriptionEventResponse;
+import org.onap.cps.ncmp.events.avc.subscription.v1.SubscriptionEventOutcome;
+import org.onap.cps.spi.model.DataNode;
 
 @Slf4j
 @RequiredArgsConstructor
 public class ResponseTimeoutTask implements Runnable {
 
     private final IMap<String, Set<String>> forwardedSubscriptionEventCache;
+    private final String subscriptionClientId;
+    private final String subscriptionName;
     private final String subscriptionEventId;
+    private final SubscriptionPersistence subscriptionPersistence;
+    private final SubscriptionEventForwarder subscriptionEventForwarder;
+    private final SubscriptionOutcomeMapper subscriptionOutcomeMapper;
 
     @Override
     public void run() {
         if (forwardedSubscriptionEventCache.containsKey(subscriptionEventId)) {
             final Set<String> dmiNames = forwardedSubscriptionEventCache.get(subscriptionEventId);
+
+            final Collection<DataNode> dataNodes = subscriptionPersistence.getDataNodesForSubscriptionEvent();
+            final List<Map<String, Serializable>> dataNodeLeaves = DataNodeHelper.getDataNodeLeaves(dataNodes);
+            final List<Collection<Serializable>> cmHandleIdToStatus =
+                    DataNodeHelper.getCmHandleIdToStatus(dataNodeLeaves);
+            final SubscriptionEventOutcome subscriptionEventOutcome =
+                    formSubscriptionOutcomeMessage(cmHandleIdToStatus);
+
             if (dmiNames.isEmpty()) {
-                //TODO full outcome response here
                 log.info("placeholder to create full outcome response for subscriptionEventId: {}.",
                     subscriptionEventId);
+                subscriptionEventOutcome.setEventType(COMPLETE_OUTCOME);
             } else {
-                //TODO partial outcome response here
                 log.info("placeholder to create partial outcome response for subscriptionEventId: {}.",
                     subscriptionEventId);
+                subscriptionEventOutcome.setEventType(PARTIAL_OUTCOME);
             }
+            subscriptionEventForwarder
+                    .forwardOutcomeEventToClientApps(subscriptionEventOutcome, subscriptionEventId);
             forwardedSubscriptionEventCache.remove(subscriptionEventId);
         }
+    }
+
+    private SubscriptionEventOutcome formSubscriptionOutcomeMessage(
+            final List<Collection<Serializable>> cmHandleIdToStatus) {
+        final Map<String, SubscriptionStatus> cmHandleIdToStatusMap = new HashMap<>();
+        final SubscriptionEventResponse forPojoConversionOnly = new SubscriptionEventResponse();
+        forPojoConversionOnly.setClientId(subscriptionClientId);
+        forPojoConversionOnly.setSubscriptionName(subscriptionName);
+
+        for (final Collection<Serializable> cmHandleToStatusBucket: cmHandleIdToStatus) {
+            final Iterator<Serializable> bucketIterator = cmHandleToStatusBucket.iterator();
+            while (bucketIterator.hasNext()) {
+                final String item = (String) bucketIterator.next();
+                if ("PENDING".equals(item)) {
+                    cmHandleIdToStatusMap.put((String) bucketIterator.next(),
+                            SubscriptionStatus.PENDING);
+                }
+                if ("REJECTED".equals(item)) {
+                    cmHandleIdToStatusMap.put((String) bucketIterator.next(),
+                            SubscriptionStatus.REJECTED);
+                }
+                if ("ACCEPTED".equals(item)) {
+                    cmHandleIdToStatusMap.put((String) bucketIterator.next(),
+                            SubscriptionStatus.ACCEPTED);
+                }
+            }
+        }
+        forPojoConversionOnly.setCmHandleIdToStatus(cmHandleIdToStatusMap);
+
+        return subscriptionOutcomeMapper.toSubscriptionEventOutcome(forPojoConversionOnly);
     }
 }
