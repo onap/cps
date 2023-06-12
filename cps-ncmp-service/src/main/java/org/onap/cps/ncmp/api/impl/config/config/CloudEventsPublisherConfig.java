e688870b036d6b3c6416f81d54ff2d766e49d74f
mpriyank <priyank.maheshwari@est.tech>
2023-06-12 09:53:53 +0100

    Cloudevents for DMI Data AVC
    - Introduced cloudevents dependencies and code to publish.
    - Changed v1 to 1.0.0 wherever possible
    - AvcConsumer can consume and forward the cloudevents now
    - Test case refactoring
    - For the time being we will have 2 kafkatemplate until we fully move to
      cloudevents for all events
    
    Issue-ID: CPS-1719
    Change-Id: Icaa9c5610d4d7d78abdb46edabc8da98c594ce68
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/config/config/CloudEventsPublisherConfig.java
@@ -0,0 +1,94 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2023 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.config.config;
+
+import io.cloudevents.kafka.CloudEventDeserializer;
+import io.cloudevents.kafka.CloudEventSerializer;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+/**
+ * Generic Cloud Events Publisher Config.
+ *
+ * @param <T> Valid CloudEvent to be published over the wire.
+ */
+@Configuration
+public class CloudEventsPublisherConfig<T> {
+
+    @Value("${spring.kafka.bootstrap-address}")
+    private String bootstrapAddress;
+
+    /**
+     * Cloud Event Producer Factory.
+     *
+     * @return cloud event producerfactory.
+     */
+    @Bean
+    public ProducerFactory<String, T> cloudEventProducerFactory() {
+        final Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CloudEventSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+
+    /**
+     * Cloud Event Consumer Factory.
+     *
+     * @return Cloud event consumer factory.
+     */
+    @Bean
+    public ConsumerFactory<String, T> cloudEventConsumerFactory() {
+        final Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, CloudEventDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+
+    /**
+     * KafkaTemplate to support cloud events.
+     *
+     * @return KafkaTemplate
+     */
+    @Bean(name = "cloudEventKafkaTemplate")
+    public KafkaTemplate<String, T> cloudEventKafkaTemplate() {
+        final KafkaTemplate<String, T> kafkaTemplate = new KafkaTemplate<>(cloudEventProducerFactory());
+        kafkaTemplate.setConsumerFactory(cloudEventConsumerFactory());
+        return kafkaTemplate;
+    }
+
+
+}
