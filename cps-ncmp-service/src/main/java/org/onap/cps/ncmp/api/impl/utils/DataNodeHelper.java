b42007441972369bebaa0c38e15369b0976a46b0
halil.cakal <halil.cakal@est.tech>
2023-06-14 08:10:15 +0100

    Streamline outcome response
    - Pivot to outcome response generated from db
      for the corresponding subscription
    - Fine tuning to wait till the cutoff time
      for all cm handles that subject to subscription
    - Update cm handles as rejected if dmi plugin or
      cm handles does not exist in db
    
    Issue-ID: CPS-1741
    Change-Id: I82ab6958e673059977abc7708974d5a2cb13e339
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/utils/DataNodeHelper.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/utils/DataNodeHelper.java
@@ -93,4 +93,15 @@ public class DataNodeHelper {
         }
         return resultMap;
     }
+
+    /**
+     * Extracts the mapping of cm handle id to status from data node collection.
+     *
+     * @param dataNodes as a collection
+     * @return cm handle id to status mapping
+     */
+    public static Map<String, SubscriptionStatus> getCmHandleIdToStatusMapFromDataNodes(
+            final Collection<DataNode> dataNodes) {
+        return getCmHandleIdToStatusMap(getCmHandleIdToStatus(getDataNodeLeaves(dataNodes)));
+    }
 }
