7c07b3fc5f453c8356eb0e498b770e82f10aec09
halil.cakal <halil.cakal@est.tech>
2023-07-03 15:24:02 +0100

    Subscription Creation: NCMP to DMI CloudEvent transformation
    - Add mapper to convert client event into ncmp event
    - Add sample json object of ncmp version
    - Change subscription event consumer to consume CloudEvents
    - Change subscription event forwarder to publish CloudEvents
    - Change test producer config to support CloudEvents
    - Change sample subscription event json to comply with new schema
    - Add more test for missing branches
    - Change packages of the mappers into relevant directory
    
    Issue-ID: CPS-1737
    Change-Id: I8c9e6e7bf713a8fb530a0586dfb2bce796a462f5
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/events/avcsubscription/ClientSubscriptionEventMapperSpec.groovy
@@ -0,0 +1,60 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2023 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.avcsubscription
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.mapstruct.factory.Mappers
+import org.onap.cps.ncmp.events.avcsubscription1_0_0.client_to_ncmp.SubscriptionEvent;
+import org.onap.cps.ncmp.utils.TestUtils
+import org.onap.cps.utils.JsonObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest(classes = [JsonObjectMapper, ObjectMapper])
+class ClientSubscriptionEventMapperSpec extends Specification {
+
+    ClientSubscriptionEventMapper objectUnderTest = Mappers.getMapper(ClientSubscriptionEventMapper)
+
+    @Autowired
+    JsonObjectMapper jsonObjectMapper
+
+    def 'Map clients subscription event to ncmps subscription event'() {
+        given: 'a Subscription Event'
+            def jsonData = TestUtils.getResourceFileContent('avcSubscriptionCreationEvent.json')
+            def testEventToMap = jsonObjectMapper.convertJsonString(jsonData, SubscriptionEvent.class)
+        when: 'the client event is mapped to a ncmp subscription event'
+            def result = objectUnderTest.toNcmpSubscriptionEvent(testEventToMap)
+        then: 'the resulting ncmp subscription event contains the correct clientId'
+            assert result.getData().getSubscription().getClientID() == "SCO-9989752"
+        and: 'subscription name'
+            assert result.getData().getSubscription().getName() == "cm-subscription-001"
+        and: 'is tagged value is false'
+            assert result.getData().getSubscription().getIsTagged() == false
+        and: 'data category is CM'
+            assert result.getData().getDataType().getDataCategory() == 'CM'
+        and: 'predicate targets is null'
+            assert result.getData().getPredicates().getTargets() == []
+        and: 'datastore is passthrough-running'
+            assert result.getData().getPredicates().getDatastore() == 'passthrough-running'
+    }
+
+}
---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/utils/CloudEventMapperSpec.groovy
@@ -0,0 +1,104 @@
+/*
+ * ============LICENSE_START========================================================
+ * Copyright (c) 2023 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.utils
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.cloudevents.core.builder.CloudEventBuilder
+import org.onap.cps.ncmp.events.avcsubscription1_0_0.client_to_ncmp.SubscriptionEvent
+import org.onap.cps.ncmp.utils.TestUtils
+import org.onap.cps.utils.JsonObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest(classes = [ObjectMapper, JsonObjectMapper])
+class CloudEventMapperSpec extends Specification {
+
+    @Autowired
+    JsonObjectMapper jsonObjectMapper
+
+    @Autowired
+    ObjectMapper objectMapper
+
+    def 'Map the data of the cloud event to subscription event'() {
+        given: 'a cloud event having a subscription event in the data part'
+            def jsonData = TestUtils.getResourceFileContent('avcSubscriptionCreationEvent.json')
+            def testEventData = jsonObjectMapper.convertJsonString(jsonData, SubscriptionEvent.class)
+            def testCloudEvent = CloudEventBuilder.v1()
+                .withData(objectMapper.writeValueAsBytes(testEventData))
+                .withId('some-event-id')
+                .withType('CREATE')
+                .withSource(URI.create('some-resource'))
+                .withExtension('correlationid', 'test-cmhandle1').build()
+        when: 'the cloud event map to subscription event'
+            def resultSubscriptionEvent = CloudEventMapper.toSubscriptionEvent(testCloudEvent)
+        then: 'the subscription event resulted having expected values'
+            resultSubscriptionEvent.getData() == testEventData.getData()
+    }
+
+    def 'Map the null of the data of the cloud event to subscription event'() {
+        given: 'a cloud event having a null subscription event in the data part'
+            def testCloudEvent = CloudEventBuilder.v1()
+                .withData(null)
+                .withId('some-event-id')
+                .withType('CREATE')
+                .withSource(URI.create('some-resource'))
+                .withExtension('correlationid', 'test-cmhandle1').build()
+        when: 'the cloud event map to subscription event'
+            def resultSubscriptionEvent = CloudEventMapper.toSubscriptionEvent(testCloudEvent)
+        then: 'the subscription event resulted having a null value'
+            resultSubscriptionEvent == null
+    }
+
+    def 'Map the subscription event to data of the cloud event'() {
+        given: 'a subscription event'
+            def jsonData = TestUtils.getResourceFileContent('avcSubscriptionCreationEventNcmpVersion.json')
+            def testEventData = jsonObjectMapper.convertJsonString(jsonData,
+                                org.onap.cps.ncmp.events.avcsubscription1_0_0.ncmp_to_dmi.SubscriptionEvent.class)
+            def testCloudEvent = CloudEventBuilder.v1()
+                .withData(objectMapper.writeValueAsBytes(testEventData))
+                .withId('some-event-key')
+                .withType('CREATE')
+                .withSource(URI.create('some-resource'))
+                .withExtension('correlationid', 'test-cmhandle1').build()
+        when: 'the subscription event map to data of cloud event'
+            def resultCloudEvent = CloudEventMapper.toCloudEvent(testEventData, 'some-event-key')
+        then: 'the subscription event resulted having expected values'
+            resultCloudEvent.getData() == testCloudEvent.getData()
+            resultCloudEvent.getId() == testCloudEvent.getId()
+            resultCloudEvent.getType() == testCloudEvent.getType()
+    }
+
+    def 'Map the subscription event to data of the cloud event with wrong content causes an exception'() {
+        given: 'an empty ncmp subscription event'
+            def testNcmpSubscriptionEvent = new org.onap.cps.ncmp.events.avcsubscription1_0_0.ncmp_to_dmi.SubscriptionEvent()
+        when: 'the subscription event map to data of cloud event'
+            def thrownException = null
+            try {
+                CloudEventMapper.toCloudEvent(testNcmpSubscriptionEvent, 'some-key')
+            } catch (Exception e) {
+                thrownException  = e
+            }
+        then: 'a run time exception is thrown'
+            assert thrownException instanceof RuntimeException
+    }
+
+}
