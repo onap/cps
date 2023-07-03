2c7f2ab68775d857c3f4b726ad6994c8b0680a93
halil.cakal <halil.cakal@est.tech>
2023-07-03 15:24:02 +0100

    Subscription Creation: NCMP to DMI CloudEvent transformation
    - Add mapper to convert client event into ncmp event
    - Add sample json object of ncmp version
    - Change subscription event consumer to consume CloudEvents
    - Change subscription event forwarder to publish CloudEvents
    - Change test producer config to support CloudEvents
    - Change sample subscription event json to comply with new schema
    - Add more test for missing branches
    
    Issue-ID: CPS-1737
    Change-Id: I8c9e6e7bf713a8fb530a0586dfb2bce796a462f5
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/avcsubscription/ClientSubscriptionEventToNcmpSubscriptionEventMapper.java
@@ -0,0 +1,34 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2023 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.avcsubscription;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.onap.cps.ncmp.events.avcsubscription1_0_0.client_to_ncmp.SubscriptionEvent;
+
+@Mapper(componentModel = "spring")
+public interface ClientSubscriptionEventToNcmpSubscriptionEventMapper {
+
+    @Mapping(target = "data.predicates.targets", ignore = true)
+    org.onap.cps.ncmp.events.avcsubscription1_0_0.ncmp_to_dmi.SubscriptionEvent toNcmpSubscriptionEvent(
+            SubscriptionEvent subscriptionEvent);
+
+}
