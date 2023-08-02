f60203ef4affa71692ccfa67b7c723f5d3db617d
Arpit Singh <as00745003@techmahindra.com>
2023-08-02 18:35:31 +0530

    CPS Delta API 1: Delta between 2 anchors
    - CPS Delta Feature Part 1: To find delta between two anchors
       - created new endpoint deltaByDataspaceAndAnchors
       - endpoint to take dataspaceName, Anchor1, Anchor2, xpath,
         descendants as input
       - added new service CpsDeltaService
       - added method to find delta between DataNodes:
         getDeltaBetweenDataNodes
       - added method to get Removed and Updated DataNodes:
         getRemovedAndUpdatedNodes
       - added method to get Updated DataNodes: getUpdatedNodes
       - added method to get Added DataNodes: getAddedNodes
       - added method to get Map of xpath to DataNode: getXpathToDataNode
       - added method to get difference between leaves of 2 DataNodes:
         getUpdatedLeaves
       - added a POJO for delta report
       - Added new JSON data for delta feature testing
       - reduced cps-ri coverage to 0.26
       - renamed anchor in query name to anchor-name-in-query
       - WIP Test coverage
    
    Issue-ID: CPS-1824
    Signed-off-by: Arpit Singh <as00745003@techmahindra.com>
    Change-Id: I313f0f71d04b03878be7643f709d8af1aa6df6ba

--- cps-rest/src/test/groovy/org/onap/cps/rest/controller/DataRestControllerSpec.groovy
+++ cps-rest/src/test/groovy/org/onap/cps/rest/controller/DataRestControllerSpec.groovy
@@ -331,7 +331,28 @@ class DataRestControllerSpec extends Specification {
         and: 'the response contains the root node identifier'
             assert response.contentAsString.contains('parent')
         and: 'the response contains child is true'
-            assert response.contentAsString.contains('"child"') == true
+            assert response.contentAsString.contains('"child"')
+    }
+
+    def 'Get delta between two anchors using #fetchDescendantsOption'() {
+        given: 'the service returns a list containing delta'
+            List<Map> deltaReport = [['xpath':"/bookstore",'payload':['bookstore-name':"Easons"],'action':"update"]]
+            def sourceAnchor = 'referenceAnchor'
+            def targetAnchor = 'comparandAnchor'
+            def xpath = 'some xpath'
+
+            def endpoint = "$dataNodeBaseEndpointV2/anchors/$sourceAnchor/delta"
+            mockCpsDataService.getDeltaByDataspaceAndAnchors(dataspaceName, sourceAnchor, targetAnchor, xpath, OMIT_DESCENDANTS) >> deltaReport
+        when: 'get delta request is performed using REST API'
+            def response =
+                mvc.perform(get(endpoint)
+                    .param('target-anchor-name', targetAnchor)
+                    .param('xpath', xpath))
+                    .andReturn().response
+        then: 'expected response code is returned'
+            assert response.status == HttpStatus.OK.value()
+        and: 'the response contains expected value'
+            assert response.contentAsString.contains("[{\"xpath\":\"/bookstore\",\"payload\":{\"bookstore-name\":\"Easons\"},\"action\":\"update\"}]")
     }
 
     def 'Update data node leaves: #scenario.'() {
