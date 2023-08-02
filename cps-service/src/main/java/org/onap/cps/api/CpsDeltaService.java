0339c71815a4ca4cbab3d263d6c4586a112cda64
Arpit Singh <as00745003@techmahindra.com>
2023-08-02 18:35:31 +0530

    CPS Delta API 1: Delta between 2 anchors
    - CPS Delta Feature Part 1: To find delta between two anchors
       - created new endpoint deltaByDataspaceAndAnchors
       - endpoint to take dataspaceName, source anchor, target anchor,
         xpath, descendants as input
       - added new service CpsDeltaService
       - added method to find delta between DataNodes:
         getDeltaReport
       - added method to find removed data nodes: getRemovedDeltaReports
       - added method to get Added DataNodes: getAddedDeltaReports
       - added method to get Map of xpath to DataNode: convertToXPathToDataNodesMap
       - added a POJO for delta report
       - Added new JSON data for delta feature testing
       - Added groovy test files CpsDeltaServiceImplSpec and DeltaReportBuilderSpec
       - code related to update operation, will be added in
         separate commit
    
    Issue-ID: CPS-1824
    Signed-off-by: Arpit Singh <as00745003@techmahindra.com>
    Change-Id: I313f0f71d04b03878be7643f709d8af1aa6df6ba

---
+++ cps-service/src/main/java/org/onap/cps/api/CpsDeltaService.java
@@ -0,0 +1,42 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 TechMahindra Ltd.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.api;
+
+import java.util.Collection;
+import java.util.List;
+import org.onap.cps.spi.model.DataNode;
+import org.onap.cps.spi.model.DeltaReport;
+
+public interface CpsDeltaService {
+
+    /**
+     * Retrieves delta between source data nodes and target data nodes. Source data nodes contain the data which acts as
+     * the point of reference for delta report, whereas target data nodes contain the data being compared against
+     * source data node. List of {@link DeltaReport}. Each Delta Report contains information such as action, xpath,
+     * source-payload and target-payload.
+     *
+     * @param sourceDataNodes  collection of {@link DataNode} as source/reference for delta generation
+     * @param targetDataNodes  collection of {@link DataNode} as target data for delta generation
+     * @return                 list of {@link DeltaReport} containing delta information
+     */
+    List<DeltaReport> getDeltaReports(Collection<DataNode> sourceDataNodes,
+                                      Collection<DataNode> targetDataNodes);
+}
