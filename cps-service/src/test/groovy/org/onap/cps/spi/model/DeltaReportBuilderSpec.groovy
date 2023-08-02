5aa3850823da0e3865b44239c599e2c4800e6c73
Arpit Singh <as00745003@techmahindra.com>
2023-08-02 18:35:31 +0530

    CPS Delta API 1: Delta between 2 anchors
    - CPS Delta Feature Part 1: To find delta between two anchors
       - created new endpoint deltaByDataspaceAndAnchors
       - endpoint to take dataspaceName, source anchor, target anchor,
         xpath, descendants as input
       - added new service CpsDeltaService
       - added method to find delta between DataNodes:
         getDeltaReport
       - added method to find removed data nodes: getRemovedDeltaReports
       - added method to get Added DataNodes: getAddedDeltaReports
       - added method to get Map of xpath to DataNode: convertToXPathToDataNodesMap
       - added a POJO for delta report
       - Added new JSON data for delta feature testing
       - renamed anchor in query name to anchor-name-in-query
       - Added groovy test files CpsDeltaServiceImplSpec and DeltaReportBuilderSpec
       - WIP Test coverage
       - code related to update operation, will be added in
         separate commit
    
    Issue-ID: CPS-1824
    Signed-off-by: Arpit Singh <as00745003@techmahindra.com>
    Change-Id: I313f0f71d04b03878be7643f709d8af1aa6df6ba

---
+++ cps-service/src/test/groovy/org/onap/cps/spi/model/DeltaReportBuilderSpec.groovy
@@ -0,0 +1,45 @@
+package org.onap.cps.spi.model
+
+import spock.lang.Specification
+
+class DeltaReportBuilderSpec extends Specification{
+
+    def 'Generating delta report with attributes'() {
+        when: 'delta report is generated'
+            def deltaReport = new DeltaReportBuilder()
+                    .actionAdd()
+                    .withXpath('/test-xpath')
+                    .withTargetData(['data':'hello world'])
+                    .build()
+        then: 'the delta report contains the attributes'
+            assert deltaReport.action == 'add'
+            assert deltaReport.xpath == '/test-xpath'
+            assert deltaReport.targetData == ['data': 'hello world']
+    }
+
+    def 'Generating delta report with attributes for remove action'() {
+        when: 'delta report is generated'
+        def deltaReport = new DeltaReportBuilder()
+                .actionRemove()
+                .withXpath('/test-xpath')
+                .withSourceData(['data':'hello world'])
+                .build()
+        then: 'the delta report contains the attributes'
+        assert deltaReport.action == 'remove'
+        assert deltaReport.xpath == '/test-xpath'
+        assert deltaReport.sourceData == ['data': 'hello world']
+    }
+
+//    def 'Generating delta report with attirbutes edge case'() {
+//        when: 'delta report is generated'
+//        def deltaReport = new DeltaReportBuilder()
+//                .actionAdd()
+//                .withXpath('/test-xpath')
+//                .withTargetData(null)
+//                .build()
+//        then: 'the delta report contains the attributes'
+//        assert deltaReport.action == 'add'
+//        assert deltaReport.xpath == '/test-xpath'
+//        assert deltaReport.targetData == null
+//    }
+}
