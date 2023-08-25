9d39a3a5302aef988fd5ed9b2eb740dd5632c7cb
emaclee <lee.anjella.macabuhay@est.tech>
2023-08-25 09:10:42 +0100

    Add memory usage reading to intergration tests
    Issue-ID: CPS-1753
    Signed-off-by: emaclee <lee.anjella.macabuhay@est.tech>
    Change-Id: Idd540740453cd5c5de800eb08fc7d22952cbf591

--- integration-test/src/test/groovy/org/onap/cps/integration/performance/base/CpsPerfTestBase.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/performance/base/CpsPerfTestBase.groovy
@@ -20,6 +20,7 @@
 
 package org.onap.cps.integration.performance.base
 
+import org.onap.cps.integration.TimeAndMemoryStopwatch
 import org.onap.cps.rest.utils.MultipartFileUtil
 import org.onap.cps.spi.FetchDescendantsOption
 import org.springframework.web.multipart.MultipartFile
@@ -32,6 +33,8 @@ class CpsPerfTestBase extends PerfTestBase {
     static final def OPENROADM_ANCHORS = 3
     static final def OPENROADM_DEVICES_PER_ANCHOR = 1000
     static final def OPENROADM_DATANODES_PER_DEVICE = 86
+
+    TimeAndMemoryStopwatch timeAndMemoryStopwatch = new TimeAndMemoryStopwatch()
 
     def printTitle() {
         println('##        C P S   P E R F O R M A N C E   T E S T   R E S U L T S          ##')
@@ -78,12 +81,15 @@ class CpsPerfTestBase extends PerfTestBase {
 
     def 'Warm the database'() {
         when: 'dummy get data nodes runs so that populating the DB does not get included in other test timings'
-            stopWatch.start()
+            timeAndMemoryStopwatch.start()
             def result = cpsDataService.getDataNodes(CPS_PERFORMANCE_TEST_DATASPACE, 'openroadm1', '/', FetchDescendantsOption.OMIT_DESCENDANTS)
             assert countDataNodesInTree(result) == 1
-            stopWatch.stop()
+            timeAndMemoryStopwatch.stop()
             def durationInMillis = stopWatch.getTotalTimeMillis()
-        then: 'all data is read within expected time'
+        then: 'memory used is within #peakMemoryUsage'
+            assert timeAndMemoryStopwatch.getTotalMemoryUsedMB() <= 30
+            println("Memory used: " + timeAndMemoryStopwatch.getTotalMemoryUsedMB())
+        and: 'all data is read within expected time'
             recordAndAssertPerformance("Warming database", TimeUnit.SECONDS.toMillis(200), durationInMillis)
     }
 
