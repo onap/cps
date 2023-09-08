2df13646375ff16237a9ba5db951c97eae187905
leventecsanyi <levente.csanyi@est.tech>
2023-09-08 15:52:24 +0200

    Set lock state and reason upon request
      - added new LockReasonCategories and improved SyncUtils.isReadyForRetry
    
    Issue-ID: CPS-1860
    Signed-off-by: leventecsanyi <levente.csanyi@est.tech>
    Change-Id: I4382d6ad1fa0a7d9dacb8c8281b0458a5afc0375

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/LockReasonCategory.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/inventory/LockReasonCategory.java
@@ -21,5 +21,5 @@
 package org.onap.cps.ncmp.api.inventory;
 
 public enum LockReasonCategory {
-    LOCKED_MODULE_SYNC_FAILED
+    LOCKED_MODULE_SYNC_FAILED, LOCKED_MODULE_UPGRADE, LOCKED_MODULE_UPGRADE_FAILED
 }
