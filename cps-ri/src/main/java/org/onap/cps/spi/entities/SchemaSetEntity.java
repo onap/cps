477bd46874d2c8266007358f9ca335b0442cea18
egernug <gerard.nugent@est.tech>
2023-09-21 17:51:21 +0100

    Reinstate Spring Boot 3.0 after revert
    Issue-ID: CPS-1789
    
    Signed-off-by: egernug <gerard.nugent@est.tech>
    Change-Id: Iee5c514ccdba36a387f83948d8a3ba26e6b1de5b

--- cps-ri/src/main/java/org/onap/cps/spi/entities/SchemaSetEntity.java
+++ cps-ri/src/main/java/org/onap/cps/spi/entities/SchemaSetEntity.java
@@ -19,20 +19,20 @@
 
 package org.onap.cps.spi.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Set;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
