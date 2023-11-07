0502c09fc00a9f049488ea28da0ac6a1d926d653
JvD_Ericsson <jeff.van.dam@est.tech>
2023-11-07 15:26:43 +0000

    Publish trust level notification event
    - Add mapper that maps attribute value change event to cloud event
    - Add publisher that publish the cloud event from ncmp to client
    - Handle notifications for the uses cases below
      - Initial registration case
      - Device hearth beat consumer case
      - Dmi status changes within dmi watch dog case
    - Added tests for mapper, publisher, and trustLevelManager
    - Change dmi watchdog to handle caches initialisation
    - Handle cps restart case
    - Added test for dmi watchdog
    
    Issue-ID: CPS-1910
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>
    Change-Id: I7c0798346a221e703da58902b9d631115de8d91a

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/trustlevel/TrustLevelManagerSpec.groovy
@@ -0,0 +1,86 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.trustlevel
+
+import org.onap.cps.ncmp.api.impl.events.avc.ncmptoclient.AttributeValueChangeEventPublisher
+import org.onap.cps.ncmp.api.models.NcmpServiceCmHandle
+import spock.lang.Specification
+
+class TrustLevelManagerSpec extends Specification {
+
+    def trustLevelPerCmHandle = [:]
+    def mockAttributeValueChangeEventPublisher = Mock(AttributeValueChangeEventPublisher)
+    def objectUnderTest = new TrustLevelManager(trustLevelPerCmHandle, mockAttributeValueChangeEventPublisher)
+
+    def 'Notification during initial cm-handle register'() {
+        given: 'cm handles to be created'
+            def cmHandleModel1 = new NcmpServiceCmHandle(cmHandleId: 'ch-1', registrationTrustLevel: TrustLevel.COMPLETE)
+            def cmHandleModel2 = new NcmpServiceCmHandle(cmHandleId: 'ch-2', registrationTrustLevel: TrustLevel.NONE)
+        when: 'the initial registration handled'
+            objectUnderTest.handleInitialRegistrationOfTrustLevels([cmHandleModel1, cmHandleModel2])
+        then: 'notifications sent for ch-2 only'
+            1 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent('ch-2', 'trustLevel', null, TrustLevel.NONE.name())
+        and: 'both cm handles populated in the cache'
+            assert trustLevelPerCmHandle.get('ch-1') == TrustLevel.COMPLETE
+            assert trustLevelPerCmHandle.get('ch-2') == TrustLevel.NONE
+    }
+
+    def 'No notification during initial cm-handle register'() {
+        given: 'the cache has been initialised and "knows" about my cm handles'
+            trustLevelPerCmHandle.put('ch-1', TrustLevel.COMPLETE)
+            trustLevelPerCmHandle.put('ch-2', TrustLevel.NONE)
+        and: 'cm handles to be created'
+            def cmHandleModel1 = new NcmpServiceCmHandle(cmHandleId: 'ch-1', registrationTrustLevel: TrustLevel.COMPLETE)
+            def cmHandleModel2 = new NcmpServiceCmHandle(cmHandleId: 'ch-2', registrationTrustLevel: TrustLevel.NONE)
+        when: 'the initial registration handled'
+            objectUnderTest.handleInitialRegistrationOfTrustLevels([cmHandleModel1, cmHandleModel2])
+        then: 'no notification is sent'
+            0 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent()
+    }
+
+    def 'Notification during trust level update'() {
+        given: 'the cache has been initialised and "knows" about my cm handle'
+            trustLevelPerCmHandle.put('ch-1', TrustLevel.NONE)
+        when: 'the update handled'
+            objectUnderTest.handleUpdateOfTrustLevels('ch-1', 'COMPLETE')
+        then: 'notification sent for trust-level changed'
+            1 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent('ch-1', 'trustLevel', 'NONE', 'COMPLETE')
+        and: 'the cache is populated'
+            assert trustLevelPerCmHandle.get('ch-1') == TrustLevel.COMPLETE
+    }
+
+    def 'No notification during trust level update'() {
+        given: 'the cache has been initialised and "knows" about my cm handle'
+            trustLevelPerCmHandle.put('ch-1', TrustLevel.NONE)
+        when: 'the update handled'
+            objectUnderTest.handleUpdateOfTrustLevels('ch-1', 'NONE')
+        then: 'no notification is sent'
+            0 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent('ch-1', 'trustLevel', 'NONE', 'NONE')
+    }
+
+    def 'Handle restart of application'() {
+        when: 'the restart handled'
+            objectUnderTest.handleRestartCpsNcmpApplication('ch-1', 'COMPLETE')
+        then: 'notification is sent'
+            1 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent('ch-1', 'trustLevel', null, 'COMPLETE')
+    }
+
+}
