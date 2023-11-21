d29d3ddf13794d05d946a40781ac4a3503e3bb54
Arpit Singh <as00745003@techmahindra.com>
2023-11-21 21:01:43 +0530

    CPS Delta API: Update action for delta service
    - Added code for Update action in Delta service
    
    Issue-ID: CPS-1824
    Signed-off-by: Arpit Singh <as00745003@techmahindra.com>
    Change-Id: I3de07ea3227988784a0892f6a92c238ecf00a7fa

--- cps-service/src/test/groovy/org/onap/cps/api/impl/CpsDeltaServiceImplSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/api/impl/CpsDeltaServiceImplSpec.groovy
@@ -35,16 +35,14 @@ class CpsDeltaServiceImplSpec extends Specification{
     @Shared
     def dataNodeWithChildDataNode = [new DataNodeBuilder().withXpath('/parent').withLeaves(['parent-leaf': 'parent-payload'])
                                              .withChildDataNodes([new DataNodeBuilder().withXpath("/parent/child").build()]).build()]
-    @Shared
-    def emptyDataNode = [new DataNodeBuilder().withXpath('/parent').build()]
 
     def 'Get delta between data nodes for removed data where source data node has #scenario'() {
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports(sourceDataNode as Collection<DataNode>, emptyDataNode)
+            def result = objectUnderTest.getDeltaReports(sourceDataNode as Collection<DataNode>, [])
         then: 'the delta report contains "remove" action with right data'
-            assert result.first().action.equals("remove")
-            assert result.first().xpath == "/parent/child"
-            assert result.first().sourceData == expectedSourceData
+            assert result.get(1).action.equals("remove")
+            assert result.get(1).xpath == "/parent/child"
+            assert result.get(1).sourceData == expectedSourceData
         where: 'following data was used'
             scenario       | sourceDataNode                   || expectedSourceData
             'leaf data'    | dataNodeWithLeafAndChildDataNode || ['child-leaf': 'child-payload']
@@ -53,14 +51,33 @@ class CpsDeltaServiceImplSpec extends Specification{
 
     def 'Get delta between data nodes with new data where target data node has #scenario'() {
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports(emptyDataNode, targetDataNode)
+            def result = objectUnderTest.getDeltaReports([], targetDataNode)
         then: 'the delta report contains "add" action with right data'
-            assert result.first().action.equals("add")
-            assert result.first().xpath == "/parent/child"
-            assert result.first().targetData == expectedTargetData
+            assert result.get(1).action.equals("add")
+            assert result.get(1).xpath == "/parent/child"
+            assert result.get(1).targetData == expectedTargetData
         where: 'following data was used'
             scenario       | targetDataNode                   || expectedTargetData
             'leaf data'    | dataNodeWithLeafAndChildDataNode || ['child-leaf': 'child-payload']
             'no leaf data' | dataNodeWithChildDataNode        || null
+    }
+
+    def 'Get delta between data nodes for updated data, #scenario'() {
+        given: 'Two data nodes'
+        def dataNode1 = [new DataNodeBuilder().withXpath('/parent').withLeaves(['parent-leaf': 'parent-payload'])
+                                                        .withChildDataNodes([new DataNodeBuilder().withXpath("/parent/child").withLeaves('child-leaf': 'child-payload').build()]).build()]
+        def dataNode2 = [new DataNodeBuilder().withXpath('/parent').withLeaves(['parent-leaf': 'parent-payload-updated'])
+                                                        .withChildDataNodes([new DataNodeBuilder().withXpath("/parent/child").withLeaves('child-leaf': 'child-payload-updated').build()]).build()]
+        when: 'attempt to get delta between 2 data nodes'
+            def result = objectUnderTest.getDeltaReports(dataNode1, dataNode2)
+        then: 'the delta report contains "update" action with right data, and the same is true for child data nodes'
+            assert result.get(index).action.equals("update")
+            assert result.get(index).xpath == expectedXpath
+            assert result.get(index).sourceData == expectedSourceData
+            assert result.get(index).targetData == expectedTargetData
+        where: 'the following data was used'
+            scenario           | index || expectedXpath   | expectedSourceData                | expectedTargetData
+            'parent data node' | 0     || "/parent"       | ['parent-leaf': 'parent-payload'] | ['parent-leaf': 'parent-payload-updated']
+            'child data node'  | 1     || "/parent/child" | ['child-leaf': 'child-payload']   | ['child-leaf': 'child-payload-updated']
     }
 }
