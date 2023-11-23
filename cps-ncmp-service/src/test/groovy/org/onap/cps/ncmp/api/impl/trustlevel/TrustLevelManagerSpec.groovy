ebece18fd7b27738bc807bc42d0fbc2ecbd9e9c3
halil.cakal <halil.cakal@est.tech>
2023-11-23 11:52:56 +0000

    Publish trust level notification event
    - Add mapper that maps attribute value change event to cloud event
    - Add publisher that publish the cloud event from ncmp to client
    - Handle notifications for the uses cases below
      - Initial registration case
      - Device hearth beat consumer case
      - Dmi status changes within dmi watch dog case
    - Added tests for mapper, publisher, and trustLevelManager
    - Added test for dmi watchdog
    
    Issue-ID: CPS-1910
    Signed-off-by: JvD_Ericsson <jeff.van.dam@est.tech>
    Change-Id: I7c0798346a221e703da58902b9d631115de8d91a
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/trustlevel/TrustLevelManagerSpec.groovy
@@ -0,0 +1,73 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.trustlevel
+
+import org.onap.cps.ncmp.api.impl.events.avc.ncmptoclient.AttributeValueChangeEventPublisher
+import spock.lang.Specification
+
+class TrustLevelManagerSpec extends Specification {
+
+    def trustLevelPerCmHandle = [:]
+    def mockAttributeValueChangeEventPublisher = Mock(AttributeValueChangeEventPublisher)
+    def objectUnderTest = new TrustLevelManager(trustLevelPerCmHandle, mockAttributeValueChangeEventPublisher)
+
+    def 'Initial cm handle registration'() {
+        given: 'two cm handles: one with no trust level and one trusted'
+            def cmHandleModelsToBeCreated = ['ch-1': null, 'ch-2': TrustLevel.COMPLETE]
+        when: 'the initial registration handled'
+            objectUnderTest.handleInitialRegistrationOfTrustLevels(cmHandleModelsToBeCreated)
+        then: 'no notification sent'
+            0 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent(*_)
+        and: 'both cm handles are in the cache and are trusted'
+            assert trustLevelPerCmHandle.get('ch-1') == TrustLevel.COMPLETE
+            assert trustLevelPerCmHandle.get('ch-2') == TrustLevel.COMPLETE
+    }
+
+    def 'Initial cm handle registration with a cm handle that is not trusted'() {
+        given: 'a not trusted cm handle'
+            def cmHandleModelsToBeCreated = ['ch-2': TrustLevel.NONE]
+        when: 'the initial registration handled'
+            objectUnderTest.handleInitialRegistrationOfTrustLevels(cmHandleModelsToBeCreated)
+        then: 'notification is sent'
+            1 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent(*_)
+    }
+
+    def 'Trust level updated'() {
+        given: 'a trusted cm handle'
+            trustLevelPerCmHandle.put('ch-1', TrustLevel.COMPLETE)
+        when: 'the update handled'
+            objectUnderTest.handleUpdateOfTrustLevels('ch-1', 'COMPLETE')
+        then: 'no notification sent'
+            0 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent(*_)
+    }
+
+    def 'Trust level updated with the different trust factor'() {
+        given: 'a not trusted cm handle'
+            trustLevelPerCmHandle.put('ch-1', TrustLevel.NONE)
+        when: 'the update handled'
+            objectUnderTest.handleUpdateOfTrustLevels('ch-1', 'COMPLETE')
+        then: 'notification is sent'
+            1 * mockAttributeValueChangeEventPublisher.publishAttributeValueChangeEvent('ch-1', 'trustLevel', 'NONE', 'COMPLETE')
+        and: 'cm handle in the cache is trusted'
+            assert trustLevelPerCmHandle.get('ch-1') == TrustLevel.COMPLETE
+    }
+
+}
