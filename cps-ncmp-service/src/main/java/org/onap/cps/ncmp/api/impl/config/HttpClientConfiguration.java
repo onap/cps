36be1d9ebffe342f154a5c527d84d78867874147
waqas.ikram <waqas.ikram@est.tech>
2023-12-14 12:17:03 +0000

    Enhancing the REST template with HttpClient5
    for better performance and allowing users to configure timeouts as per their requirements
    
    Issue-ID: CPS-1994
    Change-Id: I9fa94fb3923a50e33b3850ec0f190a51e278698f
    Signed-off-by: waqas.ikram <waqas.ikram@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/config/HttpClientConfiguration.java
@@ -0,0 +1,57 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.config;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DurationUnit;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "httpclient5", ignoreUnknownFields = true)
+public class HttpClientConfiguration {
+
+    /**
+     * The maximum time to establish a connection.
+     */
+    @DurationUnit(ChronoUnit.SECONDS)
+    private Duration connectTimeoutInSec = Duration.ofSeconds(180);
+
+    /**
+     * The maximum number of open connections per route.
+     */
+    private int maxConnPerRoute = 50;
+
+    /**
+     * The maximum total number of open connections.
+     */
+    private int maxConnTotal = maxConnPerRoute * 2;
+
+    /**
+     * The duration after which idle connections are evicted.
+     */
+    @DurationUnit(ChronoUnit.SECONDS)
+    private Duration evictIdleConnsInSec = Duration.ofSeconds(5);
+
+}
