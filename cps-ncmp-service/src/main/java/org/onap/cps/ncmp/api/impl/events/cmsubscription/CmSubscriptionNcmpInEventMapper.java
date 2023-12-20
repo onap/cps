b05e63688e94dfd4246d43a8a5b86d6f90b3b8e2
seanbeirne <sean.beirne@est.tech>
2023-12-20 16:37:37 +0000

    Read and validate incoming sub create request from Kafka
    Issue-ID: CPS-1951
    Signed-off-by: seanbeirne <sean.beirne@est.tech>
    Change-Id: I402dacc47c93e77a4ca63598acebe27c780951de

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/cmsubscription/CmSubscriptionNcmpInEventMapper.java
@@ -0,0 +1,68 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.cmsubscription;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.onap.cps.ncmp.api.impl.yangmodels.YangModelCmDataSubscriptionEvent;
+import org.onap.cps.ncmp.events.cmsubscription_merge1_0_0.client_to_ncmp.CmSubscriptionNcmpInEvent;
+import org.onap.cps.ncmp.events.cmsubscription_merge1_0_0.client_to_ncmp.Predicate;
+
+@Mapper(componentModel = "spring")
+public interface CmSubscriptionNcmpInEventMapper {
+
+    @Mapping(source = "data.subscriptionId", target = "name")
+    @Mapping(source = "data.predicates", target = "cmHandles", qualifiedByName = "mapPredicatesToCmHandles")
+    YangModelCmDataSubscriptionEvent toYangModelCmDataSubscriptionEvent(
+        CmSubscriptionNcmpInEvent cmSubscriptionNcmpInEvent);
+
+    /**
+     * Maps Predicates to list of CmHandles.
+     *
+     * @param predicates as a list
+     * @return CmHandle list
+     */
+    @Named("mapPredicatesToCmHandles")
+    default List<YangModelCmDataSubscriptionEvent.CmHandle> mapPredicatesToCmHandles(
+        List<Predicate> predicates) {
+        if (predicates == null) {
+            return null;
+        }
+        final List<YangModelCmDataSubscriptionEvent.CmHandle> cmHandles = new ArrayList<>();
+        for (final Predicate predicate: predicates) {
+            final List<YangModelCmDataSubscriptionEvent.Filter> filters = new ArrayList<>();
+            filters.add(new YangModelCmDataSubscriptionEvent.Filter(
+                predicate.getScopeFilter().getDatastore().value(),
+                predicate.getScopeFilter().getXpathFilter()));
+
+            for (final String cmhandleId : predicate.getTargetFilter()) {
+                final YangModelCmDataSubscriptionEvent.CmHandle cmHandle =
+                    new YangModelCmDataSubscriptionEvent.CmHandle(cmhandleId, filters);
+                cmHandles.add(cmHandle);
+            }
+        }
+        return cmHandles;
+    }
+
+}
