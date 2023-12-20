8802fc925c987619f47fc035bde6c08fdbe13fda
seanbeirne <sean.beirne@est.tech>
2023-12-20 16:37:37 +0000

    Read and validate incoming sub create request from Kafka
    Issue-ID: CPS-1951
    Signed-off-by: seanbeirne <sean.beirne@est.tech>
    Change-Id: I402dacc47c93e77a4ca63598acebe27c780951de

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/cmsubscription/CmSubscriptionNcmpInEventConsumer.java
@@ -0,0 +1,75 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.cmsubscription;
+
+import static org.onap.cps.ncmp.api.impl.events.mapper.CloudEventMapper.toTargetEvent;
+
+import io.cloudevents.CloudEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.onap.cps.ncmp.api.impl.yangmodels.YangModelCmDataSubscriptionEvent;
+import org.onap.cps.ncmp.events.cmsubscription_merge1_0_0.client_to_ncmp.CmSubscriptionNcmpInEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class CmSubscriptionNcmpInEventConsumer {
+
+    private final CmSubscriptionNcmpInEventMapper cmSubscriptionNcmpInEventMapper;
+
+    @Value("${notification.enabled:true}")
+    private boolean notificationFeatureEnabled;
+
+    @Value("${ncmp.model-loader.subscription:false}")
+    private boolean subscriptionModelLoaderEnabled;
+
+    /**
+     * Consume the specified event.
+     *
+     * @param subscriptionEventConsumerRecord the event to be consumed
+     */
+    @KafkaListener(topics = "${app.ncmp.avc.subscription-topic}",
+        containerFactory = "cloudEventConcurrentKafkaListenerContainerFactory")
+    public void consumeSubscriptionEvent(final ConsumerRecord<String, CloudEvent> subscriptionEventConsumerRecord) {
+        final CloudEvent cloudEvent = subscriptionEventConsumerRecord.value();
+        final CmSubscriptionNcmpInEvent cmSubscriptionNcmpInEvent =
+            toTargetEvent(cloudEvent, CmSubscriptionNcmpInEvent.class);
+        if (subscriptionModelLoaderEnabled) {
+            persistSubscriptionEvent(cmSubscriptionNcmpInEvent);
+        }
+        if ("subscriptionCreated".equals(cloudEvent.getType()) && cmSubscriptionNcmpInEvent != null) {
+            log.info("Subscription for ClientID {} with name {} ...",
+                cloudEvent.getSource(),
+                cmSubscriptionNcmpInEvent.getData().getSubscriptionId());
+        }
+    }
+
+    private void persistSubscriptionEvent(final CmSubscriptionNcmpInEvent cmSubscriptionNcmpInEvent) {
+        final YangModelCmDataSubscriptionEvent yangModelCmDataSubscriptionEvent =
+            cmSubscriptionNcmpInEventMapper.toYangModelCmDataSubscriptionEvent(cmSubscriptionNcmpInEvent);
+        log.info("Subscription Event mapped to yang model: {}", yangModelCmDataSubscriptionEvent);
+    }
+
+}
