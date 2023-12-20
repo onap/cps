0dacb714010b17f2d7667e2b9979def586b0ea50
seanbeirne <sean.beirne@est.tech>
2023-12-20 16:37:37 +0000

    Read and validate incoming sub create request from Kafka
    Issue-ID: CPS-1951
    Signed-off-by: seanbeirne <sean.beirne@est.tech>
    Change-Id: I402dacc47c93e77a4ca63598acebe27c780951de

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/events/cmsubscription/CmSubscriptionNcmpInEventConsumerSpec.groovy
@@ -0,0 +1,79 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2024 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.cmsubscription
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.cloudevents.CloudEvent
+import io.cloudevents.core.builder.CloudEventBuilder
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.onap.cps.ncmp.api.impl.events.cmsubscription.CmSubscriptionNcmpInEventConsumer
+import org.onap.cps.ncmp.api.impl.events.cmsubscription.CmSubscriptionNcmpInEventMapper
+import org.onap.cps.ncmp.api.impl.yangmodels.YangModelCmDataSubscriptionEvent
+import org.onap.cps.ncmp.api.kafka.MessagingBaseSpec
+import org.onap.cps.ncmp.events.cmsubscription_merge1_0_0.client_to_ncmp.CmSubscriptionNcmpInEvent
+import org.onap.cps.ncmp.utils.TestUtils
+import org.onap.cps.utils.JsonObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [ObjectMapper, JsonObjectMapper])
+class CmSubscriptionNcmpInEventConsumerSpec extends MessagingBaseSpec {
+
+    def mockCmSubscriptionNcmpInEventMapper = Mock(CmSubscriptionNcmpInEventMapper)
+    def objectUnderTest = new CmSubscriptionNcmpInEventConsumer(mockCmSubscriptionNcmpInEventMapper)
+
+    def yangModelCmDataSubscriptionEvent = new YangModelCmDataSubscriptionEvent()
+
+    @Autowired
+    JsonObjectMapper jsonObjectMapper
+
+    @Autowired
+    ObjectMapper objectMapper
+
+
+    def 'Consume, persist and forward valid CMSubscription create message'() {
+        given: 'a cmsubscription event'
+            def jsonData = TestUtils.getResourceFileContent('cmSubscriptionNcmpInEvent.json')
+            def testEventSent = jsonObjectMapper.convertJsonString(jsonData, CmSubscriptionNcmpInEvent.class)
+            def testCloudEventSent = CloudEventBuilder.v1()
+                .withData(objectMapper.writeValueAsBytes(testEventSent))
+                .withId('subscriptionCreated')
+                .withType('subscriptionCreated')
+                .withSource(URI.create('some-resource'))
+                .withExtension('correlationid', 'test-cmhandle1').build()
+            def consumerRecord = new ConsumerRecord<String, CloudEvent>('topic-name', 0, 0, 'event-key', testCloudEventSent)
+        and: 'notifications are enabled'
+            objectUnderTest.notificationFeatureEnabled = isNotificationEnabled
+        and: 'subscription model loader is enabled'
+            objectUnderTest.subscriptionModelLoaderEnabled = isModelLoaderEnabled
+        when: 'the valid event is consumed'
+            objectUnderTest.consumeSubscriptionEvent(consumerRecord)
+        then: 'the event is mapped to a yangModelSubscription'
+            numberOfTimesToPersist * mockCmSubscriptionNcmpInEventMapper.toYangModelCmDataSubscriptionEvent(testEventSent) >> yangModelCmDataSubscriptionEvent
+        where: 'given values are used'
+        scenario                                          | isNotificationEnabled | isModelLoaderEnabled || numberOfTimesToPersist
+        'Both model loader and notification are enabled'  |    true               |      true            ||        1
+        'Both model loader and notification are disabled' |    false              |      false           ||        0
+        'Model loader enabled and notification  disabled' |    false              |      true            ||        1
+        'Model loader disabled and notification enabled'  |    true               |      false           ||        0
+    }
+
+}
