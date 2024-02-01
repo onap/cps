e60e79c2a98f42da8715c05a05391ea2e83b7334
mpriyank <priyank.maheshwari@est.tech>
2024-02-01 10:57:02 +0000

    CmSubscription service layer method #1
    - adding a service layer method to check if we have an ongoing
      cmsubscription or not.
    - test scenario for the same.
    - Other service layer method to be introduced in the subsequent patches.
    
    Issue-ID: CPS-2042
    Change-Id: I33637138457a0c5d49b80dd4967057bd5bfa69d4
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/cmsubscription/service/CmSubscriptionServiceImpl.java
@@ -0,0 +1,57 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.cmsubscription.service;
+
+import java.util.Collection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.api.CpsDataService;
+import org.onap.cps.ncmp.api.impl.operations.DatastoreType;
+import org.onap.cps.spi.FetchDescendantsOption;
+import org.onap.cps.spi.model.DataNode;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CmSubscriptionServiceImpl implements CmSubscriptionService {
+
+    private static final String IS_ONGOING_CM_SUBSCRIPTION_CPS_PATH_QUERY = """
+            /datastores/datastore[@name='%s']/cm-handles/cm-handle[@id='%s']/filters/filter[@xpath='%s']""";
+
+    private final CpsDataService cpsDataService;
+
+    @Override
+    public boolean isOngoingCmSubscription(final DatastoreType datastoreType, final String cmHandleId,
+            final String xpath) {
+        final String isOngoingCmSubscriptionCpsPathQuery =
+                IS_ONGOING_CM_SUBSCRIPTION_CPS_PATH_QUERY.formatted(datastoreType.getDatastoreName(), cmHandleId,
+                        escapeQuotesByDoublingThem(xpath));
+        final Collection<DataNode> existingNodes =
+                cpsDataService.getDataNodes(NCMP_DATASPACE_NAME, CM_SUBSCRIPTIONS_ANCHOR_NAME,
+                        isOngoingCmSubscriptionCpsPathQuery, FetchDescendantsOption.OMIT_DESCENDANTS);
+        return !existingNodes.isEmpty();
+    }
+
+    private static String escapeQuotesByDoublingThem(final String inputXpath) {
+        return inputXpath.replace("'", "''");
+    }
+}
