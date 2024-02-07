08d92baf8811f54529ca957047c193ae5db8ca14
mpriyank <priyank.maheshwari@est.tech>
2024-02-07 14:55:03 +0000

    CmSubscription minor code refactoring
    - As per discusstion , refactoring the naming of the classes and caches
    - Combining the validation service with CmSubscriptionPersistence
      service
    - removed dmiCmNotificationScopeFilter class and the attributes are part
      of Predicates only
    
    Issue-ID: CPS-2078
    Change-Id: Ic2b3ee28a150f3ae6e0911e88296f01e22604ef5
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/api/impl/events/cmsubscription/service/CmNotificationSubscriptionPersistenceServiceImplSpec.groovy
@@ -0,0 +1,67 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2024 Nordix Foundation.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.cmsubscription.service
+
+
+import org.onap.cps.api.CpsQueryService
+import org.onap.cps.ncmp.api.impl.operations.DatastoreType
+import org.onap.cps.spi.FetchDescendantsOption
+import org.onap.cps.spi.model.DataNode
+import spock.lang.Specification
+
+class CmNotificationSubscriptionPersistenceServiceImplSpec extends Specification {
+
+    def mockCpsQueryService = Mock(CpsQueryService)
+
+    def objectUnderTest = new CmNotificationSubscriptionPersistenceServiceImpl(mockCpsQueryService)
+
+    def 'Check ongoing cm subscription #scenario'() {
+        given: 'a valid cm subscription query'
+            def cpsPathQuery = "/datastores/datastore[@name='ncmp-datastore:passthrough-running']/cm-handles/cm-handle[@id='ch-1']/filters/filter[@xpath='/cps/path']";
+        and: 'datanodes optionally returned'
+            1 * mockCpsQueryService.queryDataNodes('NCMP-Admin', 'cm-data-subscriptions',
+                cpsPathQuery, FetchDescendantsOption.OMIT_DESCENDANTS) >> dataNode
+        when: 'we check for an ongoing cm subscription'
+            def response = objectUnderTest.isOngoingCmNotificationSubscription(DatastoreType.PASSTHROUGH_RUNNING, 'ch-1', '/cps/path')
+        then: 'we get expected response'
+            assert response == isOngoingCmSubscription
+        where: 'following scenarios are used'
+            scenario                  | dataNode                                                               || isOngoingCmSubscription
+            'valid datanodes present' | [new DataNode(xpath: '/cps/path', leaves: ['subscribers': ['sub-1']])] || true
+            'no datanodes present'    | []                                                                     || false
+    }
+
+    def 'Get ongoing cm subscription #scenario'() {
+        given: 'a valid cm subscription query'
+            def cpsPathQuery = "/datastores/datastore[@name='ncmp-datastore:passthrough-running']/cm-handles/cm-handle[@id='ch-1']/filters/filter[@xpath='/cps/path']";
+        and: 'datanodes optionally returned'
+            1 * mockCpsQueryService.queryDataNodes('NCMP-Admin', 'cm-data-subscriptions',
+                cpsPathQuery, FetchDescendantsOption.OMIT_DESCENDANTS) >> dataNode
+        when: 'we check for ongoing cm notification subscription'
+            def response = objectUnderTest.getOngoingCmNotificationSubscription(DatastoreType.PASSTHROUGH_RUNNING, 'ch-1', '/cps/path')
+        then: 'we get expected response'
+            assert response == ongoingCmNotificationSubscription as Set
+        where: 'following scenarios are used'
+            scenario                  | dataNode                                                                        || ongoingCmNotificationSubscription
+            'valid datanodes present' | [new DataNode(xpath: '/cps/path', leaves: ['subscribers': ['sub-1', 'sub-2']])] || ['sub-1', 'sub-2']
+            'no datanodes present'    | []                                                                              || []
+    }
+}
