8008cde8cf7c578092571ddd51c329b4ee177381
mpriyank <priyank.maheshwari@est.tech>
2024-03-13 09:29:06 +0000

    EventDateTimeFormatter to cps-service
    - Moving this class to cps-service layer as it has to be used for
      CloudEvent support for cps-core as well
    - added dependency of commons-lang
    
    Issue-ID: CPS-2157
    Change-Id: Ic21bff8250e3e49805fd1e629ea5370e16b8c6eb
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-service/src/main/java/org/onap/cps/utils/EventDateTimeFormatter.java
+++ cps-service/src/main/java/org/onap/cps/utils/EventDateTimeFormatter.java
@@ -1,6 +1,6 @@
 /*
  * ============LICENSE_START=======================================================
- * Copyright (C) 2022-2023 Nordix Foundation
+ * Copyright (C) 2024 Nordix Foundation
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.cps.ncmp.api.impl.utils;
+package org.onap.cps.utils;
 
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
