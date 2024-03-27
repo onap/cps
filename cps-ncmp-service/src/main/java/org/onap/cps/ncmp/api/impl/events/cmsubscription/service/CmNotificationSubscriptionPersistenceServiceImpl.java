1e50fead78dd92e56e265343be0ec0a28f798602
emaclee <lee.anjella.macabuhay@est.tech>
2024-03-27 14:54:50 +0000

    Fix code smell
    	- change string to formatted
    
    Issue-ID: CPS-475
    Change-Id: I59271c63f244c4d6684b145b1afe2356668e6fd8
    Signed-off-by: emaclee <lee.anjella.macabuhay@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/cmsubscription/service/CmNotificationSubscriptionPersistenceServiceImpl.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/cmsubscription/service/CmNotificationSubscriptionPersistenceServiceImpl.java
@@ -109,14 +109,8 @@ public class CmNotificationSubscriptionPersistenceServiceImpl implements CmNotif
 
     private void addNewSubscriptionViaDatastore(final DatastoreType datastoreType, final String cmHandleId,
                                                 final String xpath, final String newSubscriptionId) {
-        final String parentXpathFormat = "/datastores/datastore[@name='%s']/cm-handles";
-        String parentXpath = "";
-        if (datastoreType == PASSTHROUGH_RUNNING) {
-            parentXpath = parentXpathFormat.formatted("ncmp-datastore:passthrough-running");
-        } else {
-            parentXpath = parentXpathFormat.formatted("ncmp-datastore:passthrough-operational");
-        }
-
+        final String parentXpath = "/datastores/datastore[@name='%s']/cm-handles"
+                .formatted(datastoreType.getDatastoreName());
         final String updatedJson = String.format("{\"cm-handle\":[{\"id\":\"%s\",\"filters\":{\"filter\":"
                 + "[{\"xpath\":\"%s\",\"subscriptionIds\":[\"%s\"]}]}}]}", cmHandleId, xpath, newSubscriptionId);
         cpsDataService.saveData(NCMP_DATASPACE_NAME, SUBSCRIPTION_ANCHOR_NAME, parentXpath, updatedJson,
