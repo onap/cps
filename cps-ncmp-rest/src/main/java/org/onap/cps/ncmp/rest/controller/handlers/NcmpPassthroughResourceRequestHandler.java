ea529226739fca8d1c5318ad5acdd58eefb9db1f
ToineSiebelink <toine.siebelink@est.tech>
2024-04-09 18:39:51 +0100

    Introduce and handle Operation Too Large Exception for batch operations
    Issue-ID: CPS-2164
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>
    Change-Id: Iec05d2013be4f971309f0e75d84dc5d0936eb8ef

--- cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/controller/handlers/NcmpPassthroughResourceRequestHandler.java
+++ cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/controller/handlers/NcmpPassthroughResourceRequestHandler.java
@@ -33,6 +33,7 @@ import org.onap.cps.ncmp.api.impl.operations.OperationType;
 import org.onap.cps.ncmp.api.models.CmResourceAddress;
 import org.onap.cps.ncmp.api.models.DataOperationRequest;
 import org.onap.cps.ncmp.rest.exceptions.OperationNotSupportedException;
+import org.onap.cps.ncmp.rest.exceptions.PayloadTooLargeException;
 import org.onap.cps.ncmp.rest.executor.CpsNcmpTaskExecutor;
 import org.onap.cps.ncmp.rest.util.TopicValidator;
 import org.springframework.http.ResponseEntity;
@@ -44,6 +45,8 @@ public class NcmpPassthroughResourceRequestHandler extends NcmpDatastoreRequestH
     private final NetworkCmProxyDataService networkCmProxyDataService;
 
     private static final Object noReturn = null;
+
+    private static final int MAXIMUM_CM_HANDLES_PER_OPERATION = 50;
 
     /**
      * Constructor.
@@ -101,16 +104,19 @@ public class NcmpPassthroughResourceRequestHandler extends NcmpDatastoreRequestH
     }
 
     private void validateDataOperationRequest(final String topicParamInQuery,
-                                              final DataOperationRequest
-                                                  dataOperationRequest) {
+                                              final DataOperationRequest dataOperationRequest) {
         TopicValidator.validateTopicName(topicParamInQuery);
         dataOperationRequest.getDataOperationDefinitions().forEach(dataOperationDetail -> {
             if (OperationType.fromOperationName(dataOperationDetail.getOperation()) != READ) {
                 throw new OperationNotSupportedException(
                     dataOperationDetail.getOperation() + " operation not yet supported");
-            } else if (DatastoreType.fromDatastoreName(dataOperationDetail.getDatastore()) == OPERATIONAL) {
+            }
+            if (DatastoreType.fromDatastoreName(dataOperationDetail.getDatastore()) == OPERATIONAL) {
                 throw new InvalidDatastoreException(dataOperationDetail.getDatastore()
                     + " datastore is not supported");
+            }
+            if (dataOperationDetail.getCmHandleIds().size() > MAXIMUM_CM_HANDLES_PER_OPERATION) {
+                throw new PayloadTooLargeException(dataOperationDetail.getCmHandleIds().size());
             }
         });
     }
