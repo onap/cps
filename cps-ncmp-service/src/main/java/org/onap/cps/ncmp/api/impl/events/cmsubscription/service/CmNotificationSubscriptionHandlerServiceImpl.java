66c47c15770b594c544fb24126587d44630cd2bb
seanbeirne <sean.beirne@est.tech>
2024-04-16 09:56:59 +0100

    Reject create request with duplicated subscriptionId
    Issue-ID: CPS-2184
    Change-Id: I1bddb02239e6a2d3f47aa731eb24a9f1f64e63b5
    Signed-off-by: emaclee <lee.anjella.macabuhay@est.tech>
    Signed-off-by: seanbeirne <sean.beirne@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/events/cmsubscription/service/CmNotificationSubscriptionHandlerServiceImpl.java
@@ -0,0 +1,69 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.api.impl.events.cmsubscription.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.ncmp.api.impl.events.cmsubscription.CmNotificationSubscriptionNcmpOutEventProducer;
+import org.onap.cps.ncmp.api.impl.events.cmsubscription.DmiCmNotificationSubscriptionCacheHandler;
+import org.onap.cps.ncmp.api.impl.events.cmsubscription.mapper.CmNotificationSubscriptionNcmpOutEventMapper;
+import org.onap.cps.ncmp.events.cmnotificationsubscription_merge1_0_0.client_to_ncmp.CmNotificationSubscriptionNcmpInEvent;
+import org.onap.cps.ncmp.events.cmnotificationsubscription_merge1_0_0.client_to_ncmp.Predicate;
+import org.onap.cps.ncmp.events.cmsubscription_merge1_0_0.ncmp_to_client.CmNotificationSubscriptionNcmpOutEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CmNotificationSubscriptionHandlerServiceImpl implements CmNotificationSubscriptionHandlerService {
+
+    private final CmNotificationSubscriptionPersistenceService cmNotificationSubscriptionPersistenceService;
+    private final CmNotificationSubscriptionNcmpOutEventMapper cmNotificationSubscriptionNcmpOutEventMapper;
+    private final CmNotificationSubscriptionNcmpOutEventProducer cmNotificationSubscriptionNcmpOutEventProducer;
+    private final DmiCmNotificationSubscriptionCacheHandler dmiCmNotificationSubscriptionCacheHandler;
+
+    @Override
+    public void processCreateSubscriptionRequest(
+        final CmNotificationSubscriptionNcmpInEvent cmNotificationSubscriptionNcmpInEvent) {
+        final String subscriptionId = cmNotificationSubscriptionNcmpInEvent.getData().getSubscriptionId();
+        final List<Predicate> predicates = cmNotificationSubscriptionNcmpInEvent.getData().getPredicates();
+
+        if (cmNotificationSubscriptionPersistenceService.isUniqueSubscriptionId(subscriptionId)) {
+            dmiCmNotificationSubscriptionCacheHandler.add(subscriptionId, predicates);
+        } else {
+            final List<String> subscriptionTargetFilters = predicates.stream().flatMap(
+                predicate -> predicate.getTargetFilter().stream()).toList();
+            rejectSubscriptionCreateRequest(subscriptionId, subscriptionTargetFilters);
+        }
+
+    }
+
+    private void rejectSubscriptionCreateRequest(final String subscriptionId,
+                                                final List<String> subscriptionTargetFilters) {
+        final CmNotificationSubscriptionNcmpOutEvent cmNotificationSubscriptionNcmpOutEvent =
+            cmNotificationSubscriptionNcmpOutEventMapper
+                .toCmNotificationSubscriptionNcmpOutEventForRejectedCreateRequest(subscriptionId,
+                    subscriptionTargetFilters);
+        cmNotificationSubscriptionNcmpOutEventProducer.publishCmNotificationSubscriptionNcmpOutEvent(subscriptionId,
+            "subscriptionCreateResponse", cmNotificationSubscriptionNcmpOutEvent, false);
+    }
+}
