353fb387296ed3027bf6bca368435f9e6afc7de3
danielhanrahan <daniel.hanrahan@est.tech>
2024-04-29 16:16:42 +0100

    Add k6 performance tests for NCMP
    This commit adds K6 tests for NCMP functionalities:
    - Registration of 20000 CM-handles
    - Synchronous single CM-handle passthrough read operation
    - CM-handle and ID searches for no filter, module, and public property
    - De-registration of 20000 CM-handles
    Also includes a README and shell scripts to run all tests.
    
    Issue-ID: CPS-2199
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: Ia2e59674057ed28f5f9ceed9465133f7c0ce8318

---
+++ k6-tests/run-k6-tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright 2024 Nordix Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -o errexit  # Exit on most errors
+set -o nounset  # Disallow expansion of unset variables
+set -o pipefail # Use last non-zero exit code in a pipeline
+#set -o xtrace   # Uncomment for debugging
+
+on_exit() {
+  rc=$?
+  ./teardown.sh
+  popd
+  echo "TEST FAILURES: $rc"
+  exit $rc
+}
+trap on_exit EXIT
+
+pushd "$(dirname "$0")" || exit 1
+
+./setup.sh
+./ncmp/run-all-tests.sh
+NCMP_RESULT=$?
+
+# Note that the final steps are done in on_exit function after this exit!
+exit $NCMP_RESULT
