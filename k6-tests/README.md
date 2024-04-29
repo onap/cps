0d2ade3c2976c5730c9dba13c3409b02cd51b306
danielhanrahan <daniel.hanrahan@est.tech>
2024-04-29 16:16:42 +0100

    Add k6 performance tests for NCMP
    This commit adds K6 tests for NCMP functionalities:
    - Registration of 20000 CM-handles
    - Synchronous single CM-handle passthrough read operation
    - CM-handle and ID searches for no filter, module, and public property
    - De-registration of 20000 CM-handles
    - A mixed load test
    Also includes a README and shell scripts to run all tests.
    
    Issue-ID: CPS-2199
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: Ia2e59674057ed28f5f9ceed9465133f7c0ce8318

---
+++ k6-tests/README.md
@@ -0,0 +1,25 @@
+# k6 tests
+
+[k6](https://k6.io/) is used for performance tests.
+k6 tests are written in JavaScript.
+
+## k6 installation
+Follow the instructions in the [k6 installation guide](https://grafana.com/docs/k6/latest/set-up/install-k6/)
+to get started.
+
+## Running the k6 test suites
+Simply run the main script. (The script assumes k6 and docker-compose have been installed).
+```shell
+./run-k6-tests.sh
+```
+
+## Running k6 tests manually
+Before running tests, ensure CPS/NCMP is running:
+```shell
+docker-compose -f docker-compose/docker-compose.yml --profile dmi-stub up
+```
+
+To run an individual test from command line, use
+```shell
+k6 run ncmp/1-create-cmhandles.js
+```
---
+++ k6-tests/ncmp/2-wait-for-cmhandles-to-be-ready.js
@@ -0,0 +1,68 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+import http from 'k6/http';
+import { sleep, fail } from 'k6';
+import { NCMP_BASE_URL, TOTAL_CM_HANDLES } from './utils.js';
+
+export const options = {
+  vus: 1,
+  iterations: 1,
+  thresholds: {
+    http_req_failed: ['rate==0'],
+    iteration_duration: ['max <= 300_000'], // 5 minutes
+  },
+};
+
+export default function() {
+    waitForCmHandlesToBeReady(TOTAL_CM_HANDLES);
+}
+
+function waitForCmHandlesToBeReady(totalCmHandles) {
+    const timeOutInSeconds = 6 * 60;
+    const pollingIntervalInSeconds = 10;
+    const maxRetries = timeOutInSeconds / pollingIntervalInSeconds;
+    let cmHandlesReady = 0;
+    for (let currentTry = 0; currentTry <= maxRetries; currentTry++) {
+        sleep(pollingIntervalInSeconds);
+        try {
+            cmHandlesReady = getNumberOfReadyCmHandles();
+        } catch (e) {
+            console.log(`Error: ${e}`);
+        }
+        console.log(`${cmHandlesReady} CM-handles are READY`);
+        if (cmHandlesReady === totalCmHandles) {
+            return;
+        }
+    }
+    fail('Timed out waiting for CM-handles to be READY');
+}
+
+function getNumberOfReadyCmHandles() {
+    let cmHandlesReady = 0;
+    const endpointUrl = NCMP_BASE_URL + '/cps/api/v2/dataspaces/NCMP-Admin/anchors/ncmp-dmi-registry/node?xpath=/dmi-registry&descendants=all';
+    const jsonData = http.get(endpointUrl).json();
+    for (let cmhandle of jsonData[0]["dmi-reg:dmi-registry"]["cm-handles"]) {
+        if (cmhandle['state']['cm-handle-state'] === 'READY') {
+            cmHandlesReady++;
+        }
+    }
+    return cmHandlesReady;
+}
