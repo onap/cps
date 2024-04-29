c0b6f3ad5ed529e4f3261cbf6eed44faec3883a4
danielhanrahan <daniel.hanrahan@est.tech>
2024-04-29 16:16:42 +0100

    Add k6 performance tests for NCMP
    This commit adds K6 tests for NCMP functionalities:
    - Registration of 20000 CM-handles
    - Synchronous single CM-handle passthrough read operation
    - CM-handle and ID searches for no filter, module, and public property
    - De-registration of 20000 CM-handles
    - A mixed load test
    Also includes a README and shell scripts to run all tests.
    
    Issue-ID: CPS-2199
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: Ia2e59674057ed28f5f9ceed9465133f7c0ce8318

---
+++ k6-tests/ncmp/2-wait-for-cmhandles-to-be-ready.js
@@ -0,0 +1,64 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+import http from 'k6/http';
+import { sleep, fail } from 'k6';
+import { NCMP_BASE_URL, TOTAL_CM_HANDLES } from './utils.js';
+
+export const options = {
+  vus: 1,
+  iterations: 1,
+  thresholds: {
+    http_req_failed: ['rate == 0'],
+    iteration_duration: ['max <= 300_000'], // 5 minutes
+  },
+};
+
+export default function() {
+    waitForCmHandlesToBeReady(TOTAL_CM_HANDLES);
+}
+
+function waitForCmHandlesToBeReady(totalCmHandles) {
+    const timeOutInSeconds = 6 * 60;
+    const pollingIntervalInSeconds = 10;
+    const maxRetries = Math.ceil(timeOutInSeconds / pollingIntervalInSeconds);
+    let cmHandlesReady = 0;
+    for (let currentTry = 0; currentTry <= maxRetries; currentTry++) {
+        sleep(pollingIntervalInSeconds);
+        try {
+            cmHandlesReady = getNumberOfReadyCmHandles();
+        } catch (error) {
+            console.error(`Attempt ${currentTry + 1} - Error fetching CM handles: ${error.message}`);
+        }
+        console.log(`Attempt ${currentTry + 1} - ${cmHandlesReady}/${totalCmHandles} CM handles are READY`);
+        if (cmHandlesReady === totalCmHandles) {
+            console.log(`All ${totalCmHandles} CM handles are READY`);
+            return;
+        }
+    }
+    fail(`Timed out after ${timeoutInSeconds} seconds waiting for ${totalCmHandles} CM handles to be READY`);
+}
+
+function getNumberOfReadyCmHandles() {
+    const endpointUrl = `${NCMP_BASE_URL}/cps/api/v2/dataspaces/NCMP-Admin/anchors/ncmp-dmi-registry/node?xpath=/dmi-registry&descendants=all`;
+    const jsonData = http.get(endpointUrl).json();
+    const cmHandles = jsonData[0]["dmi-reg:dmi-registry"]["cm-handles"];
+    return cmHandles.filter(cmhandle => cmhandle['state']['cm-handle-state'] === 'READY').length;
+}
