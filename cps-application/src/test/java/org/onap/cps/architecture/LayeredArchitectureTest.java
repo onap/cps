f7873bd1a65592539a26e49ba84cd3c90cdca252
mpriyank <priyank.maheshwari@est.tech>
2024-05-07 15:33:19 +0100

    Conditional cps change events
    - introduced a parameter to control the cps core change event
      notifications. we should be able to disable these notifications
      without affecting other notification flows.
    - fixed the LayeredArchitectureTest as we are accessing the Anchor model
      in the events package now.
    
    Issue-ID: CPS-2213
    Change-Id: Id875925bc14de1cc6e8fa3193c0df470e09fe43f
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-application/src/test/java/org/onap/cps/architecture/LayeredArchitectureTest.java
+++ cps-application/src/test/java/org/onap/cps/architecture/LayeredArchitectureTest.java
@@ -1,6 +1,6 @@
 /*
  *  ============LICENSE_START=======================================================
- *  Copyright (C) 2021 Nordix Foundation
+ *  Copyright (C) 2021-2024 Nordix Foundation
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -45,6 +45,7 @@ public class LayeredArchitectureTest {
     private static final String CPS_UTILS_PACKAGE = "org.onap.cps.utils..";
     private static final String NCMP_INIT_PACKAGE = "org.onap.cps.ncmp.init..";
     private static final String CPS_CACHE_PACKAGE = "org.onap.cps.cache..";
+    private static final String CPS_EVENTS_PACKAGE = "org.onap.cps.events..";
 
     @ArchTest
     static final ArchRule restControllerShouldOnlyDependOnRestController =
@@ -57,7 +58,7 @@ public class LayeredArchitectureTest {
             .or().resideInAPackage(SPI_SERVICE_PACKAGE).should().onlyHaveDependentClassesThat()
             .resideInAnyPackage(REST_CONTROLLER_PACKAGE, API_SERVICE_PACKAGE, SPI_SERVICE_PACKAGE, NCMP_REST_PACKAGE,
                 NCMP_SERVICE_PACKAGE, YANG_SCHEMA_PACKAGE, NOTIFICATION_PACKAGE, CPS_UTILS_PACKAGE, NCMP_INIT_PACKAGE,
-                CPS_CACHE_PACKAGE));
+                CPS_CACHE_PACKAGE, CPS_EVENTS_PACKAGE));
 
 
     @ArchTest
