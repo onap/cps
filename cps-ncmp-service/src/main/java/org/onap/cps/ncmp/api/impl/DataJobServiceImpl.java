2a6deffb41ac91bab73bb7abbbac311814846ce9
leventecsanyi <levente.csanyi@est.tech>
2024-05-09 14:37:01 +0200

    Spliting a data-job into sub-jobs forward those to dmi
        - algorithm for create a MVP sub-job request
        - added new OperationType 'ADD'
    
    Issue-ID: CPS-2142
    Change-Id: I2a647badb6b8aae85724e3c3b291f8f4b91b6ee1
    Signed-off-by: leventecsanyi <levente.csanyi@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/DataJobServiceImpl.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/api/impl/DataJobServiceImpl.java
@@ -20,14 +20,32 @@
 
 package org.onap.cps.ncmp.api.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.onap.cps.ncmp.api.DataJobService;
+import org.onap.cps.ncmp.api.impl.client.DmiRestClient;
+import org.onap.cps.ncmp.api.impl.inventory.InventoryPersistence;
+import org.onap.cps.ncmp.api.impl.operations.OperationType;
 import org.onap.cps.ncmp.api.models.datajob.DataJobMetadata;
 import org.onap.cps.ncmp.api.models.datajob.DataJobReadRequest;
 import org.onap.cps.ncmp.api.models.datajob.DataJobWriteRequest;
+import org.onap.cps.ncmp.api.models.datajob.ProducerKey;
+import org.onap.cps.ncmp.api.models.datajob.WriteOperation;
+import org.onap.cps.spi.model.DataNode;
+import org.onap.cps.utils.JsonObjectMapper;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class DataJobServiceImpl implements DataJobService {
+
+    private final InventoryPersistence inventoryPersistence;
+    private final DmiRestClient dmiRestClient;
+    protected final JsonObjectMapper jsonObjectMapper;
+    private static final String SEPARATOR = "/";
 
     @Override
     public void readDataJob(final String dataJobId, final DataJobMetadata dataJobMetadata,
@@ -38,6 +56,18 @@ public class DataJobServiceImpl implements DataJobService {
     @Override
     public void writeDataJob(final String dataJobId, final DataJobMetadata dataJobMetadata,
                              final DataJobWriteRequest dataJobWriteRequest) {
-        log.info("data job id for write operation is: {}", dataJobId);
+        if (!dataJobWriteRequest.data().isEmpty()) {
+            log.info("data job id for write operation is: {}", dataJobId);
+            final WriteOperation writeOperation = dataJobWriteRequest.data().get(0);
+            final DataNode dataNode = inventoryPersistence
+                    .getCmHandleDataNodeByLongestMatchAlternateId(writeOperation.path(), SEPARATOR);
+            final ProducerKey producerKey = new ProducerKey(dataNode.getLeaves().get("id") + "_"
+                    + dataNode.getLeaves().get("data-producer-identifier"), writeOperation);
+            final String dmiResourceUrl = String.format("/v1/dataJobWrite/%s", dataJobId);
+            dmiRestClient.postOperationWithJsonData(dmiResourceUrl, jsonObjectMapper.asJsonString(producerKey),
+                        OperationType.ADD, null);
+        } else {
+            log.info("Data job request is empty for data job id: {}.", dataJobId);
+        }
     }
 }
