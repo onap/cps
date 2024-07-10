ef67474339dbe5773be1e9926d3102d4cf7293c1
ToineSiebelink <toine.siebelink@est.tech>
2024-07-10 17:34:13 +0100

    Policy Executor Feature Toggle
    - defined config parameters for feature toggle and server details
    - log request details when feature enabled
    - improved JavaDoc in Controller
    - improved configuration properties check  in HttpClientConfigurationSpec
    
    Issue-ID: CPS-2311
    Change-Id: I1ff4bd3592ce2570ac74f9ec6e62b75001cb611a
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/data/PolicyExecutor.java
@@ -0,0 +1,65 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.impl.data;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.ncmp.api.data.models.OperationType;
+import org.onap.cps.ncmp.impl.inventory.models.YangModelCmHandle;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PolicyExecutor {
+
+    @Value("${ncmp.policy-executor.enabled:false}")
+    private boolean enabled;
+
+    @Value("${ncmp.policy-executor.server.address:http://policy-executor}")
+    private String serverAddress;
+
+    @Value("${ncmp.policy-executor.server.port:8080}")
+    private String serverPort;
+
+    private final String PAYLOAD_TYPE_PREFIX = "cm_";
+
+    public void checkPermission(final YangModelCmHandle yangModelCmHandle,
+                                final OperationType operationType,
+                                final String authorization,
+                                final String resourceIdentifier,
+                                final String changeRequestAsJson) {
+        if (enabled) {
+            final String payloadType = PAYLOAD_TYPE_PREFIX + operationType.getOperationName();
+            log.info("Policy Executor Enabled");
+            log.info("=======================");
+            log.info("Address               : {}", serverAddress);
+            log.info("Port                  : {}", serverPort);
+            log.info("Authorization         : {}", authorization);
+            log.info("Payload Type          : {}", payloadType);
+            log.info("Target FDN            : {}", yangModelCmHandle.getAlternateId());
+            log.info("CM Handle Id          : {}", yangModelCmHandle.getId());
+            log.info("Resource Identifier   : {}", resourceIdentifier);
+            log.info("Change Request (json) : {}", changeRequestAsJson);
+        }
+    }
+}
