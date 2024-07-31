4e9ad13bdd90dbe18b3893314981829be6b1976e
sourabh_sourabh <sourabh.sourabh@est.tech>
2024-07-31 17:19:19 +0100

    Updating CM Data Notification Event Schema
    - Updated "avc-event-schema" value attribute to accept type object.
    - Modified cm avc event consumer to not wrap received event into another cloud event using "CloudEventBuilder.from"
    
    Issue-ID: CPS-2299
    Change-Id: I3b2acd53a3939d992b337e98debb7f62f3a528dd
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventConsumer.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventConsumer.java
@@ -21,7 +21,6 @@
 package org.onap.cps.ncmp.impl.cmnotificationsubscription.cmavc;
 
 import io.cloudevents.CloudEvent;
-import io.cloudevents.core.builder.CloudEventBuilder;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -56,11 +55,9 @@ public class CmAvcEventConsumer {
             containerFactory = "cloudEventConcurrentKafkaListenerContainerFactory")
     public void consumeAndForward(
             final ConsumerRecord<String, CloudEvent> cmAvcEventAsConsumerRecord) {
-        log.debug("Consuming AVC event {} ...", cmAvcEventAsConsumerRecord.value());
+        final CloudEvent outgoingAvcEvent = cmAvcEventAsConsumerRecord.value();
+        log.debug("Consuming AVC event {} ...", outgoingAvcEvent);
         final String newEventId = UUID.randomUUID().toString();
-        final CloudEvent outgoingAvcEvent =
-                CloudEventBuilder.from(cmAvcEventAsConsumerRecord.value()).withId(newEventId)
-                        .build();
         eventsPublisher.publishCloudEvent(cmEventsTopicName, newEventId, outgoingAvcEvent);
     }
 }
