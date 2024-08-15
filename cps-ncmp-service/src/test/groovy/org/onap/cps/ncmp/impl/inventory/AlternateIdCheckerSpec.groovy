d81209860b73e90d9d25ec6155e433ef2a221927
danielhanrahan <daniel.hanrahan@est.tech>
2024-08-15 14:33:39 +0100

    Refactoring alternate ID checker (CPS-2366 #1)
    Issue-ID: CPS-2366
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: I07228a130ebdab0e2782e54255b5e8cc34c8d77e

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/inventory/AlternateIdCheckerSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/inventory/AlternateIdCheckerSpec.groovy
@@ -24,47 +24,14 @@ import org.onap.cps.ncmp.api.inventory.models.NcmpServiceCmHandle
 import org.onap.cps.ncmp.impl.inventory.models.YangModelCmHandle
 import org.onap.cps.spi.exceptions.DataNodeNotFoundException
 import org.onap.cps.spi.model.DataNode
-import org.onap.cps.spi.model.DataNodeBuilder
 import spock.lang.Specification
 
 class AlternateIdCheckerSpec extends Specification {
 
     def mockInventoryPersistenceService = Mock(InventoryPersistence)
-    def someDataNode = new DataNodeBuilder().build()
     def dataNodeFoundException = new DataNodeNotFoundException('', '')
 
     def objectUnderTest = new AlternateIdChecker(mockInventoryPersistenceService)
-
-    def 'Check new cm handle with new alternate id.'() {
-        given: 'inventory persistence can not find cm handle id'
-            mockInventoryPersistenceService.getYangModelCmHandle('ch 1') >> {throw dataNodeFoundException}
-        and: 'inventory persistence can not find alternate id'
-            mockInventoryPersistenceService.getCmHandleDataNodeByAlternateId('alternate id') >> {throw dataNodeFoundException}
-        expect: 'mapping can be added'
-             assert objectUnderTest.canApplyAlternateId('ch 1', 'alternate id')
-    }
-
-    def 'Check new cm handle with used alternate id.'() {
-        given: 'inventory persistence can not find cm handle id'
-            mockInventoryPersistenceService.getYangModelCmHandle('ch 1') >> {throw dataNodeFoundException}
-        and: 'inventory persistence can find alternate id'
-            mockInventoryPersistenceService.getCmHandleDataNodeByAlternateId('alternate id') >> { someDataNode }
-        expect: 'mapping can not be added'
-            assert objectUnderTest.canApplyAlternateId('ch 1', 'alternate id') == false
-    }
-
-    def 'Check for existing cm handle with #currentAlternateId.'() {
-        given: 'a cm handle with the #currentAlternateId'
-            def yangModelCmHandle = new YangModelCmHandle(alternateId: currentAlternateId)
-        and: 'inventory service finds the cm handle'
-            mockInventoryPersistenceService.getYangModelCmHandle('my cm handle') >> yangModelCmHandle
-        expect: 'add mapping returns expected result'
-            assert canAdd == objectUnderTest.canApplyAlternateId('my cm handle', 'same alternate id')
-        where: 'following alternate ids is used'
-            currentAlternateId   || canAdd
-            'same alternate id'  || true
-            'other alternate id' || false
-    }
 
     def 'Check a batch of created cm handles with #scenario.'() {
         given: 'a batch of 2 new cm handles alternate id ids #alt1 and #alt2'
@@ -78,12 +45,13 @@ class AlternateIdCheckerSpec extends Specification {
         then: 'the result contains ids of the rejected cm handles'
             assert result == expectedRejectedCmHandleIds
         where: 'the following alternate ids are used'
-            scenario                          | alt1   | alt2   | altAlreadyInDb  || expectedRejectedCmHandleIds
-            'blank alternate ids'             | ''     | ''     | ['dont matter'] || []
-            'null alternate ids'              | null   | null   | ['dont matter'] || []
-            'new alternate ids'               | 'fdn1' | 'fdn2' | ['other fdn']   || []
-            'one already used alternate id'   | 'fdn1' | 'fdn2' | ['fdn1']        || ['ch-1']
-            'duplicate alternate id in batch' | 'fdn1' | 'fdn1' | ['dont matter'] || ['ch-2']
+            scenario                          | alt1   | alt2   | altAlreadyInDb   || expectedRejectedCmHandleIds
+            'blank alternate ids'             | ''     | ''     | ['dont matter']  || []
+            'null alternate ids'              | null   | null   | ['dont matter']  || []
+            'new alternate ids'               | 'fdn1' | 'fdn2' | ['other fdn']    || []
+            'one already used alternate id'   | 'fdn1' | 'fdn2' | ['fdn1']         || ['ch-1']
+            'two already used alternate ids'  | 'fdn1' | 'fdn2' | ['fdn1', 'fdn2'] || ['ch-1', 'ch-2']
+            'duplicate alternate id in batch' | 'fdn1' | 'fdn1' | ['dont matter']  || ['ch-2']
     }
 
     def 'Check a batch of updates to existing cm handles with #scenario.'() {
