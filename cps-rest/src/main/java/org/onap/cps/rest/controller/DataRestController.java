11f23e079dcaa718dde1429962882ca62b21bcb7
Rudrangi Anupriya <ra00745022@techmahindra.com>
2024-10-07 15:23:34 +0530

    XML content support on get a node
    Issue-ID: CPS-2280
    Change-Id: Ibe7ffb66ccbb03703626132c6d5c2eade0e7ab4b
    Signed-off-by: Rudrangi Anupriya <ra00745022@techmahindra.com>

--- cps-rest/src/main/java/org/onap/cps/rest/controller/DataRestController.java
+++ cps-rest/src/main/java/org/onap/cps/rest/controller/DataRestController.java
@@ -48,6 +48,7 @@ import org.onap.cps.utils.ContentType;
 import org.onap.cps.utils.DataMapUtils;
 import org.onap.cps.utils.JsonObjectMapper;
 import org.onap.cps.utils.PrefixResolver;
+import org.onap.cps.utils.XmlFileUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -124,8 +125,9 @@ public class DataRestController implements CpsDataApi {
     @Timed(value = "cps.data.controller.datanode.get.v2",
             description = "Time taken to get data node")
     public ResponseEntity<Object> getNodeByDataspaceAndAnchorV2(final String dataspaceName, final String anchorName,
-                                                                final String xpath,
+                                                                final String contentTypeInHeader, final String xpath,
                                                                 final String fetchDescendantsOptionAsString) {
+        final ContentType contentType = getContentTypeFromHeader(contentTypeInHeader);
         final FetchDescendantsOption fetchDescendantsOption =
                 FetchDescendantsOption.getFetchDescendantsOption(fetchDescendantsOptionAsString);
         final Collection<DataNode> dataNodes = cpsDataService.getDataNodes(dataspaceName, anchorName, xpath,
@@ -137,7 +139,7 @@ public class DataRestController implements CpsDataApi {
             final Map<String, Object> dataMap = DataMapUtils.toDataMapWithIdentifier(dataNode, prefix);
             dataMaps.add(dataMap);
         }
-        return new ResponseEntity<>(jsonObjectMapper.asJsonString(dataMaps), HttpStatus.OK);
+        return buildResponseEntity(dataMaps, contentType);
     }
 
     @Override
@@ -216,6 +218,17 @@ public class DataRestController implements CpsDataApi {
                 cpsDataService.getDeltaByDataspaceAndAnchors(dataspaceName, sourceAnchorName,
                 targetAnchorName, xpath, fetchDescendantsOption);
         return new ResponseEntity<>(jsonObjectMapper.asJsonString(deltaBetweenAnchors), HttpStatus.OK);
+    }
+
+    ResponseEntity<Object> buildResponseEntity(final List<Map<String, Object>> dataMaps,
+                                               final ContentType contentType) {
+        final String responseData;
+        if (contentType == ContentType.XML) {
+            responseData = XmlFileUtils.convertDataMapsToXml(dataMaps);
+        } else {
+            responseData = jsonObjectMapper.asJsonString(dataMaps);
+        }
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
     private static ContentType getContentTypeFromHeader(final String contentTypeInHeader) {
