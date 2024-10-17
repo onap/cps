8ff452360dfa845fd60e88e977ff27fab276f019
leventecsanyi <levente.csanyi@est.tech>
2024-10-17 14:59:49 +0200

    Added arcitecture tests for CPS (REST and service and RI)
      - added Base class and extracted common method for 3pp checks
      - added ArchTests for cps REST and cps service
    
    Issue-ID: CPS-2423
    Change-Id: Icf70b79d1397b002d75ec5c8761775dfd7a6c6d9
    Signed-off-by: leventecsanyi <levente.csanyi@est.tech>

---
+++ cps-application/src/test/java/org/onap/cps/architecture/ArchitectureTestBase.java
@@ -0,0 +1,47 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.architecture;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class ArchitectureTestBase {
+
+    private static final String[] ACCEPTED_3PP_PACKAGES = { "com.fasterxml..",
+                                                            "com.google..",
+                                                            "com.hazelcast..",
+                                                            "edu..",
+                                                            "io.cloudevents..",
+                                                            "io.micrometer..",
+                                                            "io.netty..",
+                                                            "io.swagger..",
+                                                            "jakarta..",
+                                                            "java..",
+                                                            "lombok..",
+                                                            "org.apache..",
+                                                            "org.mapstruct..",
+                                                            "org.slf4j..",
+                                                            "org.springframework..",
+                                                            "reactor.."
+    };
+
+    static String[] commonAndListedPackages(final String... packageIdentifiers) {
+        return ArrayUtils.addAll(ACCEPTED_3PP_PACKAGES, packageIdentifiers);
+    }
+}
