cd4367e092f3cb94e504b0c155cd9aeaf56f2b8a
Rudrangi Anupriya <ra00745022@techmahindra.com>
2024-11-06 22:20:11 +0530

    XML content support on query data node
    Here to bring Support for XML Response Entity In Query DATA NODE
    
    - Add ContentTypeInheadr in cpsQueryV2.yml to support application/xml
    - Add contentTypeInHeader parameter to accept xml  in QueryRestController.java
    - Implement logic to convert dataNodesAsListOfMaps to XML Data
    - written testcase for above changes made
    
    Issue-ID: CPS-2359
    Change-Id: Ieb7eeb66ccbb03703626132c6d5c2eade0e7cb4b
    Signed-off-by: Rudrangi Anupriya <ra00745022@techmahindra.com>

--- cps-rest/src/main/java/org/onap/cps/rest/controller/QueryRestController.java
+++ cps-rest/src/main/java/org/onap/cps/rest/controller/QueryRestController.java
@@ -2,7 +2,7 @@
  *  ============LICENSE_START=======================================================
  *  Copyright (C) 2021-2024 Nordix Foundation
  *  Modifications Copyright (C) 2022 Bell Canada.
- *  Modifications Copyright (C) 2022-2023 TechMahindra Ltd.
+ *  Modifications Copyright (C) 2022-2024 TechMahindra Ltd.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -36,9 +36,11 @@ import org.onap.cps.spi.FetchDescendantsOption;
 import org.onap.cps.spi.PaginationOption;
 import org.onap.cps.spi.model.Anchor;
 import org.onap.cps.spi.model.DataNode;
+import org.onap.cps.utils.ContentType;
 import org.onap.cps.utils.DataMapUtils;
 import org.onap.cps.utils.JsonObjectMapper;
 import org.onap.cps.utils.PrefixResolver;
+import org.onap.cps.utils.XmlFileUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,18 +64,20 @@ public class QueryRestController implements CpsQueryApi {
         final FetchDescendantsOption fetchDescendantsOption = Boolean.TRUE.equals(includeDescendants)
             ? FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS : FetchDescendantsOption.OMIT_DESCENDANTS;
         return executeNodesByDataspaceQueryAndCreateResponse(dataspaceName, anchorName, cpsPath,
-                fetchDescendantsOption);
+                fetchDescendantsOption, ContentType.JSON);
     }
 
     @Override
     @Timed(value = "cps.data.controller.datanode.query.v2",
             description = "Time taken to query data nodes")
     public ResponseEntity<Object> getNodesByDataspaceAndAnchorAndCpsPathV2(final String dataspaceName,
-        final String anchorName, final String cpsPath, final String fetchDescendantsOptionAsString) {
+        final String anchorName, final String contentTypeInHeader, final String cpsPath,
+                                        final String fetchDescendantsOptionAsString) {
+        final ContentType contentType = ContentType.fromString(contentTypeInHeader);
         final FetchDescendantsOption fetchDescendantsOption =
             FetchDescendantsOption.getFetchDescendantsOption(fetchDescendantsOptionAsString);
         return executeNodesByDataspaceQueryAndCreateResponse(dataspaceName, anchorName, cpsPath,
-                fetchDescendantsOption);
+                fetchDescendantsOption, contentType);
     }
 
     @Override
@@ -130,7 +134,8 @@ public class QueryRestController implements CpsQueryApi {
     }
 
     private ResponseEntity<Object> executeNodesByDataspaceQueryAndCreateResponse(final String dataspaceName,
-             final String anchorName, final String cpsPath, final FetchDescendantsOption fetchDescendantsOption) {
+             final String anchorName, final String cpsPath, final FetchDescendantsOption fetchDescendantsOption,
+                                                                                 final ContentType contentType) {
         final Collection<DataNode> dataNodes =
             cpsQueryService.queryDataNodes(dataspaceName, anchorName, cpsPath, fetchDescendantsOption);
         final List<Map<String, Object>> dataNodesAsListOfMaps = new ArrayList<>(dataNodes.size());
@@ -143,6 +148,17 @@ public class QueryRestController implements CpsQueryApi {
             final Map<String, Object> dataMap = DataMapUtils.toDataMapWithIdentifier(dataNode, prefix);
             dataNodesAsListOfMaps.add(dataMap);
         }
-        return new ResponseEntity<>(jsonObjectMapper.asJsonString(dataNodesAsListOfMaps), HttpStatus.OK);
+        return buildResponseEntity(dataNodesAsListOfMaps, contentType);
+    }
+
+    ResponseEntity<Object> buildResponseEntity(final List<Map<String, Object>> dataNodesAsListOfMaps,
+                                               final ContentType contentType) {
+        final String responseData;
+        if (contentType == ContentType.XML) {
+            responseData = XmlFileUtils.convertDataMapsToXml(dataNodesAsListOfMaps);
+        } else {
+            responseData = jsonObjectMapper.asJsonString(dataNodesAsListOfMaps);
+        }
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 }
