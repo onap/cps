1de056c8443d8d21cba9df9551b7672c73c89216
Rudrangi Anupriya <ra00745022@techmahindra.com>
2024-12-10 16:48:20 +0530

    XML content support on Get delta between two anchors within a given dataspace
    Here to bring Support for XML content support on Get delta between two anchors within a given dataspace
    
    - Add deltaReportSampleXml Example in component.yaml
    - Add ContentTypeInheadr in cpsDataV2.yml to support application/xml
    - Add contentTypeInHeader parameter to accept xml  in
        DataRestController.java
    - Implemented Logic to convert deltaReport to XML
    - written testcase for above changes made
    
    Issue-ID: CPS-2452
    Change-Id: Iee7fab66ddcc03703255123c6d5c2eade0e7cb4a
    Signed-off-by: Rudrangi Anupriya <ra00745022@techmahindra.com>

--- cps-service/src/test/groovy/org/onap/cps/utils/XmlFileUtilsSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/utils/XmlFileUtilsSpec.groovy
@@ -23,12 +23,14 @@ package org.onap.cps.utils
 
 import org.onap.cps.TestUtils
 import org.onap.cps.api.exceptions.DataValidationException
+import org.onap.cps.api.model.DeltaReport
 import org.onap.cps.yang.YangTextSchemaSourceSetBuilder
 import org.w3c.dom.DOMException
 import org.xml.sax.SAXParseException
 import spock.lang.Specification
 
 import static org.onap.cps.utils.XmlFileUtils.convertDataMapsToXml
+import static org.onap.cps.utils.XmlFileUtils.buildXmlUsingDom
 
 class XmlFileUtilsSpec extends Specification {
 
@@ -79,11 +81,11 @@ class XmlFileUtilsSpec extends Specification {
         then: 'the result contains the expected XML'
             assert result == expectedXmlOutput
         where:
-            scenario                              | dataMaps                                                                                                                                 || expectedXmlOutput
-            'single XML branch'                   | [['branch': ['name': 'Left', 'nest': ['name': 'Small', 'birds': ['Sparrow', 'Owl']]]]]                                                   || '<branch><name>Left</name><nest><name>Small</name><birds>Sparrow</birds><birds>Owl</birds></nest></branch>'
-            'nested XML branch'                   | [['test-tree': [branch: [name: 'Left', nest: [name: 'Small', birds: 'Sparrow']]]]]                                                       || '<test-tree><branch><name>Left</name><nest><name>Small</name><birds>Sparrow</birds></nest></branch></test-tree>'
-            'list of branch within a test tree'   | [['test-tree': [branch: [[name: 'Left', nest: [name: 'Small', birds: 'Sparrow']], [name: 'Right', nest: [name: 'Big', birds: 'Owl']]]]]] || '<test-tree><branch><name>Left</name><nest><name>Small</name><birds>Sparrow</birds></nest></branch><branch><name>Right</name><nest><name>Big</name><birds>Owl</birds></nest></branch></test-tree>'
-            'list of birds under a nest'          | [['nest': ['name': 'Small', 'birds': ['Sparrow']]]]                                                                                      || '<nest><name>Small</name><birds>Sparrow</birds></nest>'
+            scenario                            | dataMaps                                                                                                                                 || expectedXmlOutput
+            'single XML branch'                 | [['branch': ['name': 'Left', 'nest': ['name': 'Small', 'birds': ['Sparrow', 'Owl']]]]]                                                   || '<branch><name>Left</name><nest><name>Small</name><birds>Sparrow</birds><birds>Owl</birds></nest></branch>'
+            'nested XML branch'                 | [['test-tree': [branch: [name: 'Left', nest: [name: 'Small', birds: 'Sparrow']]]]]                                                       || '<test-tree><branch><name>Left</name><nest><name>Small</name><birds>Sparrow</birds></nest></branch></test-tree>'
+            'list of branch within a test tree' | [['test-tree': [branch: [[name: 'Left', nest: [name: 'Small', birds: 'Sparrow']], [name: 'Right', nest: [name: 'Big', birds: 'Owl']]]]]] || '<test-tree><branch><name>Left</name><nest><name>Small</name><birds>Sparrow</birds></nest></branch><branch><name>Right</name><nest><name>Big</name><birds>Owl</birds></nest></branch></test-tree>'
+            'list of birds under a nest'        | [['nest': ['name': 'Small', 'birds': ['Sparrow']]]]                                                                                      || '<nest><name>Small</name><birds>Sparrow</birds></nest>'
     }
 
     def 'Convert data maps to XML with null or empty maps and lists'() {
@@ -122,7 +124,47 @@ class XmlFileUtilsSpec extends Specification {
             def exception = thrown(DataValidationException)
         and: 'the cause is a document object model exception'
             assert exception.cause instanceof DOMException
+    }
 
+    def 'Convert deltaReport to XML #scenario'() {
+        when: 'deltaReports are converted to XML'
+            def result = buildXmlUsingDom(deltaReports)
+        then: 'the result contains the expected XML'
+            assert result == expectedXmlOutput
+        where:
+            scenario                          | deltaReports                                                                                                                                                  || expectedXmlOutput
+            'Empty deltaReports list'         | []                                                                                                                                                            || '<deltaReports/>'
+            'deltaReport with Replace action' | [new DeltaReport(action: 'Replace', xpath: '/bookstore', sourceData: ['bookstore-name': 'Easons-1'], targetData: ['bookstore-name': 'Crossword Bookstores'])] || '<deltaReports><deltaReport id="1"><action>Replace</action><xpath>/bookstore</xpath><source-data><bookstore-name>Easons-1</bookstore-name></source-data><target-data><bookstore-name>Crossword Bookstores</bookstore-name></target-data></deltaReport></deltaReports>'
+            'deltaReport with Delete action'  | [new DeltaReport(action: 'Delete', xpath: "/bookstore/categories[@code=5]/books[@title= Book-1]", sourceData: ['price': '1'], targetData: null)]              || '<deltaReports><deltaReport id="1"><action>Delete</action><xpath>/bookstore/categories[@code=5]/books[@title= Book-1]</xpath><source-data><price>1</price></source-data></deltaReport></deltaReports>'
+            'deltaReport with Add action'     | [new DeltaReport(action: 'Add', xpath: "/bookstore/categories[@code=5]/books[@title= Book-1]", sourceData: null, targetData: ['price': '1'])]                 || '<deltaReports><deltaReport id="1"><action>Add</action><xpath>/bookstore/categories[@code=5]/books[@title= Book-1]</xpath><target-data><price>1</price></target-data></deltaReport></deltaReports>'
+            'deltaReport with null lists'     | [new DeltaReport(action: 'Add', xpath: '/bookstore', sourceData: null, targetData: null)]                                                                     || '<deltaReports><deltaReport id="1"><action>Add</action><xpath>/bookstore</xpath></deltaReport></deltaReports>'
+    }
+
+    def 'deltaReport with nested collections'() {
+        given: 'A deltaReport with nested collections in sourceData and targetData'
+            def deltaReports = [
+                new DeltaReport(
+                    action: 'Update',
+                    xpath: '/bookstore',
+                    sourceData: ['books': ['Book-1', 'Book-2', 'Book-3']],
+                    targetData: ['authors': ['Author-1', 'Author-2']]
+                )
+            ]
+        when: 'deltaReports are converted to XML'
+            def result = buildXmlUsingDom(deltaReports)
+        then: 'the result contains the expected XML with nested collection values as comma-separated strings'
+            assert result == '<deltaReports><deltaReport id="1"><action>Update</action><xpath>/bookstore</xpath><source-data><books>Book-1, Book-2, Book-3</books></source-data><target-data><authors>Author-1, Author-2</authors></target-data></deltaReport></deltaReports>'
+    }
+
+    def 'Converting deltaReport to XML with document syntax error'() {
+        given: 'A list of maps with an invalid entry'
+            def deltaReport = [new DeltaReport(action: 'Replace', xpath: '/bookstore', sourceData: ['bookstore-name': 'Easons-1'], targetData: ['invalid<tag>': 'value'])]
+        when: 'convert the deltaReport to XML'
+            buildXmlUsingDom(deltaReport)
+        then: 'a validation exception is thrown'
+            def exception = thrown(DataValidationException)
+        and: 'the cause is a document object model exception'
+            assert exception.cause instanceof DOMException
     }
 
 }
