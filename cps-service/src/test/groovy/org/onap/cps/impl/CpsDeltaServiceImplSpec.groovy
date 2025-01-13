96e075e23c4538972ae640271fe4e1c721b04bc8
Arpit Singh <AS00745003@techmahindra.com>
2025-01-13 18:53:30 +0530

    Part 1: Grouping of Data Nodes in Delta Report
    Add-on feature in delta report to generate condensed delta report by
    grouping data nodes based on parent child relationship.
    
    The patch adds grouping feature for "create" operation in delta report.
    
    - Added boolean flag "groupingEnabled" to enable or disable grouping of
      data nodes in delta report. Default value is false.
    - Added a method getCondensedAddedDeltaReports to generate condensed
      delta reports for create operation.
    - Updated the Delta Report POJO.
    - Source and Target Data in Delta report are now json arrays to
      accomodate the change to group data nodes under single operation.
    
    Issue-ID: CPS-2547
    Change-Id: Ibb4d35b03098be7b57cb59852a87f6b4e0c7b706
    Signed-off-by: Arpit Singh <AS00745003@techmahindra.com>

--- cps-service/src/test/groovy/org/onap/cps/impl/CpsDeltaServiceImplSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/impl/CpsDeltaServiceImplSpec.groovy
@@ -31,34 +31,40 @@ class CpsDeltaServiceImplSpec extends Specification{
     static def sourceDataNodeWithLeafData = [new DataNode(xpath: '/parent', leaves: ['parent-leaf': 'parent-payload-in-source'])]
     static def sourceDataNodeWithoutLeafData = [new DataNode(xpath: '/parent')]
     static def targetDataNodeWithLeafData = [new DataNode(xpath: '/parent', leaves: ['parent-leaf': 'parent-payload-in-target'])]
+    static def targetDataNodeWithXpath = [new DataNode(xpath: '/parent/child', leaves: ['child-leaf': 'child-payload-in-target'])]
     static def targetDataNodeWithoutLeafData = [new DataNode(xpath: '/parent')]
     static def sourceDataNodeWithMultipleLeaves = [new DataNode(xpath: '/parent', leaves: ['leaf-1': 'leaf-1-in-source', 'leaf-2': 'leaf-2-in-source'])]
     static def targetDataNodeWithMultipleLeaves = [new DataNode(xpath: '/parent', leaves: ['leaf-1': 'leaf-1-in-target', 'leaf-2': 'leaf-2-in-target'])]
 
     def 'Get delta between data nodes for REMOVED data'() {
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports(sourceDataNodeWithLeafData, [])
+            def result = objectUnderTest.getDeltaReports(sourceDataNodeWithLeafData, [], false)
         then: 'the delta report contains expected "remove" action'
             assert result[0].action.equals('remove')
         and : 'the delta report contains the expected xpath'
             assert result[0].xpath == '/parent'
         and: 'the delta report contains expected source data'
-            assert result[0].sourceData == ['parent-leaf': 'parent-payload-in-source']
+            assert result[0].sourceData == [['parent-leaf': 'parent-payload-in-source']]
         and: 'the delta report contains no target data'
             assert  result[0].targetData == null
     }
 
     def 'Get delta between data nodes for ADDED data'() {
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports([], targetDataNodeWithLeafData)
+            def result = objectUnderTest.getDeltaReports([], targetDataNode, groupingEnabled)
         then: 'the delta report contains expected "create" action'
             assert result[0].action.equals('create')
         and: 'the delta report contains expected xpath'
-            assert result[0].xpath == '/parent'
+            assert result[0].xpath == expectedXpath
         and: 'the delta report contains no source data'
             assert result[0].sourceData == null
         and: 'the delta report contains expected target data'
-            assert result[0].targetData == ['parent-leaf': 'parent-payload-in-target']
+            assert result[0].targetData == expectedTargetData
+        where:
+            scenario                                | groupingEnabled | targetDataNode             || expectedXpath | expectedTargetData
+            'grouping is disabled'                  | false           | targetDataNodeWithLeafData || '/parent'     | [['parent-leaf': 'parent-payload-in-target']]
+            'grouping is enabled with parent xpath' | true            | targetDataNodeWithLeafData || '/'           | [['parent-leaf': 'parent-payload-in-target']]
+            'grouping enabled with xpath'           | true            | targetDataNodeWithXpath    || '/parent'     | [['child-leaf' : 'child-payload-in-target']]
     }
 
     def 'Delta Report between leaves for parent and child nodes'() {
@@ -66,22 +72,22 @@ class CpsDeltaServiceImplSpec extends Specification{
             def sourceDataNode  = [new DataNode(xpath: '/parent', leaves: ['parent-leaf': 'parent-payload'], childDataNodes: [new DataNode(xpath: '/parent/child', leaves: ['child-leaf': 'child-payload'])])]
             def targetDataNode  = [new DataNode(xpath: '/parent', leaves: ['parent-leaf': 'parent-payload-updated'], childDataNodes: [new DataNode(xpath: '/parent/child', leaves: ['child-leaf': 'child-payload-updated'])])]
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports(sourceDataNode, targetDataNode)
+            def result = objectUnderTest.getDeltaReports(sourceDataNode, targetDataNode, false)
         then: 'the delta report contains expected details for parent node'
             assert result[0].action.equals('replace')
             assert result[0].xpath == '/parent'
-            assert result[0].sourceData == ['parent-leaf': 'parent-payload']
-            assert result[0].targetData == ['parent-leaf': 'parent-payload-updated']
+            assert result[0].sourceData == [['parent-leaf': 'parent-payload']]
+            assert result[0].targetData == [['parent-leaf': 'parent-payload-updated']]
         and: 'the delta report contains expected details for child node'
             assert result[1].action.equals('replace')
             assert result[1].xpath == '/parent/child'
-            assert result[1].sourceData == ['child-leaf': 'child-payload']
-            assert result[1].targetData == ['child-leaf': 'child-payload-updated']
+            assert result[1].sourceData == [['child-leaf': 'child-payload']]
+            assert result[1].targetData == [['child-leaf': 'child-payload-updated']]
     }
 
     def 'Delta report between leaves, #scenario'() {
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports(sourceDataNode, targetDataNode)
+            def result = objectUnderTest.getDeltaReports(sourceDataNode, targetDataNode, false)
         then: 'the delta report contains expected "replace" action'
             assert result[0].action.equals('replace')
         and: 'the delta report contains expected xpath'
@@ -90,17 +96,22 @@ class CpsDeltaServiceImplSpec extends Specification{
             assert result[0].sourceData == expectedSourceData
             assert result[0].targetData == expectedTargetData
         where: 'the following data was used'
-            scenario                                           | sourceDataNode                   | targetDataNode                   || expectedSourceData                                           | expectedTargetData
-            'source and target data nodes have leaves'         | sourceDataNodeWithLeafData       | targetDataNodeWithLeafData       || ['parent-leaf': 'parent-payload-in-source']                  | ['parent-leaf': 'parent-payload-in-target']
-            'only source data node has leaves'                 | sourceDataNodeWithLeafData       | targetDataNodeWithoutLeafData    || ['parent-leaf': 'parent-payload-in-source']                  | null
-            'only target data node has leaves'                 | sourceDataNodeWithoutLeafData    | targetDataNodeWithLeafData       || null                                                         | ['parent-leaf': 'parent-payload-in-target']
-            'source and target dsta node with multiple leaves' | sourceDataNodeWithMultipleLeaves | targetDataNodeWithMultipleLeaves || ['leaf-1': 'leaf-1-in-source', 'leaf-2': 'leaf-2-in-source'] | ['leaf-1': 'leaf-1-in-target', 'leaf-2': 'leaf-2-in-target']
+            scenario                                           | sourceDataNode                   | targetDataNode                   || expectedSourceData                                             | expectedTargetData
+            'source and target data nodes have leaves'         | sourceDataNodeWithLeafData       | targetDataNodeWithLeafData       || [['parent-leaf': 'parent-payload-in-source']]                  | [['parent-leaf': 'parent-payload-in-target']]
+            'only source data node has leaves'                 | sourceDataNodeWithLeafData       | targetDataNodeWithoutLeafData    || [['parent-leaf': 'parent-payload-in-source']]                  | null
+            'only target data node has leaves'                 | sourceDataNodeWithoutLeafData    | targetDataNodeWithLeafData       || null                                                           | [['parent-leaf': 'parent-payload-in-target']]
+            'source and target dsta node with multiple leaves' | sourceDataNodeWithMultipleLeaves | targetDataNodeWithMultipleLeaves || [['leaf-1': 'leaf-1-in-source', 'leaf-2': 'leaf-2-in-source']] | [['leaf-1': 'leaf-1-in-target', 'leaf-2': 'leaf-2-in-target']]
     }
 
-    def 'Get delta between data nodes for updated data, where source and target data nodes have no leaves '() {
+    def 'Get delta between data nodes for updated data,  '() {
         when: 'attempt to get delta between 2 data nodes'
-            def result = objectUnderTest.getDeltaReports(sourceDataNodeWithoutLeafData, targetDataNodeWithoutLeafData)
+            def result = objectUnderTest.getDeltaReports(sourceDataNode,targetDataNode , groupingEnabled)
         then: 'the delta report is empty'
             assert result.isEmpty()
+        where:
+            scenario                                                | sourceDataNode                | targetDataNode                | groupingEnabled
+            'where source and target data nodes have no leaves'     | sourceDataNodeWithoutLeafData | targetDataNodeWithoutLeafData | false
+            'where target data node is empty with grouping enabled' | sourceDataNodeWithoutLeafData | []                            | true
+            'source and target have data with grouping enabled'     | sourceDataNodeWithLeafData    | targetDataNodeWithLeafData    | true
     }
 }
