40c48594f236864f284969c477319e8a88265657
mpriyank <priyank.maheshwari@est.tech>
2025-01-16 13:38:52 +0000

    Event data schemas for NCMP
    - Updated the schema id as NCMP has a master copy of all the schemas
      that are used
    - Introduced an enum to keep track of the dataschemas at a single place.
    - Refactored NcmpEvent builder class to take type and dataschema as a
      parameter
    - Cleaned up dependencies
    - Refactored testware to prove the changes
    - Changes are according to the decisions recorded here https://lf-onap.atlassian.net/wiki/spaces/DW/pages/16554840/CPS-2425+Event+Schemas+in+ONAP#Agreed-Event-Schema-Names
    
    Issue-ID: CPS-2425
    Change-Id: Icbda1aa20e3c4b0f2b6797d25b59e52cf9237c92
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/data/utils/DataOperationEventCreator.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/data/utils/DataOperationEventCreator.java
@@ -20,6 +20,8 @@
 
 package org.onap.cps.ncmp.impl.data.utils;
 
+import static org.onap.cps.ncmp.events.NcmpEventDataSchema.BATCH_RESPONSE_V1;
+
 import io.cloudevents.CloudEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -57,8 +59,13 @@ public class DataOperationEventCreator {
         final Data data = createPayloadFromDataOperationResponses(cmHandleIdsPerResponseCodesPerOperation);
         dataOperationEvent.setData(data);
         final Map<String, String> extensions = createDataOperationExtensions(requestId, clientTopic);
-        return NcmpEvent.builder().type(DataOperationEvent.class.getName())
-                .data(dataOperationEvent).extensions(extensions).build().asCloudEvent();
+        return NcmpEvent.builder()
+                       .type(DataOperationEvent.class.getName())
+                       .data(dataOperationEvent)
+                       .dataSchema(BATCH_RESPONSE_V1.getDataSchema())
+                       .extensions(extensions)
+                       .build()
+                       .asCloudEvent();
     }
 
     private static Data createPayloadFromDataOperationResponses(final MultiValueMap<DmiDataOperation,
