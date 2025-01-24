406730becd362dea4b8c32e7cf42647aefb5ba09
rajesh.kumar <rk00747546@techmahindra.com>
2025-01-24 15:34:55 +0530

    Add delta report in Kafka notification
      - Add delta report in Kafka notification
      - add a properties to enable/disable delta notification
    
    Issue-ID: CPS-2523
    Change-Id: I56c34400dc73c71b936a51260efd250124ababac
    Signed-off-by: rajesh.kumar <rk00747546@techmahindra.com>

--- cps-service/src/test/groovy/org/onap/cps/impl/CpsDataServiceImplSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/impl/CpsDataServiceImplSpec.groovy
@@ -31,6 +31,7 @@ import org.onap.cps.TestUtils
 import org.onap.cps.api.CpsAnchorService
 import org.onap.cps.api.CpsDeltaService
 import org.onap.cps.api.exceptions.ConcurrencyException
+import org.onap.cps.api.exceptions.DataNodeNotFoundException
 import org.onap.cps.api.exceptions.DataNodeNotFoundExceptionBatch
 import org.onap.cps.api.exceptions.DataValidationException
 import org.onap.cps.api.exceptions.SessionManagerException
@@ -56,7 +57,7 @@ import spock.lang.Specification
 
 import java.time.OffsetDateTime
 
-import static org.onap.cps.events.model.Data.Operation.DELETE
+import static org.onap.cps.events.model.Data.Operation.REMOVE
 
 class CpsDataServiceImplSpec extends Specification {
     def mockCpsDataPersistenceService = Mock(CpsDataPersistenceService)
@@ -577,8 +578,7 @@ class CpsDataServiceImplSpec extends Specification {
         and: 'the persistence service method is invoked with the correct parameters'
             1 * mockCpsDataPersistenceService.deleteDataNodes(dataspaceName, _ as Collection<String>)
         and: 'a data update event is sent for each anchor'
-            1 * mockCpsDataUpdateEventsProducer.publishCpsDataUpdateEvent(anchor1, '/', DELETE, observedTimestamp)
-            1 * mockCpsDataUpdateEventsProducer.publishCpsDataUpdateEvent(anchor2, '/', DELETE, observedTimestamp)
+            2 * mockCpsDataUpdateEventsProducer.publishCpsDataUpdateEvent(_, null, '/', REMOVE, observedTimestamp)
     }
 
     def "Validating #scenario when dry run is enabled."() {
@@ -642,15 +642,30 @@ class CpsDataServiceImplSpec extends Specification {
     def 'Exception is thrown while publishing the notification.'(){
         given: 'schema set for given anchor and dataspace references test-tree model'
             setupSchemaSetMocks('test-tree.yang')
+            objectUnderTest.deltaNotificationEnabled = true
         when: 'publisher set to throw an exception'
-            mockCpsDataUpdateEventsProducer.publishCpsDataUpdateEvent(_, _, _, _) >> { throw new Exception("publishing failed")}
+            mockCpsDataUpdateEventsProducer.publishCpsDataUpdateEvent(_, _, _, _, _) >> { throw new Exception("publishing failed")}
         and: 'an update event is performed'
             objectUnderTest.updateNodeLeaves(dataspaceName, anchorName, '/', '{"test-tree": {"branch": []}}', observedTimestamp, ContentType.JSON)
         then: 'the exception is not bubbled up'
             noExceptionThrown()
-        and: "the exception message is logged"
+        and: 'the exception message is logged'
             def logs = loggingListAppender.list.toString()
             assert logs.contains('Failed to send message to notification service')
+    }
+
+    def 'Exception is thrown while generating update delta report for notification.'(){
+        given: 'Exception in getting data nodes'
+            mockCpsDataPersistenceService.getDataNodes('testDataspace', 'testAnchor', '/test/xpath', FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS) >>
+                    { throw new  DataNodeNotFoundException('testDataspace', 'testAnchor', '/test/xpath')}
+            objectUnderTest.deltaNotificationEnabled = true;
+        when: 'generate update delta report is executed'
+            objectUnderTest.generateUpdateDeltaReport('testDataspace', 'testAnchor', '/test/xpath', [new DataNodeBuilder().withXpath('/xpath').build()])
+        then: 'the exception is not bubbled up'
+            noExceptionThrown()
+        and: 'the exception message is logged'
+            def logs = loggingListAppender.list.toString()
+            assert logs.contains('Failed to generate delta report')
     }
     def setupSchemaSetMocks(String... yangResources) {
         def mockYangTextSchemaSourceSet = Mock(YangTextSchemaSourceSet)
