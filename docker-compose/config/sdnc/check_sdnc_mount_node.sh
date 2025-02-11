45855d5952e3f5050c33513ffb8cdf29b81821bd
danielhanrahan <daniel.hanrahan@est.tech>
2025-02-11 18:34:57 +0000

    Move SDNC and PNFSim into docker-compose for CSITs
    To improve reliability of healthchecks in CSITs, they are implemented
    in docker-compose. This commit moves SDNC and dependencies into main
    docker-compose and adds healthchecks to them. This improves the CSIT
    in that tests will quickly fail if SDNC or PNFsim containers fail to
    start (currently tests run anyway even if containers are not ready).
    
    - Move SDNC, MariaDB and PNFSim containers to main docker-compose
    - Add healthchecks and timeouts for those containers
    - Move node mounting script as a sidecar container so it is protected
      by healthchecks
    
    Issue-ID: CPS-2632
    Signed-off-by: danielhanrahan <daniel.hanrahan@est.tech>
    Change-Id: Ib53522c2f756d3ce2c6d6b7472cb4c65359fe355

---
+++ docker-compose/config/sdnc/check_sdnc_mount_node.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# ============LICENSE_START=======================================================
+# Copyright (C) 2023-2025 Nordix Foundation
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+set -x  # Enable command echoing
+apk --no-cache add curl
+
+SDNC_HOST=${SDNC_HOST:-'sdnc'}
+SDNC_PORT=${SDNC_PORT:-8181}
+SDNC_AUTH_HEADER=${SDNC_AUTH_HEADER:-'Authorization: Basic YWRtaW46S3A4Yko0U1hzek0wV1hsaGFrM2VIbGNzZTJnQXc4NHZhb0dHbUp2VXkyVQ=='}
+PNF_SIM_HOST=${PNF_SIM_HOST:-'pnf-simulator'}
+PNF_SIM_PORT=${PNF_SIM_PORT:-6513}
+NODE_ID=${NODE_ID:-'ietfYang-PNFDemo'}
+
+echo "Attempting to mount node with id '$NODE_ID' to SDNC using RestConf"
+curl --request PUT "http://$SDNC_HOST:$SDNC_PORT/restconf/config/network-topology:network-topology/topology/topology-netconf/node/$NODE_ID" \
+--silent --location \
+--header "$SDNC_AUTH_HEADER" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "node": [
+  {
+    "node-id": "'$NODE_ID'",
+    "netconf-node-topology:protocol": {
+    "name": "TLS"
+    },
+    "netconf-node-topology:host": "'$PNF_SIM_HOST'",
+    "netconf-node-topology:key-based": {
+    "username": "netconf",
+    "key-id": "ODL_private_key_0"
+    },
+    "netconf-node-topology:port": '$PNF_SIM_PORT',
+    "netconf-node-topology:tcp-only": false,
+    "netconf-node-topology:max-connection-attempts": 5
+  }
+  ]
+}'
+
+# Verify node has been mounted
+RESPONSE=$(curl --silent --location --request GET "http://$SDNC_HOST:$SDNC_PORT/restconf/config/network-topology:network-topology/topology/topology-netconf" --header "$SDNC_AUTH_HEADER")
+
+if echo "$RESPONSE" | grep -q "$NODE_ID"; then
+  echo "Node mounted successfully"
+  exit 0
+else
+  echo "Could not mount node to SNDC"
+  exit 1
+fi
