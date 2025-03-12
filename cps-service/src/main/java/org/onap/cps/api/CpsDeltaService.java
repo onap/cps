15a2bb22b0265ec7a064e881e2c9a1625fb679a2
Arpit Singh <AS00745003@techmahindra.com>
2025-03-12 12:33:35 +0530

    Refactor Cps Delta code from CpsDataService to CpsDeltaService
    - added new open api yaml file defining CPS delta endpoints
    - Added new REST controller for CPS Delta
    - Moved CPS Delta endpoints to new java interface, out from CPS Data
    - Added integration tests for CPS Delta
    - CPS Delta rest controller and java interface have the two delta
      endpoints: Delta between anchors and Delta between anchor and payload.
    
    Issue-ID: CPS-2320
    Change-Id: I556e70623a8c18f8cde3cd28bac890296019c0e2
    Signed-off-by: Arpit Singh <AS00745003@techmahindra.com>

--- cps-service/src/main/java/org/onap/cps/api/CpsDeltaService.java
+++ cps-service/src/main/java/org/onap/cps/api/CpsDeltaService.java
@@ -1,6 +1,6 @@
 /*
  *  ============LICENSE_START=======================================================
- *  Copyright (C) 2023 TechMahindra Ltd.
+ *  Copyright (C) 2023-2025 TechMahindra Ltd.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,23 +20,47 @@
 
 package org.onap.cps.api;
 
-import java.util.Collection;
 import java.util.List;
-import org.onap.cps.api.model.DataNode;
+import java.util.Map;
 import org.onap.cps.api.model.DeltaReport;
+import org.onap.cps.api.parameters.FetchDescendantsOption;
 
 public interface CpsDeltaService {
 
     /**
-     * Retrieves delta between source data nodes and target data nodes. Source data nodes contain the data which acts as
-     * the point of reference for delta report, whereas target data nodes contain the data being compared against
-     * source data node. List of {@link DeltaReport}. Each Delta Report contains information such as action, xpath,
-     * source-payload and target-payload.
+     * Retrieves the delta between two anchors by xpath within a dataspace. Returns a List of {@link DeltaReport}.
+     * Each Delta Report contains following information: action, xpath, source-payload and/or target-payload.
      *
-     * @param sourceDataNodes  collection of {@link DataNode} as source/reference for delta generation
-     * @param targetDataNodes  collection of {@link DataNode} as target data for delta generation
-     * @return                 list of {@link DeltaReport} containing delta information
+     * @param dataspaceName          dataspace name
+     * @param sourceAnchorName       source anchor name
+     * @param targetAnchorName       target anchor name
+     * @param xpath                  xpath
+     * @param fetchDescendantsOption defines the scope of data to fetch: either single node or all the descendant
+     *                               nodes (recursively) as well
+     * @return                       list containing {@link DeltaReport} objects
      */
-    List<DeltaReport> getDeltaReports(Collection<DataNode> sourceDataNodes,
-                                      Collection<DataNode> targetDataNodes);
+    List<DeltaReport> getDeltaByDataspaceAndAnchors(String dataspaceName, String sourceAnchorName,
+                                                    String targetAnchorName, String xpath,
+                                                    FetchDescendantsOption fetchDescendantsOption);
+
+    /**
+     * Retrieves the delta between an anchor and JSON payload by xpath, using dataspace name and anchor name.
+     * Returns a List of {@link DeltaReport}. Each Delta Report contains following information: action, xpath,
+     * source-payload and/or target-payload.
+     *
+     * @param dataspaceName                source dataspace name
+     * @param sourceAnchorName             source anchor name
+     * @param xpath                        xpath
+     * @param yangResourceContentPerName   YANG resources (files) map where key is a name and value is content
+     * @param targetData                   target data to be compared in JSON string format
+     * @param fetchDescendantsOption       defines the scope of data to fetch: defaulted to INCLUDE_ALL_DESCENDANTS
+     *
+     * @return                             list containing {@link DeltaReport} objects
+     */
+    List<DeltaReport> getDeltaByDataspaceAnchorAndPayload(String dataspaceName, String sourceAnchorName, String xpath,
+                                                          Map<String, String> yangResourceContentPerName,
+                                                          String targetData,
+                                                          FetchDescendantsOption fetchDescendantsOption);
+
+
 }
