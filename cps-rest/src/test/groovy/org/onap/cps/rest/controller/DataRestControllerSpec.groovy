908057dc2be9717b3c58e622754eb5af7a3d6139
Rudrangi Anupriya <ra00745022@techmahindra.com>
2025-03-19 14:54:47 +0530

    Enhance the Multipart Delta API to support JSON file uploads as payloads instead of  JSON data as text input
    Issue-ID: CPS-2657
    Change-Id: Ibc9bbb99aabb07302366321c6d5c2eade0e7e5fb
    Signed-off-by: Rudrangi Anupriya <ra00745022@techmahindra.com>

--- cps-rest/src/test/groovy/org/onap/cps/rest/controller/DataRestControllerSpec.groovy
+++ cps-rest/src/test/groovy/org/onap/cps/rest/controller/DataRestControllerSpec.groovy
@@ -4,7 +4,7 @@
  *  Modifications Copyright (C) 2021 Pantheon.tech
  *  Modifications Copyright (C) 2021-2022 Bell Canada.
  *  Modifications Copyright (C) 2022 Deutsche Telekom AG
- *  Modifications Copyright (C) 2022-2024 TechMahindra Ltd.
+ *  Modifications Copyright (C) 2022-2025 TechMahindra Ltd.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ package org.onap.cps.rest.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.onap.cps.api.CpsDataService
 import org.onap.cps.api.CpsFacade
+import org.onap.cps.api.exceptions.DataValidationException
 import org.onap.cps.impl.DeltaReportBuilder
 import org.onap.cps.utils.ContentType
 import org.onap.cps.utils.DateTimeUtility
@@ -41,6 +42,10 @@ import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Shared
 import spock.lang.Specification
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 
 import static org.onap.cps.api.parameters.FetchDescendantsOption.INCLUDE_ALL_DESCENDANTS
 import static org.onap.cps.api.parameters.FetchDescendantsOption.OMIT_DESCENDANTS
@@ -90,9 +95,22 @@ class DataRestControllerSpec extends Specification {
     @Shared
     def multipartYangFile = new MockMultipartFile("file", 'filename.yang', "text/plain", 'content'.getBytes())
 
+    @Shared
+    Path jsonFile
+
+    @Shared
+    MockMultipartFile multipartJsonFile
+
     def setup() {
         dataNodeBaseEndpointV1 = "$basePath/v1/dataspaces/$dataspaceName"
         dataNodeBaseEndpointV2 = "$basePath/v2/dataspaces/$dataspaceName"
+        jsonFile = Files.createTempFile("requestBody", ".json")
+        Files.write(jsonFile, requestBodyJson.getBytes(StandardCharsets.UTF_8), StandardOpenOption.WRITE)
+        multipartJsonFile = new MockMultipartFile("json", jsonFile.fileName.toString(), "application/json", Files.readAllBytes(jsonFile))
+    }
+
+    def cleanup() {
+        Files.deleteIfExists(jsonFile)
     }
 
     def 'Create a node: #scenario.'() {
@@ -337,7 +355,7 @@ class DataRestControllerSpec extends Specification {
             def response =
                     mvc.perform(multipart(endpoint)
                             .file(multipartYangFile)
-                            .param("json", requestBodyJson)
+                            .file(multipartJsonFile)
                             .param('xpath', xpath)
                             .contentType(MediaType.MULTIPART_FORM_DATA))
                             .andReturn().response
@@ -357,7 +375,7 @@ class DataRestControllerSpec extends Specification {
         when: 'get delta request is performed using REST API'
             def response =
                     mvc.perform(multipart(endpoint)
-                            .param("json", requestBodyJson)
+                            .file(multipartJsonFile)
                             .param('xpath', xpath)
                             .contentType(MediaType.MULTIPART_FORM_DATA))
                             .andReturn().response
@@ -365,6 +383,29 @@ class DataRestControllerSpec extends Specification {
             assert response.status == HttpStatus.OK.value()
         and: 'the response contains expected value'
             assert response.contentAsString.contains("[{\"action\":\"remove\",\"xpath\":\"some xpath\"}]")
+    }
+
+    def 'Get delta between anchor and JSON payload throws DataValidationException'() {
+        given: 'sample delta report, xpath, yang model file and empty json payload'
+            def deltaReports = new DeltaReportBuilder().actionCreate().withXpath('some xpath').build()
+            def xpath = 'some xpath'
+            def endpoint = "$dataNodeBaseEndpointV2/anchors/$anchorName/delta"
+            def emptyJsonFile = new MockMultipartFile("json", "empty.json", "application/json", new byte[0])
+        and: 'the service layer throws DataValidationException for empty jsonFile'
+            mockCpsDataService.getDeltaByDataspaceAnchorAndPayload(dataspaceName, anchorName, xpath, [:], expectedJsonData, INCLUDE_ALL_DESCENDANTS) >> {
+                throw new DataValidationException("JSON file is required.", "Invalid JSON file")
+            }
+        when: 'get delta request is performed using REST API'
+            def response =
+                mvc.perform(multipart(endpoint)
+                    .file(multipartYangFile)
+                    .file(emptyJsonFile)
+                    .param('xpath', xpath)
+                    .contentType(MediaType.MULTIPART_FORM_DATA))
+                    .andReturn().response
+        then: 'expected DataValidationException is thrown and proper error response is returned'
+            response.status == HttpStatus.BAD_REQUEST.value()
+            response.contentAsString.contains("JSON file is required.")
     }
 
     def 'Update data node leaves: #scenario.'() {
