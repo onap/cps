e871c361bf3b135913e6818fdac4acff49708a25
halil.cakal <halil.cakal@est.tech>
2025-03-27 11:13:03 +0000

    Update batch id searches to return alternate ids
    - keep the query parameter (?outputAlternateId=true) common for id-searches and searches
      since it does not have any negative impact for the searches when provided this way the code
      is more simple and readable
    - function names kept the same to comply with the response messages
    - the plot names kept the same to maintain the history in local storage of kpi plot job
    
    Issue-ID: CPS-2715
    
    Change-Id: I8146e7ecb04ddd4b907fae94e91fd78bd535b58c
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

--- k6-tests/ncmp/common/search-base.js
+++ k6-tests/ncmp/common/search-base.js
@@ -1,6 +1,6 @@
 /*
  *  ============LICENSE_START=======================================================
- *  Copyright (C) 2024 Nordix Foundation
+ *  Copyright (C) 2024-2025 OpenInfra Foundation Europe. All rights reserved.
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ export function executeCmHandleIdSearch(scenario) {
 function executeSearchRequest(searchType, scenario) {
     const searchParameters = SEARCH_PARAMETERS_PER_SCENARIO[scenario];
     const payload = JSON.stringify(searchParameters);
-    const url = `${NCMP_BASE_URL}/ncmp/v1/ch/${searchType}`;
+    const url = `${NCMP_BASE_URL}/ncmp/v1/ch/${searchType}?outputAlternateId=true`;
     return performPostRequest(url, payload, searchType);
 }
 
