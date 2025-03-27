2380422ec791686dce292ae26aef116d639da306
sourabh_sourabh <sourabh.sourabh@est.tech>
2025-03-27 10:15:46 +0000

    Add 'unpublish' REST interface to allow K6 testing
    - Created a new hidden REST endpoint to use write job for internal
      testing lile KPI and endurence.
    - Added groovy test for writeDataJob to validate subJobId, dmiServiceName, and dataProducerId in the response.
    
    Issue-ID: CPS-2718
    Change-Id: I1c4c7bc145dd522d344bd98ea04f05068b3e43eb
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/controller/DataJobController.java
@@ -0,0 +1,77 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.ncmp.api.datajobs.DataJobService;
+import org.onap.cps.ncmp.api.datajobs.models.DataJobMetadata;
+import org.onap.cps.ncmp.api.datajobs.models.DataJobRequest;
+import org.onap.cps.ncmp.api.datajobs.models.DataJobWriteRequest;
+import org.onap.cps.ncmp.api.datajobs.models.SubJobWriteResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller responsible for handling data job write operations.
+ * This class exposes an API endpoint that accepts a write request for a data job and processes it.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/donotuse")
+@RequiredArgsConstructor
+public class DataJobController  {
+
+    private final DataJobService dataJobService;
+
+    /**
+     * Handles POST requests to write data job.
+     * This endpoint processes a data job write request by extracting necessary metadata and data
+     * from the request body and delegating the operation to the {@link DataJobService}.
+     *
+     * @param authorization   The optional authorization token sent in the request header.
+     * @param dataJobId       The unique identifier for the data job, extracted from the URL path.
+     * @param dataJobRequest  The request payload containing metadata and data for the data job write operation.
+     * @return A {@link ResponseEntity} containing a list of {@link SubJobWriteResponse} objects representing the
+     *         status of each sub-job within the data job, or an error response with an appropriate HTTP status code.
+     */
+    @PostMapping("/{dataJobId}/write")
+    @Hidden
+    public ResponseEntity<List<SubJobWriteResponse>> writeDataJob(@RequestHeader(value = "Authorization",
+                                                                          required = false) final String authorization,
+                                                                  @PathVariable("dataJobId") final String dataJobId,
+                                                                  @RequestBody final DataJobRequest dataJobRequest) {
+        log.info("Internal API: writeDataJob invoked for {}", dataJobId);
+        final DataJobMetadata dataJobMetadata = dataJobRequest.getDataJobMetadata();
+        final DataJobWriteRequest dataJobWriteRequest = dataJobRequest.getDataJobWriteRequest();
+        final List<SubJobWriteResponse> response = dataJobService.writeDataJob(authorization, dataJobId,
+                dataJobMetadata, dataJobWriteRequest);
+        return ResponseEntity.ok(response);
+    }
+}
+
