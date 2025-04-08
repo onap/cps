1834283c118e7d480956a40718ebf13088c577b2
sourabh_sourabh <sourabh.sourabh@est.tech>
2025-04-08 11:13:08 +0100

    Introduced a startup delay mechanism based on the container hostname
    - Extracted hostname using InetAddress and Calculated delay or fallback hash-based delay.
    - This helps prevent the 'relation "databasechangelog" already exists' error seen in concurrent startups.
    
    Issue-ID:CPS-2752
    Change-Id: I051a8edd5ddab5a9fb012183b0526c113d90304e
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>

--- cps-application/src/main/java/org/onap/cps/Application.java
+++ cps-application/src/main/java/org/onap/cps/Application.java
@@ -1,6 +1,6 @@
 /*
  * ============LICENSE_START=======================================================
- * Copyright (C) 2020 Nordix Foundation.
+ * Copyright (C) 2020-2025 OpenInfra Foundation Europe. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,12 +20,29 @@
 
 package org.onap.cps;
 
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.startup.InstanceStartupDelayManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
+
+    static InstanceStartupDelayManager instanceStartupDelayManager = new InstanceStartupDelayManager();
+
+    /**
+     * The main method which serves as the entry point to the Spring Boot application.
+     * It first applies a hostname-based startup delay to avoid potential race conditions
+     * during schema migration in distributed environments. After applying the delay,
+     * it initializes the Spring Application context and logs the application startup status.
+     *
+     * @param args Command-line arguments passed to the application (not used in this implementation).
+     */
     public static void main(final String[] args) {
+        instanceStartupDelayManager.applyHostnameBasedStartupDelay();
+        log.info("Initializing Spring Application context...");
         SpringApplication.run(Application.class, args);
+        log.info("🚀 APPLICATION STARTED");
     }
 }
