a245a69ddb960cd77ba1ccce658ffc0fe0090ff6
sourabh_sourabh <sourabh.sourabh@est.tech>
2025-04-08 11:13:08 +0100

    Introduced a startup delay mechanism based on the container hostname to avoid race conditions during Liquibase migrations in multi-instance deployments.
    - Extracted hostname using InetAddress and Calculated delay or fallback hash-based delay.
    - This helps prevent the 'relation "databasechangelog" already exists' error seen in concurrent startups.
    
    Change-Id: I051a8edd5ddab5a9fb012183b0526c113d90304e
    Signed-off-by: sourabh_sourabh <sourabh.sourabh@est.tech>

--- cps-application/src/main/java/org/onap/cps/Application.java
+++ cps-application/src/main/java/org/onap/cps/Application.java
@@ -1,6 +1,6 @@
 /*
  * ============LICENSE_START=======================================================
- * Copyright (C) 2020 Nordix Foundation.
+ * Copyright (C) 2020-2025 OpenInfra Foundation Europe. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,12 +20,44 @@
 
 package org.onap.cps;
 
+import java.net.InetAddress;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(final String[] args) {
+        applyHostnameBasedStartupDelay();
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Applies a startup delay based on the host's name to avoid race conditions during schema migration.
+
+     * In environments with multiple instances (e.g., Docker Compose, Kubernetes),
+     * this delay helps avoid simultaneous Liquibase executions that may result in conflicts.
+
+     * Delay logic:
+     * - If the last character of the hostname is a digit, delay = digit * 1000 ms.
+     * - Otherwise, a hash-based fallback delay up to 3000 ms is applied.
+     */
+    private static void applyHostnameBasedStartupDelay() {
+        try {
+            final String hostname = InetAddress.getLocalHost().getHostName();
+            final char lastChar = hostname.charAt(hostname.length() - 1);
+
+            final int startupDelayInMillis;
+            if (Character.isDigit(lastChar)) {
+                startupDelayInMillis = Character.getNumericValue(lastChar) * 1000;
+            } else {
+                startupDelayInMillis = Math.abs(hostname.hashCode() % 3000);
+            }
+            log.info("[Startup Delay] Hostname: {} | Delay: {} ms", hostname, startupDelayInMillis);
+            Thread.sleep(startupDelayInMillis);
+        } catch (final Exception e) {
+            log.warn("[Startup Delay] Unable to apply delay. Proceeding without delay. {}", e.getMessage());
+        }
     }
 }
