e4d8963ebfc0660a3d1897c33d62e2f6c8bd10b9
rajesh.kumar <rk00747546@techmahindra.com>
2025-05-06 16:18:04 +0530

    Add requestID and clientID in cps core logs
    Add request ID and client ID in cps core logs
    
    Issue-ID: CPS-2796
    Change-Id: I56c34400dc73c71b936a51260efd060505baabdc
    Signed-off-by: rajesh.kumar <rk00747546@techmahindra.com>

---
+++ cps-rest/src/main/java/org/onap/cps/rest/utils/MdcServletFilter.java
@@ -0,0 +1,63 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 TechMahindra Ltd.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.rest.utils;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MdcServletFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger("MdcServletFilter");
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
+                         final FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        String requestId = httpRequest.getHeader("Request-Id");
+        String clientId = httpRequest.getHeader("Client-Id");
+        if (requestId == null || requestId.isBlank()) {
+            requestId = UUID.randomUUID().toString();
+        }
+
+        if (clientId == null || clientId.isBlank()) {
+            clientId = UUID.randomUUID().toString();
+        }
+
+        MDC.put("requestId", requestId);
+        MDC.put("clientId", clientId);
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
