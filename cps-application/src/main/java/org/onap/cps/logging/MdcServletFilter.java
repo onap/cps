3f5b95c3c90920b3bc77f5de70b43ce27e02d5cf
rajesh.kumar <rk00747546@techmahindra.com>
2025-05-06 16:18:04 +0530

    Add requestID and clientID in cps core logs
    - This enhancment is to signicantly improve the traceability and debugging capabilities for specific request flowing through our system.
    
    - Create new class MdcServletFilter.java and added the dofilter to handle Mapped Diagnostic Context setup for capture and link requestID and clientID.
    
    - Added testcases MdcServletFilterSpec.groovy for above changes.
    
    Issue-ID: CPS-2796
    Change-Id: I56c34400dc73c71b936a51260efd060505baabdc
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

---
+++ cps-application/src/main/java/org/onap/cps/logging/MdcServletFilter.java
@@ -0,0 +1,57 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 TechMahindra Ltd.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.logging;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MdcServletFilter implements Filter {
+
+    private static final String REQUEST_ID_HEADER = "X-ONAP-RequestID";
+    private static final String CLIENT_ID_HEADER = "X-ClientID";
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
+                         final FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        putToMdc(REQUEST_ID_HEADER, httpServletRequest);
+        putToMdc(CLIENT_ID_HEADER, httpServletRequest);
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            MDC.remove(REQUEST_ID_HEADER);
+            MDC.remove(CLIENT_ID_HEADER);
+        }
+    }
+
+    private void putToMdc(final String header, final HttpServletRequest httpServletRequest) {
+        final String value = httpServletRequest.getHeader(header);
+        MDC.put(header, value);
+    }
+}
