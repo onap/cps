0d3f242e7198b9cc983e806a9772962e635c3012
rajesh.kumar <rk00747546@techmahindra.com>
2025-05-06 16:18:04 +0530

    Add requestID and clientID in cps core logs
    - This enhancment is to signicantly improve the traceability and debugging capabilities for specific request flowing through our system.
    
    - Create new class MdcServletFilter.java and added the dofilter to handle Mapped Diagnostic Context setup for capture and link requestID and clientID.
    
    - Added testcases MdcServletFilterSpec.groovy for above changes.
    
    Issue-ID: CPS-2796
    Change-Id: I56c34400dc73c71b936a51260efd060505baabdc
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

---
+++ cps-application/src/test/groovy/org/onap/cps/logging/MdcServletFilterSpec.groovy
@@ -0,0 +1,51 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 TechMahindra Ltd.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.logging
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.MDC
+import spock.lang.Specification
+
+class MdcServletFilterSpec extends Specification {
+    def mockFilterChain = Mock(FilterChain)
+    def mockHttpServletRequest = Mock(HttpServletRequest)
+    def mockServletResponse = Mock(ServletResponse)
+
+    def objectUnderTest = new MdcServletFilter()
+
+    def 'should set Mapped Diagnostic Context from headers if present'() {
+        given: 'RequestID and ClientID present in request header'
+              mockHttpServletRequest.getHeader('X-CPS-Request-Id') >> 'test-request-id'
+              mockHttpServletRequest.getHeader('X-CPS-Client-Id') >> 'test-client-id'
+        when: 'HTTP request is filtered'
+              objectUnderTest.doFilter(mockHttpServletRequest, mockServletResponse, mockFilterChain)
+        then: 'the filter chain is called once'
+             mockFilterChain.doFilter(mockHttpServletRequest, mockServletResponse) >> {
+             assert MDC.get('X-CPS-Request-Id') == 'test-request-id'
+             assert MDC.get('X-CPS-Client-Id') == 'test-client-id'
+        }
+        and: 'MDC is cleaned up after filter chain execution'
+            assert MDC.get('X-CPS-Request-Id') == null
+            assert MDC.get('X-CPS-Client-Id') == null
+    }
+}
