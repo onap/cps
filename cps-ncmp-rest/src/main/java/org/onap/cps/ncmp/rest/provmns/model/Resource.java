0c17f53f45b0e6f5dd1ca8275706fb02870503e3
mpriyank <priyank.maheshwari@est.tech>
2025-05-20 12:48:49 +0100

    ProvMns interface generation using remote url
    - Referring to remote ProvMns spec file from the 3gpp sources
    - Generating the interface without keeping a local copy of the spec file
    - Excluding the files that are not needed in our ProvMnS implementation
      as those were causing issues while code generation
    - Have one Resource interface in org.onap.cps.ncmp.rest.provmns.model
      package that is needed to compile the generated code
    - Added exclusion in jacoco plugin for code coverage
    
    Issue-ID: CPS-2817
    Change-Id: Ie9801fac52850dbd50f975a9b65d36f818b2b474
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/provmns/model/Resource.java
@@ -0,0 +1,27 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 OpenInfra Foundation Europe
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.rest.provmns.model;
+
+/**
+ * This interface serves as a replacement for the generated Resource class, which has dependencies on the NRM-related
+ * models that we want to avoid for our implementation of Provisioning and Management Services (ProvMnS).
+ */
+public interface Resource { }
