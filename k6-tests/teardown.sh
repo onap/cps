1e6cc810f9c6690c6fa25db702e7ee59141fd809
halil.cakal <halil.cakal@est.tech>
2025-06-26 14:37:49 +0100

    Clean up setup and teardown processes for k6 tests
    - rename cps-base.yml to docker-compose.yml to use minimum
      docker-compose commands efficiently
    - rename confusing file names:
      - run-k6-tests.sh to k6-main.sh
      - run-all-tests.sh to execute-k6-scenarios.sh
      - ncmp-test-runner.js to scenarios-config.js
    - cleanup teardown.sh
      - not only endurance but also kpi should remove all ONAP docker images
        since setup function always builds them!
      - Don't remove any other images (they don't belong ONAP) because it
        bring extra pulling time during the setup, and make the pipeline
    slower.
      - combine relevant methods into one
    - add cm avc event and v3 data node get metrics to metrics.csv file to
      prevent them commit always
    - add missing import for crypto
    
    Issue-ID: CPS-2861
    
    Change-Id: If3d5cc8e33e5a5baa34834aaa178be7803dd5f7d
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

--- k6-tests/teardown.sh
+++ k6-tests/teardown.sh
@@ -23,37 +23,34 @@ chmod +x make-logs.sh
 ./make-logs.sh
 
 testProfile=$1
-docker_compose_shutdown_cmd="docker-compose -f ../docker-compose/cps-base.yml --project-name $testProfile down --volumes"
-remove_onap_docker_images_cmd="docker images | grep "onap" | awk '{print $3}' | xargs docker rmi"
+docker_compose_shutdown_cmd="docker-compose -f ../docker-compose/docker-compose.yml --project-name $testProfile down --volumes"
 
-# Verify number of docker images after deletion on teardown of endurance testing
-verify_docker_images() {
-  no_of_onap_docker_images=$(docker images | grep "onap" | wc -l)
-  if [[ "$no_of_onap_docker_images" -eq 0 ]]; then
-    echo "Successfully removed ONAP docker images!"
-  else
-    echo "Removing ONAP docker images failed."
-  fi
+# All CPS docker images:
+# nexus3.onap.org:10003/onap/cps-and-ncmp:latest
+# nexus3.onap.org:10003/onap/dmi-stub:1.8.0-SNAPSHOT
+# nexus3.onap.org:10003/onap/policy-executor-stub:latest
+remove_cps_images() {
+  local repos=(cps-and-ncmp dmi-stub policy-executor-stub)
+  for repo in "${repos[@]}"; do
+    local image="nexus3.onap.org:10003/onap/$repo"
+    # list all image IDs for this repository (all tags)
+    ids=$(docker images -q "$image")
+    if [ -n "$ids" ]; then
+      echo "Removing images for $image..."
+      docker rmi -f $ids
+    fi
+  done
 }
 
-# Remove all onap docker images
-remove_all_onap_docker_images() {
-  no_of_onap_docker_images=$(docker images | grep "onap" | wc -l)
-  if [[ "$no_of_onap_docker_images" -ne 0 ]]; then
-    echo "Removing all ONAP docker images..."
-    eval "$remove_onap_docker_images_cmd"
-    verify_docker_images
-  fi
-}
-
-# Set an environment variable CLEAN_DOCKER_IMAGES=1 to also remove docker images when done (used on jenkins job)
+# Check env. variable CLEAN_DOCKER_IMAGES=1 to decide removing CPS images
 echo "Stopping, Removing containers and volumes for $testProfile tests..."
 if [[ "${CLEAN_DOCKER_IMAGES:-0}" -eq 1 ]]; then
-  echo "Also cleaning up all images"
-  eval "$docker_compose_shutdown_cmd --rmi all"
-  if [[ "$testProfile" == "endurance" ]]; then
-    remove_all_onap_docker_images
-  fi
+  # down the compose stack, then purge any remaining CPS images,
+  # regardless of any test profile!
+  echo "Also cleaning up all CPS images"
+  eval "$docker_compose_shutdown_cmd"
+  remove_cps_images
 else
+  # for local test operations
   eval "$docker_compose_shutdown_cmd"
 fi
