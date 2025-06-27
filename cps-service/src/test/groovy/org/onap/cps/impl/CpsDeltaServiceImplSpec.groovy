92edb7aa5a1345f055cea3eca22f91069c1e49b5
Arpit Singh <AS00745003@techmahindra.com>
2025-06-27 18:35:27 +0530

    Patch 2: Refactor CPS Delta code to utility class
    - Refactoring original delta report format to a utility class
    
    Change-Id: I071c6e191c0ed5b718bd6a0e19117b2c76db44d2
    Signed-off-by: Arpit Singh <AS00745003@techmahindra.com>

--- cps-service/src/test/groovy/org/onap/cps/impl/CpsDeltaServiceImplSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/impl/CpsDeltaServiceImplSpec.groovy
@@ -31,6 +31,7 @@ import org.onap.cps.api.exceptions.DataValidationException
 import org.onap.cps.api.model.Anchor
 import org.onap.cps.api.model.DataNode
 import org.onap.cps.utils.ContentType
+import org.onap.cps.utils.CpsDeltaServiceUtils
 import org.onap.cps.utils.DataMapper
 import org.onap.cps.utils.JsonObjectMapper
 import org.onap.cps.utils.PrefixResolver
@@ -60,7 +61,8 @@ class CpsDeltaServiceImplSpec extends Specification {
     def dataMapper = new DataMapper(mockCpsAnchorService, mockPrefixResolver)
     def jsonObjectMapper = new JsonObjectMapper(new ObjectMapper())
     def deltaReportHelper = new DeltaReportHelper()
-    def objectUnderTest = new CpsDeltaServiceImpl(mockCpsAnchorService, mockCpsDataService, dataNodeFactory, dataMapper, jsonObjectMapper, deltaReportHelper)
+    def cpsDeltaServiceUtils = new CpsDeltaServiceUtils(deltaReportHelper)
+    def objectUnderTest = new CpsDeltaServiceImpl(mockCpsAnchorService, mockCpsDataService, dataNodeFactory, dataMapper, jsonObjectMapper, cpsDeltaServiceUtils, deltaReportHelper)
 
     static def bookstoreDataNodeWithParentXpath = [new DataNode(xpath: '/bookstore', leaves: ['bookstore-name': 'Easons'])]
     static def bookstoreDataNodeWithChildXpath = [new DataNode(xpath: '/bookstore/categories[@code=\'02\']', leaves: ['code': '02', 'name': 'Kids'])]
