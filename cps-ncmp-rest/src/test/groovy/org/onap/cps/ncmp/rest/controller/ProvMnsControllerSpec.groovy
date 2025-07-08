6b199e461abbcf50cac40fe78f4740e6dc200af1
seanbeirne <sean.beirne@est.tech>
2025-07-08 09:55:56 +0100

    Create controller for ProvMnS interface on NCMP
    Issue-ID: CPS-2702
    Change-Id: I38e1c518e166991ee1f78858edbcbea63ba744cc
    Signed-off-by: seanbeirne <sean.beirne@est.tech>

---
+++ cps-ncmp-rest/src/test/groovy/org/onap/cps/ncmp/rest/controller/ProvMnsControllerSpec.groovy
@@ -0,0 +1,99 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.rest.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.onap.cps.ncmp.rest.provmns.model.ResourceOneOf
+import org.onap.cps.utils.JsonObjectMapper
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+
+@WebMvcTest(ProvMnsController)
+class ProvMnsControllerSpec extends Specification {
+
+    @Autowired
+    MockMvc mvc
+
+    @SpringBean
+    ObjectMapper objectMapper = new ObjectMapper()
+
+    @SpringBean
+    JsonObjectMapper jsonObjectMapper = new JsonObjectMapper(objectMapper)
+
+    @Value('${rest.api.provmns-base-path}')
+    def provMnSBasePath
+
+    def 'Get Resource Data from pass-through operational.'() {
+        given: 'resource data url'
+            def getUrl = "$provMnSBasePath/test=another"
+        when: 'get data resource request is performed'
+            def response = mvc.perform(get(getUrl).contentType(MediaType.APPLICATION_JSON)).andReturn().response
+        then: 'response status is Ok'
+            assert response.status == HttpStatus.NOT_IMPLEMENTED.value()
+    }
+
+    def 'Put Resource Data from pass-through operational.'() {
+        given: 'resource data url'
+            def putUrl = "$provMnSBasePath/test=another"
+            def jsonBody = jsonObjectMapper.asJsonString(new ResourceOneOf('test'))
+        when: 'get data resource request is performed'
+            def response = mvc.perform(put(putUrl)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonBody))
+                    .andReturn().response
+        then: 'response status is Ok'
+            assert response.status == HttpStatus.NOT_IMPLEMENTED.value()
+    }
+
+    def 'Patch Resource Data from pass-through operational.'() {
+        given: 'resource data url'
+            def patchUrl = "$provMnSBasePath/test=another"
+            def jsonBody = jsonObjectMapper.asJsonString(new ResourceOneOf('test'))
+        when: 'get data resource request is performed'
+            def response = mvc.perform(patch(patchUrl)
+                    .contentType(new MediaType('application', 'json-patch+json'))
+                    .content(jsonBody))
+                    .andReturn().response
+        then: 'response status is Ok'
+            assert response.status == HttpStatus.NOT_IMPLEMENTED.value()
+    }
+
+    def 'Delete Resource Data from pass-through operational.'() {
+        given: 'resource data url'
+            def deleteUrl = "$provMnSBasePath/test=another"
+        when: 'delete data resource request is performed'
+            def response = mvc.perform(delete(deleteUrl)).andReturn().response
+        then: 'response status is Ok'
+            assert response.status == HttpStatus.NOT_IMPLEMENTED.value()
+    }
+
+}
