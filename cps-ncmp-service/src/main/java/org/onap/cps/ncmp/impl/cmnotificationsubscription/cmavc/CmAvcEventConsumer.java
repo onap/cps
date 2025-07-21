3cb7904e51a6f57dd19a4f9fe57be5128d52cc21
mpriyank <priyank.maheshwari@est.tech>
2025-07-21 12:21:46 +0100

    Process Cm Avc Event in NCMP
    - process the cm avc event when the source system is set as ONAP DMI
      Plugin in the header
    - handle the create , update , patch and delete operation to the CPS
      Cache.
    - Testware to support the write operation
    - Usage of the RestConf to Cps Path utility in this patch
    
    Issue-ID: CPS-2777
    Change-Id: I43233cf52f411a5d3cc20b03d0ef6e6e50399aa7
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventConsumer.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventConsumer.java
@@ -20,12 +20,19 @@
 
 package org.onap.cps.ncmp.impl.cmnotificationsubscription.cmavc;
 
+import static org.onap.cps.ncmp.utils.events.CloudEventMapper.toTargetEvent;
+
 import io.cloudevents.CloudEvent;
+import io.cloudevents.kafka.impl.KafkaHeaders;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Headers;
 import org.onap.cps.events.EventsProducer;
+import org.onap.cps.ncmp.api.inventory.models.CompositeState;
+import org.onap.cps.ncmp.events.avc1_0_0.AvcEvent;
+import org.onap.cps.ncmp.impl.inventory.InventoryPersistence;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -41,25 +48,48 @@ import org.springframework.stereotype.Component;
 public class CmAvcEventConsumer {
 
 
+    private static final String CLOUD_EVENT_SOURCE_SYSTEM_HEADER_KEY = "ce_source";
+
     @Value("${app.ncmp.avc.cm-events-topic}")
     private String cmEventsTopicName;
 
     private final EventsProducer<CloudEvent> eventsProducer;
+    private final CmAvcEventService cmAvcEventService;
+    private final InventoryPersistence inventoryPersistence;
 
     /**
      * Incoming Cm AvcEvent in the form of Consumer Record, it will be forwarded as is to a target topic.
      * The key from incoming record will be used as key for the target topic as well to preserve the message ordering.
+     * If event is coming from ONAP-DMI-PLUGIN then the event will also be processed by NCMP and the cps cache/database
+     * will be updated.
      *
      * @param cmAvcEventAsConsumerRecord Incoming raw consumer record
      */
     @KafkaListener(topics = "${app.dmi.cm-events.topic}",
             containerFactory = "cloudEventConcurrentKafkaListenerContainerFactory")
     @Timed(value = "cps.ncmp.cmnotifications.consumeandforward", description = "Time taken to forward CM AVC events")
-    public void consumeAndForward(
-            final ConsumerRecord<String, CloudEvent> cmAvcEventAsConsumerRecord) {
+    public void consumeAndForward(final ConsumerRecord<String, CloudEvent> cmAvcEventAsConsumerRecord) {
+        if (isEventFromOnapDmiPlugin(cmAvcEventAsConsumerRecord.headers())) {
+            processCmAvcEventChanges(cmAvcEventAsConsumerRecord);
+        }
         final CloudEvent outgoingAvcEvent = cmAvcEventAsConsumerRecord.value();
         final String outgoingAvcEventKey = cmAvcEventAsConsumerRecord.key();
         log.debug("Consuming AVC event with key : {} and value : {}", outgoingAvcEventKey, outgoingAvcEvent);
         eventsProducer.sendCloudEvent(cmEventsTopicName, outgoingAvcEventKey, outgoingAvcEvent);
+    }
+
+    private void processCmAvcEventChanges(final ConsumerRecord<String, CloudEvent> cmAvcEventAsConsumerRecord) {
+        final String cmHandleId = cmAvcEventAsConsumerRecord.key();
+        final CompositeState cmHandleState = inventoryPersistence.getCmHandleState(cmHandleId);
+        if (Boolean.TRUE.equals(cmHandleState.getDataSyncEnabled())) {
+            final AvcEvent cmAvcEvent = toTargetEvent(cmAvcEventAsConsumerRecord.value(), AvcEvent.class);
+            log.debug("Event to be processed to update the cache with cmHandleId : {}", cmHandleId);
+            cmAvcEventService.processCmAvcEvent(cmHandleId, cmAvcEvent);
+        }
+    }
+
+    private boolean isEventFromOnapDmiPlugin(final Headers headers) {
+        final String sourceSystem = KafkaHeaders.getParsedKafkaHeader(headers, CLOUD_EVENT_SOURCE_SYSTEM_HEADER_KEY);
+        return sourceSystem != null && sourceSystem.equals("ONAP-DMI-PLUGIN");
     }
 }
