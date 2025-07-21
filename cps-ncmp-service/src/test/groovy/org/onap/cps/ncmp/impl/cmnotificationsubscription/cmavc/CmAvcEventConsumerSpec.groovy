cd4f570d5357de9c44ddda09834abdd6385e0d3a
mpriyank <priyank.maheshwari@est.tech>
2025-07-21 12:21:46 +0100

    Process Cm Avc Event in NCMP
    - process the cm avc event when the source system is set as ONAP DMI
      Plugin in the header
    - handle the create , update , patch and delete operation to the CPS
      Cache.
    - Testware to support the write operation
    - Usage of the RestConf to Cps Path utility in this patch
    
    Issue-ID: CPS-2777
    Change-Id: I43233cf52f411a5d3cc20b03d0ef6e6e50399aa7
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventConsumerSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventConsumerSpec.groovy
@@ -28,8 +28,9 @@ import io.cloudevents.kafka.impl.KafkaHeaders
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.onap.cps.events.EventsProducer
+import org.onap.cps.ncmp.api.inventory.models.CompositeState
 import org.onap.cps.ncmp.events.avc1_0_0.AvcEvent
-import org.onap.cps.ncmp.utils.TestUtils
+import org.onap.cps.ncmp.impl.inventory.InventoryPersistence
 import org.onap.cps.ncmp.utils.events.MessagingBaseSpec
 import org.onap.cps.utils.JsonObjectMapper
 import org.spockframework.spring.SpringBean
@@ -37,8 +38,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.testcontainers.spock.Testcontainers
+
+import java.nio.charset.Charset
 import java.time.Duration
 
+import static org.onap.cps.ncmp.utils.TestUtils.getResourceFileContent
 import static org.onap.cps.ncmp.utils.events.CloudEventMapper.toTargetEvent
 
 @SpringBootTest(classes = [EventsProducer, CmAvcEventConsumer, ObjectMapper, JsonObjectMapper])
@@ -49,49 +53,84 @@ class CmAvcEventConsumerSpec extends MessagingBaseSpec {
     @SpringBean
     EventsProducer eventsProducer = new EventsProducer<CloudEvent>(legacyEventKafkaTemplate, cloudEventKafkaTemplate)
 
+    def mockCmAvcEventService = Mock(CmAvcEventService)
+    def mockInventoryPersistence = Mock(InventoryPersistence)
+
     @SpringBean
-    CmAvcEventConsumer acvEventConsumer = new CmAvcEventConsumer(eventsProducer)
+    CmAvcEventConsumer cmAvcEventConsumer = new CmAvcEventConsumer(eventsProducer, mockCmAvcEventService, mockInventoryPersistence)
 
     @Autowired
     JsonObjectMapper jsonObjectMapper
 
-    def cloudEventKafkaConsumer = new KafkaConsumer<>(eventConsumerConfigProperties('ncmp-group', CloudEventDeserializer))
+    def cloudEventKafkaConsumer = new KafkaConsumer<>(eventConsumerConfigProperties('group for Test A', CloudEventDeserializer))
 
-    def 'Consume and forward valid message'() {
-        given: 'consumer has a subscription on a topic'
-            def cmEventsTopicName = 'cm-events'
-            acvEventConsumer.cmEventsTopicName = cmEventsTopicName
-            cloudEventKafkaConsumer.subscribe([cmEventsTopicName] as List<String>)
-        and: 'an event is sent'
-            def jsonData = TestUtils.getResourceFileContent('sampleAvcInputEvent.json')
-            def testEventKey = 'sample-eventid-key'
-            def testEventSent = jsonObjectMapper.convertJsonString(jsonData, AvcEvent.class)
-            def testCloudEventSent = CloudEventBuilder.v1()
-                .withData(jsonObjectMapper.asJsonBytes(testEventSent))
-                .withId('sample-eventid')
-                .withType('sample-test-type')
-                .withSource(URI.create('sample-test-source'))
-                .withExtension('correlationid', 'test-cmhandle1').build()
-        and: 'event has header information'
-            def consumerRecord = new ConsumerRecord<String, CloudEvent>(cmEventsTopicName, 0, 0, testEventKey, testCloudEventSent)
-        when: 'the event is consumed and forwarded to target topic'
-            acvEventConsumer.consumeAndForward(consumerRecord)
-        and: 'the target topic is polled'
-            def records = cloudEventKafkaConsumer.poll(Duration.ofMillis(1500))
-        then: 'poll returns one record'
-            assert records.size() == 1
-        and: 'target record can be converted to AVC event'
-            def record = records.iterator().next()
-            def cloudEvent = record.value() as CloudEvent
-            def convertedAvcEvent = toTargetEvent(cloudEvent, AvcEvent.class)
-        and: 'the target event has the same key as the source event to maintain the ordering in a partition'
-            assert record.key() == consumerRecord.key()
-        and: 'we have correct headers forwarded where correlation id matches'
-            assert KafkaHeaders.getParsedKafkaHeader(record.headers(), 'ce_correlationid') == 'test-cmhandle1'
-        and: 'event id is same between consumed and forwarded'
-            assert KafkaHeaders.getParsedKafkaHeader(record.headers(), 'ce_id') == 'sample-eventid'
-        and: 'the event payload still matches'
-            assert testEventSent == convertedAvcEvent
+    def testAvcEvent
+
+    def setup() {
+        testAvcEvent = jsonObjectMapper.convertJsonString(getResourceFileContent('sampleAvcInputEvent.json'), AvcEvent.class)
     }
 
+    def 'Test A : Consume and forward valid message'() {
+        given: 'a cloud event'
+            def testEventKey = 'sample-eventid-key'
+            def testCloudEventSent = buildCloudEvent('sample-source', 'test-cmhandle1')
+        and: 'consumer has a subscription on the target topic for this test'
+            cmAvcEventConsumer.cmEventsTopicName = 'target-topic-for-Test-A'
+            cloudEventKafkaConsumer.subscribe([cmAvcEventConsumer.cmEventsTopicName])
+        and: 'event is wrapped in a consumer record with message key(cmHandleId) and value as cloud event'
+            def consumerRecordReceived = new ConsumerRecord<String, CloudEvent>(cmAvcEventConsumer.cmEventsTopicName, 0, 0, testEventKey, testCloudEventSent)
+        when: 'the event is consumed and forwarded to target topic'
+            cmAvcEventConsumer.consumeAndForward(consumerRecordReceived)
+        then: 'the consumer record can be read from the target topic within 2 seconds'
+            def consumerRecordOnTargetTopic = cloudEventKafkaConsumer.poll(Duration.ofMillis(2000)).iterator().next()
+        and: 'the target event has the same key as the source event to maintain the ordering in a partition'
+            def cloudEventFromTargetTopic = consumerRecordOnTargetTopic.value() as CloudEvent
+            def avcEventFromTargetTopic = toTargetEvent(cloudEventFromTargetTopic, AvcEvent.class)
+            assert consumerRecordOnTargetTopic.key() == consumerRecordReceived.key()
+        and: 'we have correct headers forwarded where correlation id matches'
+            assert KafkaHeaders.getParsedKafkaHeader(consumerRecordOnTargetTopic.headers(), 'ce_correlationid') == 'test-cmhandle1'
+        and: 'event id is same between consumed and forwarded'
+            assert KafkaHeaders.getParsedKafkaHeader(consumerRecordOnTargetTopic.headers(), 'ce_id') == 'sample-eventid'
+        and: 'the event payload still matches'
+            assert avcEventFromTargetTopic == testAvcEvent
+    }
+
+    def 'Test B : Consume and process CM Avc Event when #scenario'() {
+        given: 'a cloud event is created(what we get from ONAP-DMI-PLUGIN)'
+            def testEventKey = 'sample-key'
+            def sourceSystem = 'ONAP-DMI-PLUGIN'
+            def testCloudEventSent = buildCloudEvent(sourceSystem, 'some-cmhandle-id')
+        and: 'a separate topic for this test'
+            cmAvcEventConsumer.cmEventsTopicName =  'some-topic-for-Test-B'
+        and: 'inventory persistence service has #scenario'
+            def compositeState = new CompositeState(dataSyncEnabled: dataSyncFlag)
+            1 * mockInventoryPersistence.getCmHandleState(_) >> compositeState
+        and: 'event has source system as ONAP-DMI-PLUGIN and key(cmHandleId) and value as cloud event'
+            def consumerRecord = new ConsumerRecord<String, CloudEvent>(cmAvcEventConsumer.cmEventsTopicName, 0, 0, testEventKey, testCloudEventSent)
+            consumerRecord.headers().add('ce_source', sourceSystem.getBytes(Charset.defaultCharset()))
+        when: 'the event is consumed'
+            cmAvcEventConsumer.consumeAndForward(consumerRecord)
+        then: 'cm avc event is processed for updating the cached data'
+            expectedCallToProcessCmAvcEvent * mockCmAvcEventService.processCmAvcEvent(testEventKey, _) >> { args ->
+                {
+                    assert args[1] instanceof AvcEvent
+                }
+            }
+        where: 'following scenarios are used'
+            scenario                  | dataSyncFlag || expectedCallToProcessCmAvcEvent
+            'data sync flag enabled'  | true         || 1
+            'data sync flag disabled' | false        || 0
+
+    }
+
+    def buildCloudEvent(sourceSystem, cmHandleId){
+
+        return CloudEventBuilder.v1()
+            .withData(jsonObjectMapper.asJsonBytes(testAvcEvent))
+            .withId('sample-eventid')
+            .withType('sample-test-type')
+            .withSource(URI.create(sourceSystem as String))
+            .withExtension('correlationid', cmHandleId).build()
+
+    }
 }
---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/cmnotificationsubscription/cmavc/CmAvcEventServiceSpec.groovy
@@ -0,0 +1,148 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2025 OpenInfra Foundation Europe. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.impl.cmnotificationsubscription.cmavc
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.onap.cps.api.CpsAnchorService
+import org.onap.cps.api.CpsDataService
+import org.onap.cps.api.model.Anchor
+import org.onap.cps.cpspath.parser.CpsPathUtil
+import org.onap.cps.ncmp.events.avc1_0_0.AvcEvent
+import org.onap.cps.ncmp.events.avc1_0_0.Data
+import org.onap.cps.ncmp.events.avc1_0_0.DatastoreChanges
+import org.onap.cps.ncmp.events.avc1_0_0.Edit
+import org.onap.cps.ncmp.events.avc1_0_0.IetfYangPatchYangPatch
+import org.onap.cps.ncmp.events.avc1_0_0.PushChangeUpdate
+import org.onap.cps.utils.JsonObjectMapper
+import org.onap.cps.utils.YangParser
+import spock.lang.Specification
+
+class CmAvcEventServiceSpec extends Specification {
+
+    def mockCpsDataService = Mock(CpsDataService)
+    def mockCpsAnchorService = Mock(CpsAnchorService)
+    def jsonObjectMapper = new JsonObjectMapper(new ObjectMapper())
+    def mockYangParser = Mock(YangParser)
+
+    def static NO_TIMESTAMP = null
+    def static NO_XPATH = ''
+
+    def objectUnderTest = new CmAvcEventService(
+        mockCpsDataService,
+        mockCpsAnchorService,
+        jsonObjectMapper,
+        mockYangParser
+    )
+
+    def cmHandleId = 'test-cmhandle-id'
+    def sampleJson = '{"some-data": "test-data"}'
+
+    def 'process CREATE operation'() {
+        given: 'An edit with CREATE operation'
+            def testAvcEventForCreate = testAvcEvent('create', NO_XPATH)
+        when: 'The AVC event is processed'
+            objectUnderTest.processCmAvcEvent(cmHandleId, testAvcEventForCreate)
+        then: 'data is saved via cps service'
+            1 * mockCpsDataService.saveData(_, cmHandleId, sampleJson, NO_TIMESTAMP)
+    }
+
+    def 'Process UPDATE operation'() {
+        given: 'An edit with UPDATE operation and a valid target path'
+            def targetPath = '/test/path'
+            def anchor = Mock(Anchor)
+            mockCpsAnchorService.getAnchor(_, cmHandleId) >> anchor
+            mockYangParser.getCpsPathFromRestConfStylePath(anchor, targetPath) >> '/parsed/cps/path'
+            def testAvcEventForUpdate = testAvcEvent('update', targetPath)
+        when: 'The AVC event is processed'
+            objectUnderTest.processCmAvcEvent(cmHandleId, testAvcEventForUpdate)
+        then: 'Data node and descendants are updated via CPS service'
+            1 * mockCpsDataService.updateDataNodeAndDescendants(_, cmHandleId, _, sampleJson, NO_TIMESTAMP, _)
+    }
+
+    def 'Process PATCH operation'() {
+        given: 'An edit with PATCH operation and a valid target path'
+            def targetPath = '/test/path'
+            def anchor = Mock(Anchor)
+            mockCpsAnchorService.getAnchor(_, cmHandleId) >> anchor
+            mockYangParser.getCpsPathFromRestConfStylePath(anchor, targetPath) >> '/parsed/cps/path'
+            def testAvcEventForPatch = testAvcEvent('patch', targetPath)
+        when: 'The AVC event is processed'
+            objectUnderTest.processCmAvcEvent(cmHandleId, testAvcEventForPatch)
+        then: 'Node leaves are updated via CPS service'
+            1 * mockCpsDataService.updateNodeLeaves(_, cmHandleId, _, sampleJson, NO_TIMESTAMP, _)
+    }
+
+    def 'Process DELETE operation with target'() {
+        given: 'An edit with DELETE operation and a specific target path'
+            def targetPath = '/test/path'
+            def anchor = Mock(Anchor)
+            mockCpsAnchorService.getAnchor(_, cmHandleId) >> anchor
+            mockYangParser.getCpsPathFromRestConfStylePath(anchor, targetPath) >> '/parsed/cps/path'
+            def testAvcEventForDelete = testAvcEvent('delete', targetPath)
+        when: 'The AVC event is processed'
+            objectUnderTest.processCmAvcEvent(cmHandleId, testAvcEventForDelete)
+        then: 'Data node is deleted at the given path'
+            1 * mockCpsDataService.deleteDataNode(_, cmHandleId, '/parsed/cps/path', NO_TIMESTAMP)
+    }
+
+    def 'Process DELETE operation with no target (delete all)'() {
+        given: 'An edit with DELETE operation and no target'
+            def testAvcEventForDelete = testAvcEvent('delete', NO_XPATH)
+        when: 'The AVC event is processed'
+            objectUnderTest.processCmAvcEvent(cmHandleId, testAvcEventForDelete)
+        then: 'All data nodes for the cmHandleId are deleted'
+            1 * mockCpsDataService.deleteDataNodes(_, cmHandleId, NO_TIMESTAMP)
+        where: 'following targets are used'
+            target << [null, '']
+    }
+
+    def 'Resolve parent xpath correctly: #scenario'() {
+        expect: 'Parent xpath is resolved as expected'
+            assert objectUnderTest.resolveParentNodeXpath(inputXpath) == expectedXpath
+        where: 'following scenarios are used'
+            scenario                        | inputXpath   || expectedXpath
+            'when parentXpath is empty'     | ''           || CpsPathUtil.ROOT_NODE_XPATH
+            'when parentXpath is not empty' | '/test/path' || '/test/path'
+    }
+
+    def testAvcEvent(operation, targetXpath) {
+        new AvcEvent(
+            data: new Data(
+                pushChangeUpdate: new PushChangeUpdate(
+                    datastoreChanges: new DatastoreChanges(
+                        ietfYangPatchYangPatch: new IetfYangPatchYangPatch(
+                            patchId: 'test-patch-id',
+                            edit: [
+                                new Edit(
+                                    operation: operation,
+                                    editId: 'test-edit-id',
+                                    target: targetXpath,
+                                    value: sampleJson
+                                )
+                            ]
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+}
