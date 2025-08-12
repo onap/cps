6d73b4c6ab668c20596f74af102535d11bf5d260
Rudrangi Anupriya <ra00745022@techmahindra.com>
2025-08-12 14:21:19 +0530

    XML content support on Get delta between two anchors within a given dataspace
    Here to bring Support for XML content on Get delta between two anchors within a given dataspace
    
    - Add deltaReportSampleXml Example in component.yaml
    - Add ContentTypeInheader in cpsDataV2.yml to support application/xml in Delta
    - Add contentTypeInHeader parameter to accept xml in DataRestController.java
    - Implemented Logic to convert deltaReport to XML in XmlFileUtils.java
    - written testcase for above changes made
    
    Issue-ID: CPS-2452
    Change-Id: Iff7fab66ddcc03703255123c6d5c2eade0e7cb4a
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

---
+++ cps-service/src/main/java/org/onap/cps/utils/deltareport/DeltaReportUtils.java
@@ -0,0 +1,146 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 TechMahindra Ltd.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.utils.deltareport;
+
+import static org.onap.cps.utils.XmlUtils.getDocumentBuilderFactory;
+import static org.onap.cps.utils.XmlUtils.getTransformerFactory;
+
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.onap.cps.api.exceptions.DataValidationException;
+import org.onap.cps.api.model.DeltaReport;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DeltaReportUtils {
+    private static DocumentBuilder getDocumentBuilder() throws ParserConfigurationException {
+        return getDocumentBuilderFactory().newDocumentBuilder();
+    }
+
+    /**
+     * Converts a collection of DeltaReport objects to XML format using DOM.
+     *
+     * @param deltaReports The collection of DeltaReport objects.
+     * @return The XML string representation of the delta reports.
+     */
+    public static String buildXmlUsingDom(final List<DeltaReport> deltaReports) {
+        try {
+            final DocumentBuilder documentBuilder = getDocumentBuilder();
+            final Document document = documentBuilder.newDocument();
+
+            final Element rootElement = document.createElement("deltaReports");
+            document.appendChild(rootElement);
+
+            int id = 1;
+            for (final DeltaReport deltaReport : deltaReports) {
+                final Element deltaReportElement = document.createElement("deltaReport");
+                deltaReportElement.setAttribute("id", String.valueOf(id++));
+
+                createKeyValueElement(document, deltaReportElement, "action", deltaReport.getAction());
+                createKeyValueElement(document, deltaReportElement, "xpath", deltaReport.getXpath());
+
+                final Element sourceDataElement = convertMapToElement(document, deltaReportElement,
+                        "source-data", deltaReport.getSourceData());
+                if (sourceDataElement != null) {
+                    deltaReportElement.appendChild(sourceDataElement);
+                }
+                final Element targetDataElement = convertMapToElement(document, deltaReportElement,
+                        "target-data", deltaReport.getTargetData());
+                if (targetDataElement != null) {
+                    deltaReportElement.appendChild(targetDataElement);
+                }
+                rootElement.appendChild(deltaReportElement);
+            }
+            return transformDocumentToString(document);
+
+        } catch (final DOMException | ParserConfigurationException
+                       | TransformerException exception) {
+            throw new DataValidationException("Data Validation Failed", "Failed to build XML deltaReport: "
+                    + exception.getMessage(), exception);
+        }
+    }
+
+    private static Element convertMapToElement(final Document document, final Element parentElement, final String name,
+                                               final Map<String, Serializable> data) {
+        if (data != null && !data.isEmpty()) {
+            final Element element = document.createElement(name);
+
+            for (final Map.Entry<String, Serializable> mapentry : data.entrySet()) {
+                final Element childElement = createXmlElement(document, mapentry);
+                element.appendChild(childElement);
+            }
+            parentElement.appendChild(element);
+            return element;
+        }
+        return null;
+    }
+
+    private static Element createKeyValueElement(final Document document, final Element parentElement,
+                                                 final String name, final String value) {
+        if (value != null && !value.isEmpty()) {
+            final Element element = document.createElement(name);
+            element.appendChild(document.createTextNode(value));
+            parentElement.appendChild(element);
+            return parentElement;
+        }
+        return null;
+    }
+
+    private static Element createXmlElement(final Document document,
+                                            final Map.Entry<String, Serializable> inputMap) {
+        final Element element = document.createElement(inputMap.getKey());
+        final Serializable mapValue = inputMap.getValue();
+        if (mapValue instanceof Collection) {
+            final String collectionAsCsvString = ((Collection<?>) mapValue)
+                    .stream().map(Object::toString).collect(Collectors.joining(", "));
+            element.appendChild(document.createTextNode(collectionAsCsvString));
+        } else {
+            element.appendChild(document.createTextNode(mapValue.toString()));
+        }
+        return element;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    private static String transformDocumentToString(final Document document) throws TransformerException {
+        final Transformer transformer = getTransformerFactory().newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        final StringWriter stringWriter = new StringWriter();
+        final StreamResult streamResult = new StreamResult(stringWriter);
+        transformer.transform(new DOMSource(document), streamResult);
+        return stringWriter.toString();
+    }
+}
+
---
+++ cps-service/src/test/groovy/org/onap/cps/utils/DeltaReportUtilsSpec.groovy
@@ -0,0 +1,81 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Deutsche Telekom AG
+ *  Modifications Copyright (c) 2023-2024 Nordix Foundation
+ *  Modifications Copyright (C) 2024-2025 TechMahindra Ltd.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.onap.cps.utils
+
+import org.onap.cps.api.exceptions.DataValidationException
+import org.w3c.dom.DOMException
+import spock.lang.Specification
+import org.onap.cps.api.model.DeltaReport
+import static org.onap.cps.utils.deltareport.DeltaReportUtils.buildXmlUsingDom
+
+class DeltaReportUtilsSpec extends Specification {
+
+    def 'Converting delta report to XML scenario'() {
+        when: 'deltaReports are converted to XML'
+            def result = buildXmlUsingDom(deltaReports)
+        then: 'the result contains the expected XML'
+            assert result == expectedXmlOutput
+        where:
+            scenario                          | deltaReports || expectedXmlOutput
+            'Empty deltaReports list'         | []           || '<deltaReports/>'
+            'deltaReport with Replace action' | [new DeltaReport(action: 'replace', xpath: '/bookstore', sourceData: ['bookstore-name': 'Easons-1'], targetData: ['bookstore-name': 'Crossword Bookstores'])] || '<deltaReports><deltaReport id="1"><action>replace</action><xpath>/bookstore</xpath><source-data><bookstore-name>Easons-1</bookstore-name></source-data><target-data><bookstore-name>Crossword Bookstores</bookstore-name></target-data></deltaReport></deltaReports>'
+            'deltaReport with Delete action'  | [new DeltaReport(action: 'Delete', xpath: "/bookstore/categories[@code=5]/books[@title= Book-1]", sourceData: ['price': '1'], targetData: null)] || '<deltaReports><deltaReport id="1"><action>Delete</action><xpath>/bookstore/categories[@code=5]/books[@title= Book-1]</xpath><source-data><price>1</price></source-data></deltaReport></deltaReports>'
+            'deltaReport with Add action'     | [new DeltaReport(action: 'Add', xpath: "/bookstore/categories[@code=5]/books[@title= Book-1]", sourceData: null, targetData: ['price': '1'])] || '<deltaReports><deltaReport id="1"><action>Add</action><xpath>/bookstore/categories[@code=5]/books[@title= Book-1]</xpath><target-data><price>1</price></target-data></deltaReport></deltaReports>'
+            'deltaReport with null lists'     | [new DeltaReport(action: 'Add', xpath: '/bookstore', sourceData: null, targetData: null)]  || '<deltaReports><deltaReport id="1"><action>Add</action><xpath>/bookstore</xpath></deltaReport></deltaReports>'
+    }
+
+    def 'Converting delta reports with list nodes to XML'() {
+        given: 'A deltaReport with nested collections in sourceData and targetData'
+             def deltaReports = [
+                     new DeltaReport(
+                        action: 'Update',
+                        xpath: '/bookstore',
+                        sourceData: ['books': ['Book-1', 'Book-2', 'Book-3']],
+                        targetData: ['authors': ['Author-1', 'Author-2']]
+                )
+        ]
+        when: 'deltaReports are converted to XML'
+           def result = buildXmlUsingDom(deltaReports)
+        then: 'the result contains the expected XML with nested collection values as comma-separated strings'
+            assert result == '<deltaReports><deltaReport id="1"><action>Update</action><xpath>/bookstore</xpath><source-data><books>Book-1, Book-2, Book-3</books></source-data><target-data><authors>Author-1, Author-2</authors></target-data></deltaReport></deltaReports>'
+    }
+
+    def 'Converting deltaReport to XML with document syntax error'() {
+        given: 'A list of maps with an invalid entry'
+           def deltaReport = [new DeltaReport(action: 'replace', xpath: '/bookstore', sourceData: ['bookstore-name': 'Easons-1'], targetData: ['invalid<tag>': 'value'])]
+        when: 'convert the deltaReport to XML'
+            buildXmlUsingDom(deltaReport)
+        then: 'a validation exception is thrown'
+           def exception = thrown(DataValidationException)
+        and: 'the cause is a document object model exception'
+           assert exception.cause instanceof DOMException
+    }
+
+    def 'deltaReport with all fields null'() {
+        given: 'A deltaReport with all fields set to null'
+            def deltaReports = [new DeltaReport(action: null, xpath: null, sourceData: null, targetData: null)]
+        when: 'buildXmlUsingDom is called'
+            def result = buildXmlUsingDom(deltaReports)
+        then: 'the result contains a deltaReport element with only an id'
+            assert result == '<deltaReports><deltaReport id="1"/></deltaReports>'
+    }
+}
