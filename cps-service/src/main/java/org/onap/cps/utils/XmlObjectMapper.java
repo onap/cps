e24bbb847c6536975d4c53324ec7269d3487ead0
Rudrangi Anupriya <ra00745022@techmahindra.com>
2025-08-12 14:21:19 +0530

    [INTERNAL REVIEW] XML content support on Get delta between two anchors within a given dataspace
    Here to bring Support for XML content on Get delta between two anchors within a given dataspace
    
    - Add deltaReportSampleXml Example in component.yaml
    - Add ContentTypeInheader in cpsDataV2.yml to support application/xml in Delta
    - Add contentTypeInHeader parameter to accept xml in DataRestController.java
    - Implemented Logic to convert deltaReport to XML in XmlFileUtils.java
    - written testcase for above changes made
    
    Issue-ID: CPS-2452
    Change-Id: Iff7fab66ddcc03703255123c6d5c2eade0e7cb4a
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

---
+++ cps-service/src/main/java/org/onap/cps/utils/XmlObjectMapper.java
@@ -0,0 +1,55 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 Deutsche Telekom AG.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.utils;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import lombok.RequiredArgsConstructor;
+import org.onap.cps.api.exceptions.DataValidationException;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class XmlObjectMapper {
+
+    private final XmlMapper xmlMapper;
+
+    public XmlObjectMapper() {
+        this.xmlMapper = new XmlMapper();
+    }
+    /**
+     * Serializing generic java object to XML using Jackson.
+     *
+     * @param object any java object value
+     * @param rootName the name of the XML root name
+     * @return the generated XML as a string.
+     */
+
+    public String asXmlString(final Object object, final String rootName) {
+        try {
+            return xmlMapper.writer().withRootName(rootName).writeValueAsString(object);
+        } catch (final Exception exception) {
+            throw new DataValidationException("Data Validation Failed",
+                    "Failed to build XML: " + exception.getMessage(),
+                    exception
+            );
+        }
+    }
+}
