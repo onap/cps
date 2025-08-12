bf33e97020fbab61c8d6e25ec0f45ac6be549696
Rudrangi Anupriya <ra00745022@techmahindra.com>
2025-08-12 14:21:19 +0530

    XML content support on Get delta between two anchors within a given dataspace
    Here to bring Support for XML content on Get delta between two anchors within a given dataspace
    
    - Add deltaReportSampleXml Example in component.yaml
    - Add ContentTypeInheader in cpsDataV2.yml to support application/xml in Delta
    - Add contentTypeInHeader parameter to accept xml in DataRestController.java
    - Implemented Logic to convert deltaReport to XML in XmlFileUtils.java
    - written testcase for above changes made
    
    Issue-ID: CPS-2452
    Change-Id: Iff7fab66ddcc03703255123c6d5c2eade0e7cb4a
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

--- cps-service/src/main/java/org/onap/cps/utils/XmlUtils.java
+++ cps-service/src/main/java/org/onap/cps/utils/XmlUtils.java
@@ -55,7 +55,7 @@ import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class XmlFileUtils {
+public class XmlUtils {
 
     private static final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
     private static boolean isNewDocumentBuilderFactoryInstance = true;
@@ -186,7 +186,7 @@ public class XmlFileUtils {
             } else {
                 throw new IllegalArgumentException("Unsupported data type for XML conversion");
             }
-            return transformFragmentToString(documentFragment);
+            return transformNodeToString(documentFragment);
         } catch (final DOMException |  NullPointerException | ParserConfigurationException | TransformerException
                 exception) {
             throw new DataValidationException(
@@ -246,35 +246,44 @@ public class XmlFileUtils {
         parentNode.appendChild(element);
     }
 
-    private static String transformFragmentToString(final DocumentFragment documentFragment)
+    private static String transformNodeToString(final Node node)
             throws TransformerException {
         final Transformer transformer = getTransformerFactory().newTransformer();
         transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-        final StringWriter writer = new StringWriter();
-        final StreamResult result = new StreamResult(writer);
-        transformer.transform(new DOMSource(documentFragment), result);
-        return writer.toString();
+        final StringWriter stringWriter = new StringWriter();
+        final StreamResult streamResult = new StreamResult(stringWriter);
+        transformer.transform(new DOMSource(documentFragment), streamResult);
+        return stringWriter.toString();
     }
+    /**
+     * Provides a configured instance of DocumentBuilderFactory.
+     * This method initializes the factory with secure processing settings
+     * to prevent XML External Entity (XXE) attacks.
+     * @return a configured DocumentBuilderFactory instance
+     */
 
-    @SuppressWarnings("SameReturnValue")
-    private static DocumentBuilderFactory getDocumentBuilderFactory() {
+    public static DocumentBuilderFactory getDocumentBuilderFactory() {
         if (isNewDocumentBuilderFactoryInstance) {
             documentBuilderFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
             documentBuilderFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
             isNewDocumentBuilderFactoryInstance = false;
         }
-
         return documentBuilderFactory;
     }
+    /**
+     * Provides a configured instance of TransformerFactory.
+     * This method initializes the factory with secure settings to prevent
+     * unwanted stylesheet or DTD access.
+     * @return a configured TransformerFactory instance
+     */
 
     @SuppressWarnings("SameReturnValue")
-    private static TransformerFactory getTransformerFactory() {
+    public static TransformerFactory getTransformerFactory() {
         if (isNewTransformerFactoryInstance) {
             transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
             transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
             isNewTransformerFactoryInstance = false;
         }
-
         return transformerFactory;
     }
 }
