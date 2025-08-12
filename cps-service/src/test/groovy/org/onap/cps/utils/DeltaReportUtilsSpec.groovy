62c59da0a24dfc4cabbd8a7c5536f00ef4f79e86
Rudrangi Anupriya <ra00745022@techmahindra.com>
2025-08-12 14:21:19 +0530

    [INTERNAL REVIEW] XML content support on Get delta between two anchors within a given dataspace
    Here to bring Support for XML content on Get delta between two anchors within a given dataspace
    
    - Add deltaReportSampleXml Example in component.yaml
    - Add ContentTypeInheader in cpsDataV2.yml to support application/xml in Delta
    - Add contentTypeInHeader parameter to accept xml in DataRestController.java
    - Implemented Logic to convert deltaReport to XML in XmlFileUtils.java
    - written testcase for above changes made
    
    Issue-ID: CPS-2452
    Change-Id: Iff7fab66ddcc03703255123c6d5c2eade0e7cb4a
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

---
+++ cps-service/src/test/groovy/org/onap/cps/utils/DeltaReportUtilsSpec.groovy
@@ -0,0 +1,49 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 Deutsche Telekom AG.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.onap.cps.utils
+
+import org.onap.cps.api.exceptions.DataValidationException
+import spock.lang.Specification
+import org.onap.cps.api.model.DeltaReport
+import static org.onap.cps.utils.deltareport.DeltaReportUtils.buildXml
+
+class DeltaReportUtilsSpec extends Specification {
+
+    def 'Converting delta report to XML scenario'() {
+        when: 'deltaReports are converted to XML'
+            def result = buildXml(deltaReports)
+        then: 'the result contains the expected XML'
+            assert result == expectedResult
+        where:
+            scenario                           | deltaReports                                                                                                                                            || expectedResult
+            'Empty delta report'               | []                                                                                                                                                      ||'<deltaReports/>'
+            'delta report with replace action' | [new DeltaReport(action:'replace', xpath:'/bookstore',sourceData: ['bookstore-name':'Easons-1'],targetData: ['bookstore-name':'Crossword Bookstores'])] ||'<deltaReports><deltaReport><action>replace</action><xpath>/bookstore</xpath><sourceData><bookstore-name>Easons-1</bookstore-name></sourceData><targetData><bookstore-name>Crossword Bookstores</bookstore-name></targetData></deltaReport></deltaReports>'
+    }
+
+    def 'Converting invalid delta report to xml'() {
+        given: 'syntactically incorrect delta report'
+            def deltaReports = [new DeltaReport(action:'replace', xpath:'/bookstore', sourceData: ['bookstore-name':'Easons-1'],targetData: ['bad': { -> "value" }])]
+        when: 'attempt to build XML'
+            buildXml(deltaReports)
+        then: 'excepted exception is thrown'
+            def thrownException = thrown(DataValidationException)
+        and: 'the exception has correct message'
+            thrownException.message =='Data Validation Failed'
+    }
+}
