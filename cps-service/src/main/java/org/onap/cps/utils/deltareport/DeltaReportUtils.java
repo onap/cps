9461488c798705bc04cf4db0e44fddc32f723d4f
Rudrangi Anupriya <ra00745022@techmahindra.com>
2025-08-12 14:21:19 +0530

    [INTERNAL REVIEW] XML content support on Get delta between two anchors within a given dataspace
    Here to bring Support for XML content on Get delta between two anchors within a given dataspace
    
    - Add deltaReportSampleXml Example in component.yaml
    - Add ContentTypeInheader in cpsDataV2.yml to support application/xml in Delta
    - Add contentTypeInHeader parameter to accept xml in DataRestController.java
    - Implemented Logic to convert deltaReport to XML in XmlFileUtils.java
    - written testcase for above changes made
    
    Issue-ID: CPS-2452
    Change-Id: Iff7fab66ddcc03703255123c6d5c2eade0e7cb4a
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

---
+++ cps-service/src/main/java/org/onap/cps/utils/deltareport/DeltaReportUtils.java
@@ -0,0 +1,50 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 Deutsche Telekom AG.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.utils.deltareport;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.util.List;
+import org.onap.cps.api.exceptions.DataValidationException;
+import org.onap.cps.api.model.DeltaReport;
+import org.w3c.dom.DOMException;
+
+public final class DeltaReportUtils {
+    /**
+     * Converts a collection of DeltaReport objects to XML format using XmlMapper.
+     */
+    private DeltaReportUtils() {}
+
+    private static final XmlMapper XML_MAPPER = new XmlMapper();
+
+    public static String buildXml(final List<DeltaReport> deltaReports) {
+        try {
+            return XML_MAPPER
+                    .writer()
+                    .withRootName("deltaReports")
+                    .writeValueAsString(new DeltaReportWrapper(deltaReports));
+        } catch (final DOMException | NullPointerException | JsonProcessingException
+                exception) {
+            throw new DataValidationException("Data Validation Failed", "Failed to build XML deltaReport: "
+                    + exception.getMessage(), exception);
+        }
+    }
+}
---
+++ cps-service/src/test/groovy/org/onap/cps/utils/DeltaReportUtilsSpec.groovy
@@ -0,0 +1,54 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025 Deutsche Telekom AG.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.onap.cps.utils
+
+import org.onap.cps.api.exceptions.DataValidationException
+import spock.lang.Specification
+import org.onap.cps.api.model.DeltaReport
+import static org.onap.cps.utils.deltareport.DeltaReportUtils.buildXml
+
+class DeltaReportUtilsSpec extends Specification {
+
+    def 'Converting delta report to XML scenario'() {
+        when:'deltaReports are converted to XML'
+             def result = buildXml(deltaReports)
+        then:'the result contains the expected XML'
+             assert result == expectedXmlOutput
+        where:
+             scenario                          | deltaReports                                                                                                                                            || expectedXmlOutput
+            'Empty deltaReports list'          | []                                                                                                                                                      ||'<deltaReports/>'
+            'delta report with replace action' | [new DeltaReport(action:'replace', xpath:'/bookstore',sourceData: ['bookstore-name':'Easons-1'],targetData: ['bookstore-name':'Crossword Bookstores'])] ||'<deltaReports><deltaReport><action>replace</action><xpath>/bookstore</xpath><sourceData><bookstore-name>Easons-1</bookstore-name></sourceData><targetData><bookstore-name>Crossword Bookstores</bookstore-name></targetData></deltaReport></deltaReports>'
+            'delta report with remove action'  | [new DeltaReport(action:'remove', xpath:'/bookstore/categories[@code=5]/books[@title= Book-1]',sourceData: ['price':'1'], targetData: null)]            ||'<deltaReports><deltaReport><action>remove</action><xpath>/bookstore/categories[@code=5]/books[@title= Book-1]</xpath><sourceData><price>1</price></sourceData></deltaReport></deltaReports>'
+            'delta report with create action'  | [new DeltaReport(action:'create', xpath:'/bookstore/categories[@code=5]/books[@title= Book-1]', sourceData: null, targetData: ['price':'1'])]           ||'<deltaReports><deltaReport><action>create</action><xpath>/bookstore/categories[@code=5]/books[@title= Book-1]</xpath><targetData><price>1</price></targetData></deltaReport></deltaReports>'
+    }
+
+     def 'Converting delta report to XML with document syntax error'() {
+        given: 'A list of maps with an invalid entry'
+           def deltaReports = [
+                new DeltaReport(action:'replace', xpath:'/bookstore',
+                        sourceData: ['bookstore-name':'Easons-1'],
+                        targetData: ['bad': { -> "value" }])]
+        when: 'convert the dataMaps to XML'
+           buildXml(deltaReports)
+        then: 'a validation exception is thrown'
+           def exception = thrown(DataValidationException)
+        and: 'the cause is a document object model exception'
+           exception.message =='Data Validation Failed'
+     }
+}
