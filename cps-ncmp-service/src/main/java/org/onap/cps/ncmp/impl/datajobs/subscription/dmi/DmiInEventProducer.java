dac9d9dadd894f3eab798889a88f060b8c901f83
ToineSiebelink <toine.siebelink@est.tech>
2025-09-04 11:49:28 +0100

    Fix issue with package name cmnotificationsubscription
    - removed _1_0_0 postfix (inconsistent with others)
    - renamed/moved to ../datajobs/subscriptions
    
    Issue-ID: CPS-2941
    Change-Id: I2d461ecabc8d404bb9e936ef0a528eb98735ac03
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>

---
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/datajobs/subscription/dmi/DmiInEventProducer.java
@@ -0,0 +1,72 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2024-2025 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.impl.datajobs.subscription.dmi;
+
+import static org.onap.cps.ncmp.events.NcmpEventDataSchema.SUBSCRIPTIONS_V1;
+
+import io.cloudevents.CloudEvent;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.onap.cps.events.EventsProducer;
+import org.onap.cps.ncmp.impl.datajobs.subscription.ncmp_to_dmi.DmiInEvent;
+import org.onap.cps.ncmp.utils.events.NcmpEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "notification.enabled", havingValue = "true", matchIfMissing = true)
+public class DmiInEventProducer {
+
+    private final EventsProducer<CloudEvent> eventsProducer;
+
+    @Value("${app.ncmp.avc.cm-subscription-dmi-in}")
+    private String dmiInEventTopic;
+
+    /**
+     * Send the event to the provided dmi plugin with key as subscription id and the event is in Cloud Event format.
+     *
+     * @param subscriptionId CM subscription id
+     * @param dmiPluginName  Dmi plugin Name
+     * @param eventType      Type of event
+     * @param dmiInEvent     Cm Notification Subscription event for Dmi
+     */
+    public void sendDmiInEvent(final String subscriptionId, final String dmiPluginName,
+                               final String eventType, final DmiInEvent dmiInEvent) {
+        eventsProducer.sendCloudEvent(dmiInEventTopic, subscriptionId,
+            buildAndGetDmiInEventAsCloudEvent(subscriptionId, dmiPluginName, eventType, dmiInEvent));
+
+    }
+
+    private CloudEvent buildAndGetDmiInEventAsCloudEvent(final String subscriptionId, final String dmiPluginName,
+                                                         final String eventType, final DmiInEvent dmiInEvent) {
+        return NcmpEvent.builder()
+            .type(eventType)
+            .dataSchema(SUBSCRIPTIONS_V1.getDataSchema())
+            .extensions(Map.of("correlationid", String.join("#", subscriptionId, dmiPluginName)))
+            .data(dmiInEvent)
+            .build()
+            .asCloudEvent();
+    }
+
+
+}
---
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/datajobs/subscription/ncmp/CmSubscriptionComparatorSpec.groovy
@@ -0,0 +1,57 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (c) 2024-2025 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+package org.onap.cps.ncmp.impl.datajobs.subscription.ncmp
+
+import org.onap.cps.ncmp.impl.datajobs.subscription.models.DmiCmSubscriptionPredicate
+import org.onap.cps.ncmp.impl.datajobs.subscription.utils.CmDataJobSubscriptionPersistenceService
+import spock.lang.Specification
+
+import static org.onap.cps.ncmp.api.data.models.DatastoreType.PASSTHROUGH_OPERATIONAL
+
+class CmSubscriptionComparatorSpec extends Specification {
+
+    def mockCmSubscriptionPersistenceService = Mock(CmDataJobSubscriptionPersistenceService)
+    def objectUnderTest = new CmSubscriptionComparator(mockCmSubscriptionPersistenceService)
+
+    def 'Find the difference based on the provided predicates'() {
+        given: 'A list of predicates'
+            def predicates = [new DmiCmSubscriptionPredicate(['ch-1', 'ch-2'].toSet(), PASSTHROUGH_OPERATIONAL, ['a/1/', 'b/2'].toSet())]
+        and: '1 positive and 1 negative response.'
+            mockCmSubscriptionPersistenceService.hasAtLeastOneSubscription(PASSTHROUGH_OPERATIONAL.getDatastoreName(), 'ch-1') >> true
+            mockCmSubscriptionPersistenceService.hasAtLeastOneSubscription(PASSTHROUGH_OPERATIONAL.getDatastoreName(), 'ch-2') >> false
+        when: 'method to extract only NEW predicates for dmi is called'
+            def result = objectUnderTest.getNewDmiSubscriptionPredicates(predicates)
+        then: 'from 2 predicates only 1 remains'
+            assert result.size() == 1
+            assert result[0].targetCmHandleIds[0] == 'ch-2'
+    }
+
+    def 'Find the difference based on the provided predicates when it is an ongoing Cm Subscription'() {
+        given: 'A list of predicates'
+            def predicates = [new DmiCmSubscriptionPredicate(['ch-1'].toSet(), PASSTHROUGH_OPERATIONAL, ['a/1/'].toSet())]
+        and: 'its already present'
+            mockCmSubscriptionPersistenceService.hasAtLeastOneSubscription(PASSTHROUGH_OPERATIONAL.getDatastoreName(), 'ch-1') >> true
+        when: 'method to extract only NEW predicates for dmi is called'
+            def result = objectUnderTest.getNewDmiSubscriptionPredicates(predicates)
+        then: 'from 1 predicate, none remains'
+            assert result.size() == 0
+    }
+
+}
