a6fff4ad46a25cbd1318c76dc34d89cfee454579
GM001016278 <gourav.malviya@techmahindra.com>
2025-11-03 17:19:14 +0530

    [INTERNAL REVIEW] Inconsistency With JSON Response(List Items) Using ReplaceANode API
    Jira - [CPS-2708] Enhance Replace A Node with List Items
    Documentation - https://lf-onap.atlassian.net/wiki/x/gIDMAQ
    
    - Updated DataRestController.java to return 204 for new nodes
    - CpsDataService.java Changed method signature to return boolean
    - Added logic to detect new nodes in CpsDataServiceImpl.java
    - Implements the new logic for detecting and inserting new nodes in CpsDataPersistenceServiceImpl.java
    - Added testcases for above changes
    
    Issue-ID:CPS-2708
    Change-Id: Ibc9bbb88ccbb07302355321c6d5c2eade0e7e5fa
    Signed-off-by: GM001016278 <gourav.malviya@techmahindra.com>

--- cps-service/src/main/java/org/onap/cps/impl/CpsDataServiceImpl.java
+++ cps-service/src/main/java/org/onap/cps/impl/CpsDataServiceImpl.java
@@ -209,7 +209,11 @@ public class CpsDataServiceImpl implements CpsDataService {
         final Anchor anchor = cpsAnchorService.getAnchor(dataspaceName, anchorName);
         final Collection<DataNode> dataNodes = dataNodeFactory
                 .createDataNodesWithAnchorParentXpathAndNodeData(anchor, parentNodeXpath, nodeData, contentType);
-        cpsDataPersistenceService.updateDataNodesAndDescendants(dataspaceName, anchorName, dataNodes);
+        if (!"/".equals(parentNodeXpath)) {
+            cpsDataPersistenceService.updateDataNodesAndDescendants(dataspaceName, anchorName, dataNodes);
+        } else {
+            cpsDataPersistenceService.replaceListContent(dataspaceName, anchorName, parentNodeXpath, dataNodes);
+        }
         sendDataUpdatedEvent(anchor, parentNodeXpath, REPLACE_ACTION, observedTimestamp);
     }
 
