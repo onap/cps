e4c5255e250cbb574299e5de8c4b29c7e7f38096
emaclee <lee.anjella.macabuhay@est.tech>
2025-11-04 18:22:51 +0000

    Add Rejected scenario(s) for create subscription
    	- added consuming REJECTED create request from DMI
    	- when REJECTED request is consumed, should update database and
    	  log details (dataNodeSelector, dataJobId, dmi details)
    	- added ignoring when dataJobId is not unique with test
    
    Issue-ID: CPS-3005
    Change-Id: If06c6ddfb3cf151ebf9485d43ba9d6ee5ef57287
    Signed-off-by: emaclee <lee.anjella.macabuhay@est.tech>

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/datajobs/subscription/dmi/EventConsumerSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/datajobs/subscription/dmi/EventConsumerSpec.groovy
@@ -21,6 +21,7 @@
 package org.onap.cps.ncmp.impl.datajobs.subscription.dmi
 
 import static org.onap.cps.ncmp.impl.datajobs.subscription.models.CmSubscriptionStatus.ACCEPTED
+import static org.onap.cps.ncmp.impl.datajobs.subscription.models.CmSubscriptionStatus.REJECTED
 
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
@@ -32,6 +33,7 @@ import io.cloudevents.core.builder.CloudEventBuilder
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.onap.cps.ncmp.impl.datajobs.subscription.dmi_to_ncmp.DataJobSubscriptionDmiOutEvent
 import org.onap.cps.ncmp.impl.datajobs.subscription.ncmp.CmSubscriptionHandler
+import org.onap.cps.ncmp.impl.datajobs.subscription.utils.CmDataJobSubscriptionPersistenceService
 import org.onap.cps.ncmp.utils.TestUtils
 import org.onap.cps.utils.JsonObjectMapper
 import org.slf4j.LoggerFactory
@@ -44,7 +46,8 @@ class EventConsumerSpec extends Specification {
     def jsonObjectMapper = new JsonObjectMapper(objectMapper)
 
     def mockCmSubscriptionHandler = Mock(CmSubscriptionHandler)
-    def objectUnderTest = new EventConsumer(mockCmSubscriptionHandler)
+    def mockCmDataJobSubscriptionPersistenceService = Mock(CmDataJobSubscriptionPersistenceService)
+    def objectUnderTest = new EventConsumer(mockCmSubscriptionHandler, mockCmDataJobSubscriptionPersistenceService)
 
     void setup() {
         ((Logger) LoggerFactory.getLogger(EventConsumer.class)).addAppender(logger)
@@ -57,20 +60,12 @@ class EventConsumerSpec extends Specification {
 
     def 'Consume subscription CREATE response with status ACCEPTED from DMI Plugin'() {
         given: 'a response event from DMI'
-            def jsonData = TestUtils.getResourceFileContent(
-                'datajobs/subscription/cmNotificationSubscriptionDmiOutEvent.json')
-            def testEventSent = jsonObjectMapper.convertJsonString(jsonData, DataJobSubscriptionDmiOutEvent.class)
-            def testCloudEventSent = CloudEventBuilder.v1()
-                .withData(objectMapper.writeValueAsBytes(testEventSent))
-                .withId('random-uuid')
-                .withType('subscriptionCreateResponse')
-                .withSource(URI.create('myDmi'))
-                .withExtension('correlationid', 'sub-1#myDmi').build()
-            def consumerRecord = new ConsumerRecord<String, CloudEvent>('topic-name', 0, 0, 'event-key', testCloudEventSent)
+            def responseEvent = getResponseEventFromDmi('sub-1#myDmi', 'myDmi', 'ACCEPTED')
+            def consumerRecord = new ConsumerRecord<String, CloudEvent>('topic-name', 0, 0, 'event-key', responseEvent)
         when: 'the event is consumed'
             objectUnderTest.consumeDmiOutEvent(consumerRecord)
         then: 'an event is logged with level INFO'
-            def loggingEvent = getLoggingEvent()
+            def loggingEvent = logger.list.first()
             assert loggingEvent.level == Level.INFO
         and: 'the log indicates the task completed successfully'
             assert loggingEvent.formattedMessage == 'Consumed DMI subscription response event with details: | correlationId=sub-1#myDmi | eventType=subscriptionCreateResponse'
@@ -78,7 +73,36 @@ class EventConsumerSpec extends Specification {
             1 * mockCmSubscriptionHandler.updateCmSubscriptionStatus('sub-1', 'myDmi', ACCEPTED)
     }
 
-    def getLoggingEvent() {
-        return logger.list[0]
+    def 'Consume subscription CREATE response with status REJECTED from DMI Plugin'() {
+        given: 'a response event from DMI'
+            def responseEvent = getResponseEventFromDmi('sub-1#myDmi', 'myDmi', 'REJECTED')
+            def consumerRecord = new ConsumerRecord<String, CloudEvent>('topic-name', 0, 0, 'event-key', responseEvent)
+        and: 'persistence service returns some data node selectors'
+            mockCmDataJobSubscriptionPersistenceService.getDataNodeSelectors('sub-1') >> ["/parent=1", "/parent/child=1"]
+        when: 'the event is consumed'
+            objectUnderTest.consumeDmiOutEvent(consumerRecord)
+        then:  'the subscription handler is called to update status of subscription with correct details'
+            1 * mockCmSubscriptionHandler.updateCmSubscriptionStatus('sub-1', 'myDmi', REJECTED)
+        and: 'an event is logged with level INFO'
+            def loggingEvent = logger.list.last()
+            assert loggingEvent.level == Level.INFO
+        and: 'the log shows details of rejected create request'
+            assert loggingEvent.formattedMessage == '' +
+                'DataJob CREATE request with the following details was rejected by DMI plugin myDmi: dataJobId=sub-1 | ' +
+                'dataNodeSelector=/parent=1\n/parent/child=1'
+    }
+
+    def getResponseEventFromDmi(correlationId, dmiPluginName, status) {
+        def jsonData = TestUtils.getResourceFileContent(
+            'datajobs/subscription/cmNotificationSubscriptionDmiOutEvent.json')
+        jsonData = jsonData.replace('#statusMessage', status)
+        def testEventSent = jsonObjectMapper.convertJsonString(jsonData, DataJobSubscriptionDmiOutEvent.class)
+        def testCloudEventSent = CloudEventBuilder.v1()
+            .withData(objectMapper.writeValueAsBytes(testEventSent))
+            .withId('random-uuid')
+            .withType('subscriptionCreateResponse')
+            .withSource(URI.create(dmiPluginName))
+            .withExtension('correlationid', correlationId).build()
+        return testCloudEventSent
     }
 }
