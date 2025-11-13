5ed098c2856129cf63bdab3b48dbf503d070fd85
leventecsanyi <levente.csanyi@est.tech>
2025-11-13 10:02:45 +0100

    Update Policy Executor mapping for ProvMnS inputs
      - update mapping for Policy Executor for FDNs with a #
      - added extra testware
    
    Issue-ID: CPS-2704
    Change-Id: I250265d31bcdf61b053bf77b83a4ba8dc3e0ac67
    Signed-off-by: leventecsanyi <levente.csanyi@est.tech>

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/data/policyexecutor/PolicyExecutorSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/impl/data/policyexecutor/PolicyExecutorSpec.groovy
@@ -29,7 +29,6 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.onap.cps.ncmp.api.exceptions.NcmpException
 import org.onap.cps.ncmp.api.exceptions.PolicyExecutorException
-import org.onap.cps.ncmp.api.exceptions.ProvMnSException
 import org.onap.cps.ncmp.impl.inventory.models.YangModelCmHandle
 import org.onap.cps.ncmp.impl.provmns.RequestPathParameters
 import org.onap.cps.ncmp.impl.provmns.model.PatchItem
@@ -232,19 +231,60 @@ class PolicyExecutorSpec extends Specification {
 
     def 'Build policy executor patch operation details from ProvMnS request parameters where #scenario.'() {
         given: 'a provMnsRequestParameter and a patchItem list'
-            def path = new RequestPathParameters(uriLdnFirstPart: 'someUriLdnFirstPart', className: 'someClassName', id: 'someId')
-            def resource = new ResourceOneOf(id: 'someResourceId', attributes: ['someAttribute1:someValue1', 'someAttribute2:someValue2'], objectClass: objectClass)
+            def path = new RequestPathParameters(uriLdnFirstPart: 'someUriLdnFirstPart', className: 'classNameInUri', id: 'someId')
+            def resource = new ResourceOneOf(id: 'someResourceId', attributes: ['someAttribute1:someValue1', 'someAttribute2:someValue2'], objectClass: classNameInBody)
             def patchItemsList = [new PatchItem(op: 'ADD', 'path':'someUriLdnFirstPart', value: resource), new PatchItem(op: 'REPLACE', 'path':'someUriLdnFirstPart', value: resource)]
-        when: 'a configurationManagementOperation is created and converted to JSON'
+        when: 'patch operation details are created'
             def result = objectUnderTest.buildPatchOperationDetails(path, patchItemsList)
-        then: 'the result is as expected (using json to compare)'
-            def expectedJsonString = '{"permissionId":"Some Permission Id","changeRequestFormat":"cm-legacy","operations":[{"operation":"CREATE","targetIdentifier":"someUriLdnFirstPart","changeRequest":{"' + changeRequestClassReference + '":[{"id":"someId","attributes":["someAttribute1:someValue1","someAttribute2:someValue2"]}]}},{"operation":"UPDATE","targetIdentifier":"someUriLdnFirstPart","changeRequest":{"' + changeRequestClassReference + '":[{"id":"someId","attributes":["someAttribute1:someValue1","someAttribute2:someValue2"]}]}}]}'
-            assert expectedJsonString == jsonObjectMapper.asJsonString(result)
+        then: 'the result contain 2 operations of the correct types in the correct order'
+            result.operations.size() == 2
+            assert result.operations[0] instanceof CreateOperationDetails
+            assert result.operations[1] instanceof CreateOperationDetails
+        and: 'the replace change request has the correct class name'
+            assert result.operations[1].changeRequest.keySet()[0] == expectedChangeRequestKey
+        where: 'the following class names are used in the body'
+            scenario                          | classNameInBody || expectedChangeRequestKey
+            'class name in body is populated' | 'myClass'       || 'myClass'
+            'class name in body  is empty'    | ''              || 'classNameInUri'
+            'class name in body  is null'     | null            || 'classNameInUri'
+    }
+
+    def 'Build policy executor patch operation details from ProvMnS request parameters where the path #scenario'() {
+        given: 'a requestParameter and a patchItem list'
+            def path = new RequestPathParameters(uriLdnFirstPart: 'someUriLdnFirstPart', className: 'someClassName', id: 'someId')
+            def pathItems = [new PatchItem(op: 'REPLACE', 'path':"someUriLdnFirstPart${suffix}", value: value)]
+        when: 'patch operation details are created'
+            def result = objectUnderTest.buildPatchOperationDetails(path, pathItems)
+        then: 'the result has the correct type and populated fields'
+            assert result instanceof PatchOperationsDetails
+            assert result.operations.size() == 1
+            assert result.operations[0]['targetIdentifier'] == 'someUriLdnFirstPart'
+            assert result.operations[0]['changeRequest'].size() == 1
+            assert result.operations[0]['changeRequest'].keySet()[0] == 'someClassName'
+            assert result.operations[0]['changeRequest']['someClassName'][0]['attributes'] != null
+        and: 'the attributes structure of the result is correct'
+            assert jsonObjectMapper.asJsonString(result).contains(expectedJsonStringResult)
         where:
-            scenario                   | objectClass        || changeRequestClassReference
-            'objectClass is populated' | 'someObjectClass'  || 'someObjectClass'
-            'objectClass is empty'     | ''                 || 'someClassName'
-            'objectClass is null'      | null               || 'someClassName'
+            scenario             | suffix                         | value         || expectedJsonStringResult
+            'contains a #'       | '#/attributes/simpleAttribute' | 1             || '"attributes":{"simpleAttribute":1}}'
+            'does not contain #' | ''                             | getResource() || '"attributes":["someAttribute1:someValue1","someAttribute2:someValue2"]'
+    }
+
+    def 'Build an attribute map with different depths of hierarchy with #scenario.'() {
+        given: 'a patch item with a path'
+            def patchItem = new PatchItem(op: 'REPLACE', 'path':path, value: 123)
+        when: 'transforming the attributes'
+            def hierarchyMap = objectUnderTest.getAttributeHierarchyMap(patchItem)
+        then: 'the map depth is equal to the expected number of attributes'
+            assert hierarchyMap.toString().contains(expectedAttributesOutput)
+        where:
+            scenario                  | path                                                               || expectedAttributesOutput
+            'set a simple attribute'  | 'someUriLdnFirstPart#/attributes/simpleAttribute'                  || '[simpleAttribute:123]'
+            'set a complex attribute' | 'someUriLdnFirstPart#/attributes/complexAttribute/simpleAttribute' || '[complexAttribute:[simpleAttribute:123]]'
+    }
+
+    def getResource() {
+        return new ResourceOneOf(id: 'someResourceId', attributes: ['someAttribute1:someValue1', 'someAttribute2:someValue2'], objectClass: 'someClassName')
     }
 
     def 'Build policy executor patch operation details from ProvMnS request parameters with invalid op.'() {
