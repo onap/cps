138978c1fa500c0f84a5726c5e0d4a7debe92816
Arpit Singh6 <AS00745003@techmahindra.com>
2026-01-19 20:10:13 +0530

    [WIP POC] Generic search operations using cps query
    A new query endpoint to process composite query operations.
    
    - Added new endpoint named getNodesByDataspaceAndAnchorAndPayloadV2.
    - Added new enum class CompositeQueryOperators to handle logical
      operators (currently AND OR operators are supported)
    - Added class CompositeQueryProcessor to process nested conditions in a
      composite search query
    - Added Java POJO CompositeQuery.java which defines the structure of a
      composite query.
    
    Issue-ID: CPS-2845
    Change-Id: Iffb68e0beb6f90df26a50c78f798368500fdaeb9
    Signed-off-by: Arpit Singh6 <AS00745003@techmahindra.com>

--- cps-service/src/test/groovy/org/onap/cps/impl/CpsQueryServiceImplSpec.groovy
+++ cps-service/src/test/groovy/org/onap/cps/impl/CpsQueryServiceImplSpec.groovy
@@ -22,6 +22,7 @@
 package org.onap.cps.impl
 
 import org.onap.cps.api.CpsQueryService
+import org.onap.cps.api.model.CompositeQuery
 import org.onap.cps.utils.CpsValidator
 import org.onap.cps.spi.CpsDataPersistenceService
 import org.onap.cps.api.parameters.FetchDescendantsOption
@@ -93,5 +94,19 @@ class CpsQueryServiceImplSpec extends Specification {
             objectUnderTest.queryDataLeaf('some-dataspace', 'some-anchor', '/cps-path/@id', Object.class)
         then: 'the persistence service is called once with the correct parameters'
             1 * mockCpsDataPersistenceService.queryDataLeaf('some-dataspace', 'some-anchor', '/cps-path/@id', Object.class)
+    }
+
+    def 'Search data nodes with a composite query.'() {
+        given: 'a dataspace name, anchor name, composite query and fetch descendants option'
+            def dataspaceName = 'some-dataspace'
+            def anchorName = 'some-anchor'
+            def compositeQuery = new CompositeQuery(cpsPath: '/cps-path', operator: 'and', conditions: [])
+            def fetchDescendantsOption = FetchDescendantsOption.OMIT_DESCENDANTS
+        when: 'searchDataNodes is invoked'
+            objectUnderTest.searchDataNodes(dataspaceName, anchorName, compositeQuery, fetchDescendantsOption)
+        then: 'the persistence service is called once with the correct parameters'
+            1 * mockCpsDataPersistenceService.searchDataNodes(dataspaceName, anchorName, compositeQuery, fetchDescendantsOption)
+        and: 'the CpsValidator is called on the dataspaceName and anchorName'
+            1 * mockCpsValidator.validateNameCharacters(dataspaceName, anchorName)
     }
 }
