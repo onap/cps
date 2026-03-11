6a3e43314bb847aa5cd46790900a3f9a75e94f25
seanbeirne <sean.beirne@est.tech>
2026-03-11 17:18:01 +0000

    Introduce new ProvMnSExtension Interface
    - New YAML file
    - Manually implemented controller
    - Basic testware added to test Not Implemented endpoint
    
    Issue-ID: CPS-3175
    Change-Id: I486eb9392deaaa876a0bcfb15ddde64041ed1d82
    Signed-off-by: seanbeirne <sean.beirne@est.tech>

--- cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/provmns/ParameterHelper.java
+++ cps-ncmp-service/src/main/java/org/onap/cps/ncmp/impl/provmns/ParameterHelper.java
@@ -21,6 +21,8 @@
 package org.onap.cps.ncmp.impl.provmns;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.onap.cps.ncmp.api.exceptions.ProvMnSException;
@@ -30,10 +32,11 @@ import org.springframework.http.HttpStatus;
 public class ParameterHelper {
 
     public static final String NO_OP = null;
-    private static final String PROVMNS_BASE_PATH = "ProvMnS/v\\d+/";
+    private static final String PROVMNS_BASE_PATH = "ProvMnS/v\\d+/(.*)";
+    private static final String PROVMNS_ACTIONS_PATH = "prov-mns-extensions/v\\d+alpha\\d+/actions/(.*)";
     private static final String INVALID_PATH_DETAILS_TEMPLATE = "%s not a valid path";
-    private static final int PATH_VARIABLES_EXPECTED_LENGTH = 2;
-    private static final int REQUEST_FDN_INDEX = 1;
+    private static final Pattern PROVMNS_PATTERNS = Pattern.compile(
+        PROVMNS_ACTIONS_PATH + "|" + PROVMNS_BASE_PATH);
 
     /**
      * Converts HttpServletRequest to RequestParameters.
@@ -42,15 +45,23 @@ public class ParameterHelper {
      * @return RequestParameters object containing http method and FDN parameters
      */
     public static RequestParameters extractRequestParameters(final HttpServletRequest httpServletRequest) {
-        final String uriPath = (String) httpServletRequest.getAttribute(
-            "org.springframework.web.servlet.HandlerMapping.pathWithinHandlerMapping");
-        final String[] pathVariables = uriPath.split(PROVMNS_BASE_PATH);
-        if (pathVariables.length != PATH_VARIABLES_EXPECTED_LENGTH) {
-            throw createProvMnSException(httpServletRequest.getMethod(), uriPath);
-        }
-        final String fdn = "/" + pathVariables[REQUEST_FDN_INDEX];
+        final String fullPath = getFullPath(httpServletRequest);
         return createRequestParameters(httpServletRequest.getMethod(),
-            httpServletRequest.getHeader("Authorization"), fdn);
+            httpServletRequest.getHeader("Authorization"), fullPath);
+    }
+
+    /**
+     * Converts HttpServletRequest to RequestParameters.
+     *
+     * @param httpServletRequest HttpServletRequest object containing the path
+     * @return RequestParameters object containing action and FDN parameters
+     */
+    public static ActionRequestParameters extractActionRequestParameters(final HttpServletRequest httpServletRequest) {
+        final String fullPath = getFullPath(httpServletRequest);
+        final int lastSlashIndex = fullPath.lastIndexOf('/');
+        final String parentFdn = fullPath.substring(0, lastSlashIndex);
+        final String action = fullPath.substring(lastSlashIndex + 1);
+        return new ActionRequestParameters(parentFdn, action);
     }
 
     /**
@@ -123,4 +134,14 @@ public class ParameterHelper {
         return new ProvMnSException(httpMethodName, HttpStatus.UNPROCESSABLE_ENTITY, title, NO_OP);
     }
 
+    private static String getFullPath(final HttpServletRequest httpServletRequest) {
+        final String uriPath = (String) httpServletRequest.getAttribute(
+            "org.springframework.web.servlet.HandlerMapping.pathWithinHandlerMapping");
+        final Matcher matcher = PROVMNS_PATTERNS.matcher(uriPath);
+        if (!matcher.find()) {
+            throw createProvMnSException(httpServletRequest.getMethod(), uriPath);
+        }
+        final String matchedPath = matcher.group(1) != null ? matcher.group(1) : matcher.group(2);
+        return "/" + matchedPath;
+    }
 }
