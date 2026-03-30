e471a145168b0104920f99f98e57c8a5f3bad639
Sourabh Verma <sv001010507@techmahindra.com>
2026-03-31 01:43:04 +0530

    [Internal Review] Delta API's Performance improvement
    - Removed useless object creation and manipulations
    - Reduced Cps path parsing.
    - Implemented Parallel Processing for independent ops
    
    Issue-ID: CPS-3174
    Change-Id: Ia37204afdf2a3c91afe415f8ce7641016fe18259
    Signed-off-by: Sourabh Verma <sv001010507@techmahindra.com>

---
+++ cps-service/src/test/groovy/org/onap/cps/utils/deltareport/DeltaReportGeneratorSpec.groovy
@@ -0,0 +1,59 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025-2026 Deutsche Telekom AG
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.utils.deltareport
+
+import org.onap.cps.api.model.DataNode
+import spock.lang.Specification
+
+import java.util.concurrent.CompletionException
+
+class DeltaReportGeneratorSpec extends Specification {
+
+    def mockDeltaReportHelper = Mock(DeltaReportHelper)
+    def objectUnderTest = new DeltaReportGenerator(mockDeltaReportHelper)
+
+    def 'CompletionException with RuntimeException cause is unwrapped'() {
+        given: 'a mocked helper that throws a RuntimeException'
+            mockDeltaReportHelper.createDeltaReportsForUpdates(_, _, _) >> { throw new IllegalStateException('test error') }
+        and: 'source and target data nodes with the same xpath'
+            def source = [new DataNode(xpath: '/node', leaves: ['key': 'value1'])]
+            def target = [new DataNode(xpath: '/node', leaves: ['key': 'value2'])]
+        when: 'delta reports are created'
+            objectUnderTest.createDeltaReports(source, target)
+        then: 'the original RuntimeException is thrown unwrapped from CompletionException'
+            def thrownException = thrown(IllegalStateException)
+            thrownException.message == 'test error'
+    }
+
+    def 'CompletionException with non-RuntimeException cause is rethrown as CompletionException'() {
+        given: 'a mocked helper that throws a CompletionException wrapping a checked exception'
+            mockDeltaReportHelper.createDeltaReportsForUpdates(_, _, _) >> { throw new CompletionException(new Exception('checked error')) }
+        and: 'source and target data nodes with the same xpath'
+            def source = [new DataNode(xpath: '/node', leaves: ['key': 'value1'])]
+            def target = [new DataNode(xpath: '/node', leaves: ['key': 'value2'])]
+        when: 'delta reports are created'
+            objectUnderTest.createDeltaReports(source, target)
+        then: 'the CompletionException itself is thrown'
+            def thrownException = thrown(CompletionException)
+            thrownException.cause.message == 'checked error'
+    }
+}
+
---
+++ cps-service/src/test/groovy/org/onap/cps/utils/deltareport/GroupedDeltaReportGeneratorSpec.groovy
@@ -0,0 +1,43 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2025-2026 Deutsche Telekom AG
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.utils.deltareport
+
+import org.onap.cps.api.model.DataNode
+import spock.lang.Specification
+
+import java.util.concurrent.CompletionException
+
+class GroupedDeltaReportGeneratorSpec extends Specification {
+
+    def mockDeltaReportHelper = Mock(DeltaReportHelper)
+    def objectUnderTest = new GroupedDeltaReportGenerator(mockDeltaReportHelper)
+
+    def 'CompletionException with RuntimeException cause is unwrapped'() {
+        given: 'source data nodes with an invalid xpath that causes a failure in the async removed report task'
+            def source = [new DataNode(xpath: 'invalid[', leaves: ['key': 'value1'])]
+            def target = []
+        when: 'condensed delta reports are created'
+            objectUnderTest.createCondensedDeltaReports(source, target)
+        then: 'the RuntimeException is unwrapped from CompletionException and thrown directly'
+            thrown(RuntimeException)
+    }
+}
+
