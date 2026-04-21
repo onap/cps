6d51269602b142bc70341dc6efd506e9efea55df
ToineSiebelink <toine.siebelink@est.tech>
2026-04-21 10:51:26 +0100

    MCP PoC Inventory Query Service
    - Define MCP Tool
    
    Issue-ID: CPS-3205
    Change-Id: I832d034a3e4e93023090920948362b5767effad5
    Signed-off-by: ToineSiebelink <toine.siebelink@est.tech>

---
+++ mcp-poc/src/main/java/org/onap/cps/mcp/McpServerApplication.java
@@ -0,0 +1,12 @@
+package org.onap.cps.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerApplication.class, args);
+    }
+}
