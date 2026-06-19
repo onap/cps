d2c290250fa3fdb6eb63c8ffe5600b692b48c943
Pratheeka Shetty <ps001093161@techmahindra.com>
2026-06-19 15:56:56 +0530

    Fix for Sonar: Method has 8 parameters, which is greater than 7 authorized
    Change-Id: I4527f28023fc3068b2a72c190a6e63c044ac56e3
    Signed-off-by: Pratheeka Shetty <ps001093161@techmahindra.com>

--- integration-test/src/test/groovy/org/onap/cps/integration/functional/cps/DeltaServiceIntegrationSpec.groovy
+++ integration-test/src/test/groovy/org/onap/cps/integration/functional/cps/DeltaServiceIntegrationSpec.groovy
@@ -181,32 +181,28 @@ class DeltaServiceIntegrationSpec extends FunctionalSpecBase {
 
     def 'Get delta between anchor and JSON payload'() {
         when: 'attempt to get delta report between anchor and JSON payload'
-            def jsonPayload = '{\"book-store:bookstore\":{\"bookstore-name\":\"Crossword Bookstores\"},\"book-store:bookstore-address\":{\"address\":\"Bangalore, India\",\"postal-code\":\"560062\",\"bookstore-name\":\"Crossword Bookstores\"}}'
-            def result = objectUnderTest.getDeltaByDataspaceAnchorAndPayload(FUNCTIONAL_TEST_DATASPACE_3, BOOKSTORE_ANCHOR_3, '/', [:], jsonPayload, OMIT_DESCENDANTS, NO_GROUPING, JSON)
-        then: 'delta report contains expected number of changes'
-            result.size() == 3
-        and: 'delta report contains "replace" action with expected xpath'
-            assert result[0].getAction() == 'replace'
-            assert result[0].getXpath() == '/bookstore'
-        and: 'delta report contains "remove" action with expected xpath'
-            assert result[1].getAction() == 'remove'
-            assert result[1].getXpath() == '/bookstore-address[@bookstore-name=\'Easons-1\']'
-        and: 'delta report contains "create" action with expected xpath'
-            assert result[2].getAction() == 'create'
-            assert result[2].getXpath() == '/bookstore-address[@bookstore-name=\'Crossword Bookstores\']'
+            def jsonPayload = '{"book-store:bookstore":{"bookstore-name":"Crossword Bookstores"}}'
+            def result = objectUnderTest.getDeltaByDataspaceAnchorAndPayload(FUNCTIONAL_TEST_DATASPACE_3, BOOKSTORE_ANCHOR_3, '/bookstore', [:], jsonPayload, NO_GROUPING, JSON)
+            result = result.toList().sort { it.xpath }
+        then: 'delta report contains a replace action for the bookstore node'
+            assert result.any { it.getAction() == 'replace' && it.getXpath() == '/bookstore' }
+        and: 'the replace action has expected source and target bookstore name'
+            def replaceEntry = result.find { it.getAction() == 'replace' && it.getXpath() == '/bookstore' }
+            assert replaceEntry.getSourceData() == ['bookstore': ['bookstore-name': 'Easons-1']]
+            assert replaceEntry.getTargetData() == ['bookstore': ['bookstore-name': 'Crossword Bookstores']]
     }
 
     def 'Get delta between anchor and payload returns empty response when JSON payload is identical to anchor data'() {
         when: 'attempt to get delta report between anchor and JSON payload (replacing the string Easons with Easons-1 because the data in JSON file is modified, to append anchor number, during the setup process of the integration tests)'
             def jsonPayload = readResourceDataFile('bookstore/bookstoreData.json').replace('Easons', 'Easons-1')
-            def result = objectUnderTest.getDeltaByDataspaceAnchorAndPayload(FUNCTIONAL_TEST_DATASPACE_3, BOOKSTORE_ANCHOR_3, '/', [:], jsonPayload, INCLUDE_ALL_DESCENDANTS, NO_GROUPING, JSON)
+            def result = objectUnderTest.getDeltaByDataspaceAnchorAndPayload(FUNCTIONAL_TEST_DATASPACE_3, BOOKSTORE_ANCHOR_3, '/', [:], jsonPayload, NO_GROUPING, JSON)
         then: 'delta report is empty'
             assert result.isEmpty()
     }
 
     def 'Get delta between anchor and payload error scenario: #scenario'() {
         when: 'attempt to get delta between anchor and json payload'
-            objectUnderTest.getDeltaByDataspaceAnchorAndPayload(dataspaceName, sourceAnchor, xpath, [:], jsonPayload, INCLUDE_ALL_DESCENDANTS, NO_GROUPING, JSON)
+            objectUnderTest.getDeltaByDataspaceAnchorAndPayload(dataspaceName, sourceAnchor, xpath, [:], jsonPayload, NO_GROUPING, JSON)
         then: 'expected exception is thrown'
             thrown(expectedException)
         where: 'following data was used'
