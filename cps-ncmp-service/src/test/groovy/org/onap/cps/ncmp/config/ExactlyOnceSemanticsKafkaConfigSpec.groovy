a55790ddb8766f47913993340f17b1bd1d4f03e4
seanbeirne <sean.beirne@est.tech>
2026-08-06 15:00:01 +0100

    Add record recovery logging to EOS Kafka error handler
    - Add custom recoverer to log discarded records on non-retryable exceptions
    - Log exception message at ERROR level, full stack trace at DEBUG level
    - Configure EventBatchSendException as retryable instead of KafkaException
    - Extract logging to dedicated method using method reference
    
    Issue-ID: CPS-3315
    Change-Id: I8a885f27a894592d4e7f0357b1dd15cf0fea70ca
    Signed-off-by: seanbeirne <sean.beirne@est.tech>

--- cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/config/ExactlyOnceSemanticsKafkaConfigSpec.groovy
+++ cps-ncmp-service/src/test/groovy/org/onap/cps/ncmp/config/ExactlyOnceSemanticsKafkaConfigSpec.groovy
@@ -20,12 +20,19 @@
 
 package org.onap.cps.ncmp.config
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
 import io.cloudevents.CloudEvent
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.onap.cps.events.EventBatchSendException
+import org.slf4j.LoggerFactory
 import org.spockframework.spring.EnableSharedInjection
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.kafka.autoconfigure.KafkaProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
@@ -59,7 +66,20 @@ class ExactlyOnceSemanticsKafkaConfigSpec extends Specification {
     @Autowired
     ConcurrentKafkaListenerContainerFactory<String, CloudEvent> cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics
 
-    def 'Verify exactly once semantics kafka configuration'() {
+    def logger = new ListAppender<ILoggingEvent>()
+
+    def setup() {
+        def setupLogger = (Logger) LoggerFactory.getLogger(ExactlyOnceSemanticsKafkaConfig)
+        setupLogger.setLevel(Level.DEBUG)
+        setupLogger.addAppender(logger)
+        logger.start()
+    }
+
+    def cleanup() {
+        ((Logger) LoggerFactory.getLogger(ExactlyOnceSemanticsKafkaConfig)).detachAndStopAllAppenders()
+    }
+
+    def 'Exactly once semantics kafka configuration is as expected.'() {
         expect: 'consumer has read_committed isolation level'
             cloudEventConsumerFactoryForExactlyOnceSemantics.configurationProperties[ConsumerConfig.ISOLATION_LEVEL_CONFIG] == 'read_committed'
         and: 'consumer has auto commit disabled'
@@ -69,12 +89,52 @@ class ExactlyOnceSemanticsKafkaConfigSpec extends Specification {
         and: 'listener uses BATCH ack mode'
             cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics.containerProperties.ackMode == ContainerProperties.AckMode.BATCH
         and: 'listener is batch listener'
-            cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics.batchListener == true
+            cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics.isBatchListener() == true
         and: 'concurrency is configured'
             cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics.concurrency == 2
         and: 'producer transaction ID prefix is as expected'
             cloudEventProducerFactoryForExactlyOnceSemantics.transactionIdPrefix.startsWith('cps-tx-myPrefix-')
         and: 'KafkaTransactionManager is used instead of primary transaction manager'
             cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics.containerProperties.kafkaAwareTransactionManager instanceof KafkaTransactionManager
+    }
+
+    def 'Consuming a record with non retryable exception'() {
+        given: 'a consumer record and a non-retryable exception'
+            def consumerRecord = new ConsumerRecord('my-topic', 123, 456L, 'key', null)
+            def nonRetryableException = new RuntimeException('some non-retryable error')
+        when: 'the recoverer is invoked for the discarded record'
+            def failureTracker = getErrorHandlerField('failureTracker')
+            def recovererField = failureTracker.getClass().getDeclaredField('recoverer')
+            recovererField.accessible = true
+            recovererField.get(failureTracker).accept(consumerRecord, nonRetryableException)
+        then: 'an error log message contains the topic, partition, offset and exception message'
+            def errorLog = logger.list.find { it.level == Level.ERROR }
+            assert errorLog.formattedMessage.contains('my-topic')
+            assert errorLog.formattedMessage.contains('123')
+            assert errorLog.formattedMessage.contains('456')
+            assert errorLog.formattedMessage.contains('some non-retryable error')
+        and: 'a debug log message contains the full stack trace'
+            def debugLog = logger.list.find { it.level == Level.DEBUG }
+            assert debugLog.formattedMessage.contains('my-topic')
+            assert debugLog.throwableProxy.className == RuntimeException.name
+            assert debugLog.throwableProxy.message == 'some non-retryable error'
+            assert debugLog.throwableProxy.stackTraceElementProxyArray.length > 0
+    }
+
+    def getErrorHandlerField(def fieldName) {
+        def containerFactoryClass = cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics.getClass()
+        def handlerField = containerFactoryClass.getSuperclass().getDeclaredField('commonErrorHandler')
+        handlerField.accessible = true
+        def errorHandler = handlerField.get(cloudEventConcurrentKafkaListenerContainerFactoryForExactlyOnceSemantics)
+        def classInHierarchy = errorHandler.getClass()
+        while (classInHierarchy != null) {
+            try {
+                def field = classInHierarchy.getDeclaredField(fieldName)
+                field.accessible = true
+                return field.get(errorHandler)
+            } catch (NoSuchFieldException ignored) {
+                classInHierarchy = classInHierarchy.getSuperclass()
+            }
+        }
     }
 }
