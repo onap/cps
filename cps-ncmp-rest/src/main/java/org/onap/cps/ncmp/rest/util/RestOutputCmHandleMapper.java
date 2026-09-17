4219fc7046214eb5bdd262395865014cdca22445
egernug <gerard.nugent@est.tech>
2026-09-17 09:31:55 +0100

    fix: remove cmHandleStatus from v1 cm handle responses
      - v1 northbound (/ncmp/v1/ch/searches, /ncmp/v1/ch/{cm-handle}) and
        v1 southbound (/ncmpInventory/v1/ch/searchCmHandles, /ncmpInventory/v1/ch)
        no longer expose cmHandleStatus
      - cmHandleStatus is now populated only on v2 lightweight responses
      - keeps the shared RestOutputCmHandle schema aligned with the v1 contract
    
    Issue-ID: CPS-3421
    
    Change-Id: I79410d2139aa66bab2ffe77e6f47d0d3a163c495
    Signed-off-by: egernug <gerard.nugent@est.tech>

--- cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/util/RestOutputCmHandleMapper.java
+++ cps-ncmp-rest/src/main/java/org/onap/cps/ncmp/rest/util/RestOutputCmHandleMapper.java
@@ -60,7 +60,6 @@ public class RestOutputCmHandleMapper {
         restOutputCmHandle.setModuleSetTag(ncmpServiceCmHandle.getModuleSetTag());
         restOutputCmHandle.setAlternateId(ncmpServiceCmHandle.getAlternateId());
         restOutputCmHandle.setDataProducerIdentifier(ncmpServiceCmHandle.getDataProducerIdentifier());
-        restOutputCmHandle.setCmHandleStatus(ncmpServiceCmHandle.getCmHandleStatus());
         return restOutputCmHandle;
     }
 
